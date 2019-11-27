91f9c5b87f510d9767f56b0fc08fceeec08c646c
Tanay Soni <tanaysoni12@gmail.com>
2019-11-27 19:34:10 +0100

    Fix test

--- test/test_db.py
+++ test/test_db.py
@@ -8,7 +8,6 @@ def test_db_write_read():
     db.create_all()
 
     write_documents_to_db(document_dir="samples/docs")
-    documents = db.session.query(Document).all()
+    documents = db.session.query(Document).order_by(Document.text).all()
     assert len(documents) == 2
     assert documents[0].text == 'A Doc specifically talking about haystack.\nHaystack can be used to scale QA models to large document collections.'
-    print(documents)
