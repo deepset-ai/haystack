ec7ad29d5103f7c6a96ec5980ea76d1f4c8a89df
Tanay Soni <tanaysoni12@gmail.com>
2020-01-22 15:53:04 +0100

    Add BaseDocumentStore

---
+++ haystack/database/base.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+
+
+class BaseDocumentStore:
+    """
+    Base class for implementing DataStores.
+    """
+
+    @abstractmethod
+    def write_documents(self, documents):
+        pass
+
+    @abstractmethod
+    def get_document_by_id(self, id):
+        pass
+
+    @abstractmethod
+    def get_document_ids_by_tag(self, tag):
+        pass
+
+
