62685d036645784bf9a15b9905da18ee2ed231c0
Tanay Soni <tanaysoni12@gmail.com>
2020-01-23 14:14:22 +0100

    Update tutorial to comply with the database layer changes

--- tutorials/Tutorial1_Basic_QA_Pipeline.py
+++ tutorials/Tutorial1_Basic_QA_Pipeline.py
@@ -1,16 +1,13 @@
-from haystack.reader.farm import FARMReader
-from haystack.reader.transformers import TransformersReader
-from haystack.retriever.tfidf import TfidfRetriever
 from haystack import Finder
-from haystack.indexing.io import write_documents_to_db, fetch_archive_from_http
+from haystack.database.sql import SQLDocumentStore
 from haystack.indexing.cleaning import clean_wiki_text
+from haystack.indexing.io import write_documents_to_db, fetch_archive_from_http
+from haystack.reader.farm import FARMReader
+from haystack.retriever.tfidf import TfidfRetriever
 from haystack.utils import print_answers
 
 
 ## Indexing & cleaning documents
-# Init a database (default: sqllite)
-from haystack.database import db
-db.create_all()
 
 # Let's first get some documents that we want to query
 # Here: 517 Wikipedia articles for Game of Thrones
@@ -21,14 +18,15 @@ fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 # Now, let's write the docs to our DB.
 # You can supply a cleaning function that is applied to each doc (e.g. to remove footers)
 # It must take a str as input, and return a str.
-write_documents_to_db(document_dir=doc_dir, clean_func=clean_wiki_text, only_empty_db=True)
+datastore = SQLDocumentStore(url="sqlite:///qa.db")
 
+write_documents_to_db(datastore=datastore, document_dir=doc_dir, clean_func=clean_wiki_text, only_empty_db=True)
 
 ## Initalize Reader, Retriever & Finder
 
 # A retriever identifies the k most promising chunks of text that might contain the answer for our question
 # Retrievers use some simple but fast algorithm, here: TF-IDF
-retriever = TfidfRetriever()
+retriever = TfidfRetriever(datastore=datastore)
 
 # A reader scans the text chunks in detail and extracts the k best answers
 # Reader use more powerful but slower deep learning models, here: a BERT QA model trained via FARM on Squad 2.0
@@ -50,6 +48,3 @@ prediction = finder.get_answers(question="Who is the father of Arya Stark?", top
 #prediction = finder.get_answers(question="Who is the sister of Sansa?", top_k_reader=5)
 
 print_answers(prediction, details="minimal")
-
-
-
