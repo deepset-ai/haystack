95538f4926cb9ecb5c3042683edbf3c78f5fd926
Tanay Soni <tanaysoni12@gmail.com>
2020-02-27 14:34:55 +0100

    Remove elasticsearch_dsl dependency

--- haystack/database/elasticsearch.py
+++ haystack/database/elasticsearch.py
@@ -1,32 +1,33 @@
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Document as ESDoc, Text, connections
+from elasticsearch.helpers import scan
+
 from haystack.database.base import BaseDocumentStore
 
 
-def create_index(index_name):
-    """
-    Example to create a simple, default ES index from scratch.
-    You might want to modify the schema for your own purposes
-    """
-
-    class Document(ESDoc):
-                name = Text()
-                text = Text()
-                tags = Text()
-                document_id = Text()
-
-                class Index:
-                    name = index_name
-
-    Document.init()  # create mapping if not exists.
-
-
 class ElasticsearchDocumentStore(BaseDocumentStore):
-    def __init__(self, host="localhost", username="", password="", index="document", search_fields="text",
-                 text_field="text", name_field="name", doc_id_field="document_id", tag_fields=None):
-
+    def __init__(
+        self,
+        host="localhost",
+        username="",
+        password="",
+        index="document",
+        search_fields="text",
+        text_field="text",
+        name_field="name",
+        doc_id_field="document_id",
+        tag_fields=None,
+    ):
         self.client = Elasticsearch(hosts=[{"host": host}], http_auth=(username, password))
-        self.connections = connections.create_connection(hosts=[{"host": host}], http_auth=(username, password))
+        mapping = {
+            "mappings": {
+                "properties": {
+                    name_field: {"type": "text"},
+                    text_field: {"type": "text"},
+                    doc_id_field: {"type": "text"},
+                }
+            }
+        }
+        self.client.indices.create(index=index, ignore=400, body=mapping)
         self.index = index
 
         # configure mappings to ES fields that will be used for querying / displaying results
@@ -42,7 +43,11 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
         query = {"filter": {"term": {"_id": id}}}
         result = self.client.search(index=self.index, body=query)["hits"]["hits"]
         if result:
-            document = {"id": result["_id"], "name": result[self.name_field], "text": result[self.text_field]}
+            document = {
+                "id": result[self.doc_id_field],
+                "name": result[self.name_field],
+                "text": result[self.text_field],
+            }
         else:
             document = None
         return document
@@ -51,20 +56,18 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
         query = {"filter": {"term": {self.name_field: name}}}
         result = self.client.search(index=self.index, body=query)["hits"]["hits"]
         if result:
-            document = {"id": result["_id"], "name": result[self.name_field], "text": result[self.text_field]}
+            document = {
+                "id": result[self.doc_id_field],
+                "name": result[self.name_field],
+                "text": result[self.text_field],
+            }
         else:
             document = None
         return document
 
     def get_document_ids_by_tags(self, tags):
         term_queries = [{"terms": {key: value}} for key, value in tags.items()]
-        query = {
-            "query": {
-                "bool": {
-                    "must": term_queries
-                }
-            }
-        }
+        query = {"query": {"bool": {"must": term_queries}}}
         result = self.client.search(index=self.index, body=query, size=10000)["hits"]["hits"]
         doc_ids = []
         for hit in result:
@@ -73,26 +76,22 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
 
     def write_documents(self, documents):
         for d in documents:
-            # d = {"name": doc["name"],
-            #     "text": doc["text"],
-            #     "document_id": doc.get("document_id", None),
-            #     "tags": doc.get("tags", None)}
-
             self.client.index(index=self.index, body=d)
 
     def get_document_count(self):
-        s = Search(using=self.client, index=self.index)
-        return s.count()
+        result = self.client.count()
+        count = result["count"]
+        return count
 
     def get_all_documents(self):
-        search = Search(using=self.client, index=self.index).scan()
+        result = scan(self.client, query={"query": {"match_all": {}}}, index=self.index)
         documents = []
-        for hit in search:
+        for hit in result:
             documents.append(
                 {
-                    "id": hit.meta["id"],
-                    "name": hit[self.name_field],
-                    "text": hit[self.text_field],
+                    "id": hit["_source"][self.doc_id_field],
+                    "name": hit["_source"][self.name_field],
+                    "text": hit["_source"][self.text_field],
                 }
             )
         return documents
@@ -102,16 +101,14 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
         # for now: we keep the current structure of candidate_doc_ids for compatibility with SQL documentstores
         # midterm: get rid of it and do filtering with tags directly in this query
 
-        body = {"size": top_k,
-                'query': {
-                    "bool": {
-                        "must": [
-                            {'multi_match': {
-                                'query': query,
-                                'type': 'most_fields',
-                                'fields': self.search_fields}}
-                        ]
-                    }}}
+        body = {
+            "size": top_k,
+            "query": {
+                "bool": {
+                    "must": [{"multi_match": {"query": query, "type": "most_fields", "fields": self.search_fields}}]
+                }
+            },
+        }
         if candidate_doc_ids:
             body["query"]["bool"]["filter"] = [{"terms": {"_id": candidate_doc_ids}}]
         result = self.client.search(index=self.index, body=body)["hits"]["hits"]
@@ -119,8 +116,5 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
         meta_data = []
         for hit in result:
             paragraphs.append(hit["_source"][self.text_field])
-            meta_data.append({"paragraph_id": hit["_id"],
-                              "document_id": hit["_source"][self.doc_id_field]})
+            meta_data.append({"paragraph_id": hit["_id"], "document_id": hit["_source"][self.doc_id_field]})
         return paragraphs, meta_data
-
-
