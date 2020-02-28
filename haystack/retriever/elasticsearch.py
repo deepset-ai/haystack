ab001fcace8c7ff60e6d210090ba280f78d17fe8
Malte Pietsch <malte.pietsch@deepset.ai>
2020-02-28 17:43:27 +0100

    add basic logging to ElasticsearchRetriever

--- haystack/retriever/elasticsearch.py
+++ haystack/retriever/elasticsearch.py
@@ -1,4 +1,6 @@
 from haystack.retriever.base import BaseRetriever
+import logging
+logger = logging.getLogger(__name__)
 
 
 class ElasticsearchRetriever(BaseRetriever):
@@ -6,5 +8,6 @@ class ElasticsearchRetriever(BaseRetriever):
         self.document_store = document_store
 
     def retrieve(self, query, candidate_doc_ids=None, top_k=10):
-
-        return self.document_store.query(query, top_k, candidate_doc_ids)
+        paragraphs, meta_data = self.document_store.query(query, top_k, candidate_doc_ids)
+        logger.info(f"Got {len(paragraphs)} candidates from retriever: {meta_data}")
+        return paragraphs, meta_data
