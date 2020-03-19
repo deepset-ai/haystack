ae3ed3ae1727aca925b2c0206a782b06cef77473
Timo Moeller <timo.moeller@deepset.ai>
2020-03-20 00:36:10 +0100

    Add sentence transformers ES retriever

--- haystack/retriever/elasticsearch.py
+++ haystack/retriever/elasticsearch.py
@@ -1,5 +1,7 @@
 from haystack.retriever.base import BaseRetriever
 from farm.infer import Inferencer
+from sentence_transformers import SentenceTransformer
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -31,3 +33,22 @@ class ElasticsearchEmbeddingRetriever(BaseRetriever):
         res = self.embedding_model.extract_vectors(dicts=[{"text": text}], extraction_strategy="reduce_mean", extraction_layer=-1)
         emb = list(res[0]["vec"])
         return emb
+
+
+class ElasticsearchSentenceEmbeddingRetriever(BaseRetriever):
+    def __init__(self, document_store, embedding_model):
+        # pretrained embedding models coming from: https://github.com/UKPLab/sentence-transformers#pretrained-models
+        # e.g. 'roberta-base-nli-stsb-mean-tokens'
+        self.document_store = document_store
+        self.embedding_model = SentenceTransformer(embedding_model)
+
+    def retrieve(self, query, candidate_doc_ids=None, top_k=10):
+        query_emb = self.create_embedding(query)
+        paragraphs, meta_data = self.document_store.query_by_embedding(query_emb, top_k, candidate_doc_ids)
+        logger.info(f"Got {len(paragraphs)} candidates from retriever: {meta_data}")
+        return paragraphs, meta_data
+
+    def create_embedding(self, text):
+        # text is single string, sentence-transformers needs a list of strings
+        res = self.embedding_model.encode([text]) # get back list of numpy embedding vectors
+        return res[0].tolist()
