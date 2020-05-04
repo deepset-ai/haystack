1c97cab53f188ab7e458cd65d4cfd43c23eb132f
Tanay Soni <tanaysoni12@gmail.com>
2020-05-04 12:14:32 +0200

    Fix scope of tmpdir

--- test/conftest.py
+++ test/conftest.py
@@ -6,11 +6,16 @@ from subprocess import Popen, PIPE, STDOUT
 import pytest
 
 
+@pytest.fixture(scope='session')
+def elasticsearch_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp('elasticsearch')
+
+
 @pytest.fixture(scope="session")
-def elasticsearch_fixture(tmpdir):
+def elasticsearch_fixture(elasticsearch_dir):
     thetarfile = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.1-linux-x86_64.tar.gz"
     ftpstream = urllib.request.urlopen(thetarfile)
     thetarfile = tarfile.open(fileobj=ftpstream, mode="r|gz")
-    thetarfile.extractall(path=tmpdir)
+    thetarfile.extractall(path=elasticsearch_dir)
     es_server = Popen(["elasticsearch-7.6.1/bin/elasticsearch"], stdout=PIPE, stderr=STDOUT)
     time.sleep(20)
