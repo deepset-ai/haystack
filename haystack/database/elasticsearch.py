60be9483dd3f49ef5c4f85408f15a79527eea5f4
Malte Pietsch <malte.pietsch@deepset.ai>
2020-05-08 10:14:46 +0200

    make doc name optional

--- haystack/database/elasticsearch.py
+++ haystack/database/elasticsearch.py
@@ -189,7 +189,7 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
     def _convert_es_hit_to_document(self, hit, score_adjustment=0) -> Document:
         # We put all additional data of the doc into meta_data and return it in the API
         meta_data = {k:v for k,v in hit["_source"].items() if k not in (self.text_field, self.external_source_id_field)}
-        meta_data["name"] = meta_data.pop(self.name_field)
+        meta_data["name"] = meta_data.pop(self.name_field, None)
 
         document = Document(
             id=hit["_id"],
