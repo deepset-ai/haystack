59d89fe61815f644e5f6ad8104a98cdf370a76ae
Tanay Soni <tanaysoni12@gmail.com>
2020-06-18 11:12:05 +0200

    Fix for installing PyTorch on Windows OS

--- setup.py
+++ setup.py
@@ -2,14 +2,47 @@ from io import open
 
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    parsed_requirements = f.read().splitlines()
-# remove blank lines, comments and links to specific git commits
-parsed_requirements = [
-    x.strip()
-    for x in parsed_requirements
-    if ((x.strip()[0] != "#") and (len(x.strip()) > 3) and "-e git://" not in x)
-]
+
+def parse_requirements(filename):
+    """
+    Parse a requirements pip file returning the list of required packages. It exclude commented lines and --find-links directives.
+
+    Args:
+        filename: pip requirements requirements
+
+    Returns:
+        list of required package with versions constraints
+
+    """
+    with open(filename) as file:
+        parsed_requirements = file.read().splitlines()
+    parsed_requirements = [line.strip()
+                           for line in parsed_requirements
+                           if not ((line.strip()[0] == "#") or line.strip().startswith('--find-links'))]
+    return parsed_requirements
+
+
+def get_dependency_links(filename):
+    """
+     Parse a requirements pip file looking for the --find-links directive.
+    Args:
+        filename:  pip requirements requirements
+
+    Returns:
+        list of find-links's url
+    """
+    with open(filename) as file:
+        parsed_requirements = file.read().splitlines()
+    dependency_links = list()
+    for line in parsed_requirements:
+        line = line.strip()
+        if line.startswith('--find-links'):
+            dependency_links.append(line.split('=')[1])
+    return dependency_links
+
+
+dependency_links = get_dependency_links('requirements.txt')
+parsed_requirements = parse_requirements('requirements.txt')
 
 setup(
     name="farm-haystack",
@@ -24,6 +57,7 @@ setup(
     url="https://github.com/deepset-ai/haystack",
     download_url="https://github.com/deepset-ai/haystack/archive/0.2.1.tar.gz",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    dependency_links=dependency_links,
     install_requires=parsed_requirements,
     python_requires=">=3.6.0",
     tests_require=["pytest"],
