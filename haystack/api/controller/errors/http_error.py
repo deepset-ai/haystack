715e6f8182ac1e6bf730c045d21627d8e961d1de
Tanay Soni <tanaysoni12@gmail.com>
2020-06-19 11:21:48 +0200

    Remove API module

--- haystack/api/controller/errors/http_error.py
+++
@@ -1,7 +0,0 @@
-from fastapi import HTTPException
-from starlette.requests import Request
-from starlette.responses import JSONResponse
-
-
-async def http_error_handler(_: Request, exc: HTTPException) -> JSONResponse:
-    return JSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
--- haystack/api/controller/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from haystack.api.controller import search, feedback, file_upload
-
-router = APIRouter()
-
-router.include_router(search.router, tags=["search"])
-router.include_router(feedback.router, tags=["feedback"])
-router.include_router(file_upload.router, tags=["file-upload"])
