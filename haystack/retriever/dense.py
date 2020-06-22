be48101abc67b5efe94d33eb816c63dc118c1f6e
Malte Pietsch <malte.pietsch@deepset.ai>
2020-06-22 16:49:45 +0200

    initial version of DPR retrieval

--- haystack/retriever/dense.py
+++ haystack/retriever/dense.py
@@ -1,5 +1,9 @@
 import logging
-from typing import Type, List, Union
+from typing import Type, List, Union, Tuple
+import torch
+import numpy as np
+
+
 from farm.infer import Inferencer
 
 from haystack.database.base import Document, BaseDocumentStore
@@ -7,57 +11,164 @@ from haystack.database.elasticsearch import ElasticsearchDocumentStore
 from haystack.retriever.base import BaseRetriever
 from haystack.retriever.sparse import logger
 
+from haystack.retriever.dpr_utils import HFBertEncoder, BertTensorizer, BertTokenizer,\
+    Tensorizer, load_states_from_checkpoint
+
 logger = logging.getLogger(__name__)
 
 
-class DPRRetriever(BaseRetriever):
-    def __init__(
-        self,
-        document_store: Type[BaseDocumentStore],
-        embedding_model: str,
-        gpu: bool = True,
-    ):
+class DensePassageRetriever(BaseRetriever):
+    """
+        Retriever that uses a dual transformer encoder (one for query, one for passage). It's based on:
+        Karpukhin, Vladimir, et al. "Dense Passage Retrieval for Open-Domain Question Answering." arXiv preprint arXiv:2004.04906 (2020).
+        (See https://arxiv.org/abs/2004.04906).
+
+        You can load the model checkpoints of the author's in the original format.
+        See their readme for download instructions: https://github.com/facebookresearch/DPR#resources--data-formats
+
+    """
+    def __init__(self,
+                 document_store: Type[BaseDocumentStore],
+                 embedding_model: str,
+                 gpu: bool = True,
+                 batch_size=16,
+                 do_lower_case=False,
+                 encoder_model_type="hf_bert",
+                 fp16=False,
+                 fp16_opt_level='O1',
+                 fix_ctx_encoder=False,
+                 ):
         """
-        TODO
+
         :param document_store:
         :param embedding_model:
         :param gpu:
-        :param model_format:
+        :param batch_size:
+        :param do_lower_case:
+        :param encoder_model_type:
+        :param fp16:
+        :param fp16_opt_level:
+        :param fix_ctx_encoder:
         """
+
         self.document_store = document_store
         self.embedding_model = embedding_model
+        self.batch_size = batch_size
+        if gpu and torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
 
+        self.fp16 = fp16
+        self.fp16_opt_level = fp16_opt_level
+        self.do_lower_case = do_lower_case
+        self.encoder_model_type = encoder_model_type
+        self.fix_ctx_encoder = fix_ctx_encoder
 
-        logger.info(f"Init retriever using embeddings of model {embedding_model}")
-        self.embedding_model = SentenceTransformer(embedding_model)
+        # Load checkpoint (incl. additional model params)
+        saved_state = load_states_from_checkpoint(self.embedding_model)
+        self._set_encoder_params_from_state(saved_state.encoder_params)
 
+        # Init & Load Encoders
+        self.query_encoder = HFBertEncoder.init_encoder(self.pretrained_model_cfg,
+                                                        projection_dim=self.projection_dim,
+                                                        dropout=0.0)
+        self.passage_encoder = HFBertEncoder.init_encoder(self.pretrained_model_cfg,
+                                                          projection_dim=self.projection_dim,
+                                                          dropout=0.0)
+        self.passage_encoder = self._prepare_model(self.passage_encoder, saved_state, prefix="ctx_model.")
+        self.query_encoder = self._prepare_model(self.query_encoder, saved_state, prefix="question_model.")
+        #self.encoder = BiEncoder(question_encoder, ctx_encoder, fix_ctx_encoder=self.fix_ctx_encoder)
 
-    def retrieve(self, query: str, candidate_doc_ids: [str] = None, top_k: int = 10) -> [Document]:
-        query_emb = self.create_embedding(texts=[query])
+        # Load Tokenizer & Tensorizer
+        tokenizer = BertTokenizer.from_pretrained(self.pretrained_model_cfg, do_lower_case=self.do_lower_case)
+        self.tensorizer = BertTensorizer(tokenizer, self.sequence_length)
+
+    def retrieve(self, query: str, candidate_doc_ids: List[str] = None, top_k: int = 10) -> List[Document]:  # type: ignore
+        query_emb = self.create_embedding_query(texts=[query])
         documents = self.document_store.query_by_embedding(query_emb[0], top_k, candidate_doc_ids)
-
         return documents
 
-    def create_embedding(self, texts: [str]):
-        """
-        Create embeddings for each text in a list of texts using the retrievers model (`self.embedding_model`)
-        :param texts: texts to embed
-        :return: list of embeddings (one per input text). Each embedding is a list of floats.
-        """
+    def create_embedding_query(self, texts: List[str]) -> List[Tuple[object, np.array]]:
+        result = self._generate_batch_predictions(texts=texts, model=self.query_encoder,
+                                                  tensorizer=self.tensorizer, batch_size=self.batch_size)
+        return result
 
-        # for backward compatibility: cast pure str input
-        if type(texts) == str:
-            texts = [texts]
-        assert type(texts) == list, "Expecting a list of texts, i.e. create_embeddings(texts=['text1',...])"
+    def create_embedding_passage(self, texts: List[str]) -> List[Tuple[object, np.array]]:
+        result = self._generate_batch_predictions(texts=texts, model=self.passage_encoder,
+                                                  tensorizer=self.tensorizer, batch_size=self.batch_size)
+        return result
 
-        if self.model_format == "farm":
-            res = self.embedding_model.inference_from_dicts(dicts=[{"text": t} for t in texts])
-            emb = [list(r["vec"]) for r in res] #cast from numpy
-        elif self.model_format == "sentence_transformers":
-            # text is single string, sentence-transformers needs a list of strings
-            res = self.embedding_model.encode(texts)  # get back list of numpy embedding vectors
-            emb = [list(r) for r in res] #cast from numpy
-        return emb
+    def _generate_batch_predictions(self,
+                                    texts: List[str],
+                                    model: torch.nn.Module,
+                                    tensorizer: Tensorizer,
+                                    batch_size: int = 16) -> List[Tuple[object, np.array]]:
+        n = len(texts)
+        total = 0
+        results = []
+        for j, batch_start in enumerate(range(0, n, batch_size)):
+
+            batch_token_tensors = [tensorizer.text_to_tensor(ctx) for ctx in
+                                   texts[batch_start:batch_start + batch_size]]
+
+            ctx_ids_batch = torch.stack(batch_token_tensors, dim=0).cuda()
+            ctx_seg_batch = torch.zeros_like(ctx_ids_batch).cuda()
+            ctx_attn_mask = tensorizer.get_attn_mask(ctx_ids_batch).cuda()
+            with torch.no_grad():
+                _, out, _ = model(ctx_ids_batch, ctx_seg_batch, ctx_attn_mask)
+            out = out.cpu()
+
+            total += len(batch_token_tensors)
+
+            results.extend([
+                (out[i].view(-1).numpy())
+                for i in range(out.size(0))
+            ])
+
+            if total % 10 == 0:
+                logger.info('Encoded passages %d', total)
+
+        return results
+
+    def _prepare_model(self, encoder, saved_state, prefix):
+        encoder.to(self.device)
+        if self.fp16:
+            try:
+                import apex
+                from apex import amp
+                apex.amp.register_half_function(torch, "einsum")
+            except ImportError:
+                raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+
+            encoder, _ = amp.initialize(encoder, None, opt_level=self.fp16_opt_level)
+
+        encoder.eval()
+
+        # load weights from the model file
+        model_to_load = encoder.module if hasattr(encoder, 'module') else encoder
+        logger.info('Loading saved model state ...')
+        logger.debug('saved model keys =%s', saved_state.model_dict.keys())
+
+        prefix_len = len(prefix)
+        ctx_state = {key[prefix_len:]: value for (key, value) in saved_state.model_dict.items() if
+                     key.startswith(prefix)}
+        model_to_load.load_state_dict(ctx_state)
+        return encoder
+
+    def _set_encoder_params_from_state(self, state):
+        if state:
+            params_to_save = ['do_lower_case', 'pretrained_model_cfg', 'encoder_model_type',
+            'pretrained_file',
+            'projection_dim', 'sequence_length']
+
+            override_params = [(param, state[param]) for param in params_to_save if param in state]
+            for param, value in override_params:
+                if hasattr(self, param):
+                    logger.warning('Overriding args parameter value from checkpoint state. Param = %s, value = %s',
+                                   param,
+                                   value)
+                setattr(self, param, value)
 
 
 class EmbeddingRetriever(BaseRetriever):
--- haystack/retriever/dpr_utils.py
+++ haystack/retriever/dpr_utils.py
@@ -1,40 +1,99 @@
 #!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# Building upon the code (https://github.com/facebookresearch/DPR) published by Facebook research together with this Publication:
+# Karpukhin, Vladimir, et al. "Dense Passage Retrieval for Open-Domain Question Answering." arXiv preprint arXiv:2004.04906 (2020).
+# (https://arxiv.org/abs/2004.04906)
 
-"""
- Command line tool that produces embeddings for a large documents base based on the pretrained ctx & question encoders
- Supposed to be used in a 'sharded' way to speed up the process.
-"""
-import argparse
-import csv
 import logging
-import pickle
 from typing import List, Tuple
 
-import numpy as np
 import torch
 from torch import nn
 from torch import Tensor as T
-
-from dpr.models import init_biencoder_components
-from dpr.options import add_encoder_params, setup_args_gpu, print_args, \
-    add_tokenizer_params, add_cuda_params
-from dpr.utils.data_utils import Tensorizer
-from dpr.utils.model_utils import setup_for_distributed_mode, get_model_obj, load_states_from_checkpoint
-
-from dpr.models.hf_models import HFBertEncoder
+from torch.serialization import default_restore_location
+import collections
 
 from transformers.tokenization_bert import BertTokenizer
+from transformers.modeling_bert import BertModel, BertConfig
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+CheckpointState = collections.namedtuple("CheckpointState",
+                                         ['model_dict', 'optimizer_dict', 'scheduler_dict', 'offset', 'epoch',
+                                          'encoder_params'])
+
+# UTILS
+def load_states_from_checkpoint(model_file: str) -> CheckpointState:
+    logger.info('Reading saved model from %s', model_file)
+    state_dict = torch.load(model_file, map_location=lambda s, l: default_restore_location(s, 'cpu'))
+    logger.info('model_state_dict keys %s', state_dict.keys())
+    return CheckpointState(**state_dict)
 
 
+# CLASSES
+class HFBertEncoder(BertModel):
+
+    def __init__(self, config, project_dim: int = 0):
+        BertModel.__init__(self, config)
+        assert config.hidden_size > 0, 'Encoder hidden_size can\'t be zero'
+        self.encode_proj = nn.Linear(config.hidden_size, project_dim) if project_dim != 0 else None
+        self.init_weights()
+
+    @classmethod
+    def init_encoder(cls, cfg_name: str, projection_dim: int = 0, dropout: float = 0.1, **kwargs) -> BertModel:
+        cfg = BertConfig.from_pretrained(cfg_name if cfg_name else 'bert-base-uncased')
+        if dropout != 0:
+            cfg.attention_probs_dropout_prob = dropout
+            cfg.hidden_dropout_prob = dropout
+        return cls.from_pretrained(cfg_name, config=cfg, project_dim=projection_dim, **kwargs)
+
+    def forward(self, input_ids: torch.Tensor, token_type_ids: torch.Tensor, attention_mask: torch.Tensor) -> Tuple[torch.Tensor, ...]:
+        if self.config.output_hidden_states:
+            sequence_output, pooled_output, hidden_states = super().forward(input_ids=input_ids,
+                                                                            token_type_ids=token_type_ids,
+                                                                            attention_mask=attention_mask)
+        else:
+            hidden_states = None
+            sequence_output, pooled_output = super().forward(input_ids=input_ids, token_type_ids=token_type_ids,
+                                                             attention_mask=attention_mask)
+
+        pooled_output = sequence_output[:, 0, :]
+        if self.encode_proj:
+            pooled_output = self.encode_proj(pooled_output)
+        return sequence_output, pooled_output, hidden_states
+
+    def get_out_size(self):
+        if self.encode_proj:
+            return self.encode_proj.out_features
+        return self.config.hidden_size
+
+
+class Tensorizer(object):
+    """
+    Component for all text to model input data conversions and related utility methods
+    """
+
+    # Note: title, if present, is supposed to be put before text (i.e. optional title + document body)
+    def text_to_tensor(self, text: str, title: str = None, add_special_tokens: bool = True):
+        raise NotImplementedError
+
+    def get_pair_separator_ids(self) -> T:
+        raise NotImplementedError
+
+    def get_pad_id(self) -> int:
+        raise NotImplementedError
+
+    def get_attn_mask(self, tokens_tensor: T):
+        raise NotImplementedError
+
+    def is_sub_word_id(self, token_id: int):
+        raise NotImplementedError
+
+    def to_string(self, token_ids, skip_special_tokens=True):
+        raise NotImplementedError
+
+    def set_pad_to_max(self, pad: bool):
+        raise NotImplementedError
 
 
 class BertTensorizer(Tensorizer):
@@ -82,16 +141,3 @@ class BertTensorizer(Tensorizer):
 
     def set_pad_to_max(self, do_pad: bool):
         self.pad_to_max = do_pad
-
-
-
-
-if __name__ == "__main__":
-    r = DPRRetriever()
-    text = """Aaron Aaron ( or ; "Ahärôn") is a prophet, high priest, and the brother of Moses in the Abrahamic religions. Knowledge of Aaron, along with his brother Moses, comes exclusively from religious texts, such as the Bible and Quran. The Hebrew Bible relates that, unlike Moses, who grew up in the Egyptian royal court, Aaron and his elder sister Miriam remained with their kinsmen in the eastern border-land of Egypt (Goshen). When Moses first confronted the Egyptian king about the Israelites, Aaron served as his brother's spokesman ("prophet") to the Pharaoh. Part of the Law (Torah) that Moses received from"""
-    texts = [text]
-    res = r.create_embedding_passage(texts)
-    print(res)
-    print("#################")
-    res = r.create_embedding_query(["Who is Aaron?"])
-    print(res)
