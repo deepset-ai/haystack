688c38b8b70f65cb5f941faf18a144f40261eea6
Tanay Soni <tanaysoni12@gmail.com>
2020-06-22 12:07:12 +0200

    Move out REST API from PyPI package (#160)

--- rest_api/elasticsearch_client.py
+++ rest_api/elasticsearch_client.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch
 
-from haystack.api.config import DB_HOST, DB_USER, DB_PW, DB_PORT, ES_CONN_SCHEME
+from rest_api.config import DB_HOST, DB_USER, DB_PW, DB_PORT, ES_CONN_SCHEME
 
 elasticsearch_client = Elasticsearch(
     hosts=[{"host": DB_HOST, "port": DB_PORT}], http_auth=(DB_USER, DB_PW), scheme=ES_CONN_SCHEME, ca_certs=False, verify_certs=False
