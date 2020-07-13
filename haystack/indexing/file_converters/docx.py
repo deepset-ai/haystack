2b2e16ce2e64e1b29e2c6f4698a25af6dab1ea1c
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-14 00:16:49 +0530

    Update docx.py
    Edit 1

--- haystack/indexing/file_converters/docx.py
+++ haystack/indexing/file_converters/docx.py
@@ -5,4 +5,23 @@ logger = logging.getLogger(__name__)
 
 class PDFToTextConverter(BaseConverter):
     def __init__(self):
-    
+        verify_installation = subprocess.run(["pdftotext -v"], shell=True)
+        if verify_installation.returncode == 127:
+            raise Exception(
+                """ python-docx is not installed.
+                
+                   Run !pip install python-docx
+                   
+                   Find more info at https://python-docx.readthedocs.io/en/latest/
+                """
+            )
+            
+            def page_text(self, file_path: Path) -> str:
+                import docx
+                doc = docx.Document(file_path)  # Creating word reader object.
+                text = ""
+                fullText = []
+                for para in doc.paragraphs:
+                  fullText.append(para.text)
+                text = '\n'.join(fullText)
+                return text
