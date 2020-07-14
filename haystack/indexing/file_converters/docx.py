8f4b8f53d822057412ad68213118052f0fdc8fd7
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-14 12:13:24 +0530

    Update docx.py
    Included Raising Exceptions

--- haystack/indexing/file_converters/docx.py
+++ haystack/indexing/file_converters/docx.py
@@ -5,6 +5,18 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 class docxToTextConverter(BaseConverter):
+    def __init__(self):
+        try:
+            import docx
+        except ImportError as e:
+            raise Exception(
+                """python-docx is not installed.
+                
+                Please run !pip install python-docx
+                
+                   You can find more details here: https://python-docx.readthedocs.io/en/latest/
+                """
+            )
     def page_text(self, file_path: Path) -> str:
         import docx
         doc = docx.Document(file_path)  # Creating word reader object.
