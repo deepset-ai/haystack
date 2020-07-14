59f63abe0589265341c96cee4ad9d6ce5aa7ecfe
Malte Pietsch <malte.pietsch@deepset.ai>
2020-07-14 12:16:24 +0200

    update test. add docstring. remove empty paragraphs

--- test/test_docx_conversion.py
+++ test/test_docx_conversion.py
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 
 def test_extract_pages():
     converter = DocxToTextConverter()
-    paragraphs = converter.extract_pages(file_path=Path("samples/sample_docx.docx"))
-    assert len(paragraphs)==14 #Sampe has 14 Paragraphs
-    assert paragraphs[1]=='' #Second Paragraph is Empty
+    paragraphs = converter.extract_pages(file_path=Path("samples/docx/sample_docx.docx"))
+    assert len(paragraphs)==8 #Sample has 8 Paragraphs
+    assert paragraphs[1]== 'The US has "passed the peak" on new coronavirus cases, President Donald Trump said and predicted that some states would reopen this month.'
