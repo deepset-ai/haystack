23df2cc936fb1bce9e9bc41da3d2ce7d5725740a
Anirban Saha <anirban.saha.1996@gmail.com>
2020-07-14 14:03:20 +0530

    Update test_docx_conversion.py

--- test/test_docx_conversion.py
+++ test/test_docx_conversion.py
@@ -9,5 +9,5 @@ logger = logging.getLogger(__name__)
 def test_extract_pages():
     converter = DocxToTextConverter()
     paragraphs = converter.extract_pages(file_path=Path("samples/sample_docx.docx"))
-    assert len(text)==14 #Sampe has 14 Paragraphs
-    assert text[1]=='' #Second Paragraph is Empty
+    assert len(paragraphs)==14 #Sampe has 14 Paragraphs
+    assert paragraphs[1]=='' #Second Paragraph is Empty
