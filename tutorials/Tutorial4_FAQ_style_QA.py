77c3c28bf8f58034116945db950553a94334b544
Malte Pietsch <malte.pietsch@deepset.ai>
2020-07-17 19:31:00 +0200

    Update Tutorial 4

--- tutorials/Tutorial4_FAQ_style_QA.py
+++ tutorials/Tutorial4_FAQ_style_QA.py
@@ -68,6 +68,7 @@ print(df.head())
 # Get embeddings for our questions from the FAQs
 questions = list(df["question"].values)
 df["question_emb"] = retriever.embed_queries(texts=questions)
+df["question_emb"] = df["question_emb"].apply(list) # convert from numpy to list for ES indexing
 
 # Convert Dataframe to list of dicts and index them in our DocumentStore
 docs_to_index = df.to_dict(orient="records")
