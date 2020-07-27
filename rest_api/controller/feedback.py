e342b72745bf86aca7d570bc160128a575589fa6
Tanay Soni <tanaysoni12@gmail.com>
2020-07-27 17:19:17 +0200

    Adjust DOC QA feedback API as per the new Label objects

--- rest_api/controller/feedback.py
+++ rest_api/controller/feedback.py
@@ -44,7 +44,7 @@ document_store = ElasticsearchDocumentStore(
 
 class Feedback(BaseModel):
     question: str = Field(..., description="The question input by the user, i.e., the query.")
-    label: str = Field(..., description="The Label for the feedback, eg, relevant or irrelevant.")
+    positive_sample: bool = Field(..., description="Whether the feedback is positive or negative.")
     document_id: str = Field(..., description="The document in the query result for which feedback is given.")
     answer: Optional[str] = Field(None, description="The answer string. Only required for doc-qa feedback.")
     offset_start_in_doc: Optional[int] = Field(None, description="The answer start offset in the original doc. Only required for doc-qa feedback.")
@@ -54,7 +54,7 @@ class Feedback(BaseModel):
 @router.post("/doc-qa-feedback")
 def doc_qa_feedback(feedback: Feedback):
     if feedback.answer and feedback.offset_start_in_doc:
-        elasticsearch_client.index(index=DB_INDEX_FEEDBACK, body=feedback.dict())
+        document_store.write_labels([{"origin": "api", **feedback.dict()}])
     else:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -68,34 +68,29 @@ def faq_qa_feedback(feedback: Feedback):
 
 
 @router.get("/export-doc-qa-feedback")
-def export_doc_qa_feedback():
+def export_doc_qa_feedback(context_size: int = 10_000):
     """
     SQuAD format JSON export for question/answer pairs that were marked as "relevant".
     """
-    #TODO filter out faq-qa feedback.
-    #TODO Reduce length of context for large documents
-
-    relevant_feedback_query = {"query": {"bool": {"must": [{"term": {"label": "relevant"}}]}}}
-    result = scan(elasticsearch_client, index=DB_INDEX_FEEDBACK, query=relevant_feedback_query)
-
-    per_document_feedback = defaultdict(list)
-    for feedback in result:
-        document_id = feedback["_source"]["document_id"]
-        per_document_feedback[document_id].append(
-            {
-                "question": feedback["_source"]["question"],
-                "id": feedback["_id"],
-                "answers": [
-                    {"text": feedback["_source"]["answer"], "answer_start": feedback["_source"]["offset_start_in_doc"]}
-                ],
-            }
-        )
+    # TODO filter out faq-qa feedback.
+    labels = document_store.get_all_labels(index=DB_INDEX_FEEDBACK, filters={"positive_sample": [True], "origin": ["api"]})
 
     export_data = []
-    for document_id, feedback in per_document_feedback.items():
-        document = document_store.get_document_by_id(document_id)
+    for label in labels:
+        document = document_store.get_document_by_id(label.document_id)
         context = document.text
-        export_data.append({"paragraphs": [{"qas": feedback, "context": context}],})
+
+        context_to_add = int((context_size - len(label.answer)) / 2)
+        start_pos = max(label.offset_start_in_doc - context_to_add, 0)
+        if context_to_add > label.offset_start_in_doc:
+            append_at_end = context_to_add - label.offset_start_in_doc
+        else:
+            append_at_end = 0
+        end_pos = min(context_to_add + label.offset_start_in_doc + len(label.answer) + append_at_end, len(context))
+
+        context_to_export = context[start_pos:end_pos]
+
+        export_data.append({"paragraphs": [{"qas": label, "context": context_to_export}]})
 
     export = {"data": export_data}
 
