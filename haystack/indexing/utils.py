043f6fe0eb1c9e2a4c49350f85d1015c0a19a1a1
Dan <yudan@svr.dansden.home-yu.net>
2020-08-11 17:08:19 -0700

    More accurately split paragraphs

--- haystack/indexing/utils.py
+++ haystack/indexing/utils.py
@@ -1,3 +1,4 @@
+import re
 import logging
 import tarfile
 import tempfile
@@ -103,7 +104,8 @@ def convert_files_to_dicts(dir_path: str, clean_func: Optional[Callable] = None,
     return documents
 
 
-def tika_convert_files_to_dicts(dir_path: str, clean_func: Optional[Callable] = None, split_paragraphs: bool = False) -> List[dict]:
+def tika_convert_files_to_dicts(dir_path: str, clean_func: Optional[Callable] = None\
+    , split_paragraphs: bool = False, merge_short: bool = True, merge_lowercase: bool = True) -> List[dict]:
     """
     Convert all files(.txt, .pdf) in the sub-directories of the given path to Python dicts that can be written to a
     Document Store.
@@ -114,7 +116,7 @@ def tika_convert_files_to_dicts(dir_path: str, clean_func: Optional[Callable] = 
 
     :return: None
     """
-    converter = TikaConverter(remove_header_footer=True)
+    converter = TikaConverter(remove_header_footer=True, return_meta=True)
     file_paths = [p for p in Path(dir_path).glob("**/*")]
 
     documents = []
@@ -124,23 +126,46 @@ def tika_convert_files_to_dicts(dir_path: str, clean_func: Optional[Callable] = 
         meta["name"] = path.name
         text = ' '.join(pages)
 
-        if clean_func:
-            text = clean_func(text)
-
         if split_paragraphs:
-            paragraph = ''
-            for para in text.split("\n\n"):
-                para = para.strip()
-                if not para:  # skip empty and segements
-                    continue
-                paragraph += ' ' + para
-                if para[-1] in '.?!\'")]':
-                    documents.append({"text": paragraph, "meta": meta})
-                    paragraph = ''
-            if paragraph:
-                documents.append({"text": paragraph, "meta": meta})
+            # for para in text.split("\n\n"):
+            #     if not para.strip():  # skip empty paragraphs
+            #         continue
+            #     documents.append({"text": para, "meta": {"name": path.name}})
+            if pages:
+                paras = pages[0].split("\n\n")
+                # pop the last paragraph from the first page
+                last_para = paras.pop(-1) if paras else ''
+                for page in pages[1:]:
+                    page_paras = page.split("\n\n")
+                    # merge the last paragraph in previous page to the first paragraph in this page
+                    if page_paras:
+                        page_paras[0] = last_para + ' ' + page_paras[0]
+                        last_para = page_paras.pop(-1)
+                        paras += page_paras
+                if last_para:
+                    paras.append(last_para)
+                if paras:
+                    last_para = ''
+                    for para in paras:
+                        para = para.strip()
+                        if not para: continue
+                        # merge paragraphs to improve qa
+                        # merge this paragraph if less than 10 characters or 2 words
+                        # or this paragraph starts with a lower case and last paragraph does not end with a punctuation
+                        if merge_short and len(para) < 10 or len(re.findall('\s+', para)) < 2 \
+                            or merge_lowercase and para and para[0].islower() and last_para and last_para[-1] not in '.?!"\'\]\)':
+                            last_para += ' ' + para
+                        else:
+                            if last_para:
+                                documents.append({"text": last_para, "meta": meta})
+                            last_para = para
+                    # don't forget the last one
+                    if last_para:
+                        documents.append({"text": last_para, "meta": meta})
         else:
-            documents.append({"text": ' '.join(pages), "meta": meta})
+            if clean_func:
+                text = clean_func(text)
+            documents.append({"text": text, "meta": meta})
 
     return documents
 
