200bb4bafdc6c9fdfaea58a4d240656482b92801
Tanay Soni <tanaysoni12@gmail.com>
2020-08-17 13:30:02 +0200

    Refactor the DPR tutorial to use FAISS (#317)

--- tutorials/Tutorial6_Better_Retrieval_via_DPR.py
+++ tutorials/Tutorial6_Better_Retrieval_via_DPR.py
@@ -1,36 +1,17 @@
-import logging
-import subprocess
-import time
-
 from haystack import Finder
-from haystack.database.elasticsearch import ElasticsearchDocumentStore
+from haystack.database.faiss import FAISSDocumentStore
 from haystack.indexing.cleaning import clean_wiki_text
 from haystack.indexing.utils import convert_files_to_dicts, fetch_archive_from_http
 from haystack.reader.farm import FARMReader
-from haystack.reader.transformers import TransformersReader
 from haystack.utils import print_answers
-from haystack.retriever.sparse import ElasticsearchRetriever
 from haystack.retriever.dense import DensePassageRetriever
 
-LAUNCH_ELASTICSEARCH = True
 
-# Start an Elasticsearch server
-# You can start Elasticsearch on your local machine instance using Docker. If Docker is not readily available in
-# your environment (eg., in Colab notebooks), then you can manually download and execute Elasticsearch from source.
-
-if LAUNCH_ELASTICSEARCH:
-    logging.info("Starting Elasticsearch ...")
-    status = subprocess.run(
-        ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2'], shell=True
-    )
-    if status.returncode:
-        raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
-                        "then set LAUNCH_ELASTICSEARCH in the script to False.")
-    time.sleep(15)
-
-# Connect to Elasticsearch
-document_store = ElasticsearchDocumentStore(host="localhost", username="", password="",
-                                            index="document", embedding_dim=768, embedding_field="embedding")
+# FAISS is a library for efficient similarity search on a cluster of dense vectors.
+# The FAISSDocumentStore uses a SQL(SQLite in-memory be default) database under-the-hood
+# to store the document text and other meta data. The vector embeddings of the text are
+# indexed on a FAISS Index that later is queried for searching answers.
+document_store = FAISSDocumentStore()
 
 # ## Cleaning & indexing documents
 # Let's first get some documents that we want to query
@@ -42,7 +23,7 @@ fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 dicts = convert_files_to_dicts(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
 
 # Now, let's write the docs to our DB.
-document_store.write_documents(dicts[:16])
+document_store.write_documents(dicts)
 
 ### Retriever
 retriever = DensePassageRetriever(document_store=document_store, embedding_model="dpr-bert-base-nq",
