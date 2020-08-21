7d2a8f19fc4b8e5aec61559dc2fa2915afa30dbe
Tanay Soni <tanaysoni12@gmail.com>
2020-08-21 09:24:49 +0200

    Improve speed for SQLDocumentStore (#330)

--- haystack/database/faiss.py
+++ haystack/database/faiss.py
@@ -150,9 +150,9 @@ class FAISSDocumentStore(SQLDocumentStore):
         _, vector_id_matrix = self.faiss_index.search(hnsw_vectors, top_k)
         vector_ids_for_query = [str(vector_id) for vector_id in vector_id_matrix[0] if vector_id != -1]
 
-        documents = [
-            self.get_all_documents(filters={"vector_id": [vector_id]})[0] for vector_id in vector_ids_for_query
-        ]
+        documents = self.get_all_documents(filters={"vector_id": vector_ids_for_query}, index=index)
+        # sort the documents as per query results
+        documents = sorted(documents, key=lambda doc: vector_ids_for_query.index(doc.meta["vector_id"]))  # type: ignore
 
         return documents
 
