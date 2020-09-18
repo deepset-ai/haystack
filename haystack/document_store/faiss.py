26946e2da2f75b9a321e296584a768a77311d1ec
Malte Pietsch <malte.pietsch@deepset.ai>
2020-09-18 09:48:05 +0200

    revert score change

--- haystack/document_store/faiss.py
+++ haystack/document_store/faiss.py
@@ -161,7 +161,8 @@ class FAISSDocumentStore(SQLDocumentStore):
         # assign query score to each document
         scores_for_vector_ids: Dict[str, float] = {str(v_id): s for v_id, s in zip(vector_id_matrix[0], score_matrix[0])}
         for doc in documents:
-            doc.query_score = scores_for_vector_ids[doc.meta["vector_id"]]  # type: ignore
+            doc.score = scores_for_vector_ids[doc.meta["vector_id"]]  # type: ignore
+            doc.probability = (doc.score + 1) / 2
 
         return documents
 
