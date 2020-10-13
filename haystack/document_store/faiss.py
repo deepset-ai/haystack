12c4dd7b4b54a6ecd9a3d0f024ca87f94c8987e9
Lalit Pagaria <pagaria.lalit@gmail.com>
2020-10-13 17:12:24 +0200

    Adjust requirements for Windows (#480)

--- haystack/document_store/faiss.py
+++ haystack/document_store/faiss.py
@@ -1,16 +1,21 @@
 import logging
+from sys import platform
 from pathlib import Path
 from typing import Union, List, Optional, Dict
 from tqdm import tqdm
-import faiss
 import numpy as np
-import random
 
 from haystack import Document
 from haystack.document_store.sql import SQLDocumentStore
 from haystack.retriever.base import BaseRetriever
 
+if platform != 'win32' and platform != 'cygwin':
+    import faiss
+else:
+    raise ModuleNotFoundError("FAISSDocumentStore on windows platform is not supported")
+
 logger = logging.getLogger(__name__)
+
 
 class FAISSDocumentStore(SQLDocumentStore):
     """
