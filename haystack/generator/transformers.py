9465fc5ff5892bfe31e37504e6efe8f199264f27
lalitpagaria <pagaria.lalit@gmail.com>
2020-10-18 11:23:09 +0200

    Refined the implementation of RAGGenerator and now it is in clean shape

--- haystack/generator/transformers.py
+++ haystack/generator/transformers.py
@@ -4,7 +4,8 @@ from typing import List, Optional
 
 import numpy
 import torch
-from transformers import RagSequenceForGeneration, RagTokenForGeneration, RagTokenizer
+from transformers import DPRContextEncoder, DPRContextEncoderTokenizerFast, RagSequenceForGeneration, RagTokenizer, \
+    RagTokenForGeneration
 
 from haystack import Document
 from haystack.generator.base import BaseGenerator
@@ -18,91 +19,227 @@ class RAGGeneratorType(Enum):
 
 
 class RAGGenerator(BaseGenerator):
+    """
+        Implementation of Facebook's Retrieval-Augmented Generator (https://arxiv.org/abs/2005.11401) based on
+        HuggingFace's transformers (https://huggingface.co/transformers/model_doc/rag.html).
 
+        |  With the generator, you can:
+
+            - directly get generate predictions via predict()
+    """
     def __init__(
             self,
             model_name_or_path: str = "facebook/rag-token-nq",
+            passage_embedding_model: str = "facebook/dpr-ctx_encoder-single-nq-base",
             generator_type: RAGGeneratorType = RAGGeneratorType.TOKEN,
-            top_k_answers: int = 5,
+            top_k_answers: int = 2,
             max_length: int = 200,
             min_length: int = 2,
+            num_beams: int = 2,
+            embed_title: bool = True,
+            prefix: Optional[str] = None,
+            use_gpu: bool = True
     ):
+        """
+        Load a RAG model from Transformers along with passage_embedding_model.
+        See https://huggingface.co/transformers/model_doc/rag.html for more details
+
+        :param model_name_or_path: Directory of a saved model or the name of a public model e.g.
+                                   'facebook/rag-token-nq', 'facebook/rag-sequence-nq'.
+                                   See https://huggingface.co/models for full list of available models.
+        :param passage_embedding_model: Local path or remote name of passage encoder checkpoint. The format equals the
+                                        one used by hugging-face transformers' model hub models.
+                                        Currently available remote names: ``"facebook/dpr-ctx_encoder-single-nq-base"``
+        :param generator_type: Which RAG generator implementation to use? RAG-TOKEN or RAG-SEQUENCE
+        :param top_k_answers: Number of independently generated text to return
+        :param max_length: Maximum length of generated text
+        :param min_length: Minimum length of generated text
+        :param num_beams: Number of beams for beam search. 1 means no beam search.
+        :param embed_title: Embedded the title of passage while generating embedding
+        :param prefix: The prefix used by the generator's tokenizer.
+        :param use_gpu: Whether to use GPU (if available)
+        """
+
         self.model_name_or_path = model_name_or_path
         self.top_k_answers = top_k_answers
         self.max_length = max_length
         self.min_length = min_length
-        self.tokenizer = RagTokenizer.from_pretrained(model_name_or_path)
         self.generator_type = generator_type
+        self.num_beams = num_beams
+        self.embed_title = embed_title
+        self.prefix = prefix
+
+        if use_gpu and torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+
+        self.tokenizer = RagTokenizer.from_pretrained(model_name_or_path)
 
         if self.generator_type == RAGGeneratorType.SEQUENCE:
             self.model = RagSequenceForGeneration.from_pretrained(model_name_or_path)
         else:
             self.model = RagTokenForGeneration.from_pretrained(model_name_or_path)
 
+        self.passage_tokenizer = DPRContextEncoderTokenizerFast.from_pretrained(passage_embedding_model)
+        self.passage_encoder = DPRContextEncoder.from_pretrained(passage_embedding_model).to(self.device)
 
-    # TODO: Copy from RagRetriever and modified
-    # Need to understand the logic
-    def _postprocess_docs(self, retrieved_texts: List[str], question: str, return_tensors: str = "pt"):
-        def cat_input_and_doc(doc_text, input_string):
-            out = (" / " + doc_text + " // " + input_string).replace("  ", " ")
-            return out
+    # Copied cat_input_and_doc method from transformers.RagRetriever
+    def _cat_input_and_doc(self, doc_title: str, doc_text: str, input_string: str, prefix: Optional[str]):
+        if doc_title.startswith('"'):
+            doc_title = doc_title[1:]
+        if doc_title.endswith('"'):
+            doc_title = doc_title[:-1]
+        if prefix is None:
+            prefix = ""
+        out = (prefix + doc_title + self.model.config.title_sep + doc_text + self.model.config.doc_sep +
+               input_string).replace("  ", " ")
+
+        return out
+
+    # Copied postprocess_docs method from transformers.RagRetriever and modified
+    def _get_contextualized_embedding(self, texts: List[str], question: str, titles: Optional[List[str]] = None,
+                                      return_tensors: str = "pt"):
+
+        titles_list = titles if self.embed_title and titles is not None else [""] * len(texts)
+        prefix = self.prefix if self.prefix is not None else self.model.config.generator.prefix
 
         rag_input_strings = [
-            cat_input_and_doc(
-                retrieved_texts[i],
-                question,
+            self._cat_input_and_doc(
+                doc_title=titles_list[i],
+                doc_text=texts[i],
+                input_string=question,
+                prefix=prefix
             )
-            for i in range(len(retrieved_texts))
+            for i in range(len(texts))
         ]
 
-        contextualized_inputs = self.tokenizer.question_encoder.batch_encode_plus(
+        contextualized_embedding = self.tokenizer.question_encoder.batch_encode_plus(
             rag_input_strings,
-            max_length=300,
+            max_length=self.model.config.max_length,
             return_tensors=return_tensors,
             padding="max_length",
             truncation=True,
         )
 
-        return contextualized_inputs["input_ids"], contextualized_inputs["attention_mask"]
+        return contextualized_embedding["input_ids"].to(self.device), \
+               contextualized_embedding["attention_mask"].to(self.device)
+
+    def _embed_passage(self, title: str, text: str) -> torch.Tensor:
+        """Compute the DPR embeddings of document passages"""
+        input_ids = self.passage_tokenizer(
+            title,
+            text,
+            truncation=True,
+            padding="longest",
+            return_tensors="pt"
+        )["input_ids"]
+
+        embeddings = self.passage_encoder(
+            input_ids.to(device=self.device),
+            return_dict=True
+        ).pooler_output
+
+        return embeddings.detach().to(self.device)
+
+    def embed_passage_in_tensor(self, titles: List[str], texts: List[str],
+                                embeddings: List[Optional[numpy.ndarray]]) -> torch.Tensor:
+
+        embeddings_in_tensor: List[torch.Tensor] = []
+
+        # If of document missing embedding, then need embedding for all the documents
+        is_embedding_required = embeddings.__contains__(None)
+
+        if is_embedding_required:
+            for title, text in zip(titles, texts):
+                embeddings_in_tensor.append(
+                    self._embed_passage(
+                        title=title,
+                        text=text
+                    )
+                )
+        else:
+            embeddings_in_tensor = [torch.from_numpy(embedding) for embedding in embeddings]
+
+        return torch.cat(embeddings_in_tensor, dim=0).to(self.device)
 
     def predict(self, question: str, documents: List[Document], top_k: Optional[int] = None):
-        if len(documents) == 0 or documents[0].embedding is None:
-            raise AttributeError("generator need documents with embedding")
+        if len(documents) == 0:
+            raise AttributeError("generator need documents to predict the answer")
 
-        top_k_answers = top_k or self.top_k_answers
+        top_k_answers = top_k if top_k is None else self.top_k_answers
 
-        docs_embedding = []
-        retrieved_texts = []
-        for doc in documents:
-            docs_embedding.append(doc.embedding)
-            retrieved_texts.append(doc.text)
+        # Flatten the documents so easy to reference
+        flat_docs_dict = {}
+        for document in documents:
+            for k, v in document.__dict__.items():
+                if k not in flat_docs_dict:
+                    flat_docs_dict[k] = []
+                flat_docs_dict[k].append(v)
 
-        # Question tokenization and encoding
-        input_dict = self.tokenizer.prepare_seq2seq_batch(src_texts=[question], return_tensors="pt")
-        question_hidden_states = self.model.question_encoder(input_dict["input_ids"])[0]
-        embedding_in_tensor = torch.from_numpy(numpy.array(docs_embedding)).to(question_hidden_states).float()
+        # Extract title
+        titles = [d.meta["name"] if d.meta and "name" in d.meta else "" for d in documents]
 
-        # Compute doc scores
-        doc_scores = torch.bmm(question_hidden_states.unsqueeze(1),
-                               embedding_in_tensor.unsqueeze(0).transpose(1, 2)).squeeze(1)
+        # Raw document embedding and set device of question_embedding
+        passage_embedding = self.embed_passage_in_tensor(
+            titles=titles,
+            texts=flat_docs_dict["text"],
+            embeddings=flat_docs_dict["embedding"]
+        )
 
-        # Get context_input_ids and context_attention_mask
-        context_input_ids, context_attention_mask = self._postprocess_docs(retrieved_texts=retrieved_texts,
-                                                                           question=question)
+        # Question tokenization
+        input_dict = self.tokenizer.prepare_seq2seq_batch(
+            src_texts=[question],
+            return_tensors="pt"
+        )
+
+        # Question embedding
+        question_embedding = self.model.question_encoder(input_dict["input_ids"])[0]
+
+        # Prepare contextualized input_ids of documents
+        # (will be transformed into contextualized embedding inside generator)
+        context_input_ids, context_attention_mask = self._get_contextualized_embedding(
+            texts=flat_docs_dict["text"],
+            titles=titles,
+            question=question
+        )
+
+        # Compute doc scores from docs_embedding
+        doc_scores = torch.bmm(question_embedding.unsqueeze(1),
+                               passage_embedding.unsqueeze(0).transpose(1, 2)).squeeze(1)
+
+        # TODO Bug in extend_enc_output function of generator
+        # Refer https://github.com/huggingface/transformers/issues/7874
+        self.model.config.n_docs = len(flat_docs_dict["text"])
 
         # Get generated ids from generator
+        # TODO: Handle RagSequenceForGeneration case refer https://github.com/huggingface/transformers/issues/7829
         generator_ids = self.model.generate(
-            input_ids=input_dict["input_ids"],
             context_input_ids=context_input_ids,
             context_attention_mask=context_attention_mask,
             doc_scores=doc_scores,
-            num_beams=top_k_answers,
+            num_return_sequences=top_k_answers,
+            num_beams=self.num_beams,
             max_length=self.max_length,
             min_length=self.min_length
         )
 
+        result = {"question": question, "answers": []}
         answers = self.tokenizer.batch_decode(generator_ids, skip_special_tokens=True)
 
-        result = {"question": question, "answers": answers}
+        for answer in answers:
+            cur_answer = {
+                "question": question,
+                "answer": answer,
+                "meta": {
+                    "doc_ids": flat_docs_dict["id"],
+                    "doc_scores": flat_docs_dict["score"],
+                    "doc_probabilities": flat_docs_dict["probability"],
+                    "texts": flat_docs_dict["text"],
+                    "titles": titles
+                    # TODO: Meta as well?
+                }
+            }
+            result["answers"].append(cur_answer)
 
         return result
--- tutorials/Tutorial7_generator.py
+++ tutorials/Tutorial7_generator.py
@@ -1,109 +1,33 @@
 from typing import List
 
-import numpy
-from transformers import RagTokenizer, RagRetriever, RagTokenForGeneration, RagSequenceForGeneration
-import torch
-
 from haystack import Document
 from haystack.document_store.faiss import FAISSDocumentStore
 from haystack.generator.transformers import RAGGenerator
 from haystack.retriever.dense import DensePassageRetriever
 
-
-# Copy pasted from RagRetriever
-def postprocess_docs(tokenizer, docs, input_strings, prefix, n_docs, return_tensors=None):
-    r"""
-    Postprocessing retrieved ``docs`` and combining them with ``input_strings``.
-
-    Args:
-        doc_scores (:obj:`np.ndarray` of shape :obj:`(batch_size, n_docs)`):
-            Retrieval scores of respective docs - passed for logging.
-        docs  (:obj:`dict`):
-            Retrieved documents.
-        input_strings (:obj:`str`):
-            Input strings decoded by ``preprocess_query``.
-        prefix (:obj:`str`):
-            Prefix added at the beginning of each input, typically used with T5-based models.
-
-    Return:
-        :obj:`tuple(tensors)`:
-            a tuple consisting of two elements: contextualized ``input_ids`` and a compatible ``attention_mask``.
-    """
-
-    def cat_input_and_doc(doc_title, doc_text, input_string, prefix):
-        # TODO(Patrick): if we train more RAG models, I want to put the input first to take advantage of effortless truncation
-        # TODO(piktus): better handling of truncation
-        if doc_title.startswith('"'):
-            doc_title = doc_title[1:]
-        if doc_title.endswith('"'):
-            doc_title = doc_title[:-1]
-        if prefix is None:
-            prefix = ""
-        out = (prefix + doc_title + " / " + doc_text + " // " + input_string).replace(
-            "  ", " "
-        )
-        return out
-
-    rag_input_strings = [
-        cat_input_and_doc(
-            docs[i]["title"][j],
-            docs[i]["text"][j],
-            input_strings[i],
-            prefix,
-        )
-        for i in range(len(docs))
-        for j in range(n_docs)
-    ]
-
-    contextualized_inputs = tokenizer.batch_encode_plus(
-        rag_input_strings,
-        max_length=300,
-        return_tensors=return_tensors,
-        padding="max_length",
-        truncation=True,
-    )
-
-    return contextualized_inputs["input_ids"], contextualized_inputs["attention_mask"]
-
-
-# Copied from https://github.com/huggingface/transformers/pull/7763/files
-def split_text(text: str, n=100, character=" ") -> List[str]:
-    """Split the text every ``n``-th occurence of ``character``"""
-    text = text.split(character)
-    return [character.join(text[i : i + n]).strip() for i in range(0, len(text), n)]
-
-
-def split_documents(documents: dict) -> dict:
-    """Split documents into passages"""
-    titles, texts = [], []
-    for title, text in zip(documents["title"], documents["text"]):
-        for passage in split_text(text):
-            titles.append(title)
-            texts.append(passage)
-    return {"title": titles, "text": texts}
-
-
 # haystack part
 
 documents = [
     Document(
-        text="""Aaron Aaron ( or ; ""Ahärôn"") is a prophet, high priest, and the brother of Moses in the Abrahamic religions. Knowledge of Aaron, along with his brother Moses, comes exclusively from religious texts, such as the Bible and Quran. The Hebrew Bible relates that, unlike Moses, who grew up in the Egyptian royal court, Aaron and his elder sister Miriam remained with their kinsmen in the eastern border-land of Egypt (Goshen). When Moses first confronted the Egyptian king about the Israelites, Aaron served as his brother's spokesman (""prophet"") to the Pharaoh. Part of the Law (Torah) that Moses received from""",
-        meta={"name": "0"}
+        text="""Aaron Aaron ( or ; ""Ahärôn"") is a prophet, high priest, and the brother of Moses in the Abrahamic religions. Knowledge of Aaron, along with his brother Moses, comes exclusively from religious texts, such as the Bible and Quran. The Hebrew Bible relates that, unlike Moses, who grew up in the Egyptian royal court, Aaron and his elder sister Miriam remained with their kinsmen in the eastern border-land of Egypt (Goshen). When Moses first confronted the Egyptian king about the Israelites, Aaron served as his brother's spokesman (""prophet"") to the Pharaoh. Part of the Law (Torah) that Moses received from"""
     ),
     Document(
         text="""Democratic Republic of the Congo to the south. Angola's capital, Luanda, lies on the Atlantic coast in the northwest of the country. Angola, although located in a tropical zone, has a climate that is not characterized for this region, due to the confluence of three factors: As a result, Angola's climate is characterized by two seasons: rainfall from October to April and drought, known as ""Cacimbo"", from May to August, drier, as the name implies, and with lower temperatures. On the other hand, while the coastline has high rainfall rates, decreasing from North to South and from to , with""",
     ),
     Document(
-        text="""Schopenhauer, describing him as an ultimately shallow thinker: ""Schopenhauer has quite a crude mind ... where real depth starts, his comes to an end."" His friend Bertrand Russell had a low opinion on the philosopher, and attacked him in his famous ""History of Western Philosophy"" for hypocritically praising asceticism yet not acting upon it. On the opposite isle of Russell on the foundations of mathematics, the Dutch mathematician L. E. J. Brouwer incorporated the ideas of Kant and Schopenhauer in intuitionism, where mathematics is considered a purely mental activity, instead of an analytic activity wherein objective properties of reality are""",
-        meta={"name": "1"}
+        text="""Schopenhauer, describing him as an ultimately shallow thinker: ""Schopenhauer has quite a crude mind ... where real depth starts, his comes to an end."" His friend Bertrand Russell had a low opinion on the philosopher, and attacked him in his famous ""History of Western Philosophy"" for hypocritically praising asceticism yet not acting upon it. On the opposite isle of Russell on the foundations of mathematics, the Dutch mathematician L. E. J. Brouwer incorporated the ideas of Kant and Schopenhauer in intuitionism, where mathematics is considered a purely mental activity, instead of an analytic activity wherein objective properties of reality are"""
     ),
     Document(
-        text="""The Dothraki vocabulary was created by David J. Peterson well in advance of the adaptation. HBO hired the Language Creatio""",
-        meta={"name": "2"}
+        text="""Berlin is Germany capital"""
     ),
     Document(
-        text="""The title of the episode refers to the Great Sept of Baelor, the main religious building in King's Landing, where the episode's pivotal scene takes place. In the world created by George R. R. Martin""",
-        meta={}
+        text="""Berlin is the capital and largest city of Germany by both area and population.""",
+    ),
+    Document(
+        text="""Today, Germany is a federal parliamentary republic led by a chancellor. With over 83 million inhabitants of its 16 constituent states.""",
+    ),
+    Document(
+        text="""The title of the episode refers to the Great Sept of Baelor, the main religious building in King's Landing, where the episode's pivotal scene takes place. In the world created by George R. R. Martin"""
     )
 ]
 
@@ -121,68 +45,9 @@ document_store.update_embeddings(retriever=retriever)
 
 docs_with_emb = document_store.get_all_documents()
 
-question = "Dothraki vocabulary"
-retriever_results = retriever.retrieve(query=question, top_k=5)
+question = "Population of Germany?"
+retriever_results = retriever.retrieve(query=question, top_k=2)
 
-stored_emb = []
-retriever_texts = []
-rag_format_doc = {"text": [], "title": []}
-for retriever_result in retriever_results:
-    retriever_texts.append(retriever_result.text)
-    rag_format_doc['text'].append(retriever_result.text)
-    rag_format_doc['title'].append("")
-    retriever_result.embedding = document_store.faiss_index.reconstruct(int(retriever_result.meta["vector_id"]))
-    stored_emb.append(retriever_result.embedding)
-
-rag_format_docs = [rag_format_doc]
-
-# Without RagRetriever
-# Currently not working
-def generator_test_1():
-    tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-base")
-    model = RagTokenForGeneration.from_pretrained("facebook/rag-token-base")
-
-    input_dict = tokenizer.prepare_seq2seq_batch(src_texts=[question], return_tensors="pt")
-    print("input_dict: ", input_dict)
-
-    doc_dict = tokenizer.prepare_seq2seq_batch(src_texts=[question],
-                                               tgt_texts=retriever_texts, return_tensors="pt")
-
-    print("doc_dict: ", doc_dict)
-
-    question_hidden_states = model.question_encoder(input_dict["input_ids"])[0]
-    embedding_in_tensor = torch.from_numpy(numpy.array(stored_emb)).to(question_hidden_states).float()
-
-    doc_scores = torch.bmm(question_hidden_states.unsqueeze(1),
-                           embedding_in_tensor.unsqueeze(0).transpose(1, 2)).squeeze(1)
-
-    context_input_ids, context_attention_mask = postprocess_docs(tokenizer=retriever.query_tokenizer,
-                docs=rag_format_docs, input_strings=[question], prefix=None, n_docs=len(retriever_texts), return_tensors="pt"
-            )
-    outputs = model.generate(
-        input_ids=input_dict["input_ids"],
-        context_input_ids=context_input_ids, # doc_dict["input_ids"],
-        context_attention_mask=context_attention_mask, # doc_dict["attention_mask"],
-        doc_scores=doc_scores
-    )
-
-    generated_string = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    print("====>1): ", generated_string)
-
-    # outputs = model.generate(
-    #     input_ids=input_dict["input_ids"],
-    #     context_input_ids=doc_dict["input_ids"],
-    #     context_attention_mask=doc_dict["attention_mask"],
-    #     doc_scores=doc_scores
-    # )
-    # generated_string = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    # print("====>2): ", generated_string)
-
-
-def generator_test_2():
-    haystack_generator = RAGGenerator()
-    predicted_result = haystack_generator.predict(question=question, documents=retriever_results)
-    print(predicted_result)
-
-
-generator_test_2()
+haystack_generator = RAGGenerator()
+predicted_result = haystack_generator.predict(question=question, documents=retriever_results, top_k=2)
+print(predicted_result)
