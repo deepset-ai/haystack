079e60c2a5bc9cf7aaf4c27f4c2cc969239d14ff
Florian Zimmermeister <flozi00.fz@gmail.com>
2020-11-11 11:36:16 +0100

    code style and qucik fix

--- haystack/preprocessor/queryexpander.py
+++ haystack/preprocessor/queryexpander.py
@@ -1,47 +1,41 @@
-from whatthelang import WhatTheLang
+from langdetect import detect
 import spacy
 
 
 class QueryExpander:
-  def __init__(num_keyword_repeats = 3, num_synonyms_per_word = 3):
-    self.num_keyword_repeats = num_keyword_repeats
-    self.num_synonyms_per_word = num_synonyms_per_word
-    self.spacy_models = {}
-    self.spacy_models["multi"] = spacy.load("xx_ent_wiki_sm")
-    self.spacy_models["en"] = spacy.load("en_core_web_sm")
-    
-    self.wtl = WhatTheLang()
-    
-    
-  def remove_stop(query,lang = None):
-    if(lang == None):
-      lang  = self.detect(query)
-    if(lang in ["en"]):
-        keyword_model = spacy_models[lang]
+    def __init__(num_keyword_repeats=3, num_synonyms_per_word=3):
+        self.num_keyword_repeats = num_keyword_repeats
+        self.num_synonyms_per_word = num_synonyms_per_word
+        self.spacy_models = {}
+        self.spacy_models["multi"] = spacy.load("xx_ent_wiki_sm")
+        self.spacy_models["en"] = spacy.load("en_core_web_sm")
 
-    elif(lang in ["de","fr","es","pt","it","nl","el","nb","lt"]):
-        try:
+    def remove_stop(query, lang=None):
+        if lang == None:
+            lang = detect(query)
+        if lang in ["en"]:
             keyword_model = self.spacy_models[lang]
-        except:
+
+        if lang in ["de", "fr", "es", "pt", "it", "nl", "el", "nb", "lt"]:
             try:
-              self.spacy_models[lang] = spacy.load(lang + "_core_news_sm")
-              keyword_model = self.spacy_models[lang]
+                keyword_model = self.spacy_models[lang]
             except:
-              print("Please install the \"" + lang + "_core_news_sm\" spacy model"
-    
-    else:
-        keyword_model = spacy_models["multi"]
+                try:
+                    self.spacy_models[lang] = spacy.load(lang + "_core_news_sm")
+                    keyword_model = self.spacy_models[lang]
+                except:
+                    print('Please install the "' + lang + '_core_news_sm" spacy model')
 
-    query = keyword_model(query)
-    notStopWords = [notStopWords.text for notStopWords in query if not notStopWords.is_stop]
-    result = ""
-    for xy in range(num_keyword_repeats):
-        for x in notStopWords:
-            result += x + " "
+        if keyword_model == None:
+            keyword_model = spacy_models["multi"]
 
-    return result
-    
-    
-  # detect language using the wtl api
-  def detect(text):
-    return self.wtl.predict_lang(text)
+        query = keyword_model(query)
+        notStopWords = [
+            notStopWords.text for notStopWords in query if not notStopWords.is_stop
+        ]
+        result = ""
+        for xy in range(self.num_keyword_repeats):
+            for x in notStopWords:
+                result += x + " "
+
+        return result
