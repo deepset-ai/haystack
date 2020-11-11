18141f021767c8ab8226128805d3f30a513efe2c
flozi00 <flozi00.fz@gmail.com>
2020-11-11 09:24:19 +0100

    first draft QueryExpander

---
+++ haystack/preprocessor/queryexpander.py
@@ -0,0 +1,43 @@
+class QueryExpander:
+  def __init__(num_keyword_repeats = 3, num_synonyms_per_word = 3):
+    self.num_keyword_repeats = num_keyword_repeats
+    self.num_synonyms_per_word = num_synonyms_per_word
+    self.spacy_models = {}
+    self.spacy_models["multi"] = spacy.load("xx_ent_wiki_sm")
+    self.spacy_models["en"] = spacy.load("en_core_web_sm")
+    
+    self.wtl = WhatTheLang()
+    
+    
+  def remove_stop(query,lang = None):
+    if(lang == None):
+      lang  = self.detect(query)
+    if(lang in ["en"]):
+        keyword_model = spacy_models[lang]
+
+    elif(lang in ["de","fr","es","pt","it","nl","el","nb","lt"]):
+        try:
+            keyword_model = self.spacy_models[lang]
+        except:
+            try:
+              self.spacy_models[lang] = spacy.load(lang + "_core_news_sm")
+              keyword_model = self.spacy_models[lang]
+            except:
+              print("Please install the \"" + lang + "_core_news_sm\" spacy model"
+    
+    else:
+        keyword_model = spacy_models["multi"]
+
+    query = keyword_model(query)
+    notStopWords = [notStopWords.text for notStopWords in query if not notStopWords.is_stop]
+    result = ""
+    for xy in range(num_keyword_repeats):
+        for x in notStopWords:
+            result += x + " "
+
+    return result
+    
+    
+  # detect language using the wtl api
+  def detect(text):
+    return self.wtl.predict_lang(text)
