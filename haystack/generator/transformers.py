b511f9903eb866c18b8b46e425d2acd78eff3654
Lalit Pagaria <pagaria.lalit@gmail.com>
2020-11-16 09:41:30 +0100

    [RAG] Fix top_k generator issue (#590)
    * Removing device information from generator model arguments as it is handled by itself.
    
    * num_return_sequences of should not be greate than num_beams
    
    * Raise error when user use generator with GPU as currently it is not supported

--- haystack/generator/transformers.py
+++ haystack/generator/transformers.py
@@ -90,7 +90,6 @@ class RAGenerator(BaseGenerator):
         """
 
         self.model_name_or_path = model_name_or_path
-        self.top_k_answers = top_k_answers
         self.max_length = max_length
         self.min_length = min_length
         self.generator_type = generator_type
@@ -99,8 +98,15 @@ class RAGenerator(BaseGenerator):
         self.prefix = prefix
         self.retriever = retriever
 
+        if top_k_answers > self.num_beams:
+            top_k_answers = self.num_beams
+            logger.warning(f'top_k_answers value should not be greater than num_beams, hence setting it to {num_beams}')
+
+        self.top_k_answers = top_k_answers
+
         if use_gpu and torch.cuda.is_available():
             self.device = torch.device("cuda")
+            raise AttributeError("Currently RAGenerator does not support GPU, try with use_gpu=False")
         else:
             self.device = torch.device("cpu")
 
@@ -201,6 +207,11 @@ class RAGenerator(BaseGenerator):
             raise AttributeError("generator need documents to predict the answer")
 
         top_k_answers = top_k if top_k is not None else self.top_k_answers
+
+        if top_k_answers > self.num_beams:
+            top_k_answers = self.num_beams
+            logger.warning(f'top_k_answers value should not be greater than num_beams, '
+                           f'hence setting it to {top_k_answers}')
 
         # Flatten the documents so easy to reference
         flat_docs_dict: Dict[str, Any] = {}
