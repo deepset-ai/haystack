58675fe8bfd6fbdd66a886ca275be34a6383c4d9
brandenchan <brandenchan@icloud.com>
2021-01-07 15:17:34 +0100

    Clean tutorial

--- tutorials/Tutorial9_DPR_training.py
+++ tutorials/Tutorial9_DPR_training.py
@@ -1,68 +1,77 @@
 
+# Training Your Own "Dense Passage Retrieval" Model
 
-#%%
+# Here are some imports that we'll need
 
-from haystack.document_store.memory import InMemoryDocumentStore
 from haystack.retriever.dense import DensePassageRetriever
-import wget
-import gzip
-
-#%%
 from haystack.preprocessor.utils import fetch_archive_from_http
 
+# Download original DPR data
+# WARNING: the train set is 7.4GB and the dev set is 800MB
 
-doc_dir = "data/dpr_training"
-train_filename = "biencoder-nq-train.json.gz"
-dev_filename = "biencoder-nq-dev.json.gz"
+doc_dir = "data/dpr_training/"
+
 s3_url_train = "https://dl.fbaipublicfiles.com/dpr/data/retriever/biencoder-nq-train.json.gz"
 s3_url_dev = "https://dl.fbaipublicfiles.com/dpr/data/retriever/biencoder-nq-dev.json.gz"
 
-fetch_archive_from_http(s3_url_train, output_dir=doc_dir)
-fetch_archive_from_http(s3_url_dev, output_dir=doc_dir)
+fetch_archive_from_http(s3_url_train, output_dir=doc_dir + "train/")
+fetch_archive_from_http(s3_url_dev, output_dir=doc_dir + "dev/")
 
+## Option 1: Training DPR from Scratch
 
-# Download datasets?
-# format
+# Here are the variables to specify our training data, the models that we use to initialize DPR
+# and the directory where we'll be saving the model
 
+doc_dir = "data/dpr_training/"
 
-#%%
+train_filename = "train/biencoder-nq-train.json"
+dev_filename = "dev/biencoder-nq-dev.json"
 
+query_model = "bert-base-uncased"
+passage_model = "bert-base-uncased"
 
-# RECOMMENDED for finetuning "facebook/dpr-ctx_encoder-single-nq-base"
+save_dir = "../saved_models/dpr"
 
-document_store = InMemoryDocumentStore()
+# ## Option 2: Finetuning DPR
+#
+# # Here are the variables you might want to use instead of the set above
+# # in order to perform pretraining
+#
+# doc_dir = "PATH_TO_YOUR_DATA_DIR"
+# train_filename = "TRAIN_FILENAME"
+# dev_filename = "DEV_FILENAME"
+#
+# query_model = "facebook/dpr-question_encoder-single-nq-base"
+# passage_model = "facebook/dpr-ctx_encoder-single-nq-base"
+#
+# save_dir = "..saved_models/dpr"
+
+## Initialize DPR model
+
 retriever = DensePassageRetriever(
-    document_store=document_store,
-    query_embedding_model="bert-base-uncased",
-    passage_embedding_model="bert-base-uncased",
+    document_store=None,
+    query_embedding_model=query_model,
+    passage_embedding_model=passage_model,
     max_seq_len_query=64,
-    max_seq_len_passage=256,
-    embed_title=True,
+    max_seq_len_passage=256
 )
 
-
-#%% 
-
-# Train
-# Saves
-
+# Start training our model and save it when it is finished
 
 retriever.train(
     data_dir=doc_dir,
-    train_filename=dev_filename,
+    train_filename=train_filename,
     dev_filename=dev_filename,
     test_filename=dev_filename,
-    save_dir="..saved_models/dpr",
     n_epochs=1,
     batch_size=4,
-    embed_title=True,
-    num_hard_negatives=1,
-    num_positives=1,
-    evaluate_every=1000,
-    learning_rate=1e-5,
-    grad_acc_steps=1
+    grad_acc_steps=4,
+    save_dir=save_dir,
+    evaluate_every=3000,
+    embed_title=True
 )
 
-#%%
 
-# Load?
+## Loading
+
+reloaded_retriever = DensePassageRetriever.load(load_dir=save_dir, document_store=None)
