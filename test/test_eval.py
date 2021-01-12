b9df83bc6b4ed9bae039e961c16229a0d989003b
bogdankostic <bogdankostic@web.de>
2021-01-12 17:54:43 +0100

    Make batchwise adding of evaluation data possible (#717)
    * Make batchwise adding of evaluation data possible
    
    * Fix typos in docstrings
    
    * Merge add_eval_data and add_eval_data_batchwise
    
    * Improve import statements
    
    * Move add_eval_data to BaseDocumentStore
    
    * Add batch_size param to write_documents and write_labels in EsDocStore
    
    * Adjust docstring
    
    Co-authored-by: Malte Pietsch <malte.pietsch@deepset.ai>

--- test/test_eval.py
+++ test/test_eval.py
@@ -41,6 +41,46 @@ def test_add_eval_data(document_store):
 
 
 @pytest.mark.elasticsearch
+def test_add_eval_data_batchwise(document_store):
+    # add eval data (SQUAD format in jsonl)
+    document_store.delete_all_documents(index="test_eval_document")
+    document_store.delete_all_documents(index="test_feedback")
+    document_store.add_eval_data(filename="samples/squad/small.json",
+                                 doc_index="test_eval_document",
+                                 label_index="test_feedback",
+                                 batch_size=20)
+
+    assert document_store.get_document_count(index="test_eval_document") == 87
+    assert document_store.get_label_count(index="test_feedback") == 1214
+
+    # test documents
+    docs = document_store.get_all_documents(index="test_eval_document")
+    assert docs[0].text[:10] == "The Norman"
+    assert docs[0].meta["name"] == "Normans"
+    assert len(docs[0].meta.keys()) == 1
+
+    # test labels
+    labels = document_store.get_all_labels(index="test_feedback")
+    assert labels[0].answer == "France"
+    assert labels[0].no_answer == False
+    assert labels[0].is_correct_answer == True
+    assert labels[0].is_correct_document == True
+    assert labels[0].question == 'In what country is Normandy located?'
+    assert labels[0].origin == "gold_label"
+    assert labels[0].offset_start_in_doc == 159
+
+    # check combination
+    assert labels[0].document_id == docs[0].id
+    start = labels[0].offset_start_in_doc
+    end = start+len(labels[0].answer)
+    assert docs[0].text[start:end] == "France"
+
+    # clean up
+    document_store.delete_all_documents(index="test_eval_document")
+    document_store.delete_all_documents(index="test_feedback")
+
+
+@pytest.mark.elasticsearch
 @pytest.mark.parametrize("reader", ["farm"], indirect=True)
 def test_eval_reader(reader, document_store: BaseDocumentStore):
     # add eval data (SQUAD format)
