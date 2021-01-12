565586788beb8683494ffb3399896d2f566055cc
Julian Risch <julian.risch@hpi.de>
2021-01-12 13:11:27 +0100

    Adding a guard that prevents the tutorial code from being executed in every subprocess when using multiprocessing on windows

--- tutorials/Tutorial5_Evaluation.py
+++ tutorials/Tutorial5_Evaluation.py
@@ -10,101 +10,107 @@ import logging
 import subprocess
 import time
 
-logger = logging.getLogger(__name__)
 
-##############################################
-# Settings
-##############################################
-LAUNCH_ELASTICSEARCH = True
+def tutorial5_evaluation():
+    logger = logging.getLogger(__name__)
 
-eval_retriever_only = True
-eval_reader_only = False
-eval_both = False
+    ##############################################
+    # Settings
+    ##############################################
+    LAUNCH_ELASTICSEARCH = True
 
-# make sure these indices do not collide with existing ones, the indices will be wiped clean before data is inserted
-doc_index = "tutorial5_docs"
-label_index = "tutorial5_labels"
-##############################################
-# Code
-##############################################
-device, n_gpu = initialize_device_settings(use_cuda=True)
-# Start an Elasticsearch server
-# You can start Elasticsearch on your local machine instance using Docker. If Docker is not readily available in
-# your environment (eg., in Colab notebooks), then you can manually download and execute Elasticsearch from source.
-if LAUNCH_ELASTICSEARCH:
-    logging.info("Starting Elasticsearch ...")
-    status = subprocess.run(
-        ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2'], shell=True
-    )
-    if status.returncode:
-        raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
-                        "then set LAUNCH_ELASTICSEARCH in the script to False.")
-    time.sleep(30)
+    eval_retriever_only = True
+    eval_reader_only = False
+    eval_both = False
 
-# Download evaluation data, which is a subset of Natural Questions development set containing 50 documents
-doc_dir = "../data/nq"
-s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/nq_dev_subset_v2.json.zip"
-fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
+    # make sure these indices do not collide with existing ones, the indices will be wiped clean before data is inserted
+    doc_index = "tutorial5_docs"
+    label_index = "tutorial5_labels"
+    ##############################################
+    # Code
+    ##############################################
+    device, n_gpu = initialize_device_settings(use_cuda=True)
+    # Start an Elasticsearch server
+    # You can start Elasticsearch on your local machine instance using Docker. If Docker is not readily available in
+    # your environment (eg., in Colab notebooks), then you can manually download and execute Elasticsearch from source.
+    if LAUNCH_ELASTICSEARCH:
+        logging.info("Starting Elasticsearch ...")
+        status = subprocess.run(
+            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2'], shell=True
+        )
+        if status.returncode:
+            raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
+                            "then set LAUNCH_ELASTICSEARCH in the script to False.")
+        time.sleep(30)
 
-# Connect to Elasticsearch
-document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document",
-                                            create_index=False, embedding_field="emb",
-                                            embedding_dim=768, excluded_meta_data=["emb"])
+    # Download evaluation data, which is a subset of Natural Questions development set containing 50 documents
+    doc_dir = "../data/nq"
+    s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/nq_dev_subset_v2.json.zip"
+    fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
-
-# Add evaluation data to Elasticsearch document store
-# We first delete the custom tutorial indices to not have duplicate elements
-document_store.delete_all_documents(index=doc_index)
-document_store.delete_all_documents(index=label_index)
-document_store.add_eval_data(filename="../data/nq/nq_dev_subset_v2.json", doc_index=doc_index, label_index=label_index)
-
-# Initialize Retriever
-retriever = ElasticsearchRetriever(document_store=document_store)
-
-# Alternative: Evaluate DensePassageRetriever
-# Note, that DPR works best when you index short passages < 512 tokens as only those tokens will be used for the embedding.
-# Here, for nq_dev_subset_v2.json we have avg. num of tokens = 5220(!).
-# DPR still outperforms Elastic's BM25 by a small margin here.
-# retriever = DensePassageRetriever(document_store=document_store,
-#                                   query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-#                                   passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-#                                   use_gpu=True,
-#                                   embed_title=True,
-#                                   remove_sep_tok_from_untitled_passages=True)
-# document_store.update_embeddings(retriever, index=doc_index)
+    # Connect to Elasticsearch
+    document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document",
+                                                create_index=False, embedding_field="emb",
+                                                embedding_dim=768, excluded_meta_data=["emb"])
 
 
-# Initialize Reader
-reader = FARMReader("deepset/roberta-base-squad2", top_k_per_candidate=4)
+    # Add evaluation data to Elasticsearch document store
+    # We first delete the custom tutorial indices to not have duplicate elements
+    document_store.delete_all_documents(index=doc_index)
+    document_store.delete_all_documents(index=label_index)
+    document_store.add_eval_data(filename="../data/nq/nq_dev_subset_v2.json", doc_index=doc_index, label_index=label_index)
 
-# Initialize Finder which sticks together Reader and Retriever
-finder = Finder(reader, retriever)
+    # Initialize Retriever
+    retriever = ElasticsearchRetriever(document_store=document_store)
 
-
-## Evaluate Retriever on its own
-if eval_retriever_only:
-    retriever_eval_results = retriever.eval(top_k=10, label_index=label_index, doc_index=doc_index)
-    ## Retriever Recall is the proportion of questions for which the correct document containing the answer is
-    ## among the correct documents
-    print("Retriever Recall:", retriever_eval_results["recall"])
-    ## Retriever Mean Avg Precision rewards retrievers that give relevant documents a higher rank
-    print("Retriever Mean Avg Precision:", retriever_eval_results["map"])
-
-# Evaluate Reader on its own
-if eval_reader_only:
-    reader_eval_results = reader.eval(document_store=document_store, device=device, label_index=label_index, doc_index=doc_index)
-    # Evaluation of Reader can also be done directly on a SQuAD-formatted file without passing the data to Elasticsearch
-    #reader_eval_results = reader.eval_on_file("../data/nq", "nq_dev_subset_v2.json", device=device)
-
-    ## Reader Top-N-Accuracy is the proportion of predicted answers that match with their corresponding correct answer
-    print("Reader Top-N-Accuracy:", reader_eval_results["top_n_accuracy"])
-    ## Reader Exact Match is the proportion of questions where the predicted answer is exactly the same as the correct answer
-    print("Reader Exact Match:", reader_eval_results["EM"])
-    ## Reader F1-Score is the average overlap between the predicted answers and the correct answers
-    print("Reader F1-Score:", reader_eval_results["f1"])
+    # Alternative: Evaluate DensePassageRetriever
+    # Note, that DPR works best when you index short passages < 512 tokens as only those tokens will be used for the embedding.
+    # Here, for nq_dev_subset_v2.json we have avg. num of tokens = 5220(!).
+    # DPR still outperforms Elastic's BM25 by a small margin here.
+    # retriever = DensePassageRetriever(document_store=document_store,
+    #                                   query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+    #                                   passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+    #                                   use_gpu=True,
+    #                                   embed_title=True,
+    #                                   remove_sep_tok_from_untitled_passages=True)
+    # document_store.update_embeddings(retriever, index=doc_index)
 
 
-# Evaluate combination of Reader and Retriever through Finder
-if eval_both:
-    finder_eval_results = finder.eval(top_k_retriever=1, top_k_reader=10, label_index=label_index, doc_index=doc_index)
-    finder.print_eval_results(finder_eval_results)
+    # Initialize Reader
+    reader = FARMReader("deepset/roberta-base-squad2", top_k_per_candidate=4)
+
+    # Initialize Finder which sticks together Reader and Retriever
+    finder = Finder(reader, retriever)
+
+
+    ## Evaluate Retriever on its own
+    if eval_retriever_only:
+        retriever_eval_results = retriever.eval(top_k=10, label_index=label_index, doc_index=doc_index)
+        ## Retriever Recall is the proportion of questions for which the correct document containing the answer is
+        ## among the correct documents
+        print("Retriever Recall:", retriever_eval_results["recall"])
+        ## Retriever Mean Avg Precision rewards retrievers that give relevant documents a higher rank
+        print("Retriever Mean Avg Precision:", retriever_eval_results["map"])
+
+    # Evaluate Reader on its own
+    if eval_reader_only:
+        reader_eval_results = reader.eval(document_store=document_store, device=device, label_index=label_index, doc_index=doc_index)
+        # Evaluation of Reader can also be done directly on a SQuAD-formatted file without passing the data to Elasticsearch
+        #reader_eval_results = reader.eval_on_file("../data/nq", "nq_dev_subset_v2.json", device=device)
+
+        ## Reader Top-N-Accuracy is the proportion of predicted answers that match with their corresponding correct answer
+        print("Reader Top-N-Accuracy:", reader_eval_results["top_n_accuracy"])
+        ## Reader Exact Match is the proportion of questions where the predicted answer is exactly the same as the correct answer
+        print("Reader Exact Match:", reader_eval_results["EM"])
+        ## Reader F1-Score is the average overlap between the predicted answers and the correct answers
+        print("Reader F1-Score:", reader_eval_results["f1"])
+
+
+    # Evaluate combination of Reader and Retriever through Finder
+    if eval_both:
+        finder_eval_results = finder.eval(top_k_retriever=1, top_k_reader=10, label_index=label_index, doc_index=doc_index)
+        finder.print_eval_results(finder_eval_results)
+
+
+if __name__ == "__main__":
+    tutorial5_evaluation()
--- tutorials/Tutorial6_Better_Retrieval_via_DPR.py
+++ tutorials/Tutorial6_Better_Retrieval_via_DPR.py
@@ -6,63 +6,67 @@ from haystack.reader.farm import FARMReader
 from haystack.utils import print_answers
 from haystack.retriever.dense import DensePassageRetriever
 
+def tutorial6_better_retrieval_via_dpr():
+    # FAISS is a library for efficient similarity search on a cluster of dense vectors.
+    # The FAISSDocumentStore uses a SQL(SQLite in-memory be default) document store under-the-hood
+    # to store the document text and other meta data. The vector embeddings of the text are
+    # indexed on a FAISS Index that later is queried for searching answers.
+    # The default flavour of FAISSDocumentStore is "Flat" but can also be set to "HNSW" for
+    # faster search at the expense of some accuracy. Just set the faiss_index_factor_str argument in the constructor.
+    # For more info on which suits your use case: https://github.com/facebookresearch/faiss/wiki/Guidelines-to-choose-an-index
+    document_store = FAISSDocumentStore(faiss_index_factory_str="Flat")
 
-# FAISS is a library for efficient similarity search on a cluster of dense vectors.
-# The FAISSDocumentStore uses a SQL(SQLite in-memory be default) document store under-the-hood
-# to store the document text and other meta data. The vector embeddings of the text are
-# indexed on a FAISS Index that later is queried for searching answers.
-# The default flavour of FAISSDocumentStore is "Flat" but can also be set to "HNSW" for
-# faster search at the expense of some accuracy. Just set the faiss_index_factor_str argument in the constructor.
-# For more info on which suits your use case: https://github.com/facebookresearch/faiss/wiki/Guidelines-to-choose-an-index
-document_store = FAISSDocumentStore(faiss_index_factory_str="Flat")
+    # ## Preprocessing of documents
+    # Let's first get some documents that we want to query
+    doc_dir = "data/article_txt_got"
+    s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt.zip"
+    fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
-# ## Preprocessing of documents
-# Let's first get some documents that we want to query
-doc_dir = "data/article_txt_got"
-s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt.zip"
-fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
+    # convert files to dicts containing documents that can be indexed to our datastore
+    dicts = convert_files_to_dicts(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
 
-# convert files to dicts containing documents that can be indexed to our datastore
-dicts = convert_files_to_dicts(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
+    # Now, let's write the docs to our DB.
+    document_store.write_documents(dicts)
 
-# Now, let's write the docs to our DB.
-document_store.write_documents(dicts)
+    ### Retriever
+    retriever = DensePassageRetriever(document_store=document_store,
+                                      query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+                                      passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+                                      max_seq_len_query=64,
+                                      max_seq_len_passage=256,
+                                      batch_size=2,
+                                      use_gpu=True,
+                                      embed_title=True,
+                                      use_fast_tokenizers=True
+                                      )
 
-### Retriever
-retriever = DensePassageRetriever(document_store=document_store,
-                                  query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-                                  passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-                                  max_seq_len_query=64,
-                                  max_seq_len_passage=256,
-                                  batch_size=2,
-                                  use_gpu=True,
-                                  embed_title=True,
-                                  use_fast_tokenizers=True
-                                  )
+    # Important:
+    # Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
+    # previously indexed documents and update their embedding representation.
+    # While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
+    # At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
+    document_store.update_embeddings(retriever)
 
-# Important:
-# Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
-# previously indexed documents and update their embedding representation.
-# While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
-# At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
-document_store.update_embeddings(retriever)
+    ### Reader
+    # Load a  local model or any of the QA models on
+    # Hugging Face's model hub (https://huggingface.co/models)
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
 
-### Reader
-# Load a  local model or any of the QA models on
-# Hugging Face's model hub (https://huggingface.co/models)
-reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+    ### Finder
+    # The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
+    finder = Finder(reader, retriever)
 
-### Finder
-# The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
-finder = Finder(reader, retriever)
-
-### Voilà! Ask a question!
-# You can configure how many candidates the reader and retriever shall return
-# The higher top_k_retriever, the better (but also the slower) your answers.
-prediction = finder.get_answers(question="Who is the father of Arya Stark?", top_k_retriever=10, top_k_reader=5)
+    ### Voilà! Ask a question!
+    # You can configure how many candidates the reader and retriever shall return
+    # The higher top_k_retriever, the better (but also the slower) your answers.
+    prediction = finder.get_answers(question="Who is the father of Arya Stark?", top_k_retriever=10, top_k_reader=5)
 
 
-# prediction = finder.get_answers(question="Who created the Dothraki vocabulary?", top_k_reader=5)
-# prediction = finder.get_answers(question="Who is the sister of Sansa?", top_k_reader=5)
+    # prediction = finder.get_answers(question="Who created the Dothraki vocabulary?", top_k_reader=5)
+    # prediction = finder.get_answers(question="Who is the sister of Sansa?", top_k_reader=5)
 
-print_answers(prediction, details="minimal")
+    print_answers(prediction, details="minimal")
+
+
+if __name__ == "__main__":
+    tutorial6_better_retrieval_via_dpr()
