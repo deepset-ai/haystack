337376c81ddad4a742a4590c086756983cce7ece
Tanay Soni <tanaysoni12@gmail.com>
2021-01-21 16:00:08 +0100

    Add `batch_size` and generators to document stores. (#733)
    * Add batch update of embeddings in document stores
    
    * Resolve merge conflict
    
    * Remove document ordering dependency in tests
    
    * Adjust index buffer size for tests
    
    * Adjust ES Scroll Slice
    
    * Use generator for document store pagination
    
    * Add pagination for InMemoryDocumentStore
    
    * Fix missing index parameter in FAISS update_embeddings()
    
    * Fix FAISS update_embeddings()
    
    * Update FAISS tests
    
    * Update eval tests
    
    * Revert code formatting change
    
    * Fix document count in FAISS update embeddings
    
    * Fix vector_ids reset in SQLDocumentStore
    
    * Update doctrings
    
    * Update docstring

--- test/test_dpr_retriever.py
+++ test/test_dpr_retriever.py
@@ -18,40 +18,47 @@ def test_dpr_retrieval(document_store, retriever, return_embedding):
     documents = [
         Document(
             text="""Aaron Aaron ( or ; ""Ahärôn"") is a prophet, high priest, and the brother of Moses in the Abrahamic religions. Knowledge of Aaron, along with his brother Moses, comes exclusively from religious texts, such as the Bible and Quran. The Hebrew Bible relates that, unlike Moses, who grew up in the Egyptian royal court, Aaron and his elder sister Miriam remained with their kinsmen in the eastern border-land of Egypt (Goshen). When Moses first confronted the Egyptian king about the Israelites, Aaron served as his brother's spokesman (""prophet"") to the Pharaoh. Part of the Law (Torah) that Moses received from""",
-            meta={"name": "0"}
+            meta={"name": "0"},
+            id="1",
         ),
         Document(
             text="""Democratic Republic of the Congo to the south. Angola's capital, Luanda, lies on the Atlantic coast in the northwest of the country. Angola, although located in a tropical zone, has a climate that is not characterized for this region, due to the confluence of three factors: As a result, Angola's climate is characterized by two seasons: rainfall from October to April and drought, known as ""Cacimbo"", from May to August, drier, as the name implies, and with lower temperatures. On the other hand, while the coastline has high rainfall rates, decreasing from North to South and from to , with""",
+            id="2",
         ),
         Document(
             text="""Schopenhauer, describing him as an ultimately shallow thinker: ""Schopenhauer has quite a crude mind ... where real depth starts, his comes to an end."" His friend Bertrand Russell had a low opinion on the philosopher, and attacked him in his famous ""History of Western Philosophy"" for hypocritically praising asceticism yet not acting upon it. On the opposite isle of Russell on the foundations of mathematics, the Dutch mathematician L. E. J. Brouwer incorporated the ideas of Kant and Schopenhauer in intuitionism, where mathematics is considered a purely mental activity, instead of an analytic activity wherein objective properties of reality are""",
-            meta={"name": "1"}
+            meta={"name": "1"},
+            id="3",
         ),
         Document(
             text="""The Dothraki vocabulary was created by David J. Peterson well in advance of the adaptation. HBO hired the Language Creatio""",
-            meta={"name": "2"}
+            meta={"name": "2"},
+            id="4",
         ),
         Document(
             text="""The title of the episode refers to the Great Sept of Baelor, the main religious building in King's Landing, where the episode's pivotal scene takes place. In the world created by George R. R. Martin""",
-            meta={}
+            meta={},
+            id="5",
         )
     ]
 
     document_store.return_embedding = return_embedding
     document_store.write_documents(documents)
     document_store.update_embeddings(retriever=retriever)
-
     time.sleep(1)
 
-    docs_with_emb = document_store.get_all_documents()
-
     if return_embedding is True:
-        assert (len(docs_with_emb[0].embedding) == 768)
-        assert (abs(docs_with_emb[0].embedding[0] - (-0.3063)) < 0.001)
-        assert (abs(docs_with_emb[1].embedding[0] - (-0.3914)) < 0.001)
-        assert (abs(docs_with_emb[2].embedding[0] - (-0.2470)) < 0.001)
-        assert (abs(docs_with_emb[3].embedding[0] - (-0.0802)) < 0.001)
-        assert (abs(docs_with_emb[4].embedding[0] - (-0.0551)) < 0.001)
+        doc_1 = document_store.get_document_by_id("1")
+        assert (len(doc_1.embedding) == 768)
+        assert (abs(doc_1.embedding[0] - (-0.3063)) < 0.001)
+        doc_2 = document_store.get_document_by_id("2")
+        assert (abs(doc_2.embedding[0] - (-0.3914)) < 0.001)
+        doc_3 = document_store.get_document_by_id("3")
+        assert (abs(doc_3.embedding[0] - (-0.2470)) < 0.001)
+        doc_4 = document_store.get_document_by_id("4")
+        assert (abs(doc_4.embedding[0] - (-0.0802)) < 0.001)
+        doc_5 = document_store.get_document_by_id("5")
+        assert (abs(doc_5.embedding[0] - (-0.0551)) < 0.001)
 
     res = retriever.retrieve(query="Which philosopher attacked Schopenhauer?")
 
--- test/test_eval.py
+++ test/test_eval.py
@@ -1,106 +1,71 @@
 import pytest
 from haystack.document_store.base import BaseDocumentStore
-from haystack.document_store.memory import InMemoryDocumentStore
 from haystack.preprocessor.preprocessor import PreProcessor
 from haystack.finder import Finder
 
 
+@pytest.mark.parametrize("batch_size", [None, 20])
 @pytest.mark.elasticsearch
-def test_add_eval_data(document_store):
+def test_add_eval_data(document_store, batch_size):
     # add eval data (SQUAD format)
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-    document_store.add_eval_data(filename="samples/squad/small.json", doc_index="test_eval_document", label_index="test_feedback")
+    document_store.add_eval_data(
+        filename="samples/squad/small.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+        batch_size=batch_size,
+    )
 
-    assert document_store.get_document_count(index="test_eval_document") == 87
-    assert document_store.get_label_count(index="test_feedback") == 1214
+    assert document_store.get_document_count(index="haystack_test_eval_document") == 87
+    assert document_store.get_label_count(index="haystack_test_feedback") == 1214
 
     # test documents
-    docs = document_store.get_all_documents(index="test_eval_document")
-    assert docs[0].text[:10] == "The Norman"
+    docs = document_store.get_all_documents(index="haystack_test_eval_document", filters={"name": ["Normans"]})
     assert docs[0].meta["name"] == "Normans"
     assert len(docs[0].meta.keys()) == 1
 
     # test labels
-    labels = document_store.get_all_labels(index="test_feedback")
-    assert labels[0].answer == "France"
-    assert labels[0].no_answer == False
-    assert labels[0].is_correct_answer == True
-    assert labels[0].is_correct_document == True
-    assert labels[0].question == 'In what country is Normandy located?'
-    assert labels[0].origin == "gold_label"
-    assert labels[0].offset_start_in_doc == 159
+    labels = document_store.get_all_labels(index="haystack_test_feedback")
+    label = None
+    for l in labels:
+        if l.question == "In what country is Normandy located?":
+            label = l
+            break
+    assert label.answer == "France"
+    assert label.no_answer == False
+    assert label.is_correct_answer == True
+    assert label.is_correct_document == True
+    assert label.question == "In what country is Normandy located?"
+    assert label.origin == "gold_label"
+    assert label.offset_start_in_doc == 159
 
     # check combination
-    assert labels[0].document_id == docs[0].id
-    start = labels[0].offset_start_in_doc
-    end = start+len(labels[0].answer)
-    assert docs[0].text[start:end] == "France"
-
-    # clean up
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-
-
-@pytest.mark.elasticsearch
-def test_add_eval_data_batchwise(document_store):
-    # add eval data (SQUAD format in jsonl)
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-    document_store.add_eval_data(filename="samples/squad/small.json",
-                                 doc_index="test_eval_document",
-                                 label_index="test_feedback",
-                                 batch_size=20)
-
-    assert document_store.get_document_count(index="test_eval_document") == 87
-    assert document_store.get_label_count(index="test_feedback") == 1214
-
-    # test documents
-    docs = document_store.get_all_documents(index="test_eval_document")
-    assert docs[0].text[:10] == "The Norman"
-    assert docs[0].meta["name"] == "Normans"
-    assert len(docs[0].meta.keys()) == 1
-
-    # test labels
-    labels = document_store.get_all_labels(index="test_feedback")
-    assert labels[0].answer == "France"
-    assert labels[0].no_answer == False
-    assert labels[0].is_correct_answer == True
-    assert labels[0].is_correct_document == True
-    assert labels[0].question == 'In what country is Normandy located?'
-    assert labels[0].origin == "gold_label"
-    assert labels[0].offset_start_in_doc == 159
-
-    # check combination
-    assert labels[0].document_id == docs[0].id
-    start = labels[0].offset_start_in_doc
-    end = start+len(labels[0].answer)
-    assert docs[0].text[start:end] == "France"
-
-    # clean up
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
+    doc = document_store.get_document_by_id(label.document_id, index="haystack_test_eval_document")
+    start = label.offset_start_in_doc
+    end = start + len(label.answer)
+    assert doc.text[start:end] == "France"
 
 
 @pytest.mark.elasticsearch
 @pytest.mark.parametrize("reader", ["farm"], indirect=True)
 def test_eval_reader(reader, document_store: BaseDocumentStore):
     # add eval data (SQUAD format)
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-    document_store.add_eval_data(filename="samples/squad/tiny.json", doc_index="test_eval_document", label_index="test_feedback")
-    assert document_store.get_document_count(index="test_eval_document") == 2
+    document_store.add_eval_data(
+        filename="samples/squad/tiny.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+    )
+    assert document_store.get_document_count(index="haystack_test_eval_document") == 2
     # eval reader
-    reader_eval_results = reader.eval(document_store=document_store, label_index="test_feedback",
-                                      doc_index="test_eval_document", device="cpu")
+    reader_eval_results = reader.eval(
+        document_store=document_store,
+        label_index="haystack_test_feedback",
+        doc_index="haystack_test_eval_document",
+        device="cpu",
+    )
     assert reader_eval_results["f1"] > 0.65
     assert reader_eval_results["f1"] < 0.67
     assert reader_eval_results["EM"] == 0.5
     assert reader_eval_results["top_n_accuracy"] == 1.0
-
-    # clean up
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
 
 
 @pytest.mark.elasticsearch
@@ -109,21 +74,21 @@ def test_eval_reader(reader, document_store: BaseDocumentStore):
 @pytest.mark.parametrize("retriever", ["elasticsearch"], indirect=True)
 def test_eval_elastic_retriever(document_store: BaseDocumentStore, open_domain, retriever):
     # add eval data (SQUAD format)
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-    document_store.add_eval_data(filename="samples/squad/tiny.json", doc_index="test_eval_document", label_index="test_feedback")
-    assert document_store.get_document_count(index="test_eval_document") == 2
+    document_store.add_eval_data(
+        filename="samples/squad/tiny.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+    )
+    assert document_store.get_document_count(index="haystack_test_eval_document") == 2
 
     # eval retriever
-    results = retriever.eval(top_k=1, label_index="test_feedback", doc_index="test_eval_document", open_domain=open_domain)
+    results = retriever.eval(
+        top_k=1, label_index="haystack_test_feedback", doc_index="haystack_test_eval_document", open_domain=open_domain
+    )
     assert results["recall"] == 1.0
     assert results["mrr"] == 1.0
     if not open_domain:
         assert results["map"] == 1.0
-
-    # clean up
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
 
 
 @pytest.mark.elasticsearch
@@ -134,13 +99,17 @@ def test_eval_finder(document_store: BaseDocumentStore, reader, retriever):
     finder = Finder(reader=reader, retriever=retriever)
 
     # add eval data (SQUAD format)
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-    document_store.add_eval_data(filename="samples/squad/tiny.json", doc_index="test_eval_document", label_index="test_feedback")
-    assert document_store.get_document_count(index="test_eval_document") == 2
+    document_store.add_eval_data(
+        filename="samples/squad/tiny.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+    )
+    assert document_store.get_document_count(index="haystack_test_eval_document") == 2
 
     # eval finder
-    results = finder.eval(label_index="test_feedback", doc_index="test_eval_document", top_k_retriever=1, top_k_reader=5)
+    results = finder.eval(
+        label_index="haystack_test_feedback", doc_index="haystack_test_eval_document", top_k_retriever=1, top_k_reader=5
+    )
     assert results["retriever_recall"] == 1.0
     assert results["retriever_map"] == 1.0
     assert abs(results["reader_topk_f1"] - 0.66666) < 0.001
@@ -151,24 +120,19 @@ def test_eval_finder(document_store: BaseDocumentStore, reader, retriever):
     assert results["reader_top1_accuracy"] <= results["reader_topk_accuracy"]
 
     # batch eval finder
-    results_batch = finder.eval_batch(label_index="test_feedback", doc_index="test_eval_document", top_k_retriever=1,
-                          top_k_reader=5)
+    results_batch = finder.eval_batch(
+        label_index="haystack_test_feedback", doc_index="haystack_test_eval_document", top_k_retriever=1, top_k_reader=5
+    )
     assert results_batch["retriever_recall"] == 1.0
     assert results_batch["retriever_map"] == 1.0
     assert results_batch["reader_top1_f1"] == results["reader_top1_f1"]
     assert results_batch["reader_top1_em"] == results["reader_top1_em"]
     assert results_batch["reader_topk_accuracy"] == results["reader_topk_accuracy"]
 
-    # clean up
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
 
 @pytest.mark.elasticsearch
-def test_eval_data_splitting(document_store):
+def test_eval_data_split_word(document_store):
     # splitting by word
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
-
     preprocessor = PreProcessor(
         clean_empty_lines=False,
         clean_whitespace=False,
@@ -176,22 +140,24 @@ def test_eval_data_splitting(document_store):
         split_by="word",
         split_length=4,
         split_overlap=0,
-        split_respect_sentence_boundary=False
+        split_respect_sentence_boundary=False,
     )
 
-    document_store.add_eval_data(filename="samples/squad/tiny.json",
-                                 doc_index="test_eval_document",
-                                 label_index="test_feedback",
-                                 preprocessor=preprocessor)
-    labels = document_store.get_all_labels_aggregated(index="test_feedback")
-    docs = document_store.get_all_documents(index="test_eval_document")
+    document_store.add_eval_data(
+        filename="samples/squad/tiny.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+        preprocessor=preprocessor,
+    )
+    labels = document_store.get_all_labels_aggregated(index="haystack_test_feedback")
+    docs = document_store.get_all_documents(index="haystack_test_eval_document")
     assert len(docs) == 5
     assert len(set(labels[0].multiple_document_ids)) == 2
 
-    # splitting by passage
-    document_store.delete_all_documents(index="test_eval_document")
-    document_store.delete_all_documents(index="test_feedback")
 
+@pytest.mark.elasticsearch
+def test_eval_data_split_passage(document_store):
+    # splitting by passage
     preprocessor = PreProcessor(
         clean_empty_lines=False,
         clean_whitespace=False,
@@ -202,10 +168,12 @@ def test_eval_data_splitting(document_store):
         split_respect_sentence_boundary=False
     )
 
-    document_store.add_eval_data(filename="samples/squad/tiny_passages.json",
-                                 doc_index="test_eval_document",
-                                 label_index="test_feedback",
-                                 preprocessor=preprocessor)
-    docs = document_store.get_all_documents(index="test_eval_document")
+    document_store.add_eval_data(
+        filename="samples/squad/tiny_passages.json",
+        doc_index="haystack_test_eval_document",
+        label_index="haystack_test_feedback",
+        preprocessor=preprocessor,
+    )
+    docs = document_store.get_all_documents(index="haystack_test_eval_document")
     assert len(docs) == 2
     assert len(docs[1].text) == 56
