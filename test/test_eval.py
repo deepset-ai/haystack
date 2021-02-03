f3a3b73d9b7a0446a17f48dd398449677521eeb9
Branden Chan <33759007+brandenchan@users.noreply.github.com>
2021-02-03 11:45:18 +0100

    Choose correct similarity fns during benchmark runs & re-run benchmarks (#773)
    * Adapt to new dataset_from_dicts return signature
    
    * rename fn
    
    * Align similarity fn in benchmark doc store
    
    * Better choice of similarity fn
    
    * Increase postgres wait time
    
    * Add more expected returned variables
    
    * update benchmark results
    
    * Fix typo
    
    * update all benchmark runs
    
    * multiply stats by 100
    
    * Specify similarity fns for website
    
    Co-authored-by: Malte Pietsch <malte.pietsch@deepset.ai>

--- test/test_eval.py
+++ test/test_eval.py
@@ -62,10 +62,10 @@ def test_eval_reader(reader, document_store: BaseDocumentStore):
         doc_index="haystack_test_eval_document",
         device="cpu",
     )
-    assert reader_eval_results["f1"] > 0.65
-    assert reader_eval_results["f1"] < 0.67
-    assert reader_eval_results["EM"] == 0.5
-    assert reader_eval_results["top_n_accuracy"] == 1.0
+    assert reader_eval_results["f1"] > 66.65
+    assert reader_eval_results["f1"] < 66.67
+    assert reader_eval_results["EM"] == 50
+    assert reader_eval_results["top_n_accuracy"] == 100.0
 
 
 @pytest.mark.elasticsearch
