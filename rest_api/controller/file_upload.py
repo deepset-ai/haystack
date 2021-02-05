f95b70df38adbf96ba8da567c8c981a91a3d4e32
Tanay Soni <tanaysoni12@gmail.com>
2021-02-05 12:17:38 +0100

    Fix file upload API (#808)

--- rest_api/controller/file_upload.py
+++ rest_api/controller/file_upload.py
@@ -12,10 +12,12 @@ from fastapi import UploadFile, File, Form
 from rest_api.config import DB_HOST, DB_PORT, DB_USER, DB_PW, DB_INDEX, DB_INDEX_FEEDBACK, ES_CONN_SCHEME, TEXT_FIELD_NAME, \
     SEARCH_FIELD_NAME, FILE_UPLOAD_PATH, EMBEDDING_DIM, EMBEDDING_FIELD_NAME, EXCLUDE_META_DATA_FIELDS, VALID_LANGUAGES, \
     FAQ_QUESTION_FIELD_NAME, REMOVE_NUMERIC_TABLES, REMOVE_WHITESPACE, REMOVE_EMPTY_LINES, REMOVE_HEADER_FOOTER, \
-    CREATE_INDEX, UPDATE_EXISTING_DOCUMENTS, VECTOR_SIMILARITY_METRIC
+    CREATE_INDEX, UPDATE_EXISTING_DOCUMENTS, VECTOR_SIMILARITY_METRIC, SPLIT_BY, SPLIT_LENGTH, SPLIT_OVERLAP, \
+    SPLIT_RESPECT_SENTENCE_BOUNDARY
 from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from haystack.file_converter.pdf import PDFToTextConverter
 from haystack.file_converter.txt import TextConverter
+from haystack.preprocessor.preprocessor import PreProcessor
 
 
 logger = logging.getLogger(__name__)
@@ -30,7 +32,7 @@ document_store = ElasticsearchDocumentStore(
     index=DB_INDEX,
     label_index=DB_INDEX_FEEDBACK,
     scheme=ES_CONN_SCHEME,
-    ca_certs=False,
+    ca_certs=None,
     verify_certs=False,
     text_field=TEXT_FIELD_NAME,
     search_fields=SEARCH_FIELD_NAME,
@@ -54,6 +56,10 @@ def upload_file_to_document_store(
     remove_empty_lines: Optional[bool] = Form(REMOVE_EMPTY_LINES),
     remove_header_footer: Optional[bool] = Form(REMOVE_HEADER_FOOTER),
     valid_languages: Optional[List[str]] = Form(VALID_LANGUAGES),
+    split_by: Optional[str] = Form(SPLIT_BY),
+    split_length: Optional[int] = Form(SPLIT_LENGTH),
+    split_overlap: Optional[int] = Form(SPLIT_OVERLAP),
+    split_respect_sentence_boundary: Optional[bool] = Form(SPLIT_RESPECT_SENTENCE_BOUNDARY),
 ):
     try:
         file_path = Path(FILE_UPLOAD_PATH) / f"{uuid.uuid4().hex}_{file.filename}"
@@ -62,27 +68,31 @@ def upload_file_to_document_store(
 
         if file.filename.split(".")[-1].lower() == "pdf":
             pdf_converter = PDFToTextConverter(
-                remove_numeric_tables=remove_numeric_tables,
-                remove_whitespace=remove_whitespace,
-                remove_empty_lines=remove_empty_lines,
-                remove_header_footer=remove_header_footer,
-                valid_languages=valid_languages,
+                remove_numeric_tables=remove_numeric_tables, valid_languages=valid_languages
             )
             document = pdf_converter.convert(file_path)
         elif file.filename.split(".")[-1].lower() == "txt":
             txt_converter = TextConverter(
-                remove_numeric_tables=remove_numeric_tables,
-                remove_whitespace=remove_whitespace,
-                remove_empty_lines=remove_empty_lines,
-                remove_header_footer=remove_header_footer,
-                valid_languages=valid_languages,
+                remove_numeric_tables=remove_numeric_tables, valid_languages=valid_languages,
             )
             document = txt_converter.convert(file_path)
         else:
             raise HTTPException(status_code=415, detail=f"Only .pdf and .txt file formats are supported.")
 
-        document_to_write = {TEXT_FIELD_NAME: document["text"], "name": file.filename}
-        document_store.write_documents([document_to_write])
+        document = {TEXT_FIELD_NAME: document["text"], "name": file.filename}
+
+        preprocessor = PreProcessor(
+            clean_whitespace=remove_whitespace,
+            clean_header_footer=remove_header_footer,
+            clean_empty_lines=remove_empty_lines,
+            split_by=split_by,
+            split_length=split_length,
+            split_overlap=split_overlap,
+            split_respect_sentence_boundary=split_respect_sentence_boundary,
+        )
+
+        documents = preprocessor.process(document)
+        document_store.write_documents(documents)
         return "File upload was successful."
     finally:
         file.file.close()
