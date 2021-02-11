7285eeb8cd30c1ffb8f386308270954473757d5e
oryx1729 <oryx1729@protonmail.com>
2021-02-11 15:44:37 +0100

    Fix Elasticsearch query_by_embedding

--- test/test_retriever.py
+++ test/test_retriever.py
@@ -1,21 +1,119 @@
-import pytest
 import time
-import numpy as np
 
+import numpy as np
+import pytest
+from elasticsearch import Elasticsearch
 from haystack import Document
+from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from haystack.document_store.faiss import FAISSDocumentStore
 from haystack.document_store.milvus import MilvusDocumentStore
 from haystack.retriever.dense import DensePassageRetriever
-
+from haystack.retriever.sparse import ElasticsearchRetriever, ElasticsearchFilterOnlyRetriever
 from transformers import DPRContextEncoderTokenizerFast, DPRQuestionEncoderTokenizerFast
+
+
+@pytest.mark.elasticsearch
+@pytest.mark.parametrize(
+    "retriever_with_docs,document_store_with_docs",
+    [
+        ("dpr", "elasticsearch"),
+        ("dpr", "faiss"),
+        ("dpr", "memory"),
+        ("dpr", "milvus"),
+        ("embedding", "elasticsearch"),
+        ("embedding", "faiss"),
+        ("embedding", "memory"),
+        ("embedding", "milvus"),
+        ("elasticsearch", "elasticsearch"),
+        ("es_filter_only", "elasticsearch"),
+    ],
+    indirect=True,
+)
+def test_retrieval(retriever_with_docs, document_store_with_docs):
+    if not isinstance(retriever_with_docs, (ElasticsearchRetriever, ElasticsearchFilterOnlyRetriever)):
+        document_store_with_docs.update_embeddings(retriever_with_docs)
+
+    # test without filters
+    res = retriever_with_docs.retrieve(query="Who lives in Berlin?")
+    assert res[0].text == "My name is Carla and I live in Berlin"
+    assert len(res) == 3
+    assert res[0].meta["name"] == "filename1"
+
+    # test with filters
+    if not isinstance(document_store_with_docs, (FAISSDocumentStore, MilvusDocumentStore)):
+        # single filter
+        result = retriever_with_docs.retrieve(query="godzilla", filters={"name": ["filename3"]}, top_k=5)
+        assert len(result) == 1
+        assert type(result[0]) == Document
+        assert result[0].text == "My name is Christelle and I live in Paris"
+        assert result[0].meta["name"] == "filename3"
+
+        # multiple filters
+        result = retriever_with_docs.retrieve(
+            query="godzilla", filters={"name": ["filename2"], "meta_field": ["test2", "test3"]}, top_k=5
+        )
+        assert len(result) == 1
+        assert type(result[0]) == Document
+        assert result[0].meta["name"] == "filename2"
+
+        result = retriever_with_docs.retrieve(
+            query="godzilla", filters={"name": ["filename1"], "meta_field": ["test2", "test3"]}, top_k=5
+        )
+        assert len(result) == 0
+
+
+@pytest.mark.elasticsearch
+def test_elasticsearch_custom_query(elasticsearch_fixture):
+    client = Elasticsearch()
+    client.indices.delete(index="haystack_test_custom", ignore=[404])
+    document_store = ElasticsearchDocumentStore(
+        index="haystack_test_custom", text_field="custom_text_field", embedding_field="custom_embedding_field"
+    )
+    documents = [
+        {"text": "test_1", "meta": {"year": "2019"}},
+        {"text": "test_2", "meta": {"year": "2020"}},
+        {"text": "test_3", "meta": {"year": "2021"}},
+        {"text": "test_4", "meta": {"year": "2021"}},
+        {"text": "test_5", "meta": {"year": "2021"}},
+    ]
+    document_store.write_documents(documents)
+
+    # test custom "terms" query
+    retriever = ElasticsearchRetriever(
+        document_store=document_store,
+        custom_query="""
+            {
+                "size": 10, 
+                "query": {
+                    "bool": {
+                        "should": [{
+                            "multi_match": {"query": ${query}, "type": "most_fields", "fields": ["text"]}}],
+                            "filter": [{"terms": {"year": ${years}}}]}}}""",
+    )
+    results = retriever.run(query="test", filters={"years": ["2020", "2021"]})[0]["documents"]
+    assert len(results) == 4
+
+    # test custom "term" query
+    retriever = ElasticsearchRetriever(
+        document_store=document_store,
+        custom_query="""
+                {
+                    "size": 10, 
+                    "query": {
+                        "bool": {
+                            "should": [{
+                                "multi_match": {"query": ${query}, "type": "most_fields", "fields": ["text"]}}],
+                                "filter": [{"term": {"year": ${years}}}]}}}""",
+    )
+    results = retriever.run(query="test", filters={"years": "2021"})[0]["documents"]
+    assert len(results) == 3
+
 
 @pytest.mark.slow
 @pytest.mark.elasticsearch
 @pytest.mark.parametrize("document_store", ["elasticsearch", "faiss", "memory", "milvus"], indirect=True)
 @pytest.mark.parametrize("retriever", ["dpr"], indirect=True)
-@pytest.mark.parametrize("return_embedding", [True, False])
-def test_dpr_retrieval(document_store, retriever, return_embedding):
-
+def test_dpr_embeddings(document_store, retriever):
     documents = [
         Document(
             text="""Aaron Aaron ( or ; ""Ahärôn"") is a prophet, high priest, and the brother of Moses in the Abrahamic religions. Knowledge of Aaron, along with his brother Moses, comes exclusively from religious texts, such as the Bible and Quran. The Hebrew Bible relates that, unlike Moses, who grew up in the Egyptian royal court, Aaron and his elder sister Miriam remained with their kinsmen in the eastern border-land of Egypt (Goshen). When Moses first confronted the Egyptian king about the Israelites, Aaron served as his brother's spokesman (""prophet"") to the Pharaoh. Part of the Law (Torah) that Moses received from""",
@@ -40,55 +138,56 @@ def test_dpr_retrieval(document_store, retriever, return_embedding):
             text="""The title of the episode refers to the Great Sept of Baelor, the main religious building in King's Landing, where the episode's pivotal scene takes place. In the world created by George R. R. Martin""",
             meta={},
             id="5",
-        )
+        ),
     ]
 
-    document_store.return_embedding = return_embedding
+    document_store.return_embedding = True
     document_store.write_documents(documents)
     document_store.update_embeddings(retriever=retriever)
     time.sleep(1)
 
-    if return_embedding is True:
-        doc_1 = document_store.get_document_by_id("1")
-        assert (len(doc_1.embedding) == 768)
-        assert (abs(doc_1.embedding[0] - (-0.3063)) < 0.001)
-        doc_2 = document_store.get_document_by_id("2")
-        assert (abs(doc_2.embedding[0] - (-0.3914)) < 0.001)
-        doc_3 = document_store.get_document_by_id("3")
-        assert (abs(doc_3.embedding[0] - (-0.2470)) < 0.001)
-        doc_4 = document_store.get_document_by_id("4")
-        assert (abs(doc_4.embedding[0] - (-0.0802)) < 0.001)
-        doc_5 = document_store.get_document_by_id("5")
-        assert (abs(doc_5.embedding[0] - (-0.0551)) < 0.001)
+    doc_1 = document_store.get_document_by_id("1")
+    assert len(doc_1.embedding) == 768
+    assert abs(doc_1.embedding[0] - (-0.3063)) < 0.001
+    doc_2 = document_store.get_document_by_id("2")
+    assert abs(doc_2.embedding[0] - (-0.3914)) < 0.001
+    doc_3 = document_store.get_document_by_id("3")
+    assert abs(doc_3.embedding[0] - (-0.2470)) < 0.001
+    doc_4 = document_store.get_document_by_id("4")
+    assert abs(doc_4.embedding[0] - (-0.0802)) < 0.001
+    doc_5 = document_store.get_document_by_id("5")
+    assert abs(doc_5.embedding[0] - (-0.0551)) < 0.001
 
     res = retriever.retrieve(query="Which philosopher attacked Schopenhauer?")
 
     assert res[0].meta["name"] == "1"
 
-    # test embedding
-    if return_embedding is True:
-        assert res[0].embedding is not None
-    else:
-        assert res[0].embedding is None
-
-    # test filtering
-    if not isinstance(document_store, FAISSDocumentStore) and not isinstance(document_store, MilvusDocumentStore):
-        res = retriever.retrieve(query="Which philosopher attacked Schopenhauer?", filters={"name": ["0", "2"]})
-        assert len(res) == 2
-        for r in res:
-            assert r.meta["name"] in ["0", "2"]
+    # # test embedding
+    # if return_embedding is True:
+    #     assert res[0].embedding is not None
+    # else:
+    #     assert res[0].embedding is None
+    #
+    # # test filtering
+    # if not isinstance(document_store, FAISSDocumentStore) and not isinstance(document_store, MilvusDocumentStore):
+    #     res = retriever.retrieve(query="Which philosopher attacked Schopenhauer?", filters={"name": ["0", "2"]})
+    #     assert len(res) == 2
+    #     for r in res:
+    #         assert r.meta["name"] in ["0", "2"]
 
 
 @pytest.mark.parametrize("retriever", ["dpr"], indirect=True)
 @pytest.mark.parametrize("document_store", ["memory"], indirect=True)
 def test_dpr_saving_and_loading(retriever, document_store):
     retriever.save("test_dpr_save")
+
     def sum_params(model):
         s = []
         for p in model.parameters():
             n = p.cpu().data.numpy()
             s.append(np.sum(n))
         return sum(s)
+
     original_sum_query = sum_params(retriever.query_encoder)
     original_sum_passage = sum_params(retriever.passage_encoder)
     del retriever
@@ -123,4 +222,3 @@ def test_dpr_saving_and_loading(retriever, document_store):
     assert loaded_retriever.query_tokenizer.vocab_size == 30522
     assert loaded_retriever.passage_tokenizer.model_max_length == 512
     assert loaded_retriever.query_tokenizer.model_max_length == 512
-
