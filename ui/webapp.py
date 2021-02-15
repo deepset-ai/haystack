0eaae3c0dd2f1521733d78eb163f25ab490ea1e5
Malte Pietsch <malte.pietsch@deepset.ai>
2021-02-15 14:27:17 +0100

    Fix UI when API returns fewer answers than expected (#828)
    * fix ui for few answers from api. add top_k_per_sample env
    
    * Add latest docstring and tutorial changes
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ui/webapp.py
+++ ui/webapp.py
@@ -9,8 +9,8 @@ def annotate_answer(answer,context):
       
 st.write("# Haystack Demo")
 st.sidebar.header("Options")
-top_k_reader = st.sidebar.slider("Number of answers",min_value=1,max_value=10,value=5,step=1)
-top_k_retriever = st.sidebar.slider("Number of documents from retriever",min_value=1,max_value=10,value=3,step=1)
+top_k_reader = st.sidebar.slider("Max. number of answers",min_value=1,max_value=10,value=3,step=1)
+top_k_retriever = st.sidebar.slider("Max. number of documents from retriever",min_value=1,max_value=10,value=3,step=1)
 question = st.text_input("Please provide your query:",value="Who is the father of Arya Starck?")
 run_query = st.button("Run")
 debug = st.sidebar.checkbox("Show debug info")
@@ -22,7 +22,7 @@ if run_query:
     st.write("## Retrieved answers:")
     for result in results:
         annotate_answer(result['answer'],result['context'])
-        '**Relevance:** ', result['relevance'] , '**source:** ' , result['source']
+        '**Relevance:** ', result['relevance'] , '**Source:** ' , result['source']
     if debug:
         st.subheader('REST API JSON response')
         st.write(raw_json)
