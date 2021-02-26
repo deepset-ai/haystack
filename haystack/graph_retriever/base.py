93c934f7e06dd8133432e9643978d2edc106b525
oryx1729 <oryx1729@protonmail.com>
2021-02-26 11:28:38 +0100

    Add graph retriver module

---
+++ haystack/graph_retriever/base.py
@@ -0,0 +1,15 @@
+from haystack.knowledge_graph.base import BaseKnowledgeGraph
+
+
+class BaseGraphRetriever:
+    knowledge_graph: BaseKnowledgeGraph
+    outgoing_edges = 1
+
+    def retrieve(self, query, top_k_graph):
+        raise NotImplementedError
+
+    def eval(self):
+        raise NotImplementedError
+
+    def run(self, query, top_k_graph, **kwargs):
+        raise NotImplementedError
