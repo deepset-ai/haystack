cc446397b07be96886d42e8c1c760b5177ef19d3
Julian Risch <julian.risch@hpi.de>
2021-03-02 14:02:06 +0100

    Added logging and using relations extracted from Knowledge graph for linking

--- haystack/graph_retriever/query.py
+++ haystack/graph_retriever/query.py
@@ -1,8 +1,10 @@
+import logging
 from typing import Optional, Set
 
 from haystack.graph_retriever.question import QuestionType
 from haystack.graph_retriever.triple import Triple
 
+logger = logging.getLogger(__name__)
 
 class Query:
 
