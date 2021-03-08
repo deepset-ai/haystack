ebc66849a080aabead1c99d9f4845a3c2b8df790
oryx1729 <oryx1729@protonmail.com>
2021-03-08 14:42:53 +0100

    Support knowledge graphs with Pipelines

--- haystack/knowledge_graph/base.py
+++ haystack/knowledge_graph/base.py
@@ -1,7 +1,11 @@
 from typing import Optional
 
+from haystack import BaseComponent
 
-class BaseKnowledgeGraph:
+
+class BaseKnowledgeGraph(BaseComponent):
+    outgoing_edges = 1
+
     def run(self, sparql_query: str, index: Optional[str] = None, **kwargs):
         result = self.query(query=sparql_query, index=index)
         output = {"sparql_result": result, **kwargs}
--- haystack/pipeline.py
+++ haystack/pipeline.py
@@ -15,6 +15,8 @@ from haystack.reader.base import BaseReader
 from haystack.retriever.base import BaseRetriever
 from haystack.summarizer.base import BaseSummarizer
 from haystack.translator.base import BaseTranslator
+from haystack.knowledge_graph.base import BaseKnowledgeGraph
+from haystack.graph_retriever.base import BaseGraphRetriever
 
 
 class Pipeline(ABC):
