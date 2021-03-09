58d6dfe0cdebfcde085d5f945b19814eaa96f20e
Malte Pietsch <malte.pietsch@deepset.ai>
2021-03-09 13:16:02 +0100

    quick fix for the feedback export in the api

--- rest_api/controller/feedback.py
+++ rest_api/controller/feedback.py
@@ -1,5 +1,5 @@
 from typing import Optional
-import time
+import logging
 
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
@@ -9,9 +9,14 @@ router = APIRouter()
 
 from .search import PIPELINE
 
+logger = logging.getLogger(__name__)
+
 #TODO make this generic for other pipelines with different naming
 retriever = PIPELINE.get_node(name="ESRetriever")
 document_store = retriever.document_store
+
+retriever = PIPELINE.get_node(name="DPRRetriever")
+faiss_document_store = retriever.document_store
 
 DB_INDEX_FEEDBACK = "label"
 
@@ -83,41 +88,93 @@ def eval_doc_qa_feedback(filters: FilterRequest = None):
     return res
 
 @router.get("/export-feedback")
-def export_doc_qa_feedback(context_size: int = 2_000):
+def export_doc_qa_feedback(context_size: int = 100_000, full_document_context=True, only_positive_labels=False):
     """
     SQuAD format JSON export for question/answer pairs that were marked as "relevant".
 
     The context_size param can be used to limit response size for large documents.
     """
-    labels = document_store.get_all_labels(
-        index=DB_INDEX_FEEDBACK, filters={"is_correct_answer": [True], "origin": ["user-feedback"]}
-    )
+    if only_positive_labels:
+        labels = document_store.get_all_labels(
+            index=DB_INDEX_FEEDBACK, filters={"is_correct_answer": [True], "origin": ["user-feedback"]}
+        )
+    else:
+        labels = document_store.get_all_labels(
+            index=DB_INDEX_FEEDBACK, filters={"origin": ["user-feedback"]}
+        )
+        # Filter out the labels where the passage is correct but answer is wrong (in SQuAD this matches neither a "positive example" nor a negative "is_impossible" one)
+        labels = [l for l in labels if not (l.is_correct_document==True and l.is_correct_answer==False)]
 
     export_data = []
+    c = 0
     for label in labels:
+        print(c)
+        c += 1
         document = document_store.get_document_by_id(label.document_id)
-        text = document.text
+        if document is None:
+            document = faiss_document_store.get_document_by_id(label.document_id)
+            if document is None:
+                logger.error("Could not find document with id {label.document_id} for label id {label.id}")
 
-        # the final length of context(including the answer string) is 'context_size'.
-        # we try to add equal characters for context before and after the answer string.
-        # if either beginning or end of text is reached, we correspondingly
-        # append more context characters at the other end of answer string.
-        context_to_add = int((context_size - len(label.answer)) / 2)
+        if label.is_correct_answer == False and label.is_correct_document == False:
+            label.no_answer = True
+        else:
+            label.no_answer = False
 
-        start_pos = max(label.offset_start_in_doc - context_to_add, 0)
-        additional_context_at_end = max(context_to_add - label.offset_start_in_doc, 0)
+        if full_document_context:
+            squad_label = {"paragraphs": [{
+                "context": document.text,
+                "id": label.document_id,
+                "qas": [
+                    {"question": label.question,
+                     "id": label.id,
+                     "is_impossible": label.no_answer,
+                     "answers": [{
+                         "text": label.answer,
+                         "answer_start": label.offset_start_in_doc
+                     }]
+                     }
+                ]
+            }
+            ]
+            }
 
-        end_pos = min(label.offset_start_in_doc + len(label.answer) + context_to_add, len(text) - 1)
-        additional_context_at_start = max(label.offset_start_in_doc + len(label.answer) + context_to_add - len(text), 0)
-
-        start_pos = max(0, start_pos - additional_context_at_start)
-        end_pos = min(len(text) - 1, end_pos + additional_context_at_end)
-
-        context_to_export = text[start_pos:end_pos]
-
-        export_data.append({"paragraphs": [{"qas": label, "context": context_to_export}]})
+            # quality check
+            start = squad_label["paragraphs"][0]["qas"][0]["answers"][0]["answer_start"]
+            answer = squad_label["paragraphs"][0]["qas"][0]["answers"][0]["text"]
+            context = squad_label["paragraphs"][0]["context"]
+            if not context[start:start + len(answer)] == answer:
+                logger.error(
+                    f"Skipping invalid squad label as string via offsets ('{context[start:start + len(answer)]}') does not match answer string ('{answer}') ")
+            export_data.append(squad_label)
+        else:
+            raise NotImplementedError()
+            # # TODO fix offsets for context and return  proper Squad format!
+            # text = document.text
+            #
+            # # the final length of context(including the answer string) is 'context_size'.
+            # # we try to add equal characters for context before and after the answer string.
+            # # if either beginning or end of text is reached, we correspondingly
+            # # append more context characters at the other end of answer string.
+            # context_to_add = int((context_size - len(label.answer)) / 2)
+            #
+            # start_pos = max(label.offset_start_in_doc - context_to_add, 0)
+            # additional_context_at_end = max(context_to_add - label.offset_start_in_doc, 0)
+            #
+            # end_pos = min(label.offset_start_in_doc + len(label.answer) + context_to_add, len(text) - 1)
+            # additional_context_at_start = max(label.offset_start_in_doc + len(label.answer) + context_to_add - len(text), 0)
+            #
+            # start_pos = max(0, start_pos - additional_context_at_start)
+            # end_pos = min(len(text) - 1, end_pos + additional_context_at_end)
+            #
+            # context_to_export = text[start_pos:end_pos]
+            #
+            # # squad_label = [label]
+            # # squad_label["answers"] = [{"text": label.answer, "answer_start": label.}]
+            # export_data.append({"paragraphs": [{"qas": label, "context": context_to_export}]})
 
     export = {"data": export_data}
 
     return export
+
 
