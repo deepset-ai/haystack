5e30662626b3141b5e6d7e16c45228f6f5155280
brandenchan <brandenchan@icloud.com>
2021-03-31 15:03:46 +0200

    Update tutorial script

--- tutorials/Tutorial5_Evaluation.py
+++ tutorials/Tutorial5_Evaluation.py
@@ -2,46 +2,45 @@ from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from haystack.preprocessor.utils import fetch_archive_from_http
 from haystack.retriever.sparse import ElasticsearchRetriever
 from haystack.retriever.dense import DensePassageRetriever
+from haystack.eval import EvalReader, EvalRetriever
 from haystack.reader.farm import FARMReader
-from haystack.finder import Finder
+from haystack.preprocessor import PreProcessor
+from haystack import Pipeline
+
 from farm.utils import initialize_device_settings
 
 import logging
 import subprocess
 import time
 
+logger = logging.getLogger(__name__)
+
 
 def tutorial5_evaluation():
-    logger = logging.getLogger(__name__)
 
     ##############################################
     # Settings
     ##############################################
-    LAUNCH_ELASTICSEARCH = True
 
-    eval_retriever_only = True
-    eval_reader_only = False
-    eval_both = False
+    # Choose from Evaluation style from ['retriever_closed', 'reader_closed', 'retriever_reader_open']
+    # 'retriever_closed' - evaluates only the retriever, based on whether the gold_label document is retrieved.
+    # 'reader_closed' - evaluates only the reader in a closed domain fashion i.e. the reader is given one query
+    #     and one document and metrics are calculated on whether the right position in this text is selected by
+    #     the model as the answer span (i.e. SQuAD style)
+    # 'retriever_reader_open' - evaluates retriever and reader in open domain fashion i.e. a document is considered
+    #     correctly retrieved if it contains the answer string within it. The reader is evaluated based purely on the
+    #     predicted string, regardless of which document this came from and the position of the extracted span.
+    style = "reader_closed"
 
     # make sure these indices do not collide with existing ones, the indices will be wiped clean before data is inserted
     doc_index = "tutorial5_docs"
     label_index = "tutorial5_labels"
+
     ##############################################
     # Code
     ##############################################
+    launch_es()
     device, n_gpu = initialize_device_settings(use_cuda=True)
-    # Start an Elasticsearch server
-    # You can start Elasticsearch on your local machine instance using Docker. If Docker is not readily available in
-    # your environment (eg., in Colab notebooks), then you can manually download and execute Elasticsearch from source.
-    if LAUNCH_ELASTICSEARCH:
-        logging.info("Starting Elasticsearch ...")
-        status = subprocess.run(
-            ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2'], shell=True
-        )
-        if status.returncode:
-            raise Exception("Failed to launch Elasticsearch. If you want to connect to an existing Elasticsearch instance"
-                            "then set LAUNCH_ELASTICSEARCH in the script to False.")
-        time.sleep(30)
 
     # Download evaluation data, which is a subset of Natural Questions development set containing 50 documents
     doc_dir = "../data/nq"
@@ -49,16 +48,39 @@ def tutorial5_evaluation():
     fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
     # Connect to Elasticsearch
-    document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document",
-                                                create_index=False, embedding_field="emb",
-                                                embedding_dim=768, excluded_meta_data=["emb"])
-
+    document_store = ElasticsearchDocumentStore(
+        host="localhost", username="", password="", index=doc_index,
+        create_index=False, embedding_field="emb",
+        embedding_dim=768, excluded_meta_data=["emb"]
+    )
 
     # Add evaluation data to Elasticsearch document store
     # We first delete the custom tutorial indices to not have duplicate elements
+    # and also split our documents into shorter passages using the PreProcessor
+    preprocessor = PreProcessor(
+        split_length=500,
+        split_overlap=0,
+        split_respect_sentence_boundary=False,
+        clean_empty_lines=False,
+        clean_whitespace=False
+    )
     document_store.delete_all_documents(index=doc_index)
     document_store.delete_all_documents(index=label_index)
-    document_store.add_eval_data(filename="../data/nq/nq_dev_subset_v2.json", doc_index=doc_index, label_index=label_index)
+    document_store.add_eval_data(
+        filename="../data/nq/nq_dev_subset_v2.json",
+        doc_index=doc_index,
+        label_index=label_index,
+        preprocessor=preprocessor
+    )
+
+    # Let's prepare the labels that we need for the retriever and the reader
+    labels = document_store.get_all_labels_aggregated(index=label_index)
+    q_to_l_dict = {
+        l.question: {
+            "retriever": l,
+            "reader": l
+        } for l in labels
+    }
 
     # Initialize Retriever
     retriever = ElasticsearchRetriever(document_store=document_store)
@@ -68,23 +90,27 @@ def tutorial5_evaluation():
     # Here, for nq_dev_subset_v2.json we have avg. num of tokens = 5220(!).
     # DPR still outperforms Elastic's BM25 by a small margin here.
     # retriever = DensePassageRetriever(document_store=document_store,
-    #                                   query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+    #                                   query_embedding_model="facebook/dpr-qPreProuestion_encoder-single-nq-base",
     #                                   passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
     #                                   use_gpu=True,
     #                                   embed_title=True,
     #                                   remove_sep_tok_from_untitled_passages=True)
     # document_store.update_embeddings(retriever, index=doc_index)
 
-
     # Initialize Reader
-    reader = FARMReader("deepset/roberta-base-squad2", top_k_per_candidate=4)
+    reader = FARMReader(
+        model_name_or_path="deepset/roberta-base-squad2",
+        top_k_per_candidate=4,
+        return_no_answer=True
+    )
 
-    # Initialize Finder which sticks together Reader and Retriever
-    finder = Finder(reader, retriever)
+    # Here we initialize the nodes that perform evaluation
+    eval_retriever = EvalRetriever()
+    eval_reader = EvalReader(debug=True)
 
 
-    ## Evaluate Retriever on its own
-    if eval_retriever_only:
+    ## Evaluate Retriever on its own in closed domain fashion
+    if style == "retriever_closed":
         retriever_eval_results = retriever.eval(top_k=10, label_index=label_index, doc_index=doc_index)
         ## Retriever Recall is the proportion of questions for which the correct document containing the answer is
         ## among the correct documents
@@ -92,8 +118,8 @@ def tutorial5_evaluation():
         ## Retriever Mean Avg Precision rewards retrievers that give relevant documents a higher rank
         print("Retriever Mean Avg Precision:", retriever_eval_results["map"])
 
-    # Evaluate Reader on its own
-    if eval_reader_only:
+    # Evaluate Reader on its own in closed domain fashion (i.e. SQuAD style)
+    elif style == "reader_closed":
         reader_eval_results = reader.eval(document_store=document_store, device=device, label_index=label_index, doc_index=doc_index)
         # Evaluation of Reader can also be done directly on a SQuAD-formatted file without passing the data to Elasticsearch
         #reader_eval_results = reader.eval_on_file("../data/nq", "nq_dev_subset_v2.json", device=device)
@@ -106,11 +132,52 @@ def tutorial5_evaluation():
         print("Reader F1-Score:", reader_eval_results["f1"])
 
 
-    # Evaluate combination of Reader and Retriever through Finder
-    if eval_both:
-        finder_eval_results = finder.eval(top_k_retriever=1, top_k_reader=10, label_index=label_index, doc_index=doc_index)
-        finder.print_eval_results(finder_eval_results)
+    # Evaluate combination of Reader and Retriever in open domain fashion
+    elif style == "retriever_reader_open":
 
+        # Here is the pipeline definition
+        p = Pipeline()
+        p.add_node(component=retriever, name="ESRetriever", inputs=["Query"])
+        p.add_node(component=eval_retriever, name="EvalRetriever", inputs=["ESRetriever"])
+        p.add_node(component=reader, name="QAReader", inputs=["EvalRetriever"])
+        p.add_node(component=eval_reader, name="EvalReader", inputs=["QAReader"])
+        results = []
+
+        for i, (q, l) in enumerate(q_to_l_dict.items()):
+            res = p.run(
+                query=q,
+                top_k_retriever=10,
+                labels=l,
+                top_k_reader=10,
+                index=doc_index,
+            )
+            results.append(res)
+
+        eval_retriever.print()
+        print()
+        retriever.print_time()
+        print()
+        eval_reader.print(mode="reader")
+        print()
+        reader.print_time()
+        print()
+        eval_reader.print(mode="pipeline")
+
+
+def launch_es():
+    # Start an Elasticsearch server
+    # You can start Elasticsearch on your local machine instance using Docker. If Docker is not readily available in
+    # your environment (eg., in Colab notebooks), then you can manually download and execute Elasticsearch from source.
+
+    logger.info("Starting Elasticsearch ...")
+    status = subprocess.run(
+        ['docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2'], shell=True
+    )
+    if status.returncode:
+        logger.warning("Tried to start Elasticsearch through Docker but this failed. "
+                       "It is likely that there is already an existing Elasticsearch instance running. ")
+    else:
+        time.sleep(15)
 
 if __name__ == "__main__":
     tutorial5_evaluation()
