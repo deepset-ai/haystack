5d16f0368a227f9a56f88f6040592888def10340
Timo Moeller <timo.moeller@deepset.ai>
2021-04-01 18:23:05 +0200

    Update farm version (#936)
    * Update farm version
    
    * Add new DPR loading, fix dpr param name
    
    * Add QA model confidence as answer probability, fix prams in test

--- test/test_retriever.py
+++ test/test_retriever.py
@@ -194,10 +194,10 @@ def test_dpr_saving_and_loading(retriever, document_store):
     #     assert (p1.data.ne(p2.data).sum() == 0)
 
     # attributes
-    assert loaded_retriever.embed_title == True
+    assert loaded_retriever.processor.embed_title == True
     assert loaded_retriever.batch_size == 16
-    assert loaded_retriever.max_seq_len_passage == 256
-    assert loaded_retriever.max_seq_len_query == 64
+    assert loaded_retriever.processor.max_seq_len_passage == 256
+    assert loaded_retriever.processor.max_seq_len_query == 64
 
     # Tokenizer
     assert isinstance(loaded_retriever.passage_tokenizer, DPRContextEncoderTokenizerFast)
