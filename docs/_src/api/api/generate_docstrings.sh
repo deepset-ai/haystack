ba7c371955841c098480cc6750a7ce19d7f0095a
Branden Chan <brandenchan@icloud.com>
2021-04-12 16:19:07 +0200

    Add api pages

--- docs/_src/api/api/generate_docstrings.sh
+++ docs/_src/api/api/generate_docstrings.sh
@@ -9,3 +9,6 @@ pydoc-markdown pydoc-markdown-reader.yml
 pydoc-markdown pydoc-markdown-generator.yml
 pydoc-markdown pydoc-markdown-retriever.yml
 pydoc-markdown pydoc-markdown-pipelines.yml
+pydoc-markdown pydoc-markdown-knowledge-graph.yml
+pydoc-markdown pydoc-markdown-graph-retriever.yml
+pydoc-markdown pydoc-markdown-evaluation.yml
