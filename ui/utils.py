3b47322cfd75aa454e43376abbdf55c68d4998e3
oryx1729 <78848855+oryx1729@users.noreply.github.com>
2021-04-30 10:43:25 +0200

    Fix file upload

--- ui/utils.py
+++ ui/utils.py
@@ -6,75 +6,58 @@ import streamlit as st
 API_ENDPOINT = os.getenv("API_ENDPOINT", "http://localhost:8000")
 DOC_REQUEST = "query"
 DOC_FEEDBACK = "feedback"
-DOC_UPLOAD = "file_upload"
+DOC_UPLOAD = "file-upload"
+
 
 @st.cache(show_spinner=False)
-def retrieve_doc(query,filters=None,top_k_reader=5,top_k_retriever=5):
-   # Query Haystack API
-   url = f"{API_ENDPOINT}/{DOC_REQUEST}"
-   req = {"query": query, "filters": filters, "top_k_retriever": top_k_retriever, "top_k_reader": top_k_reader}
-   response_raw = requests.post(url,json=req).json()
-   
-   # Format response
-   result = []
-   answers = response_raw["answers"]
-   for i in range(len(answers)):
-       answer = answers[i]['answer']
-       if answer:
-           context = '...' + answers[i]['context'] + '...'
-           meta_name = answers[i]['meta']['name']
-           relevance = round(answers[i]['probability']*100,2)
-           document_id = answers[i]['document_id']
-           offset_start_in_doc = answers[i]['offset_start_in_doc']
-           result.append({'context':context,'answer':answer,'source':meta_name,'relevance':relevance, 'document_id':document_id,'offset_start_in_doc':offset_start_in_doc})
-   return result, response_raw
+def retrieve_doc(query, filters=None, top_k_reader=5, top_k_retriever=5):
+    # Query Haystack API
+    url = f"{API_ENDPOINT}/{DOC_REQUEST}"
+    req = {"query": query, "filters": filters, "top_k_retriever": top_k_retriever, "top_k_reader": top_k_reader}
+    response_raw = requests.post(url, json=req).json()
 
-def feedback_doc(question,is_correct_answer,document_id,model_id,is_correct_document,answer,offset_start_in_doc):
-   # Feedback Haystack API
-   url = f"{API_ENDPOINT}/{DOC_FEEDBACK}"
-   req = {
-         "question": question,
-         "is_correct_answer": is_correct_answer,
-         "document_id":  document_id,
-         "model_id": model_id,
-         "is_correct_document": is_correct_document,
-         "answer": answer,
-         "offset_start_in_doc": offset_start_in_doc
-         }
-   response_raw = requests.post(url,json=req).json()
-   return response_raw
+    # Format response
+    result = []
+    answers = response_raw["answers"]
+    for i in range(len(answers)):
+        answer = answers[i]["answer"]
+        if answer:
+            context = "..." + answers[i]["context"] + "..."
+            meta_name = answers[i]["meta"]["name"]
+            relevance = round(answers[i]["probability"] * 100, 2)
+            document_id = answers[i]["document_id"]
+            offset_start_in_doc = answers[i]["offset_start_in_doc"]
+            result.append(
+                {
+                    "context": context,
+                    "answer": answer,
+                    "source": meta_name,
+                    "relevance": relevance,
+                    "document_id": document_id,
+                    "offset_start_in_doc": offset_start_in_doc,
+                }
+            )
+    return result, response_raw
 
 
-def upload_doc(
-   file,
-   split_overlap=None,
-   meta=None,
-   split_respect_sentence_boundary=None,
-   split_length=None,
-   remove_numeric_tables=None,
-   remove_empty_lines=None,
-   remove_header_footer=None,
-   remove_whitespace=None,
-   valid_languages=None,
-   split_by=None):
-   # Feedback Haystack API
-   url = f"{API_ENDPOINT}/{DOC_UPLOAD}"
-   custom_header = {
-      "accept": "application/json",
-      "Content-Type": "multipart/form-data"
-      }
-   req = {
-         "file": file,
-         "split_overlap": split_overlap,
-         "meta":  meta,
-         "split_respect_sentence_boundary": split_respect_sentence_boundary,
-         "split_length": split_length,
-         "remove_numeric_tables": remove_numeric_tables,
-         "remove_empty_lines": remove_empty_lines,
-         "remove_header_footer": remove_header_footer,
-         "remove_whitespace": remove_whitespace,
-         "valid_languages": valid_languages,
-         "split_by":split_by
-         }
-   response_raw = requests.post(url,json=req, headers = custom_header).json()
-   return response_raw
+def feedback_doc(question, is_correct_answer, document_id, model_id, is_correct_document, answer, offset_start_in_doc):
+    # Feedback Haystack API
+    url = f"{API_ENDPOINT}/{DOC_FEEDBACK}"
+    req = {
+        "question": question,
+        "is_correct_answer": is_correct_answer,
+        "document_id": document_id,
+        "model_id": model_id,
+        "is_correct_document": is_correct_document,
+        "answer": answer,
+        "offset_start_in_doc": offset_start_in_doc,
+    }
+    response_raw = requests.post(url, json=req).json()
+    return response_raw
+
+
+def upload_doc(file):
+    url = f"{API_ENDPOINT}/{DOC_UPLOAD}"
+    files = [("file", file)]
+    response_raw = requests.post(url, files=files).json()
+    return response_raw
