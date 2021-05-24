9a3e66f074d51d0519417389e5e85fa0db730bf7
Vladimir Blagojevic <dovlex@gmail.com>
2021-05-24 09:21:40 +0200

    Properly initialize conftest default value for vector_dim

--- test/conftest.py
+++ test/conftest.py
@@ -350,7 +350,7 @@ def document_store_with_docs(request, test_docs_xs):
 
 @pytest.fixture(params=["elasticsearch", "faiss", "memory", "sql", "milvus"])
 def document_store(request, test_docs_xs):
-    vector_dim = request.node.get_closest_marker("vector_dim", 768)
+    vector_dim = request.node.get_closest_marker("vector_dim", pytest.mark.vector_dim(768))
     document_store = get_document_store(request.param, vector_dim.args[0])
     yield document_store
     document_store.delete_all_documents()
