044fddaa3937c0ec9f784cd47d05f9729025455a
Branden Chan <33759007+brandenchan@users.noreply.github.com>
2021-06-02 13:09:45 +0200

    Update Milvus benchmarks (#1128)
    * Update Milvus benchmarks
    
    * Add sentence transformers
    
    * Update sentence transformers index results
    
    * Remove duplicate row

--- test/benchmarks/results_to_json.py
+++ test/benchmarks/results_to_json.py
@@ -41,12 +41,13 @@ def retriever(index_csv="retriever_index_results.csv", query_csv="retriever_quer
     name_cleaning = {
         "dpr": "DPR",
         "elastic": "BM25",
-        "elasticsearch": "ElasticSearch",
+        "elasticsearch": "Elasticsearch",
         "faiss": "FAISS",
         "faiss_flat": "FAISS (flat)",
         "faiss_hnsw": "FAISS (HNSW)",
         "milvus_flat": "Milvus (flat)",
-        "milvus_hnsw": "Milvus (HNSW)"
+        "milvus_hnsw": "Milvus (HNSW)",
+        "sentence_transformers": "Sentence Transformers"
     }
 
     index = pd.read_csv(index_csv)
