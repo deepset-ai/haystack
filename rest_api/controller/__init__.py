16b5efca8e1c951ce992cfac3e2271f125202e7a
oryx1729 <oryx1729@protonmail.com>
2021-06-22 17:05:07 +0200

    Move custom_component.py to a new pipeline dir

--- rest_api/controller/__init__.py
+++ rest_api/controller/__init__.py
@@ -1,1 +1,1 @@
-from rest_api import custom_component  # this import is required for the Custom Components to be registered
+from rest_api.pipeline import custom_component  # this import is required for the Custom Components to be registered
