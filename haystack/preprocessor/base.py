d3bc254a2f964a3b29fef3a14bb7d1ae17f52e27
Branden Chan <33759007+brandenchan@users.noreply.github.com>
2021-06-23 18:13:51 +0200

    Make PreProcessor.process() work on lists of documents (#1163)
    * Add process_batch method
    
    * Rename methods
    
    * Fix doc string, satisfy mypy
    
    * Fix mypy CI
    
    * Fix typp
    
    * Update tutorial
    
    * Fix argument name
    
    * Change arg name
    
    * Incorporate reviewer feedback

--- haystack/preprocessor/base.py
+++ haystack/preprocessor/base.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Union
 
 from haystack import BaseComponent
 
@@ -8,7 +8,7 @@ class BasePreProcessor(BaseComponent):
 
     def process(
         self,
-        document: dict,
+        documents: Union[dict, List[dict]],
         clean_whitespace: Optional[bool] = True,
         clean_header_footer: Optional[bool] = False,
         clean_empty_lines: Optional[bool] = True,
@@ -50,7 +50,7 @@ class BasePreProcessor(BaseComponent):
         **kwargs,
     ):
         documents = self.process(
-            document=document,
+            documents=document,
             clean_whitespace=clean_whitespace,
             clean_header_footer=clean_header_footer,
             clean_empty_lines=clean_empty_lines,
