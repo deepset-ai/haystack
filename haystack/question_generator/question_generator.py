7b92154c4409ba2156726720b7f600e6583376c0
brandenchan <brandenchan@icloud.com>
2021-07-09 12:52:01 +0200

    Split texts into 50 word chunks

--- haystack/question_generator/question_generator.py
+++ haystack/question_generator/question_generator.py
@@ -1,6 +1,7 @@
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 from haystack import BaseComponent
+from haystack.preprocessor import PreProcessor
 
 text = "generate questions: Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum \
 and first released in 1991, Python's design philosophy emphasizes code \
@@ -11,9 +12,13 @@ readability with its notable use of significant whitespace."
 TODO
 top_k instead of length
 device and use gpu
-Why does __init__ not work?
 split to base class?
 Not totally sure about args
+text limits?
+run method
+Does it clip long texts??
+https://discuss.huggingface.co/t/t5-finetuning-tips/684
+Inefficient because we feed it 50 words
 """
 
 class QuestionGenerator(BaseComponent):
@@ -24,34 +29,64 @@ class QuestionGenerator(BaseComponent):
                  max_length=256,
                  no_repeat_ngram_size=3,
                  length_penalty=1.5,
-                 early_stopping=True):
+                 early_stopping=True,
+                 split_length=50,
+                 split_overlap=10):
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.set_config(
             model_name_or_path=model_name_or_path, model_version=model_version,
             max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=no_repeat_ngram_size,
-            length_penalty=length_penalty, early_stopping=early_stopping
+            length_penalty=length_penalty, early_stopping=early_stopping, split_length=split_length,
+            split_overlap=split_overlap
         )
         self.num_beams = num_beams
         self.max_length = max_length
         self.no_repeat_ngram_size = no_repeat_ngram_size
         self.length_penalty = length_penalty
         self.early_stopping = early_stopping
+        self.split_length = split_length
+        self.split_overlap = split_overlap
+        self.preprocessor = PreProcessor()
+        self.prompt = "generate questions:"
 
     def generate(self, text):
-        tokenized = self.tokenizer([text], return_tensors="pt")
-        input_ids = tokenized["input_ids"]
-        attention_mask = tokenized["attention_mask"]   # necessary if padding is enabled so the model won't attend pad tokens
-        tokens_output = self.model.generate(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            num_beams=self.num_beams,
-            max_length=self.max_length,
-            no_repeat_ngram_size=self.no_repeat_ngram_size,
-            length_penalty=self.length_penalty,
-            early_stopping=self.early_stopping,
+        # Performing splitting because T5 has a max input length
+        # Also currently, it seems that it only generates about 3 questions for the beginning section of text
+        split_texts_dict = self.preprocessor.split(
+            document={"text": text},
+            split_by="word",
+            split_respect_sentence_boundary=False,
+            split_overlap=self.split_overlap,
+            split_length=self.split_length
         )
+        split_texts = [x["text"] for x in split_texts_dict]
+        ret = []
+        for split_text in split_texts:
+            if self.prompt not in split_text:
+                split_text = self.prompt + " " + split_text
+            tokenized = self.tokenizer([split_text], return_tensors="pt")
+            input_ids = tokenized["input_ids"]
+            attention_mask = tokenized["attention_mask"]   # necessary if padding is enabled so the model won't attend pad tokens
+            tokens_output = self.model.generate(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                num_beams=self.num_beams,
+                max_length=self.max_length,
+                no_repeat_ngram_size=self.no_repeat_ngram_size,
+                length_penalty=self.length_penalty,
+                early_stopping=self.early_stopping,
+            )
 
-        ret = [self.tokenizer.decode(t) for t in tokens_output]
+            string_output = self.tokenizer.decode(tokens_output[0])
+            string_output = string_output.replace("<pad>", "").replace("</s>", "")
+            questions_string = string_output.split("<sep>")
+            questions = [x for x in questions_string if x]
+
+            # Doing this instead of set to maintain order since the generated questions seem to have answers
+            # that occur in order in the text
+            for q in questions:
+                if q not in ret:
+                    ret.append(q)
         return ret
 
