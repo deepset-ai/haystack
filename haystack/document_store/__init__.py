0290a7dbfadefa06a6bcf3b43c90a8e87b53a855
Ikram Ali <mrikram1989@gmail.com>
2021-07-21 23:06:25 +0500

    [document_store] Import normalize for standardization of dependency management

--- haystack/document_store/__init__.py
+++
@@ -1,5 +0,0 @@
-from haystack.document_store.elasticsearch import ElasticsearchDocumentStore, OpenDistroElasticsearchDocumentStore
-from haystack.document_store.faiss import FAISSDocumentStore
-from haystack.document_store.memory import InMemoryDocumentStore
-from haystack.document_store.milvus import MilvusDocumentStore
-from haystack.document_store.sql import SQLDocumentStore
