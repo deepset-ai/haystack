3b1869954f07c58cd5485efafa7d420fd3792f72
brandenchan <brandenchan@icloud.com>
2021-07-22 16:45:29 +0200

    Add Kendra scripts

---
+++ kendra/eval.py
@@ -0,0 +1,147 @@
+from pathlib import Path
+import json
+from haystack.eval import calculate_f1_str_multi, calculate_em_str_multi
+import pandas as pd
+from tqdm import tqdm
+from pprint import pprint
+
+INVALID_Q_STR = "INVALID QUESTION FOR KENDRA"
+
+label_dir = Path("../data/nq")
+pred_dir = Path(".")
+preds_haystack_filename = "predictions_haystack_7k.json"
+preds_kendra_filename = "predictions_kendra_300k.json"
+labels_filename = "nq_dev_labels.json"
+top_k_eval = 10
+
+def reduce_haystack_preds(preds_haystack_full, top_k_eval):
+    ret = {}
+    for p in preds_haystack_full:
+        query = p["query"]
+        answers_str = [a["answer"] for a in p["answers"]]
+        ret[query] = answers_str[:top_k_eval]
+    return ret
+
+def reduce_kendra_preds(preds_kendra_full):
+    ret = {}
+    for p in preds_kendra_full:
+        query = p["Question"]
+        pred_kendra_list = []
+        for r in p["ResultItems"]:
+            if len(r["AdditionalAttributes"]) > 0:
+                for highlights in r['AdditionalAttributes'][0]['Value']['TextWithHighlightsValue']['Highlights']:
+                    if highlights['TopAnswer'] == True:
+                        answer_text = r['AdditionalAttributes'][0]['Value']['TextWithHighlightsValue']['Text']
+                        pred_kendra = answer_text[highlights['BeginOffset']:highlights['EndOffset']]
+                        pred_kendra_list.append(pred_kendra)
+        ret[query] = pred_kendra_list
+    return ret
+
+def reduce_labels(labels):
+    ret = {}
+    for l in labels:
+        query = l["question"]
+        ret[query] = l["short_answers"]
+    return ret
+
+def join_preds_labels(preds_haystack, preds_kendra, labels):
+    ret = {}
+    for query in labels:
+        pk = preds_kendra.get(query, INVALID_Q_STR)
+        ph = preds_haystack[query]
+        ret[query] = {"preds_haystack": ph,
+                      "preds_kendra": pk,
+                      "labels": labels[query]}
+    return ret
+
+def generate_results(preds_labels):
+    results = []
+    for query in tqdm(preds_labels):
+        ph = preds_labels[query]["preds_haystack"]
+        pk = preds_labels[query]["preds_kendra"]
+        l = preds_labels[query]["labels"]
+        if len(pk) == 0 or INVALID_Q_STR in pk:
+            kendra_pred_available = False
+        else:
+            kendra_pred_available = True
+        if len(l) == 0:
+            f1_haystack_top_k = None
+            em_haystack_top_k = None
+            f1_kendra_top_k = None
+            em_kendra_top_k = None
+            label_available = False
+        else:
+            f1_haystack_top_k = max([calculate_f1_str_multi(l, p) for p in ph])
+            em_haystack_top_k = max([calculate_em_str_multi(l, p) for p in ph])
+            if not kendra_pred_available:
+                f1_kendra_top_k = 0
+                em_kendra_top_k = 0
+            else:
+                f1_kendra_top_k = max([calculate_f1_str_multi(l, p) for p in pk])
+                em_kendra_top_k = max([calculate_em_str_multi(l, p) for p in pk])
+            label_available = True
+
+        d = {"query": query,
+             "labels": l,
+             "preds_haystack": ph,
+             "f1_haystack_top_k": f1_haystack_top_k,
+             "em_haystack_top_k": em_haystack_top_k,
+             "f1_kendra_top_k": f1_kendra_top_k,
+             "em_kendra_top_k": em_kendra_top_k,
+             "label_available": label_available,
+             "kendra_pred_available": kendra_pred_available,
+             "top_k_eval": top_k_eval}
+        results.append(d)
+    df = pd.DataFrame.from_records(results)
+    return df
+
+def main():
+    # File loading
+    preds_haystack_full = json.load(open(pred_dir/preds_haystack_filename))
+    preds_kendra_full = json.load(open(pred_dir/preds_kendra_filename))
+    labels_full = json.load(open(label_dir/labels_filename))
+
+    # Data prep
+    preds_haystack = reduce_haystack_preds(preds_haystack_full, top_k_eval)
+    preds_kendra = reduce_kendra_preds(preds_kendra_full)
+    labels = reduce_labels(labels_full)
+    preds_labels = join_preds_labels(preds_haystack, preds_kendra, labels)
+
+    # Metric calculation
+    df_results = generate_results(preds_labels)
+
+    # Final metric
+    df_filtered = df_results[df_results["label_available"] == True]
+    valid_questions = len(df_filtered)
+    f1_haystack_top_k_1 = df_filtered["f1_haystack_top_k"].mean()
+    em_haystack_top_k_1 = df_filtered["em_haystack_top_k"].mean()
+    f1_kendra_top_k_1 = df_filtered["f1_kendra_top_k"].mean()
+    em_kendra_top_k_1 = df_filtered["em_kendra_top_k"].mean()
+
+    haystack_results_1 = {"f1_haystack_top_k": f1_haystack_top_k_1,
+                        "em_haystack_top_k": em_haystack_top_k_1}
+    kendra_results_1 = {"f1_kendra_top_k": f1_kendra_top_k_1,
+                      "em_kendra_top_k": em_kendra_top_k_1}
+
+    df_filtered_filtered = df_filtered[df_filtered["kendra_pred_available"] == True]
+    kendra_valid_answer_count = len(df_filtered_filtered)
+    f1_haystack_top_k_2 = df_filtered_filtered["f1_haystack_top_k"].mean()
+    em_haystack_top_k_2 = df_filtered_filtered["em_haystack_top_k"].mean()
+    f1_kendra_top_k_2 = df_filtered_filtered["f1_kendra_top_k"].mean()
+    em_kendra_top_k_2 = df_filtered_filtered["em_kendra_top_k"].mean()
+
+    haystack_results_2 = {"f1_haystack_top_k": f1_haystack_top_k_2,
+                        "em_haystack_top_k": em_haystack_top_k_2}
+    kendra_results_2 = {"f1_kendra_top_k": f1_kendra_top_k_2,
+                      "em_kendra_top_k": em_kendra_top_k_2}
+
+    print(valid_questions)
+    print(haystack_results_1)
+    print(kendra_results_1)
+    print(kendra_valid_answer_count)
+    print(haystack_results_2)
+    print(kendra_results_2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ kendra/predict.py
@@ -0,0 +1,94 @@
+import json
+import logging
+import subprocess
+import time
+
+from tqdm import tqdm
+
+from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
+from haystack.preprocessor.cleaning import clean_wiki_text
+from haystack.preprocessor.utils import fetch_archive_from_http
+from haystack.reader.farm import FARMReader
+from haystack.utils import print_answers, launch_es
+from haystack.retriever.sparse import ElasticsearchRetriever
+from pathlib import Path
+from haystack.preprocessor import PreProcessor
+
+def kendra_benchmark():
+    # Define variables
+    logger = logging.getLogger(__name__)
+    doc_dir = Path("../data/nq")
+    doc_index = "document"
+    outfile = "predictions.json"
+    summary_file = "summary.json"
+    model = "deepset/roberta-base-squad2"
+    top_k_retriever = 5
+    top_k_reader = 10
+
+    # Start document store
+    launch_es()
+    document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index=doc_index)
+
+    document_store.delete_documents(index=doc_index)
+    preprocessor = PreProcessor(clean_header_footer=True, split_length=1000, split_overlap=50)
+
+    def prepare_docs(filepath, preprocessor):
+        lines = json.load(open(filepath))
+        print(len(lines))
+        lines = set(lines)
+        print(len(lines))
+        docs = [{"text": l} for l in lines]
+        docs = preprocessor.process(docs)
+        print(len(docs))
+        return docs
+
+    docs = prepare_docs(doc_dir / "nq_dev_texts.json", preprocessor)
+    tic = time.perf_counter()
+    document_store.write_documents(docs, index=doc_index)
+    toc = time.perf_counter()
+    index_time = toc - tic
+
+    retriever = ElasticsearchRetriever(document_store=document_store, top_k=top_k_retriever)
+
+    reader = FARMReader(model_name_or_path=model, use_gpu=True, top_k=top_k_reader)
+
+    from haystack.pipeline import ExtractiveQAPipeline
+    pipe = ExtractiveQAPipeline(reader, retriever)
+
+    label_data = json.load(open(doc_dir / "nq_dev_labels.json"))
+    questions = [x["question"] for x in label_data]
+
+    # querying
+    results = []
+    tic = time.perf_counter()
+    for i, q in tqdm(enumerate(questions)):
+        result = pipe.run(query=q)
+        results.append(result)
+    toc = time.perf_counter()
+    query_time = toc - tic
+    n_docs = len(questions)
+    summary = {
+        "query_time": query_time,
+        "index_time": index_time,
+        "n_docs": n_docs,
+        "top_k_reader": top_k_reader,
+        "top_k_retriever": top_k_retriever,
+        "reader": str(type(reader)),
+        "reader_model": model,
+        "retriever": str(type(retriever))
+    }
+    json.dump(summary, open(doc_dir / summary_file, "w"), indent=2)
+    json.dump(results, open(doc_dir / outfile, "w"), indent=2)
+
+
+    ## Voilà! Ask a question!
+    # prediction = pipe.run(query="Who is the father of Arya Stark?", top_k_retriever=10, top_k_reader=5)
+    #
+    # prediction = pipe.run(query="Who created the Dothraki vocabulary?", top_k_reader=5)
+    # prediction = pipe.run(query="Who is the sister of Sansa?", top_k_reader=5)
+    #
+    # print_answers(prediction, details="minimal")
+
+
+if __name__ == "__main__":
+    kendra_benchmark()
