d06118394f8540cac8271f55b6d4751f23c4c373
Ikram Ali <mrikram1989@gmail.com>
2021-07-29 18:22:05 +0500

    [Summarizer] warning added Fixes #1296

--- haystack/summarizer/transformers.py
+++ haystack/summarizer/transformers.py
@@ -132,6 +132,16 @@ class TransformersSummarizer(BaseSummarizer):
             # Different order of same documents produce different summary.
             contexts = [self.separator_for_single_summary.join(contexts)]
 
+        encoded_input = self.summarizer.tokenizer(contexts, verbose=False)
+        for input_id in encoded_input['input_ids']:
+            if len(input_id) > self.summarizer.tokenizer.model_max_length:
+                logger.warning(
+                        "Token indices sequence length is longer than the specified maximum sequence length "
+                        f"for this model ({len(input_id)} > {self.summarizer.tokenizer.model_max_length}). "
+                        f"Generating summary from first {self.summarizer.tokenizer.model_max_length}"
+                        f" tokens."
+                )
+
         summaries = self.summarizer(
             contexts,
             min_length=self.min_length,
