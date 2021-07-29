e9cc6c26eaaa231679ff46f9146db6b062c1bcfe
oryx1729 <78848855+oryx1729@users.noreply.github.com>
2021-07-29 14:32:50 +0200

    Suppress FAISS logs & apex warnings (#1315)

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -1,5 +1,14 @@
 import logging
 
+# Change log-levels before modules are loaded to avoid verbose log messages.
+logging.getLogger('farm').setLevel(logging.WARNING)
+logging.getLogger('farm.utils').setLevel(logging.INFO)
+logging.getLogger('farm.infer').setLevel(logging.INFO)
+logging.getLogger('transformers').setLevel(logging.WARNING)
+logging.getLogger('farm.eval').setLevel(logging.INFO)
+logging.getLogger('farm.modeling.optimization').setLevel(logging.INFO)
+logging.getLogger('faiss.loader').setLevel(logging.WARNING)
+
 import pandas as pd
 from haystack.schema import Document, Label, MultiLabel, BaseComponent
 from haystack.finder import Finder
@@ -9,12 +18,3 @@ from haystack._version import __version__
 pd.options.display.max_colwidth = 80
 
 logger = logging.getLogger(__name__)
-
-logging.getLogger('farm').setLevel(logging.WARNING)
-logging.getLogger('farm.utils').setLevel(logging.INFO)
-logging.getLogger('farm.infer').setLevel(logging.INFO)
-logging.getLogger('transformers').setLevel(logging.WARNING)
-logging.getLogger('farm.eval').setLevel(logging.INFO)
-logging.getLogger('farm.modeling.optimization').setLevel(logging.INFO)
-
-
