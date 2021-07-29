87ba84241efac180f96cf8036b9d640ae812a5c9
oryx1729 <oryx1729@protonmail.com>
2021-07-29 13:57:05 +0200

    Test CI

--- test/test_ray.py
+++ test/test_ray.py
@@ -7,7 +7,7 @@ from haystack.pipeline import RayPipeline
 @pytest.mark.parametrize("document_store_with_docs", ["elasticsearch"], indirect=True)
 def test_load_pipeline(document_store_with_docs):
     pipeline = RayPipeline.load_from_yaml(
-        Path("samples/pipeline/test_pipeline.yaml"), pipeline_name="query_pipeline", address="auto"
+        Path("samples/pipeline/test_pipeline.yaml"), pipeline_name="ray_test_pipeline", address="auto"
     )
     prediction = pipeline.run(query="Who lives in Berlin?", top_k_retriever=10, top_k_reader=3)
     assert prediction["query"] == "Who lives in Berlin?"
