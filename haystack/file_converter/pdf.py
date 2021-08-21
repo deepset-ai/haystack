7091538c5c6293f19205b812070940c35d4724da
shahrukhx01 <sk28671@gmail.com>
2021-08-21 06:18:32 +0200

    update imagetotext pipeline

--- haystack/file_converter/pdf.py
+++ haystack/file_converter/pdf.py
@@ -201,7 +201,7 @@ class PDFToTextOCRConverter(BaseConverter):
         remove_numeric_tables: Optional[bool] = None,
         valid_languages: Optional[List[str]] = None,
         encoding: Optional[str] = "utf-8",
-    ) -> List[Any]:
+    ) -> Dict[str, Any]:
         """
         Convert a file to a dictionary containing the text and any associated meta data.
 
