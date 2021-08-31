90c8fd94dc69606cb1809c3ee72543ac752c591d
oryx1729 <78848855+oryx1729@users.noreply.github.com>
2021-08-31 10:14:55 +0200

    Refactor `replicas` config for Ray Pipelines (#1378)

--- test/test_ray.py
+++ test/test_ray.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 
 import pytest
+import ray
+
 from haystack.pipeline import RayPipeline
 
 
 @pytest.mark.parametrize("document_store_with_docs", ["elasticsearch"], indirect=True)
 def test_load_pipeline(document_store_with_docs):
     pipeline = RayPipeline.load_from_yaml(
-        Path("samples/pipeline/test_pipeline.yaml"), pipeline_name="query_pipeline", num_cpus=8,
+        Path("samples/pipeline/test_pipeline.yaml"), pipeline_name="ray_query_pipeline", num_cpus=8,
     )
     prediction = pipeline.run(query="Who lives in Berlin?", top_k_retriever=10, top_k_reader=3)
+
+    assert ray.serve.get_deployment(name="ESRetriever").num_replicas == 2
+    assert ray.serve.get_deployment(name="Reader").num_replicas == 1
     assert prediction["query"] == "Who lives in Berlin?"
     assert prediction["answers"][0]["answer"] == "Carla"
