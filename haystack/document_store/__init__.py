26bde86a888872b9cc6db2947ec9d35ab55edc57
Branden Chan <33759007+brandenchan@users.noreply.github.com>
2021-08-31 15:23:06 +0200

    Add weaviate to init (#1379)

--- haystack/document_store/__init__.py
+++ haystack/document_store/__init__.py
@@ -3,3 +3,4 @@ from haystack.document_store.faiss import FAISSDocumentStore
 from haystack.document_store.memory import InMemoryDocumentStore
 from haystack.document_store.milvus import MilvusDocumentStore
 from haystack.document_store.sql import SQLDocumentStore
+from haystack.document_store.weaviate import WeaviateDocumentStore
