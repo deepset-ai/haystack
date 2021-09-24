1089e5da1e35352090cc29714823add91654c29d
ZanSara <sarazanzo94@gmail.com>
2021-09-24 15:37:22 +0200

    Expose BaseClassifier from haystack.classifier

--- haystack/classifier/__init__.py
+++ haystack/classifier/__init__.py
@@ -1,3 +1,4 @@
+from haystack.classifier.document_classifier.base import BaseClassifier
 from haystack.classifier.document_classifier.farm import FARMClassifier
 from haystack.classifier.query_classifier.sklearn import SklearnQueryClassifier
 from haystack.classifier.query_classifier.transformers import TransformersQueryClassifier
