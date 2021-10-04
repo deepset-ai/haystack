17cfebe8f2bbf616a90ed3b977ff916c5a840840
Sara Zan <sara.zanzottera@deepset.ai>
2021-10-04 11:21:00 +0200

    WIP Add rest api endpoint to delete documents by filter (#1546)
    * Add rest api endpoint to delete documents by filter.
    
    * Remove parametrization of rest api tests
    
    * Make the paths in rest_api/config.py absolute
    
    * Fix path to pipelines.yaml
    
    * Restructuring test_rest_api.py to be able to test only my endpoint (and to make the suite more structured)
    
    * Convert DELETE /documents into POST /documents/delete_by_filters
    
    Co-authored by:  sarthakj2109 <54064348+sarthakj2109@users.noreply.github.com>

--- rest_api/application.py
+++ rest_api/application.py
@@ -1,17 +1,30 @@
 import logging
 
+from pathlib import Path
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
+from haystack import Pipeline
+from rest_api.config import PIPELINE_YAML_PATH, QUERY_PIPELINE_NAME
 from rest_api.controller.errors.http_error import http_error_handler
-from rest_api.controller.router import router as api_router
 from rest_api.config import ROOT_PATH
+
 
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
 logger = logging.getLogger(__name__)
 logging.getLogger("elasticsearch").setLevel(logging.WARNING)
 logging.getLogger("haystack").setLevel(logging.INFO)
+
+
+PIPELINE = Pipeline.load_from_yaml(Path(PIPELINE_YAML_PATH), pipeline_name=QUERY_PIPELINE_NAME)
+# TODO make this generic for other pipelines with different naming
+RETRIEVER = PIPELINE.get_node(name="Retriever")
+DOCUMENT_STORE = RETRIEVER.document_store if RETRIEVER else None
+logging.info(f"Loaded pipeline nodes: {PIPELINE.graph.nodes.keys()}")
+
+
+from rest_api.controller.router import router as api_router
 
 
 def get_application() -> FastAPI:
@@ -31,6 +44,7 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
 
 logger.info("Open http://127.0.0.1:8000/docs to see Swagger API Documentation.")
 logger.info(
---
+++ rest_api/controller/document.py
@@ -0,0 +1,26 @@
+import logging
+
+from fastapi import APIRouter
+
+from rest_api.application import DOCUMENT_STORE
+from rest_api.config import LOG_LEVEL
+from rest_api.schema import FilterRequest
+
+
+logging.getLogger("haystack").setLevel(LOG_LEVEL)
+logger = logging.getLogger("haystack")
+
+
+router = APIRouter()
+
+
+@router.post("/documents/delete_by_filters", response_model=bool)
+def delete_documents(filters: FilterRequest):
+    """
+    Can be used to delete documents from a document store.
+
+    :param filters: Filters to narrow down the documents to delete.
+                    Example: {"name": ["some", "more"], "category": ["only_one"]}
+    """
+    DOCUMENT_STORE.delete_documents(filters=filters.filters)
+    return True
