25d76f508d77ff62126b84af7e8d51c0fa4436e9
Sara Zan <sara.zanzottera@deepset.ai>
2021-10-11 11:04:11 +0200

    Create EntityExtractor (#1573)
    * Create extractor/entity.py
    
    * Aggregate NER words into entities
    
    * Support indexing
    
    * Add doc strings
    
    * Add utility for printing
    
    * Update signature of run() to match BaseComponent
    
    * Add test
    
    * Modify simplify_ner_for_qa to return the dictionary and add its test
    
    Co-authored-by: brandenchan <brandenchan@icloud.com>

---
+++ haystack/extractor/entity.py
@@ -0,0 +1,75 @@
+from typing import List, Union, Dict, Optional, Tuple
+
+import json
+from haystack import BaseComponent, Document, MultiLabel
+from transformers import AutoTokenizer, AutoModelForTokenClassification, TokenClassificationPipeline
+from transformers import pipeline
+
+
+class EntityExtractor(BaseComponent):
+    """
+    This node is used to extract entities out of documents.
+    The most common use case for this would be as a named entity extractor.
+    The default model used is dslim/bert-base-NER.
+    This node can be placed in a querying pipeline to perform entity extraction on retrieved documents only,
+    or it can be placed in an indexing pipeline so that all documents in the document store have extracted entities.
+    The entities extracted by this Node will populate Document.entities
+    """
+    outgoing_edges = 1
+
+    def __init__(self,
+                 model_name_or_path="dslim/bert-base-NER"):
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        token_classifier = AutoModelForTokenClassification.from_pretrained(model_name_or_path)
+        self.model = pipeline("ner", model=token_classifier, tokenizer=tokenizer, aggregation_strategy="simple")
+
+    def run(self, documents: Optional[Union[List[Document], List[dict]]] = None) -> Tuple[Dict, str]:  # type: ignore
+        """
+        This is the method called when this node is used in a pipeline
+        """
+        if documents:
+            for doc in documents:
+                # In a querying pipeline, doc is a haystack.schema.Document object
+                try:
+                    doc.meta["entities"] = self.extract(doc.text)  # type: ignore
+                # In an indexing pipeline, doc is a dictionary
+                except AttributeError:
+                    doc["meta"]["entities"] = self.extract(doc["text"])  # type: ignore
+        output = {"documents": documents}
+        return output, "output_1"
+
+    def extract(self, text):
+        """
+        This function can be called to perform entity extraction when using the node in isolation.
+        """
+        entities = self.model(text)
+        return entities
+
+
+def simplify_ner_for_qa(output): 
+    """
+    Returns a simplified version of the output dictionary
+    with the following structure:
+    [
+        { 
+            answer: { ... }
+            entities: [ { ... }, {} ]
+        }
+    ]
+    The entities included are only the ones that overlap with
+    the answer itself.
+    """
+    compact_output = []
+    for answer in output["answers"]:
+
+        entities = []
+        for entity in answer["meta"]["entities"]:
+            if entity["start"] >= answer["offset_start_in_doc"] and entity["end"] <= answer["offset_end_in_doc"]:
+                entities.append(entity["word"])  
+
+        compact_output.append({
+            "answer": answer["answer"],
+            "entities": entities
+        })
+    return compact_output
