5318df4860799a0bb88317abea1abab46499f446
Malte Pietsch <malte.pietsch@deepset.ai>
2021-10-11 18:32:57 +0200

    keep meta data of answers in reader.run()

--- haystack/reader/base.py
+++ haystack/reader/base.py
@@ -64,10 +64,15 @@ class BaseReader(BaseComponent):
 
         # Add corresponding document_name and more meta data, if an answer contains the document_id
         for ans in results["answers"]:
-            ans.meta = {}
+            # get meta from doc
+            meta_from_doc = {}
             for doc in documents:
                 if doc.id == ans.document_id:
-                    ans.meta = deepcopy(doc.meta)
+                    meta_from_doc = deepcopy(doc.meta)
+                    break
+            # append to "own" meta
+            if ans.meta:
+                ans.meta.update(meta_from_doc)
 
         return results, "output_1"
 
