91d084c9dfd832257c9dec26241e47eb2f2755c3
ZanSara <sarazanzo94@gmail.com>
2021-10-19 10:50:43 +0200

    Implement deletion by ID for weaviate

--- haystack/document_store/weaviate.py
+++ haystack/document_store/weaviate.py
@@ -689,18 +689,25 @@ class WeaviateDocumentStore(BaseDocumentStore):
 
         :param index: Index name to delete the document from. If None, the
                       DocumentStore's default index (self.index) will be used.
+        :param ids: Optional list of IDs to narrow down the documents to be deleted.
         :param filters: Optional filters to narrow down the documents to be deleted.
-                        Example filters: {"name": ["some", "more"], "category": ["only_one"]}
+            Example filters: {"name": ["some", "more"], "category": ["only_one"]}.
+            If filters are provided along with a list of IDs, this method deletes the
+            intersection of the two query results (documents that match the filters and
+            have their ID in the list).
         :return: None
         """
         index = index or self.index
-        if filters:
-            docs_to_delete = self.get_all_documents(index, filters=filters)
-            for doc in docs_to_delete:
-                self.weaviate_client.data_object.delete(doc.id)
-        else:
+        if not filters and not ids:
             self.weaviate_client.schema.delete_class(index)
             self._create_schema_and_index_if_not_exist(index)
+        else:
+            docs_to_delete = self.get_all_documents(index, filters=filters)
+            if ids:
+                docs_to_delete = [doc for doc in docs_to_delete if doc.id in ids]
+            for doc in docs_to_delete:
+                self.weaviate_client.data_object.delete(doc.id)
+            
 
 
 
