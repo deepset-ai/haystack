0147932fc3eaa51ed886353b81f605e9db5c12cc
ZanSara <sarazanzo94@gmail.com>
2021-10-20 18:36:00 +0200

    Create a file_classifier module

--- haystack/nodes/__init__.py
+++ haystack/nodes/__init__.py
@@ -5,6 +5,7 @@ from haystack.nodes.connector import Crawler
 from haystack.nodes.document_classifier import BaseDocumentClassifier, TransformersDocumentClassifier
 from haystack.nodes.evaluator import EvalDocuments, EvalAnswers
 from haystack.nodes.extractor import EntityExtractor, simplify_ner_for_qa
+from haystack.nodes.file_classifier.file_type import FileTypeClassifier
 from haystack.nodes.file_converter import (
     BaseConverter,
     DocxToTextConverter,
@@ -16,7 +17,7 @@ from haystack.nodes.file_converter import (
     TikaXHTMLParser,
     TextConverter,
 )
-from haystack.nodes.other import Docs2Answers, FileTypeClassifier, JoinDocuments
+from haystack.nodes.other import Docs2Answers, JoinDocuments
 from haystack.nodes.preprocessor import BasePreProcessor, PreProcessor
 from haystack.nodes.query_classifier import SklearnQueryClassifier, TransformersQueryClassifier
 from haystack.nodes.question_generator import QuestionGenerator
