3c42b6f298618b503183912aab33dd387367bcc1
ZanSara <sarazanzo94@gmail.com>
2021-10-22 13:49:39 +0200

    Add set_config to EntityExtractor

--- haystack/extractor/entity.py
+++ haystack/extractor/entity.py
@@ -19,7 +19,8 @@ class EntityExtractor(BaseComponent):
 
     def __init__(self,
                  model_name_or_path="dslim/bert-base-NER"):
-        
+
+        self.set_config(model_name_or_path=model_name_or_path)
         tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         token_classifier = AutoModelForTokenClassification.from_pretrained(model_name_or_path)
         self.model = pipeline("ner", model=token_classifier, tokenizer=tokenizer, aggregation_strategy="simple")
