6bb41d22dfd3c4c3b98ed67a725cfe0f27e2c91e
Lalit Pagaria <pagaria.lalit@gmail.com>
2021-10-28 17:31:33 +0530

    Addressing review comments

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,7 +4,6 @@ from haystack.preprocessor.utils import convert_files_to_dicts, tika_convert_fil
 from haystack.preprocessor.cleaning import clean_wiki_text
 
 
-@pytest.mark.tika
 def test_convert_files_to_dicts(xpdf_fixture):
     documents = convert_files_to_dicts(dir_path="samples", clean_func=clean_wiki_text, split_paragraphs=True)
     assert documents and len(documents) > 0
