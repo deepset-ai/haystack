92b1447881cbeb2e304c7943b1315a4af2d70443
Lalit Pagaria <pagaria.lalit@gmail.com>
2021-10-28 20:47:35 +0530

    removing tika fixure

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from haystack.utils.preprocessing import convert_files_to_dicts
+from haystack.utils.preprocessing import convert_files_to_dicts, tika_convert_files_to_dicts
 from haystack.utils.cleaning import clean_wiki_text
 
 
@@ -10,11 +10,7 @@ def test_convert_files_to_dicts(xpdf_fixture):
 
 
 @pytest.mark.tika
-def test_tika_convert_files_to_dicts(tika_fixture):
-    try:
-        from haystack.utils.preprocessing import tika_convert_files_to_dicts
-    except Exception as ex:
-        raise ex
+def test_tika_convert_files_to_dicts():
     documents = tika_convert_files_to_dicts(dir_path="samples", clean_func=clean_wiki_text, split_paragraphs=True)
     assert documents and len(documents) > 0
 
