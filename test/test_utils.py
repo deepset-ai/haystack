14e142e7e9f21163c913e4d2bf5dc8156061cba5
Lalit Pagaria <pagaria.lalit@gmail.com>
2021-10-28 18:04:54 +0530

    Tika init is being called when importing tika function

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from haystack.preprocessor.utils import convert_files_to_dicts, tika_convert_files_to_dicts
+from haystack.preprocessor.utils import convert_files_to_dicts
 from haystack.preprocessor.cleaning import clean_wiki_text
 
 
@@ -11,6 +11,10 @@ def test_convert_files_to_dicts(xpdf_fixture):
 
 @pytest.mark.tika
 def test_tika_convert_files_to_dicts(tika_fixture):
+    try:
+        from haystack.preprocessor.utils import tika_convert_files_to_dicts
+    except Exception as ex:
+        raise ex
     documents = tika_convert_files_to_dicts(dir_path="samples", clean_func=clean_wiki_text, split_paragraphs=True)
     assert documents and len(documents) > 0
 
