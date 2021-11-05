4d4365c023089e94426e510645f442537fcffe34
Thomas Stadelmann <thomas.stadelmann@deepset.ai>
2021-11-05 16:36:36 +0100

    test for batch_size use added

--- test/test_document_classifier.py
+++ test/test_document_classifier.py
@@ -46,3 +46,25 @@ def test_zero_shot_document_classifier(zero_shot_document_classifier):
     expected_labels = ["positive", "negative"]
     for i, doc in enumerate(results):
         assert doc.to_dict()["meta"]["classification"]["label"] == expected_labels[i]
+
+
+@pytest.mark.slow
+def test_document_classifier_batch_size(document_classifier):
+    assert isinstance(document_classifier, BaseDocumentClassifier)
+
+    docs = [
+        Document(
+            content="""That's good. I like it."""*700,  # extra long text to check truncation
+            meta={"name": "0"},
+            id="1",
+        ),
+        Document(
+            content="""That's bad. I don't like it.""",
+            meta={"name": "1"},
+            id="2",
+        ),
+    ]
+    results = document_classifier.predict(documents=docs, batch_size=16)
+    expected_labels = ["joy", "sadness"]
+    for i, doc in enumerate(results):
+        assert doc.to_dict()["meta"]["classification"]["label"] == expected_labels[i]
