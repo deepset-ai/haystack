7b9e7835cd52c061c3f614022a93e08dd54627d1
oryx1729 <oryx1729@protonmail.com>
2021-11-08 17:10:34 +0100

    Remove package_data

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ setup(
     url="https://github.com/deepset-ai/haystack",
     download_url=f"https://github.com/deepset-ai/haystack/archive/{_version}.tar.gz",
     packages=find_packages(exclude=["*.rest_api", "*.rest_api.*", "rest_api.*", "rest_api"]),
-    include_package_data=True,
     dependency_links=dependency_links,
     install_requires=parsed_requirements,
     python_requires=">=3.7.0",
