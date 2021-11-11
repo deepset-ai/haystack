3e0caa8145ac00d1a459b75decf5fb732ac5f825
ZanSara <sarazanzo94@gmail.com>
2021-11-11 18:21:21 +0100

    remove typing, mypy can't tolerate that

--- haystack/utils/export_utils.py
+++ haystack/utils/export_utils.py
@@ -36,7 +36,7 @@ def print_answers(results: dict, details: str = "all", max_text_len: Optional[in
     if "query" in results.keys():
         print(f"\nQuery: {results['query']}\nAnswers:")
 
-    answers: List[Answer] = results["answers"]
+    answers: List[Answer] = results["answers"]  # type:ignore
     pp = pprint.PrettyPrinter(indent=4)
 
     # Filter the results by detail level
