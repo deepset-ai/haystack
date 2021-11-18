7258a6f045e2652dba3537a97c25f7594ccf4a9d
cvgoudar <cvgoudar@users.noreply.github.com>
2021-11-18 18:51:14 +0530

    Explicitly check if embeddings are present if no results are returned by EmbeddingRetriever for Elasticsearch

--- haystack/document_stores/elasticsearch.py
+++ haystack/document_stores/elasticsearch.py
@@ -825,9 +825,28 @@ class ElasticsearchDocumentStore(BaseDocumentStore):
             logger.debug(f"Retriever query: {body}")
             try:
                 result = self.client.search(index=index, body=body, request_timeout=300)["hits"]["hits"]
-                if len(result) <= 0:
-                    raise RequestError(400, "search_phase_execution_exception",
-                                       {"error": "No documents with embeddings"})
+                if len(result) == 0:
+                    body = {
+                        "query": {
+                            "bool": {
+                                "filter": {
+                                    "bool": {
+                                        "must": [
+                                            {
+                                                "exists": {
+                                                    "field": self.embedding_field
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    count_embeddings = self.client.count(index=index, body=body)["count"]
+                    if count_embeddings == 0:
+                        raise RequestError(400, "search_phase_execution_exception",
+                                           {"error": "No documents with embeddings"})
             except RequestError as e:
                 if e.error == "search_phase_execution_exception":
                     error_message: str = "search_phase_execution_exception: Likely some of your stored documents don't have embeddings." \
