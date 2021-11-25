57964dcf817b02b5627e868d9de17a8169de6959
Michel Bartels <login@michelbartels.com>
2021-11-25 17:45:46 +0100

    improved model distillation benchmark config

--- test/benchmarks/model_distillation.py
+++ test/benchmarks/model_distillation.py
@@ -3,7 +3,7 @@ import json
 import requests
 from pathlib import Path
 
-from typing import Union
+from typing import Union, List
 import logging
 
 logger = logging.getLogger(__name__)
@@ -24,6 +24,25 @@ download_links = {
 def load_config(path: str) -> dict:
     with open(path, "r") as f:
         return json.load(f)
+
+def combine_config(configs: dict) -> List[dict]:
+    combinations_list = [[]]
+    for config_key, config in configs.items():
+        if not isinstance(config, list):
+            config = [config]
+        current_combinations = combinations_list
+        combinations_list = []
+        for item in config:
+            combinations_list += [c + [(config_key, item)] for c in current_combinations]
+    
+    combinations = []
+    for combination in combinations_list:
+        combinations.append(dict(combination))
+    
+    return combinations
+        
+        
+        
 
 def download_file(url: str, path: Path):
     request = requests.get(url, allow_redirects=True)
@@ -50,72 +69,66 @@ def download_dataset(dataset: Union[dict, str], download_folder: Path):
 def eval(model: FARMReader, download_folder: Path, test_file: str):
     return model.eval_on_file(data_dir=download_folder, test_filename=test_file)
 
-def train_student(model_name: str, download_folder: Path, train_file: str, test_file: str, epochs: int, batch_size: int) -> dict:
+def train_student(student: dict, download_folder: Path, train_file: str, test_file: str, **kwargs) -> dict:
     # loading student model
-    model = FARMReader(model_name_or_path=model_name)
+    model = FARMReader(model_name_or_path=student["model_name_or_path"])
     # training student model
-    model.train(data_dir=download_folder, train_filename=train_file, n_epochs=epochs, batch_size=batch_size, caching=True, max_seq_len=512, learning_rate=3e-5)
+    model.train(data_dir=download_folder, train_filename=train_file, batch_size=student["batch_size"], caching=True, **kwargs)
     return eval(model, download_folder, test_file)
 
-def train_student_with_distillation(student_name: str, teacher_name: str, download_folder: Path, train_file: str, test_file: str,
-epochs: int, student_batch_size: int, teacher_batch_size: int, distillation_loss: str, distillation_loss_weight: float,
-temperature: float) -> dict:
+def train_student_with_distillation(student: dict, teacher: dict, download_folder: Path, train_file: str, test_file: str, **kwargs) -> dict:
     # loading student and teacher models
-    student = FARMReader(model_name_or_path=student_name)
-    teacher = FARMReader(model_name_or_path=teacher_name)
+    student_model = FARMReader(model_name_or_path=student["model_name_or_path"])
+    teacher_model = FARMReader(model_name_or_path=teacher["model_name_or_path"])
     # distilling
-    student.distil_from(teacher, data_dir=download_folder, train_filename=train_file, n_epochs=epochs, caching=True,
-    student_batch_size=student_batch_size, teacher_batch_size=teacher_batch_size, distillation_loss=distillation_loss,
-    distillation_loss_weight=distillation_loss_weight, temperature=temperature, max_seq_len=512, learning_rate=3e-5)
-    return eval(student, download_folder, test_file)
+    student_model.distil_from(teacher_model, data_dir=download_folder, train_filename=train_file, student_batch_size=student["batch_size"], teacher_batch_size=teacher["batch_size"], caching=True,
+     **kwargs)
+    return eval(student_model, download_folder, test_file)
 
 def main():
     # loading config
     parent = Path(__file__).parent.resolve()
-    config = load_config(parent/"distillation_config.json")
+    config = load_config(parent/"distillation_config copy.json")
     download_folder = parent/config["download_folder"]
     student = config["student_model"]
     teacher = config["teacher_model"]
 
-    temperatures = config["temperature"]
-    distillation_loss_weights = config["distillation_loss_weight"]
+    distillation_settings = config["distillation_settings"]
+    training_settings = config["training_settings"]
 
-    if not isinstance(temperatures, list):
-        temperatures = [temperatures]
-
-    if not isinstance(distillation_loss_weights, list):
-        distillation_loss_weights = [distillation_loss_weights]
     
     # loading dataset
     logger.info("Downloading dataset")
     train_file, test_file = download_dataset(config["dataset"], download_folder)
 
-    results_student_with_distillation = []
-    for temperature in temperatures:
-        for distillation_loss_weight in distillation_loss_weights:
-            # distillation training
-            logger.info(f"Training student with distillation (temperature: {temperature} distillation loss weight: {distillation_loss_weight}")
-            results_student_with_distillation.append((temperature, distillation_loss_weight, train_student_with_distillation(student["model_name_or_path"], teacher["model_name_or_path"], download_folder,
-            train_file, test_file, config["epochs"], student["batch_size"], teacher["batch_size"], config["distillation_loss"], distillation_loss_weight,
-            temperature)))
+    results = []
+    descriptions = []
+
+    for current_config in combine_config(distillation_settings):
+        descriptions.append(f"Results of student with distillation (config: {current_config}")
+        # distillation training
+        logger.info(f"Training student with distillation (config: {current_config}")
+        results.append(train_student_with_distillation(student, teacher, download_folder, train_file, test_file, **current_config, **training_settings))
 
     # baseline
-    logger.info("Training student without distillation as a baseline")
-    results_student = train_student(student["model_name_or_path"], download_folder, train_file, test_file, config["epochs"], student["batch_size"])
+    if config["evaluate_student_without_distillation"]:
+        logger.info("Training student without distillation as a baseline")
+        descriptions.append("Results of student without distillation")
+        results.append(train_student(student["model_name_or_path"], download_folder, train_file, test_file, **training_settings))
 
-    # evaluating teacher as upper bound for performance
-    logger.info("Evaluating teacher")
-    results_teacher = eval(FARMReader(model_name_or_path=teacher["model_name_or_path"]), download_folder, test_file)
+    if config["evaluate_teacher"]:
+        # evaluating teacher as upper bound for performance
+        logger.info("Evaluating teacher")
+        descriptions.append("Results of teacher")
+        results.append(eval(FARMReader(model_name_or_path=teacher["model_name_or_path"]), download_folder, test_file))
 
     # printing evaluation results
     logger.info("Evaluation results:")
-    descriptions = ["Results of teacher", "Results of student without distillation (baseline)"] \
-         + [f"Results of student with distillation (temperature: {temperature} distillation loss weight: {distillation_loss_weight}" for temperature, distillation_loss_weight, _ in results_student_with_distillation]
-    for evaluation, description in zip([results_teacher, results_student] + [res for _, _, res in results_student_with_distillation], descriptions):
+    for result, description in zip(results, descriptions):
         logger.info(description)
-        logger.info(f"EM: {evaluation['EM']}")
-        logger.info(f"F1: {evaluation['f1']}")
-        logger.info(f"Top n accuracy: {evaluation['top_n_accuracy']}")
+        logger.info(f"EM: {result['EM']}")
+        logger.info(f"F1: {result['f1']}")
+        logger.info(f"Top n accuracy: {result['top_n_accuracy']}")
 
 
 
