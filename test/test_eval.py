92bdd5465a1640f6e34e11b8a6ab4418acee57b9
Thomas Stadelmann <thomas.stadelmann@deepset.ai>
2021-12-02 18:41:22 +0100

    use standard summarizer in test_eval again

--- test/test_eval.py
+++ test/test_eval.py
@@ -816,19 +816,10 @@ def test_generativeqa_calculate_metrics(document_store_with_docs: InMemoryDocume
 
 
 @pytest.mark.parametrize("document_store_with_docs", ["memory"], indirect=True)
-def test_summarizer_calculate_metrics(document_store_with_docs: ElasticsearchDocumentStore):
-    summarizer = TransformersSummarizer(
-        model_name_or_path="sshleifer/distill-pegasus-xsum-16-4",
-        use_gpu=False
-    )
-    document_store_with_docs.embedding_dim = 384
-    retriever = EmbeddingRetriever(
-            document_store=document_store_with_docs,
-            embedding_model="sentence-transformers/all-MiniLM-L6-v2",
-            use_gpu=False
-        )
-    document_store_with_docs.update_embeddings(retriever=retriever)
-    pipeline = SearchSummarizationPipeline(retriever=retriever, summarizer=summarizer, return_in_answer_format=True)
+@pytest.mark.parametrize("retriever_with_docs", ["embedding"], indirect=True)
+def test_summarizer_calculate_metrics(document_store_with_docs: ElasticsearchDocumentStore, summarizer, retriever_with_docs):
+    document_store_with_docs.update_embeddings(retriever=retriever_with_docs)
+    pipeline = SearchSummarizationPipeline(retriever=retriever_with_docs, summarizer=summarizer, return_in_answer_format=True)
     eval_result: EvaluationResult = pipeline.eval(
         labels=EVAL_LABELS,
         params={"Retriever": {"top_k": 5}}
@@ -845,8 +836,8 @@ def test_summarizer_calculate_metrics(document_store_with_docs: ElasticsearchDoc
     assert metrics["Retriever"]["recall_multi_hit"] == 0.5
     assert metrics["Retriever"]["recall_single_hit"] == 0.5
     assert metrics["Retriever"]["precision"] == 1.0/6
-    assert metrics["Summarizer"]["mrr"] == 0.0
-    assert metrics["Summarizer"]["map"] == 0.0
-    assert metrics["Summarizer"]["recall_multi_hit"] == 0.0
-    assert metrics["Summarizer"]["recall_single_hit"] == 0.0
-    assert metrics["Summarizer"]["precision"] == 0.0
+    assert metrics["Summarizer"]["mrr"] == 0.5
+    assert metrics["Summarizer"]["map"] == 0.5
+    assert metrics["Summarizer"]["recall_multi_hit"] == 0.5
+    assert metrics["Summarizer"]["recall_single_hit"] == 0.5
+    assert metrics["Summarizer"]["precision"] == 1.0/6
