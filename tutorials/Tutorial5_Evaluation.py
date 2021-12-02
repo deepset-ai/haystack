da78702e8d511ce6b96847508f8e35a0e0803677
Julian Risch <julian.risch@deepset.ai>
2021-12-02 14:54:09 +0100

    Incorporate review feedback on text comments

--- tutorials/Tutorial5_Evaluation.py
+++ tutorials/Tutorial5_Evaluation.py
@@ -28,7 +28,7 @@ def tutorial5_evaluation():
     launch_es()
     devices, n_gpu = initialize_device_settings(use_cuda=True)
 
-    # Download evaluation data, which is a subset of Natural Questions development set containing 50 documents
+    # Download evaluation data, which is a subset of Natural Questions development set containing 50 documents with one question per document and multiple annotated answers
     doc_dir = "../data/nq"
     s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/nq_dev_subset_v2.json.zip"
     fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
@@ -53,6 +53,10 @@ def tutorial5_evaluation():
     )
     document_store.delete_documents(index=doc_index)
     document_store.delete_documents(index=label_index)
+
+    # The add_eval_data() method converts the given dataset in json format into Haystack document and label objects.
+    # Those objects are then indexed in their respective document and label index in the document store.
+    # The method can be used with any dataset in SQuAD format.
     document_store.add_eval_data(
         filename="../data/nq/nq_dev_subset_v2.json",
         doc_index=doc_index,
@@ -64,13 +68,13 @@ def tutorial5_evaluation():
     # Initialize Retriever
     retriever = ElasticsearchRetriever(document_store=document_store)
 
-    # Alternative: Evaluate dense Retrievers (DPR and SentenceTransformers)
-    # Dense Passage Retrieval uses a separate transformer based encoder for query and document each
-    # SentenceTransformers have a single encoder for both
-    # Please make sure the "embedding_dim" parameter in the DocumentStore above matches the output dimension of you model
+    # Alternative: Evaluate dense retrievers (DensePassageRetriever or EmbeddingRetriever)
+    # DensePassageRetriever uses two separate transformer based encoders for query and document.
+    # In contrast, EmbeddingRetriever uses a single encoder for both.
+    # Please make sure the "embedding_dim" parameter in the DocumentStore above matches the output dimension of your models!
     # Please also take care that the PreProcessor splits your files into chunks that can be completely converted with
     #        the max_seq_len limitations of Transformers
-    # The SentenceTransformer model "all-mpnet-base-v2" generelly works well on any kind of english text.
+    # The SentenceTransformer model "all-mpnet-base-v2" generally works well with the EmbeddingRetriever on any kind of English text.
     # For more information check out the documentation at: https://www.sbert.net/docs/pretrained_models.html
     # retriever = DensePassageRetriever(document_store=document_store,
     #                                   query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
@@ -90,6 +94,12 @@ def tutorial5_evaluation():
     )
 
     # Define a pipeline consisting of the initialized retriever and reader
+    # Here we evaluate retriever and reader in open domain fashion on the full corpus of documents i.e. a document is considered
+    # correctly retrieved if it contains the gold answer string within it. The reader is evaluated based purely on the
+    # predicted answer string, regardless of which document this came from and the position of the extracted span.
+    # The generation of predictions is seperated from the calculation of metrics.
+    # This allows you to run the computation-heavy model predictions only once and then iterate flexibly on the metrics or reports you want to generate.
+
     pipeline = ExtractiveQAPipeline(reader=reader, retriever=retriever)
 
     # The evaluation also works with any other pipeline.
@@ -103,9 +113,15 @@ def tutorial5_evaluation():
     # Alternative: Define queries and labels directly
     # eval_queries = ["who is written in the book of life"]
     # eval_labels = [
-    #        MultiLabel(labels=[Label(query="who is written in the book of life", answer=Answer(answer="every person who is destined for Heaven or the World to Come", offsets_in_context=[Span(374, 434)]),
-    #            document=Document(id='1b090aec7dbd1af6739c4c80f8995877-0', content_type="text", content='Book of Life - wikipedia Book of Life Jump to: navigation, search This article is about the book mentioned in Christian and Jewish religious teachings. For other uses, see The Book of Life. In Christianity and Judaism, the Book of Life (Hebrew: ספר החיים, transliterated Sefer HaChaim; Greek: βιβλίον τῆς ζωῆς Biblíon tēs Zōēs) is the book in which God records the names of every person who is destined for Heaven or the World to Come. According to the Talmud it is open on Rosh Hashanah, as is its analog for the wicked, the Book of the Dead. For this reason extra mention is made for the Book of Life during Amidah recitations during the Days of Awe, the ten days between Rosh Hashanah, the Jewish new year, and Yom Kippur, the day of atonement (the two High Holidays, particularly in the prayer Unetaneh Tokef). Contents (hide) 1 In the Hebrew Bible 2 Book of Jubilees 3 References in the New Testament 4 The eschatological or annual roll-call 5 Fundraising 6 See also 7 Notes 8 References In the Hebrew Bible(edit) In the Hebrew Bible the Book of Life - the book or muster-roll of God - records forever all people considered righteous before God'),
-    #            is_correct_answer=True, is_correct_document=True, origin="gold-label")])
+    #        MultiLabel(labels=[Label(query="who is written in the book of life",
+    #        answer=Answer(answer="every person who is destined for Heaven or the World to Come",
+    #        offsets_in_context=[Span(374, 434)]),
+    #        document=Document(id='1b090aec7dbd1af6739c4c80f8995877-0',
+    #        content_type="text",
+    #        content='Book of Life - wikipedia Book of Life Jump to: navigation, search This article is about the book mentioned in Christian and Jewish religious teachings. For other uses, see The Book of Life. In Christianity and Judaism, the Book of Life (Hebrew: ספר החיים, transliterated Sefer HaChaim; Greek: βιβλίον τῆς ζωῆς Biblíon tēs Zōēs) is the book in which God records the names of every person who is destined for Heaven or the World to Come. According to the Talmud it is open on Rosh Hashanah, as is its analog for the wicked, the Book of the Dead. For this reason extra mention is made for the Book of Life during Amidah recitations during the Days of Awe, the ten days between Rosh Hashanah, the Jewish new year, and Yom Kippur, the day of atonement (the two High Holidays, particularly in the prayer Unetaneh Tokef). Contents (hide) 1 In the Hebrew Bible 2 Book of Jubilees 3 References in the New Testament 4 The eschatological or annual roll-call 5 Fundraising 6 See also 7 Notes 8 References In the Hebrew Bible(edit) In the Hebrew Bible the Book of Life - the book or muster-roll of God - records forever all people considered righteous before God'),
+    #        is_correct_answer=True,
+    #        is_correct_document=True,
+    #        origin="gold-label")])
     #    ]
 
     # Similar to pipeline.run() we can execute pipeline.eval()
