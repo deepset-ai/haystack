2941818fc8911db0e30dbd6fd646ae9570d20c94
ZanSara <sarazanzo94@gmail.com>
2022-01-13 14:55:41 +0100

    Add composite extras_require

--- setup.py
+++ setup.py
@@ -1,24 +1,2 @@
-import os
 from setuptools import setup
-
-
-def get_version() -> str:
-    """
-    Read the version from haystack/_version.py without importing haystack.
-    """
-    path = os.path.join(os.path.dirname(__file__), "haystack/_version.py")
-    path = os.path.normpath(os.path.abspath(path))
-    with open(path) as f:
-        for line in f:
-            if line.startswith("__version__"):
-                _, version = line.split(" = ", 1)
-                version = version.replace("\"", "").strip()
-                return version
-    raise RuntimeError("Unable to find version string in {}.".format(path))
-
-
-if __name__ == '__main__':
-    setup(
-        name="haystack",
-        version=get_version()
-    )
+setup()
