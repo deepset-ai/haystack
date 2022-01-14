32f9c0c8fedfbf901e964112a2098b9f99d9a508
ZanSara <sarazanzo94@gmail.com>
2022-01-14 17:05:34 +0100

    Fix package name

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -7,7 +7,7 @@ except ModuleNotFoundError:
     # Python <= 3.7
     import importlib_metadata as metadata  # type: ignore
 
-__version__ = metadata.version('haystack')
+__version__ = metadata.version('farm-haystack')
 
 # This configuration must be done before any import to apply to all submodules
 import logging
