4c5acab63b9ce296e1545219387fb03fdd60b740
Thomas Stadelmann <thomas.stadelmann@deepset.ai>
2022-01-20 10:51:08 +0100

    don't use abbreviation DC in comments and errors

--- haystack/document_stores/dc.py
+++ haystack/document_stores/dc.py
@@ -33,22 +33,22 @@ class DCDocumentStore(KeywordDocumentStore):
         similarity: str = "dot_product",
         return_embedding: bool = False):
         """
-        A DocumentStore facade enabling you to interact with the documents stored in DC.
+        A DocumentStore facade enabling you to interact with the documents stored in Deepset Cloud.
         Thus you can run experiments like trying new nodes, pipelines, etc. without having to index your data again.
         
         DCDocumentStore is not intended to be used in production-like scenarios.
 
         :param api_key: Secret value of the API key. 
                         If not specified, will be read from DEEPSET_CLOUD_API_KEY environment variable.
-        :param workspace: workspace in DC
-        :param index: index to access within the DC workspace
+        :param workspace: workspace in Deepset Cloud
+        :param index: index to access within the Deepset Cloud workspace
         :param duplicate_documents: Handle duplicates document based on parameter options.
                                     Parameter options : ( 'skip','overwrite','fail')
                                     skip: Ignore the duplicates documents
                                     overwrite: Update any existing documents with the same ID when adding documents.
                                     fail: an error is raised if the document ID of the document being added already
                                     exists.
-        :param api_endpoint: The URL of the DC API. 
+        :param api_endpoint: The URL of the Deepset Cloud API. 
                              If not specified, will be read from DEEPSET_CLOUD_API_ENDPOINT environment variable.
         :param similarity: The similarity function used to compare document vectors. 'dot_product' is the default since it is
                            more performant with DPR embeddings. 'cosine' is recommended if you are using a Sentence BERT model.
@@ -75,7 +75,7 @@ class DCDocumentStore(KeywordDocumentStore):
         init_url = self._get_index_endpoint()       
         response = requests.get(init_url, auth=BearerAuth(self.api_key))
         if response.status_code != 200:
-            raise Exception(f"Could not connect to DC: HTTP {response.status_code} - {response.reason}\n{response.content.decode()}")
+            raise Exception(f"Could not connect to Deepset Cloud: HTTP {response.status_code} - {response.reason}\n{response.content.decode()}")
         
         index_info = response.json()
         indexing_info = index_info["indexing"]
@@ -106,7 +106,7 @@ class DCDocumentStore(KeywordDocumentStore):
         :param batch_size: Number of documents that are passed to bulk function at a time.
         :param headers: Custom HTTP headers to pass to document store client if supported (e.g. {'Authorization': 'Basic YWRtaW46cm9vdA=='} for basic authentication)
         """
-        logging.warning("`get_all_documents()` can get very slow and resource-heavy since all documents must be loaded from DC. "
+        logging.warning("`get_all_documents()` can get very slow and resource-heavy since all documents must be loaded from Deepset Cloud. "
                         "Consider using `get_all_documents_generator()` instead.")
         return list(self.get_all_documents_generator(index=index, filters=filters, 
                                 return_embedding=return_embedding, batch_size=batch_size, headers=headers))
@@ -174,7 +174,7 @@ class DCDocumentStore(KeywordDocumentStore):
             doc_dict = response.json()
             doc = Document.from_dict(doc_dict)
         else:
-            logger.warning(f"Document {id} could not be fetched from DC: HTTP {response.status_code} - {response.reason}\n{response.content.decode()}")
+            logger.warning(f"Document {id} could not be fetched from Deepset Cloud: HTTP {response.status_code} - {response.reason}\n{response.content.decode()}")
         
         return doc
 
