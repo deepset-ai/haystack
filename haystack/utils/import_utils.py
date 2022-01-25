0e41024c44aea2cf5ec988ce314cbfd29344bc50
ZanSara <sarazanzo94@gmail.com>
2022-01-25 17:42:34 +0100

    Include tstadel review and re-introduce Milvus1 in the tests suite, to fix

--- haystack/utils/import_utils.py
+++ haystack/utils/import_utils.py
@@ -12,25 +12,25 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 
-def safe_import(import_path, classname, dep_group):
+def safe_import(import_path: str, classname: str, dep_group: str):
     """
-    Method that allows the import of "non-existing" document stores.
-    Doc stores can be installed one by one with extras_require (see setup.cfg)
-    but they need to be all imported in haystack.document_stores.__init__()
+    Method that allows the import of nodes that depend on missing dependencies.
+    These nodes can be installed one by one with extras_require (see setup.cfg)
+    but they need to be all imported in their respective package's __init__()
     
     Therefore, in case of an ImportError, the class to import is replaced by
     a hollow MissingDependency function, which will throw an error when
     inizialized.
     """
     try:
-        module = importlib.import_module(f"{import_path}")
+        module = importlib.import_module(import_path)
         classs = vars(module).get(classname)
     except ImportError as ie:
         classs = _missing_dependency_stub_factory(classname, dep_group, ie)
     return classs
 
 
-def _missing_dependency_stub_factory(classname, dep_group, import_error):
+def _missing_dependency_stub_factory(classname: str, dep_group: str, import_error: Exception):
     """
     Create custom versions of MissingDependency using the given parameters.
     See `safe_import()`
@@ -46,7 +46,7 @@ def _missing_dependency_stub_factory(classname, dep_group, import_error):
     return MissingDependency
 
 
-def _optional_component_not_installed(component, dep_group, source_error): 
+def _optional_component_not_installed(component: str, dep_group: str, source_error: Exception): 
     raise ImportError (f"Failed to import '{component}', " \
            "which is an optional component in Haystack.\n" \
            f"Run 'pip install farm-haystack[{dep_group}]' " \
