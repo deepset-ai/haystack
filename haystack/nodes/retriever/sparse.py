8a32d8da92e4548e308bc971910e94bedb320029
tstadel <60758086+tstadel@users.noreply.github.com>
2022-01-25 20:36:28 +0100

    Introduce readonly DCDocumentStore (without labels support) (#1991)
    * minimal DCDocumentStore
    
    * support filters
    
    * implement get_documents_by_id
    
    * handle not existing documents
    
    * add docstrings
    
    * auth added
    
    * add tests
    
    * generate docs
    
    * Add latest docstring and tutorial changes
    
    * add responses to dev dependencies
    
    * fix tests
    
    * support query() and quey_by_embedding()
    
    * Add latest docstring and tutorial changes
    
    * query tests added
    
    * read api_key and api_endpoint from env
    
    * Add latest docstring and tutorial changes
    
    * support query() and quey_by_embedding()
    
    * query tests added
    
    * Add latest docstring and tutorial changes
    
    * Add latest docstring and tutorial changes
    
    * support dynamic similarity and return_embedding values
    
    * Add latest docstring and tutorial changes
    
    * adjust KeywordDocumentStore description
    
    * refactoring
    
    * Add latest docstring and tutorial changes
    
    * implement get_document_count and raise on all not implemented methods
    
    * Add latest docstring and tutorial changes
    
    * don't use abbreviation DC in comments and errors
    
    * Add latest docstring and tutorial changes
    
    * docstring added to KeywordDocumentStore
    
    * Add latest docstring and tutorial changes
    
    * enhanced api key set
    
    * split tests into two parts
    
    * change setup.py in order to work around build cache
    
    * added link
    
    * Add latest docstring and tutorial changes
    
    * rename DCDocumentStore to DeepsetCloudDocumentStore
    
    * Add latest docstring and tutorial changes
    
    * remove dc.py
    
    * reinsert link to docs
    
    * fix imports
    
    * Add latest docstring and tutorial changes
    
    * better test structure
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ArzelaAscoIi <kristof.herrmann@rwth-aachen.de>

--- haystack/nodes/retriever/sparse.py
+++ haystack/nodes/retriever/sparse.py
@@ -6,7 +6,7 @@ from collections import OrderedDict, namedtuple
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from haystack.schema import Document
-from haystack.document_stores import BaseDocumentStore, ElasticsearchDocumentStore
+from haystack.document_stores import BaseDocumentStore, KeywordDocumentStore
 from haystack.nodes.retriever import BaseRetriever
 
 
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 class ElasticsearchRetriever(BaseRetriever):
-    def __init__(self, document_store: ElasticsearchDocumentStore, top_k: int = 10, custom_query: str = None):
+    def __init__(self, document_store: KeywordDocumentStore, top_k: int = 10, custom_query: str = None):
         """
         :param document_store: an instance of a DocumentStore to retrieve documents from.
         :param custom_query: query string as per Elasticsearch DSL with a mandatory query placeholder(query).
@@ -54,7 +54,7 @@ class ElasticsearchRetriever(BaseRetriever):
         # save init parameters to enable export of component config as YAML
         self.set_config(document_store=document_store, top_k=top_k, custom_query=custom_query)
 
-        self.document_store: ElasticsearchDocumentStore = document_store
+        self.document_store: KeywordDocumentStore = document_store
         self.top_k = top_k
         self.custom_query = custom_query
 
