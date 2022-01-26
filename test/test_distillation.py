cbb4581d894b198a13cea46b91161904a89a3664
ZanSara <sarazanzo94@gmail.com>
2022-01-26 17:01:03 +0100

    Fix some more paths

--- test/test_distillation.py
+++ test/test_distillation.py
@@ -77,8 +77,8 @@ def test_intermediate_layer_distillation_from_scratch():
     student_weights.pop(-1) # last layer is not affected by tinybert loss
     student_weights.pop(-1) # pooler is not updated due to different attention head
     
-    processor = UnlabeledTextProcessor(tokenizer=teacher.inferencer.processor.tokenizer, max_seq_len=128, train_filename="doc_2.txt", data_dir="samples/docs")
-    student.distil_intermediate_layers_from(teacher_model=teacher, data_dir="samples/squad", train_filename="tiny.json", processor=processor)
+    processor = UnlabeledTextProcessor(tokenizer=teacher.inferencer.processor.tokenizer, max_seq_len=128, train_filename="doc_2.txt", data_dir=SAMPLES_PATH/"docs")
+    student.distil_intermediate_layers_from(teacher_model=teacher, data_dir=SAMPLES_PATH/"squad", train_filename="tiny.json", processor=processor)
 
     # create new checkpoint
     new_student_weights = create_checkpoint(student)
