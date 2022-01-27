5fc074ed25537fabb6e51239e14d3883f5a32499
ZanSara <sarazanzo94@gmail.com>
2022-01-27 12:26:51 +0100

    Replace importlib with a sys.path hack

--- docs/_src/api/openapi/generate_openapi_specs.py
+++ docs/_src/api/openapi/generate_openapi_specs.py
@@ -1,24 +1,20 @@
 import json
-import importlib
 from pathlib import Path
 import os
+import sys
+sys.path.append("../../../../")
 
-rest_path = Path("../../../../rest_api")
+rest_path = Path("../../../../rest_api").absolute()
 pipeline_path = str(rest_path/"pipeline"/"pipeline_empty.yaml")
 app_path = str(rest_path/"application.py")
 print(f"Loading OpenAPI specs from {app_path} with pipeline at {pipeline_path}")
 
 os.environ["PIPELINE_YAML_PATH"] = pipeline_path
 
-# Magic import from the rest_api folder
-import types
-import importlib.machinery
-loader = importlib.machinery.SourceFileLoader('application.py', app_path)
-mod = types.ModuleType(loader.name)
-loader.exec_module(mod)
+from rest_api.application import get_openapi_specs
 
 # Generate the openapi specs
-specs = mod.get_openapi_specs()
+specs = get_openapi_specs()
 
 # Dump the specs into a JSON file
 with open(f"openapi.json", "w") as f:
