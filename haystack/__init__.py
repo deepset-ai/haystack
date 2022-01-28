5c10baa223eb293e5540bd5543494ac74c7d2e42
ZanSara <sarazanzo94@gmail.com>
2022-01-28 12:19:26 +0100

    Revert

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -1,14 +1,13 @@
 from typing import Union
 from types import ModuleType
 
-__version__ = None
 try:
     import importlib.metadata as metadata
-    __version__ = metadata.version('farm-haystack')
 except ModuleNotFoundError:
     # Python <= 3.7
     import importlib_metadata as metadata  # type: ignore
-    __version__ = metadata.version('haystack')
+
+__version__ = metadata.version('farm-haystack')
 
 # This configuration must be done before any import to apply to all submodules
 import logging
