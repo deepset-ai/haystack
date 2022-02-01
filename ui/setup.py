6009b0796dd93ea0f12f84e5021d7383f0a38c37
ZanSara <sarazanzo94@gmail.com>
2022-02-01 18:11:34 +0100

    Add version info to 'farm0haystack-ui'

--- ui/setup.py
+++ ui/setup.py
@@ -3,8 +3,20 @@ import logging
 from pathlib import Path
 
 
+VERSION = None
+try:
+    # After git clone, VERSION.txt is in the root folder
+    VERSION = open(Path(__file__).parent.parent/'VERSION.txt', "r").read()
+except Exception:
+    try:
+        # In Docker, VERSION.txt is in the same folder
+        VERSION = open(Path(__file__).parent/'VERSION.txt', "r").read()
+    except Exception as e:
+        logging.exception("No VERSION.txt found!", e)
+
 setup(
     name="farm-haystack-ui",
+    version=VERSION,
     description='Demo UI for Haystack (https://github.com/deepset-ai/haystack)',
     author='deepset.ai',
     author_email='malte.pietsch@deepset.ai',
