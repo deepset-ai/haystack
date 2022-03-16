e53fef2b7f94301f08befda11e195950d7fe8c6b
Thomas Stadelmann <thomas.stadelmann@deepset.ai>
2022-03-16 21:56:22 +0100

    added tests and heuristic for one-sided overlaps

--- haystack/utils/context_matching.py
+++ haystack/utils/context_matching.py
@@ -11,44 +11,84 @@ _CandidateScore = namedtuple("_CandidateScore", ["context_id", "candidate_id", "
 
 
 def _score_candidate(args: Tuple[Union[str, Tuple[object, str]], Tuple[object, str], int]):
-    context, candidate, min_words = args
+    context, candidate, min_length = args
     candidate_id, candidate_text = candidate
     context_id, context_text = (None, context) if isinstance(context, str) else context
-    score = calculate_context_similarity(context_text, candidate_text, min_words)
+    score = calculate_context_similarity(context_text, candidate_text, min_length)
     return _CandidateScore(context_id=context_id, candidate_id=candidate_id, score=score)
 
 
-def normalize_white_space_and_case(str):
+def normalize_white_space_and_case(str: str) -> str:
     return re.sub(r"\s+", " ", str).lower().strip()
 
 
-def calculate_context_similarity(context: str, candidate: str, min_words: int = 25) -> float:
+def _no_processor(str: str) -> str:
+    return str
+
+
+def calculate_context_similarity(context: str, candidate: str, min_length: int = 100) -> float:
     """
     Calculates the text similarity score of context and candidate.
     The score's value ranges between 0.0 and 100.0.
 
     :param context: The context to match.
     :param candidate: The candidate to match the context.
-    :param min_words: The minimum number of words context and candidate need to have in order to be scored.
-                      Returns 0.0 otherwise.
+    :param min_length: The minimum string length context and candidate need to have in order to be scored.
+                              Returns 0.0 otherwise.
     """
     # we need to handle short contexts/contents (e.g single word)
     # as they produce high scores by matching if the chars of the word are contained in the other one
-    n_context = len(context.split())
-    n_content = len(candidate.split())
-    if n_content < min_words or n_context < min_words:
+    context = normalize_white_space_and_case(context)
+    candidate = normalize_white_space_and_case(candidate)
+    context_len = len(context)
+    candidate_len = len(candidate)
+    if candidate_len < min_length or context_len < min_length:
         return 0.0
-    return fuzz.partial_ratio(context, candidate, processor=normalize_white_space_and_case)
+
+    if context_len < candidate_len:
+        shorter = context
+        longer = candidate
+        shorter_len = context_len
+        longer_len = candidate_len
+    else:
+        shorter = candidate
+        longer = context
+        shorter_len = candidate_len
+        longer_len = context_len
+    
+    score_alignment = fuzz.partial_ratio_alignment(shorter, longer, processor=_no_processor)
+    score = score_alignment.score
+
+    # Special handling for one-sided overlaps (e.g. AB <-> BC):
+    # If we detect that the score is near a half match and the best fitting part of longer is at its boundaries
+    # we cut the shorter on the other side, recalculate the score and take the new one if it is higher.
+    # Thus AB <-> BC (score ~50) gets recalculated with B <-> B (score ~100) scoring ~100 in total
+    if score >= 40 and score < 75:
+        cut_shorter_left = score_alignment.dest_start == 0
+        cut_shorter_right = score_alignment.dest_end == longer_len
+        cut_len = shorter_len//2
+
+        if cut_shorter_left:
+            cut_score = fuzz.partial_ratio(shorter[cut_len:], longer, processor=_no_processor)
+            if cut_score > score:
+                score = cut_score
+        if cut_shorter_right:
+            cut_score = fuzz.partial_ratio(shorter[:-cut_len], longer, processor=_no_processor)
+            if cut_score > score:
+                score = cut_score
+    
+    return score
+
 
 
 def match_context(
     context: str,
     candidates: Generator[Tuple[str, str], None, None],
-    threshold: float = 60.0,
+    threshold: float = 75.0,
     show_progress: bool = False,
     num_processes: int = None,
     chunksize: int = 1,
-    min_words: int = 25,
+    min_length: int = 100,
 ) -> List[Tuple[str, float]]:
     """
     Matches the context against multiple candidates. Candidates consist of a tuple of an id and its text.
@@ -64,11 +104,11 @@ def match_context(
     :param chunksize: The chunksize used during parallel processing.
                       If not specified chunksize is 1.
                       For very long iterables using a large value for chunksize can make the job complete much faster than using the default value of 1.
-    :param min_words: The minimum number of words context and candidate need to have in order to be scored.
-                      Score will be 0.0 otherwise.
+    :param min_length: The minimum string length context and candidate need to have in order to be scored.
+                              Returns 0.0 otherwise.
     """
     with Pool(processes=num_processes) as pool:
-        score_candidate_args = ((context, candidate, min_words) for candidate in candidates)
+        score_candidate_args = ((context, candidate, min_length) for candidate in candidates)
         candidate_scores = pool.imap_unordered(_score_candidate, score_candidate_args, chunksize=chunksize)
         if show_progress:
             candidate_scores = tqdm(candidate_scores)
@@ -83,11 +123,11 @@ def match_context(
 def match_contexts(
     contexts: List[str],
     candidates: Generator[Tuple[str, str], None, None],
-    threshold: float = 60.0,
+    threshold: float = 75.0,
     show_progress: bool = False,
     num_processes: int = None,
     chunksize: int = 1,
-    min_words: int = 25,
+    min_length: int = 100,
 ) -> List[List[Tuple[str, float]]]:
     """
     Matches the contexts against multiple candidates. Candidates consist of a tuple of an id and its string text.
@@ -104,12 +144,12 @@ def match_contexts(
     :param chunksize: The chunksize used during parallel processing.
                       If not specified chunksize is 1.
                       For very long iterables using a large value for chunksize can make the job complete much faster than using the default value of 1.
-    :param min_words: The minimum number of words context and candidate need to have in order to be scored.
-                      Score will be 0.0 otherwise.
+    :param min_length: The minimum string length context and candidate need to have in order to be scored.
+                              Returns 0.0 otherwise.
     """
     with Pool(processes=num_processes) as pool:
         score_candidate_args = (
-            (context, candidate, min_words) for candidate in candidates for context in enumerate(contexts)
+            (context, candidate, min_length) for candidate in candidates for context in enumerate(contexts)
         )
         candidate_scores = pool.imap_unordered(_score_candidate, score_candidate_args, chunksize=chunksize)
         if show_progress:
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import pandas as pd
 from pathlib import Path
@@ -6,8 +7,19 @@ from haystack.utils.preprocessing import convert_files_to_dicts, tika_convert_fi
 from haystack.utils.cleaning import clean_wiki_text
 from haystack.utils.augment_squad import augment_squad
 from haystack.utils.squad_data import SquadData
+from haystack.utils.context_matching import calculate_context_similarity
 
 from .conftest import SAMPLES_PATH
+
+TEST_CONTEXT = context = """Der Merkantilismus förderte Handel und Verkehr mit teils marktkonformen, teils dirigistischen Maßnahmen. 
+An der Schwelle zum 19. Jahrhundert entstand ein neuer Typus des Nationalstaats, der die Säkularisation durchsetzte, 
+moderne Bildungssysteme etablierte und die Industrialisierung vorantrieb.\n
+Beim Begriff der Aufklärung geht es auch um die Prozesse zwischen diesen frühneuzeitlichen Eckpunkten. 
+Man versucht die fortschrittlichen Faktoren zu definieren, die in das 19. Jahrhundert führten. 
+Widerstände gegen diesen Fortschritt werden anti-aufklärerischen Kräften oder unreflektierten Traditionen zugeordnet. 
+Die Epochendefinition rückt vor allem publizistisch tätige Gruppen in den gesellschaftlichen Fokus, 
+die zunächst selten einen bürgerlichen Hintergrund aufwiesen, sondern weitaus häufiger der Geistlichkeit oder Aristokratie angehörten: 
+Wissenschaftler, Journalisten, Autoren, sogar Regenten, die Traditionen der Kritik unterzogen, indem sie sich auf die Vernunftperspektive beriefen."""
 
 
 def test_convert_files_to_dicts():
@@ -69,3 +81,104 @@ def test_squad_to_df():
     result = SquadData.df_to_data(df)
 
     assert result == expected_result
+
+
+def test_calculate_context_similarity_on_parts_of_whole_document():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    margin = 5
+    context_size = min_length + margin
+    for i in range(len(whole_document)-context_size):
+        partial_context = whole_document[i:i+context_size]
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score == 100.0
+
+
+def test_calculate_context_similarity_on_parts_of_whole_document_different_case():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    margin = 5
+    context_size = min_length + margin
+    for i in range(len(whole_document)-context_size):
+        partial_context = whole_document[i:i+context_size].lower()
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score == 100.0
+
+
+def test_calculate_context_similarity_on_parts_of_whole_document_different_whitesapce():
+    whole_document = TEST_CONTEXT
+    words = whole_document.split()
+    min_length = 100
+    context_word_size = 20
+    for i in range(len(words)-context_word_size):
+        partial_context = "\n\t\t\t".join(words[i:i+context_word_size])
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score == 100.0
+
+
+def test_calculate_context_similarity_min_length():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    context_size = min_length-1
+    for i in range(len(whole_document)-context_size):
+        partial_context = whole_document[i:i+context_size]
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score == 0.0
+
+
+def test_calculate_context_similarity_on_partially_overlapping_contexts():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    margin = 5
+    context_size = min_length + margin
+    stride = context_size//2
+    for i in range(len(whole_document)-context_size-stride):
+        partial_context_1 = whole_document[i:i+context_size]
+        partial_context_2 = whole_document[i+stride:i+stride+context_size]
+        score = calculate_context_similarity(partial_context_1, partial_context_2, min_length=min_length)
+        assert score >= 75.0
+
+
+def test_calculate_context_similarity_on_non_matching_contexts():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    margin = 5
+    context_size = min_length + margin
+    for i in range(len(whole_document)-context_size):
+        partial_context = whole_document[i:i+context_size//2] + _get_random_chars(context_size//2)
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score < 75.0
+    for i in range(len(whole_document)-context_size):
+        partial_context = _get_random_chars(context_size//2) + whole_document[i+context_size//2:i+context_size]
+        score = calculate_context_similarity(partial_context, whole_document, min_length=min_length)
+        assert score < 75.0
+
+
+def test_calculate_context_similarity_on_partially_overlapping_contexts_with_noise():
+    whole_document = TEST_CONTEXT
+    min_length = 100
+    margin = 5
+    context_size = min_length + margin
+    stride = context_size//2
+    scores = []
+    for i in range(len(whole_document)-context_size-stride):
+        partial_context_1 = whole_document[i:i+context_size]
+        partial_context_2 = _insert_noise(whole_document[i+stride:i+stride+context_size], 0.1)
+        score = calculate_context_similarity(partial_context_1, partial_context_2, min_length=min_length)
+        scores.append(score)
+    accuracy = np.where(np.array(scores) >= 75, 1, 0).mean()
+    assert accuracy > 0.99
+
+
+def _get_random_chars(size: int):
+    chars = np.random.choice(list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZß?/.,;:-#äöüÄÖÜ+*~1234567890$€%&!§ "), size=size)
+    return "".join(list(chars))
+
+
+def _insert_noise(input: str, ratio):
+    size = int(ratio * len(input))
+    insert_idxs = sorted(np.random.choice(range(len(input)), size=size, replace=False), reverse=True)
+    insert_chars = np.random.choice(list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZß?/.,;:-#äöüÄÖÜ+*~1234567890$€%&!§"), size=size)
+    for idx, char in zip(insert_idxs, insert_chars):
+        input = input[:idx] + char + input[idx:]
+    return input
