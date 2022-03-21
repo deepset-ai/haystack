871d537e00f7d26b810ba5c2676b31389286906c
Sara Zan <sara.zanzottera@deepset.ai>
2022-03-21 14:47:24 +0100

    Improve error message for nodes failing validation (#2313)
    * Similar test case seems to pass
    
    * Update Documentation & Code Style
    
    * Improve error message
    
    * Slightly clarify info message
    
    * Fix mismatch between node and node_class in the schema generation
    
    * Remove condition that node class names cannot begin with Base and update tests
    
    * Indentation
    
    * Update Documentation & Code Style
    
    * feedback
    
    * Update Documentation & Code Style
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- haystack/nodes/base.py
+++ haystack/nodes/base.py
@@ -9,6 +9,7 @@ import inspect
 import logging
 
 from haystack.schema import Document, MultiLabel
+from haystack.errors import PipelineSchemaError
 from haystack.telemetry import send_custom_event
 from haystack.errors import HaystackError
 
@@ -73,6 +74,7 @@ class BaseComponent(ABC):
 
         # Keeps track of all available subclasses by name.
         # Enables generic load() for all specific component implementations.
+        # Registers abstract classes and base classes too.
         cls._subclasses[cls.__name__] = cls
 
     @property
@@ -108,7 +110,7 @@ class BaseComponent(ABC):
     @classmethod
     def get_subclass(cls, component_type: str):
         if component_type not in cls._subclasses.keys():
-            raise HaystackError(f"Haystack component with the name '{component_type}' does not exist.")
+            raise PipelineSchemaError(f"Haystack component with the name '{component_type}' not found.")
         subclass = cls._subclasses[component_type]
         return subclass
 
