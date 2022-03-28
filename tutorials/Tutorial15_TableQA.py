d8ef34088bee95e0e73de035641486efe693d8f0
Michel Bartels <login@michelbartels.com>
2022-03-28 16:07:53 +0200

    update telemetry with tutorial changes

--- tutorials/Tutorial15_TableQA.py
+++ tutorials/Tutorial15_TableQA.py
@@ -26,7 +26,7 @@ def tutorial15_tableqa():
     # Here: 1000 tables + texts
 
     doc_dir = "data/tutorial15"
-    s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/ottqa_tables_sample.json.zip"
+    s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/table_text_dataset.zip"
     fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
     # Add the tables to the DocumentStore
