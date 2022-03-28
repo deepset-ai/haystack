1713e4c2ec99afaf5f8f7b92f8f9be5bc2daf977
Sara Zan <sarazanzo94@gmail.com>
2022-03-28 10:50:07 +0200

    change in with endswith

--- haystack/nodes/base.py
+++ haystack/nodes/base.py
@@ -39,10 +39,10 @@ def exportable_to_yaml(init_func):
             self._component_config = {"params": {}, "type": type(self).__name__}
 
         # Make sure it runs only on the __init__of the implementations, not in superclasses
-        # NOTE: we use 'in' because inner classes's __qualname__ will include the parent class'
+        # NOTE: we use '.endswith' because inner classes's __qualname__ will include the parent class'
         #   name, like: ParentClass.InnerClass.__init__.
         #   Inner classes are heavily used in tests.
-        if f"{self.__class__.__name__}.{init_func.__name__}" in init_func.__qualname__:
+        if init_func.__qualname__.endswith(f"{self.__class__.__name__}.{init_func.__name__}"):
 
             # Store all the named input parameters in self._component_config
             for k, v in kwargs.items():
