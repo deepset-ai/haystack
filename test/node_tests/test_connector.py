6048fec8433e066baef30a90c2326713eb8fd2f4
ZanSara <sarazanzo94@gmail.com>
2022-05-03 17:53:49 +0200

    Improve Crawler tests

--- test/node_tests/test_connector.py
+++ test/node_tests/test_connector.py
@@ -1,17 +1,15 @@
 import json
 import pytest
 from pathlib import Path
-from re import search
 
 import pytest
-
 from selenium import webdriver
-from selenium.webdriver import Chrome
+
 from haystack.nodes.connector import Crawler
 from haystack.schema import Document
 
 
-TEST_BASE_URL = "http://www.test.com"
+TEST_BASE_URL = "http://test.test.test.test.test"  # Non existing but valid URL
 
 TEST_HOME_PAGE = """
 <html>
@@ -62,40 +60,37 @@ TEST_UNWANTED = """
 """
 
 
-class MockWebDriver(Chrome):
-    def get(link):
-        if link == TEST_BASE_URL:
-            return TEST_HOME_PAGE
-        if "page1" in link:
-            return TEST_PAGE1
-        if "page2" in link:
-            return TEST_PAGE2
-        else:
-            return TEST_UNWANTED
-
-
 @pytest.fixture(autouse=True)
 def mock_webdriver(request, monkeypatch):
     # Do not patch integration tests
     if "integration" in request.keywords:
         return
+    
+    def mock_get(self, url: str) -> None:
+        if url == TEST_BASE_URL:
+            return TEST_HOME_PAGE
+        if "page1" in url:
+            return TEST_PAGE1
+        if "page2" in url:
+            return TEST_PAGE2
+        else:
+            return TEST_UNWANTED
 
-    monkeypatch.setattr(webdriver, "Chrome", MockWebDriver())
+    monkeypatch.setattr(webdriver.Chrome, "get", mock_get)
 
 
 #
 # Integration
 #
 
-
 @pytest.mark.integration
 def test_crawler(tmp_path):
-    tmp_dir = tmp_path / "crawled_files"
+    tmp_dir = tmp_path
     url = ["https://haystack.deepset.ai/"]
 
     crawler = Crawler(output_dir=tmp_dir)
-    docs_path = crawler.crawl(urls=url, crawler_depth=1)
-    results, _ = crawler.run(urls=url, crawler_depth=1, return_documents=True)
+    docs_path = crawler.crawl(urls=url, crawler_depth=0)
+    results, _ = crawler.run(urls=url, crawler_depth=0, return_documents=True)
     documents = results["documents"]
 
     for json_file, document in zip(docs_path, documents):
@@ -112,20 +107,39 @@ def test_crawler(tmp_path):
 # Unit tests
 #
 
-
 def test_crawler_url_none_exception(tmp_path):
-    crawler = Crawler(tmp_path / "crawled_files")
+    crawler = Crawler(tmp_path)
     with pytest.raises(ValueError):
         crawler.crawl()
 
 
-def test_crawler_output_file_structure(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
-    doc_paths = crawler.crawl(urls=["http://www.test.com"], crawler_depth=0)
+def test_crawler_depth_0_single_url(tmp_path):
+    crawler = Crawler(output_dir=tmp_path)
+    paths = crawler.crawl(urls=[TEST_BASE_URL], crawler_depth=0)
+    assert len(paths) == 1
 
-    doc_path = doc_paths[0]
-    assert isinstance(doc_path, Path)
-    with open(doc_path.absolute(), "r") as doc_file:
+
+def test_crawler_depth_0_many_urls(tmp_path):
+    crawler = Crawler(output_dir=tmp_path)
+    _urls = [
+        TEST_BASE_URL,
+        TEST_BASE_URL+"/page1",
+        TEST_BASE_URL+"/page2",
+    ]
+    paths = crawler.crawl(urls=_urls, crawler_depth=0)
+    assert len(paths) == 3
+
+
+def test_crawler_depth_1_single_url(tmp_path):
+    crawler = Crawler(output_dir=tmp_path)
+    paths = crawler.crawl(urls=[TEST_BASE_URL], crawler_depth=1)
+    assert len(paths) == 3
+
+
+def test_crawler_output_file_structure(tmp_path):
+    crawler = Crawler(output_dir=tmp_path)
+    paths = crawler.crawl(urls=[TEST_BASE_URL], crawler_depth=0)
+    with open(paths[0].absolute(), "r") as doc_file:
         data = json.load(doc_file)
         assert "content" in data
         assert "meta" in data
@@ -133,31 +147,12 @@ def test_crawler_output_file_structure(tmp_path):
         assert len(data["content"].split()) > 2
 
 
-def test_crawler_depth_0_single_url(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
-    doc_path = crawler.crawl(urls=["http://www.test.com"], crawler_depth=0)
-    assert len(doc_path) == 1
-
-
-def test_crawler_depth_0_many_urls(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
-    _urls = [TEST_BASE_URL, TEST_BASE_URL + "/page1", TEST_BASE_URL + "/page2"]
-    doc_path = crawler.crawl(urls=_urls, crawler_depth=0)
-    assert len(doc_path) == 3
-
-
-def test_crawler_depth_1_single_urls(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
-    doc_path = crawler.crawl(urls=[TEST_BASE_URL], crawler_depth=1)
-    assert len(doc_path) > 3
-
-
 def test_crawler_filter_urls(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
+    crawler = Crawler(output_dir=tmp_path)
 
-    assert len(crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["page1"])) == 1
-    assert not crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["page3"])
-    assert not crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["google\.com"])
+    assert len(crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["page1"], crawler_depth=1)) == 1
+    assert not crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["page3"], crawler_depth=1)
+    assert not crawler.crawl(urls=[TEST_BASE_URL], filter_urls=["google\.com"], crawler_depth=1)
 
 
 def test_crawler_content(tmp_path):
@@ -167,22 +162,20 @@ def test_crawler_content(tmp_path):
         {"url": TEST_BASE_URL + "/page2", "partial_content": ["test page 2 content"]},
     ]
 
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
+    crawler = Crawler(output_dir=tmp_path)
     for result in expected_results:
-
-        doc_path = crawler.crawl(urls=[result["url"]], crawler_depth=0)
-        for json_file in doc_path[0]:
-            with open(json_file.absolute(), "r") as read_file:
-                content = json.load(read_file)
-                assert result["partial_content"] in content["content"]
+        paths = crawler.crawl(urls=[result["url"]], crawler_depth=0)
+        with open(paths[0].absolute(), "r") as read_file:
+            content = json.load(read_file)
+            assert result["partial_content"] in content["content"]
 
 
 def test_crawler_return_document(tmp_path):
-    crawler = Crawler(output_dir=tmp_path / "crawled_files")
+    crawler = Crawler(output_dir=tmp_path)
     documents, _ = crawler.run(urls=[TEST_BASE_URL], crawler_depth=0, return_documents=True)
     paths, _ = crawler.run(urls=[TEST_BASE_URL], crawler_depth=0, return_documents=False)
 
-    for path, document in zip(paths["files"], documents["documents"]):
+    for path, document in zip(paths["paths"], documents["documents"]):
         with open(path.absolute(), "r") as doc_file:
             file_content = json.load(doc_file)
             assert file_content["meta"] == document.meta
