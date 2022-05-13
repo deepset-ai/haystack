19bad05dda9600fa2b5d84f37a5dec39f2b21991
agnieszka-m <amarzec13@gmail.com>
2022-05-13 14:05:24 +0200

    Add Bogdan's suggestions

--- haystack/pipelines/base.py
+++ haystack/pipelines/base.py
@@ -429,10 +429,10 @@ class Pipeline:
         :param query: The search query (for query pipelines only).
         :param file_paths: The files to index (for indexing pipelines only).
         :param labels: Ground-truth labels that you can use to perform an isolated evaluation of pipelines. These labels are input to nodes in the pipeline.
-        :param documents: A list of Document objects or a list of lists of Document objects to be processed by the Pipeline Nodes.
+        :param documents: A list of Document objects to be processed by the Pipeline Nodes.
         :param meta: Files' metadata. Used in indexing pipelines in combination with `file_paths`.
         :param params: Dictionary of parameters to be dispatched to the nodes.
-                       To pass a parameter to all Nodes, use: `{"top_k":10}`.
+                       To pass a parameter to all Nodes, use: `{"top_k": 10}`.
                        To pass a parameter to targeted Nodes, run:
                         `{"Retriever": {"top_k": 10}, "Reader": {"top_k": 3, "debug": True}}`
         :param debug: Specifies whether the Pipeline should instruct Nodes to collect debug information
@@ -541,7 +541,11 @@ class Pipeline:
         """
         Runs the Pipeline in a batch mode, one node at a time. The batch mode means that the Pipeline can take more than one query as input. You can use this method for query pipelines only. When used with an indexing pipeline, it calls the pipeline `run()` method.
 
-        Here's what this method returns:
+        Here's what this method returns for Retriever-Reader pipelines:
+        - Single query: Retrieves top-k relevant Docments and returns a list of answers for each retrieved Document.
+        - A list of queries: Retrieves top-k relevant Documents for each query and returns a list of answers for each query.
+
+        Here's what this method returns for Reader-only pipelines:
         - Single query + a list of Documents: Applies the query to each Document individually and returns answers    for each single Document.
         - Single query + a list of lists of Documents: Applies the query to each list of Documents and returns aggregated answers for each list of Documents.
         - A list of queries + a list of Documents: Applies each query to each Document individually and returns answers for each query-document pair.
