550d5d3474fb7e33b9aa96b77d507bc76985df2c
Thomas Stadelmann <thomas.stadelmann@deepset.ai>
2022-05-17 15:43:28 +0200

    simplify by using decorator for write operation functions

---
+++ haystack/utils/reflection.py
@@ -0,0 +1,12 @@
+import inspect
+from typing import Any, Dict, Tuple, Callable
+
+
+def args_to_kwargs(args: Tuple, func: Callable) -> Dict[str, Any]:
+    sig = inspect.signature(func)
+    arg_names = list(sig.parameters.keys())
+    # skip self and cls args for instance and class methods
+    if any(arg_names) and arg_names[0] in ["self", "cls"]:
+        arg_names = arg_names[1 : 1 + len(args)]
+    args_as_kwargs = {arg_name: arg for arg, arg_name in zip(args, arg_names)}
+    return args_as_kwargs
