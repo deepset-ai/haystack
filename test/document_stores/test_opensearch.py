8f84bc30375463e7579b104dbb9d7868826c8caa
Massimiliano Pippi <mpippi@gmail.com>
2022-06-07 16:28:14 +0200

    mark test with (sigh) elasticsearch

--- test/document_stores/test_opensearch.py
+++ test/document_stores/test_opensearch.py
@@ -3,5 +3,6 @@ import pytest
 from haystack.document_stores import OpenSearchDocumentStore
 
 
+@pytest.mark.elasticsearch
 def test_init_opensearch_client():
     OpenSearchDocumentStore(index="test_index", port=9201)
