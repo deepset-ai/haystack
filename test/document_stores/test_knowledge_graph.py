5a5f44ec554416851dac230f9e945b506a14921d
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2022-06-22 19:16:33 +0200

    Add InMemoryKnowledgeGraph (#2678)
    * draft for InMemoryKnowledgeGraph
    
    * remove comments
    
    * Update Documentation & Code Style
    
    * fix import and signature
    
    * Fix dependencies for in_memory_knowlede_graph
    
    * updated tutorials
    
    * Update Documentation & Code Style
    
    * fix bug in notebook
    
    * fix other notebook bug
    
    * Update Documentation & Code Style
    
    * improved tutorial notebook
    
    * Update Documentation & Code Style
    
    * better implementation of InMemoryKnowledgeGraph
    
    * fix
    
    * Update Documentation & Code Style
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/document_stores/test_knowledge_graph.py
+++ test/document_stores/test_knowledge_graph.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from haystack.nodes import Text2SparqlRetriever
-from haystack.document_stores import GraphDBKnowledgeGraph
+from haystack.document_stores import GraphDBKnowledgeGraph, InMemoryKnowledgeGraph
 from haystack.utils import fetch_archive_from_http
 
 
@@ -39,6 +39,53 @@ def test_graph_retrieval():
     PREFIX hp: <https://deepset.ai/harry_potter/>
     """
     kg.prefixes = prefixes
+
+    kgqa_retriever = Text2SparqlRetriever(knowledge_graph=kg, model_name_or_path=model_dir + "hp_v3.4")
+
+    result = kgqa_retriever.retrieve(query="In which house is Harry Potter?")
+    assert result[0] == {
+        "answer": ["https://deepset.ai/harry_potter/Gryffindor"],
+        "prediction_meta": {
+            "model": "Text2SparqlRetriever",
+            "sparql_query": "select ?a { hp:Harry_potter hp:house ?a . }",
+        },
+    }
+
+    result = kgqa_retriever._query_kg(
+        sparql_query="select distinct ?sbj where { ?sbj hp:job hp:Keeper_of_keys_and_grounds . }"
+    )
+    assert result[0][0] == "https://deepset.ai/harry_potter/Rubeus_hagrid"
+
+    result = kgqa_retriever._query_kg(
+        sparql_query="select distinct ?obj where { <https://deepset.ai/harry_potter/Hermione_granger> <https://deepset.ai/harry_potter/patronus> ?obj . }"
+    )
+    assert result[0][0] == "https://deepset.ai/harry_potter/Otter"
+
+
+@pytest.mark.integration
+def test_inmemory_graph_retrieval():
+    # TODO rename doc_dir
+    graph_dir = "../data/tutorial10_knowledge_graph/"
+    s3_url = "https://fandom-qa.s3-eu-west-1.amazonaws.com/triples_and_config.zip"
+    fetch_archive_from_http(url=s3_url, output_dir=graph_dir)
+
+    # Fetch a pre-trained BART model that translates natural language questions to SPARQL queries
+    model_dir = "../saved_models/tutorial10_knowledge_graph/"
+    s3_url = "https://fandom-qa.s3-eu-west-1.amazonaws.com/saved_models/hp_v3.4.zip"
+    fetch_archive_from_http(url=s3_url, output_dir=model_dir)
+
+    kg = InMemoryKnowledgeGraph(index="tutorial_10_index")
+    kg.delete_index()
+    kg.create_index()
+    kg.import_from_ttl_file(index="tutorial_10_index", path=Path(graph_dir + "triples.ttl"))
+    triple = {
+        "p": {"type": "uri", "value": "https://deepset.ai/harry_potter/_paternalgrandfather"},
+        "s": {"type": "uri", "value": "https://deepset.ai/harry_potter/Melody_fawley"},
+        "o": {"type": "uri", "value": "https://deepset.ai/harry_potter/Marshall_fawley"},
+    }
+    triples = kg.get_all_triples()
+    assert len(triples) > 0
+    assert triple in triples
 
     kgqa_retriever = Text2SparqlRetriever(knowledge_graph=kg, model_name_or_path=model_dir + "hp_v3.4")
 
