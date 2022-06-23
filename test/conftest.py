05f33745b8f5f5eb53ad74cb3d26edc974325bc8
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2022-06-23 14:44:31 +0200

    Adapt conftest to new MDR signature

--- test/conftest.py
+++ test/conftest.py
@@ -682,7 +682,6 @@ def get_retriever(retriever_type, document_store):
             document_store=document_store,
             embedding_model="deutschmann/mdr_roberta_q_encoder",  # or "facebook/dpr-ctx_encoder-single-nq-base"
             use_gpu=False,
-            embed_title=True,
         )
     elif retriever_type == "tfidf":
         retriever = TfidfRetriever(document_store=document_store)
