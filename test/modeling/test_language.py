9b738c558aa69fa0c902a84a64b88650d6596067
Vladimir Blagojevic <dovlex@gmail.com>
2022-07-19 16:36:45 +0200

    Parametrize language unit tests

--- test/modeling/test_language.py
+++ test/modeling/test_language.py
@@ -1,68 +1,18 @@
 import pytest
-from transformers import BertModel, ElectraModel, DistilBertModel, DPRContextEncoder
-from transformers.models.bert.modeling_bert import BertEncoder
-from transformers.models.electra.modeling_electra import ElectraEncoder
+from transformers import BertModel, DPRContextEncoder
 
-from haystack.modeling.model.language_model import get_language_model, HFLanguageModel, HFLanguageModelWithPooler, \
-    HFLanguageModelNoSegmentIds, DPREncoder
+from haystack.modeling.model.language_model import get_language_model, HFLanguageModel, DPREncoder
 
 
-def test_basic_loading():
-    lm = get_language_model("google/bert_uncased_L-2_H-128_A-2")
-    assert lm is not None and isinstance(lm, HFLanguageModel)
-    assert lm.name == "Bert"
-    assert lm.output_dims == 128
-    assert isinstance(lm.model, BertModel)
-    assert lm.language == "english"
-    assert isinstance(lm.encoder, BertEncoder)
-
-
-def test_basic_loading_with_model_type():
-    lm = get_language_model("google/bert_uncased_L-2_H-128_A-2", "bert")
-    assert lm is not None and isinstance(lm, HFLanguageModel)
-    assert lm.name == "Bert"
-    assert lm.output_dims == 128
-    assert isinstance(lm.model, BertModel)
-    assert lm.language == "english"
-    assert isinstance(lm.encoder, BertEncoder)
-
-
-def test_basic_loading_with_pooler():
-    lm = get_language_model("google/electra-small-generator", "electra")
-    assert lm is not None and isinstance(lm, HFLanguageModelWithPooler)
-    assert lm.name == "Electra"
-    assert lm.output_dims == 256
-    assert isinstance(lm.model, ElectraModel)
-    assert lm.language == "english"
-    assert isinstance(lm.encoder, ElectraEncoder)
-
-
-def test_basic_loading_with_pooler():
-    lm = get_language_model("google/electra-small-generator", "electra")
-    assert lm is not None and isinstance(lm, HFLanguageModelWithPooler)
-    assert lm.name == "Electra"
-    assert lm.output_dims == 256
-    assert isinstance(lm.model, ElectraModel)
-    assert lm.language == "english"
-    assert isinstance(lm.encoder, ElectraEncoder)
-
-
-def test_basic_loading_with_segment_id():
-    lm = get_language_model("distilbert-base-uncased")
-    assert lm is not None and isinstance(lm, HFLanguageModelNoSegmentIds)
-    assert lm.name == "DistilBert"
-    assert lm.output_dims == 768
-    assert isinstance(lm.model, DistilBertModel)
-    assert lm.language == "english"
-
-
-def test_basic_loading_with_segment_id():
-    lm = get_language_model("distilbert-base-uncased")
-    assert lm is not None and isinstance(lm, HFLanguageModelNoSegmentIds)
-    assert lm.name == "DistilBert"
-    assert lm.output_dims == 768
-    assert isinstance(lm.model, DistilBertModel)
-    assert lm.language == "english"
+@pytest.mark.parametrize("pretrained_model_name_or_path, lm_class",
+                         [("google/bert_uncased_L-2_H-128_A-2", "HFLanguageModel"),
+                          ("google/electra-small-generator", "HFLanguageModelWithPooler"),
+                          ("distilbert-base-uncased", "HFLanguageModelNoSegmentIds")])
+def test_basic_loading(pretrained_model_name_or_path, lm_class):
+    lm = get_language_model(pretrained_model_name_or_path)
+    mod = __import__('haystack.modeling.model.language_model', fromlist=[lm_class])
+    klass = getattr(mod, lm_class)
+    assert lm is not None and isinstance(lm, klass)
 
 
 def test_basic_loading_with_dpr_encoder_no_model_type():
