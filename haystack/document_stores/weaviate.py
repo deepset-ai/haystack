0440d8882fb638ad6d92bcbe3998f876d816a738
Igor Tarlinskiy <tarlinskyigoresha@gmail.com>
2022-07-19 12:57:36 +0300

    Raise error upon duplicate document key found within meta info

--- haystack/document_stores/weaviate.py
+++ haystack/document_stores/weaviate.py
@@ -489,6 +489,7 @@ class WeaviateDocumentStore(BaseDocumentStore):
                     # we "unnest" all value within "meta"
                     if "meta" in _doc.keys():
                         for k, v in _doc["meta"].items():
+                            assert k not in _doc.keys(), "\"meta\" info contains duplicate key \"{k}\" with the top-level document structure"
                             _doc[k] = v
                         _doc.pop("meta")
 
