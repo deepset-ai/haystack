2e41faa8e36f3dc149fdf49fc5b70bd4645a8fc3
Zoltan Fedor <zoltan.1.fedor@gmail.com>
2022-07-20 13:22:42 -0400

    Running Black on the recent code changes

--- haystack/document_stores/weaviate.py
+++ haystack/document_stores/weaviate.py
@@ -932,8 +932,7 @@ class WeaviateDocumentStore(BaseDocumentStore):
                 )
             else:
                 raise NotImplementedError(
-                    "Weaviate does not support the retrieval of records without "
-                    "specifying a query or a filter!"
+                    "Weaviate does not support the retrieval of records without " "specifying a query or a filter!"
                 )
 
         # Default Retrieval via BM25 using the user's query on `self.content_field`
@@ -948,8 +947,7 @@ class WeaviateDocumentStore(BaseDocumentStore):
             # Retrieval with BM25 AND filtering
             if filters:
                 raise NotImplementedError(
-                    "Weaviate currently (v1.14.1) does not support filters WITH "
-                    "inverted index text query (eg BM25)!"
+                    "Weaviate currently (v1.14.1) does not support filters WITH " "inverted index text query (eg BM25)!"
                 )
 
                 # Once Weaviate starts supporting filters with BM25:
@@ -964,12 +962,12 @@ class WeaviateDocumentStore(BaseDocumentStore):
 
             # BM25 retrieval without filtering
             else:
-                gql_query = gql.get.GetBuilder(class_name=index,
-                                               properties=properties,
-                                               connection=self.weaviate_client) \
-                    .with_near_vector({'vector': [0, 0]}) \
-                    .with_limit(top_k) \
+                gql_query = (
+                    gql.get.GetBuilder(class_name=index, properties=properties, connection=self.weaviate_client)
+                    .with_near_vector({"vector": [0, 0]})
+                    .with_limit(top_k)
                     .build()
+                )
 
             # Build the BM25 part of the GQL manually.
             # Currently the GetBuilder of the Weaviate-client (v3.6.0)
