2ce3259d0eabaca1ca975cda354d9a90f6cbfaca
Julian Risch <julian.risch@deepset.ai>
2022-07-25 17:57:30 +0200

    Remove logging config from Haystack (#2848)
    * move logging config from haystack lib to application
    
    * Update Documentation & Code Style
    
    * config logging before importing haystack
    
    * Update Documentation & Code Style
    
    * add logging config to all tutorials
    
    * Update Documentation & Code Style
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tutorials/Tutorial13_Question_generation.py
+++ tutorials/Tutorial13_Question_generation.py
@@ -1,3 +1,12 @@
+import logging
+
+# We configure how logging messages should be displayed and which log level should be used before importing Haystack.
+# Example log message:
+# INFO - haystack.utils.preprocessing -  Converting data/tutorial1/218_Olenna_Tyrell.txt
+# Default log level in basicConfig is WARNING so the explicit parameter is not necessary but can be changed easily:
+logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
+logging.getLogger("haystack").setLevel(logging.INFO)
+
 from tqdm import tqdm
 from haystack.nodes import QuestionGenerator, BM25Retriever, FARMReader, TransformersTranslator
 from haystack.document_stores import ElasticsearchDocumentStore
