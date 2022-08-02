2c56305ed3e91e5b3850d79c3fdba45ed535be12
tstadel <60758086+tstadel@users.noreply.github.com>
2022-08-02 09:49:32 +0200

    Fix serialization of numpy arrays and pandas dataframes in REST API (#2838)
    * correct serialization of numpy arrays and pandas dataframes
    
    * Update Documentation & Code Style
    
    * set additional json_encoders globally
    
    * Update Documentation & Code Style
    
    * add tests for non primitive return types
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rest_api/schema.py
+++ rest_api/schema.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from typing import Dict, List, Optional, Union
+import numpy as np
+import pandas as pd
 
 try:
     from typing import Literal
@@ -10,50 +12,40 @@ except ImportError:
 from pydantic import BaseModel, Field, Extra
 from pydantic import BaseConfig
 
-from haystack.schema import Answer, Document, Label
+from haystack.schema import Answer, Document
 
 
 BaseConfig.arbitrary_types_allowed = True
+BaseConfig.json_encoders = {np.ndarray: lambda x: x.tolist(), pd.DataFrame: lambda x: x.to_dict(orient="records")}
+
 
 PrimitiveType = Union[str, int, float, bool]
 
 
-class QueryRequest(BaseModel):
-    query: str
-    params: Optional[dict] = None
-    debug: Optional[bool] = False
-
+class RequestBaseModel(BaseModel):
     class Config:
         # Forbid any extra fields in the request to avoid silent failures
         extra = Extra.forbid
 
 
-class FilterRequest(BaseModel):
+class QueryRequest(RequestBaseModel):
+    query: str
+    params: Optional[dict] = None
+    debug: Optional[bool] = False
+
+
+class FilterRequest(RequestBaseModel):
     filters: Optional[Dict[str, Union[PrimitiveType, List[PrimitiveType], Dict[str, PrimitiveType]]]] = None
 
 
-class AnswerSerialized(Answer):
-    context: Optional[str] = None
-
-
-class DocumentSerialized(Document):
-    content: str
-    embedding: Optional[List[float]]  # type: ignore
-
-
-class LabelSerialized(Label, BaseModel):
-    document: DocumentSerialized
-    answer: Optional[AnswerSerialized] = None
-
-
-class CreateLabelSerialized(BaseModel):
+class CreateLabelSerialized(RequestBaseModel):
     id: Optional[str] = None
     query: str
-    document: DocumentSerialized
+    document: Document
     is_correct_answer: bool
     is_correct_document: bool
     origin: Literal["user-feedback", "gold-label"]
-    answer: Optional[AnswerSerialized] = None
+    answer: Optional[Answer] = None
     no_answer: Optional[bool] = None
     pipeline_id: Optional[str] = None
     created_at: Optional[str] = None
@@ -61,13 +53,9 @@ class CreateLabelSerialized(BaseModel):
     meta: Optional[dict] = None
     filters: Optional[dict] = None
 
-    class Config:
-        # Forbid any extra fields in the request to avoid silent failures
-        extra = Extra.forbid
-
 
 class QueryResponse(BaseModel):
     query: str
-    answers: List[AnswerSerialized] = []
-    documents: List[DocumentSerialized] = []
+    answers: List[Answer] = []
+    documents: List[Document] = []
     debug: Optional[Dict] = Field(None, alias="_debug")
