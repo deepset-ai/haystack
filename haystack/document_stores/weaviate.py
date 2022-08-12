89632e5866b7082df4f838f45444f3df5803e437
Sara Zan <sarazanzo94@gmail.com>
2022-08-12 16:25:39 +0200

    Raise exception instead of asserting

--- haystack/document_stores/weaviate.py
+++ haystack/document_stores/weaviate.py
@@ -489,9 +489,8 @@ class WeaviateDocumentStore(BaseDocumentStore):
                     # we "unnest" all value within "meta"
                     if "meta" in _doc.keys():
                         for k, v in _doc["meta"].items():
-                            assert (
-                                k not in _doc.keys()
-                            ), f'"meta" info contains duplicate key "{k}" with the top-level document structure'
+                            if k in _doc.keys():
+                                raise ValueError(f'"meta" info contains duplicate key "{k}" with the top-level document structure')
                             _doc[k] = v
                         _doc.pop("meta")
 
