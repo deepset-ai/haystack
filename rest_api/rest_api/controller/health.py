41877ac72962d49da7167d619a2a5ef00b54bb80
Daniel Bichuetti <daniel.bichuetti@gmail.com>
2022-09-06 07:17:39 -0300

    refactor: remove some sys-wide details

--- rest_api/rest_api/controller/health.py
+++ rest_api/rest_api/controller/health.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import logging
 
@@ -24,22 +24,18 @@ app: FastAPI = get_app()
 
 
 class CPUUsage(BaseModel):
-    avg_1m: str = Field(..., description="Average CPU usage over the last 1 minute")
-    avg_5m: str = Field(..., description="Average CPU usage over the last 5 minutes")
-    avg_15m: str = Field(..., description="Average CPU usage over the last 15 minutes")
+    used: float = Field(..., description="Average CPU usage")
 
 
 class MemoryUsage(BaseModel):
-    total: str = Field(..., description="Total memory in megabytes")
-    used: str = Field(..., description="Used memory in megabytes")
-    percent: str = Field(..., description="Percentage of used memory")
+    total: float = Field(..., description="Total memory in megabytes")
+    used: float = Field(..., description="REST API Used memory in megabytes")
 
 
 class GPUUsage(BaseModel):
-    memory_total: str = Field(..., description="Total GPU memory in megabytes")
-    memory_used: str = Field(..., description="Used GPU memory in megabytes")
-    memory_percent: str = Field(..., description="Percentage of used GPU memory")
-    kernel_usage: str = Field(..., description="GPU kernel usage in percent")
+    memory_total: float = Field(..., description="Total GPU memory in megabytes")
+    memory_used: Optional[float] = Field(..., description="REST API Used GPU memory in megabytes")
+    kernel_usage: float = Field(..., description="GPU kernel usage in percent")
 
 
 class GPUInfo(BaseModel):
@@ -47,21 +43,11 @@ class GPUInfo(BaseModel):
     usage: GPUUsage = Field(..., description="GPU usage details")
 
 
-class HaystackInfo(BaseModel):
-    version: str = Field(..., description="Haystack version")
-    cpu_usage: str = Field(..., description="REST API CPU usage details")
-    memory_usage: str = Field(..., description="REST API memory usage details")
-
-
-class SystemInfo(BaseModel):
-    cpu: CPUUsage = Field(..., description="System CPU usage details")
-    memory: MemoryUsage = Field(..., description="System memory usage details")
-    gpu_info: List[GPUInfo] = Field(default_factory=list, description="System GPU usage details")
-
-
 class HealthResponse(BaseModel):
-    haystack: HaystackInfo = Field(..., description="Haystack REST API health details")
-    system: SystemInfo = Field(..., description="System health details")
+    version: str = Field(..., description="Haystack version")
+    cpu: CPUUsage = Field(..., description="CPU usage details")
+    memory: MemoryUsage = Field(..., description="Memory usage details")
+    gpus: List[GPUInfo] = Field(default_factory=list, description="GPU usage details")
 
 
 @router.get("/health", response_model=HealthResponse, status_code=200)
@@ -70,57 +56,46 @@ def get_health_status():
     This endpoint allows external systems to monitor the health of the Haystack REST API.
     """
 
-    cpu_1m, cpu_5m, cpu_15m = psutil.getloadavg()
-    cpu_count = float(os.cpu_count() or 0)
-    cpu_1m = (cpu_1m / cpu_count) * 100
-    cpu_5m = (cpu_5m / cpu_count) * 100
-    cpu_15m = (cpu_15m / cpu_count) * 100
-    cpu_current = CPUUsage(avg_1m=f"{cpu_1m:.2f}%", avg_5m=f"{cpu_5m:.2f}%", avg_15m=f"{cpu_15m:.2f}%")
-
-    memory_data = psutil.virtual_memory()
-    memory_total = memory_data.total / 1024 / 1024
-    memory_used = memory_data.used / 1024 / 1024
-    memory_percent = memory_data.percent
-    memory_usage = MemoryUsage(
-        total=f"{memory_total:.2f}MB", used=f"{memory_used:.2f}MB", percent=f"{memory_percent:.2f}%"
-    )
-
     gpus: List[GPUInfo] = []
 
     try:
         pynvml.nvmlInit()
         gpu_count = pynvml.nvmlDeviceGetCount()
-        gpu_mem_total = ""
-        gpu_mem_used = ""
-        gpu_mem_free = ""
         for i in range(gpu_count):
             handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             info = pynvml.nvmlDeviceGetMemoryInfo(handle)
             gpu_mem_total = float(info.total) / 1024 / 1024
-            gpu_mem_used = float(info.used) / 1024 / 1024
+            gpu_mem_used = None
+            for proc in pynvml.nvmlDeviceGetComputeRunningProcesses(handle):
+                if proc.pid == os.getpid():
+                    gpu_mem_used = float(proc.usedGpuMemory) / 1024 / 1024
+                    break
+
             gpu_info = GPUInfo(
                 index=i,
                 usage=GPUUsage(
-                    memory_total=f"{gpu_mem_total:.2f}MB",
-                    memory_used=f"{gpu_mem_used:.2f}MB",
-                    memory_percent=f"{(gpu_mem_used/gpu_mem_total)*100:.2f}%",
-                    kernel_usage=f"{pynvml.nvmlDeviceGetUtilizationRates(handle).gpu:.2f}%",
+                    memory_total=gpu_mem_total,
+                    kernel_usage=pynvml.nvmlDeviceGetUtilizationRates(handle).gpu,
+                    memory_used=gpu_mem_used,
                 ),
             )
+
             gpus.append(gpu_info)
     except pynvml.NVMLError:
         logger.warning("No NVIDIA GPU found.")
 
+    memory_data = psutil.virtual_memory()
+    memory_total = memory_data.total / 1024 / 1024
+
     p_cpu_usage = 0
     p_memory_usage = 0
+    cpu_count = os.cpu_count() or 1
     p = psutil.Process()
     with p.oneshot():
         p_cpu_usage = p.cpu_percent() / cpu_count
         p_memory_usage = p.memory_percent()
 
-    return HealthResponse(
-        haystack=HaystackInfo(
-            version=haystack.__version__, cpu_usage=f"{p_cpu_usage:.2f}%", memory_usage=f"{p_memory_usage:.2f}%"
-        ),
-        system=SystemInfo(cpu=cpu_current, memory=memory_usage, gpu_info=gpus),
-    )
+    cpu_usage = CPUUsage(used=p_cpu_usage)
+    memory_usage = MemoryUsage(total=memory_total, used=p_memory_usage)
+
+    return HealthResponse(version=haystack.__version__, cpu=cpu_usage, memory=memory_usage, gpus=gpus)
