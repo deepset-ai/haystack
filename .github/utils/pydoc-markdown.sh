7f76e8e0960fc37cef0c84b5e78d8a02f66e11b3
brandenchan <brandenchan@icloud.com>
2022-09-21 12:50:25 +0200

    Adjust for on the fly generation and sync

--- .github/utils/pydoc-markdown.sh
+++ .github/utils/pydoc-markdown.sh
@@ -3,6 +3,8 @@
 set -e   # Fails on any error in the following loop
 export PYTHONPATH=$PWD/docs/pydoc # Make the renderers available to pydoc
 cd docs/_src/api/api/
-for file in ../pydoc/* ; do
+mkdir temp
+cd temp
+for file in ../../pydoc/* ; do
     pydoc-markdown "$file"
 done
