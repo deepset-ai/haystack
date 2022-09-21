eedb2ebbe5a381331df96bc2953019190754a85a
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2022-09-21 11:30:14 +0200

    Update test_document_classifier.py

--- test/nodes/test_document_classifier.py
+++ test/nodes/test_document_classifier.py
@@ -10,14 +10,11 @@ def test_document_classifier(document_classifier):
 
     docs = [
         Document(
-            content="""That's good. I like it."""
-            * 700,  # extra long text to check truncation
+            content="""That's good. I like it.""" * 700,  # extra long text to check truncation
             meta={"name": "0"},
             id="1",
         ),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     results = document_classifier.predict(documents=docs)
     expected_labels = ["joy", "sadness"]
@@ -42,9 +39,7 @@ def test_document_classifier_details(document_classifier):
 def test_document_classifier_batch_single_doc_list(document_classifier):
     docs = [
         Document(content="""That's good. I like it.""", meta={"name": "0"}, id="1"),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     results = document_classifier.predict_batch(documents=docs)
     expected_labels = ["joy", "sadness"]
@@ -56,9 +51,7 @@ def test_document_classifier_batch_single_doc_list(document_classifier):
 def test_document_classifier_batch_multiple_doc_lists(document_classifier):
     docs = [
         Document(content="""That's good. I like it.""", meta={"name": "0"}, id="1"),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     results = document_classifier.predict_batch(documents=[docs, docs])
     assert len(results) == 2  # 2 Document lists
@@ -73,14 +66,11 @@ def test_zero_shot_document_classifier(zero_shot_document_classifier):
 
     docs = [
         Document(
-            content="""That's good. I like it."""
-            * 700,  # extra long text to check truncation
+            content="""That's good. I like it.""" * 700,  # extra long text to check truncation
             meta={"name": "0"},
             id="1",
         ),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     results = zero_shot_document_classifier.predict(documents=docs)
     expected_labels = ["positive", "negative"]
@@ -107,14 +97,11 @@ def test_document_classifier_batch_size(batched_document_classifier):
 
     docs = [
         Document(
-            content="""That's good. I like it."""
-            * 700,  # extra long text to check truncation
+            content="""That's good. I like it.""" * 700,  # extra long text to check truncation
             meta={"name": "0"},
             id="1",
         ),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     results = batched_document_classifier.predict(documents=docs)
     expected_labels = ["joy", "sadness"]
@@ -128,22 +115,14 @@ def test_document_classifier_as_index_node(indexing_document_classifier):
 
     docs = [
         {
-            "content": """That's good. I like it."""
-            * 700,  # extra long text to check truncation
+            "content": """That's good. I like it.""" * 700,  # extra long text to check truncation
             "meta": {"name": "0"},
             "id": "1",
             "class_field": "That's bad.",
         },
-        {
-            "content": """That's bad. I like it.""",
-            "meta": {"name": "1"},
-            "id": "2",
-            "class_field": "That's good.",
-        },
+        {"content": """That's bad. I like it.""", "meta": {"name": "1"}, "id": "2", "class_field": "That's good."},
     ]
-    output, output_name = indexing_document_classifier.run(
-        documents=docs, root_node="File"
-    )
+    output, output_name = indexing_document_classifier.run(documents=docs, root_node="File")
     expected_labels = ["sadness", "joy"]
     for i, doc in enumerate(output["documents"]):
         assert doc["meta"]["classification"]["label"] == expected_labels[i]
@@ -155,14 +134,11 @@ def test_document_classifier_as_query_node(document_classifier):
 
     docs = [
         Document(
-            content="""That's good. I like it."""
-            * 700,  # extra long text to check truncation
+            content="""That's good. I like it.""" * 700,  # extra long text to check truncation
             meta={"name": "0"},
             id="1",
         ),
-        Document(
-            content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"
-        ),
+        Document(content="""That's bad. I don't like it.""", meta={"name": "1"}, id="2"),
     ]
     output, output_name = document_classifier.run(documents=docs, root_node="Query")
     expected_labels = ["joy", "sadness"]
