5b6beefa9c29a42af9e9ac2c4c463a5b7552ee14
brandenchan <brandenchan@icloud.com>
2022-09-22 11:34:03 +0200

    Test sync

--- haystack/nodes/answer_generator/base.py
+++ haystack/nodes/answer_generator/base.py
@@ -10,7 +10,7 @@ from haystack.nodes.base import BaseComponent
 
 class BaseGenerator(BaseComponent):
     """
-    Abstract class for Generators
+    Abstract class for Generatorswwww
     """
 
     outgoing_edges = 1
