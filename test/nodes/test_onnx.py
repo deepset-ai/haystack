bf611e717cfae96ecdfed9d37e21a8e5cf7a95d2
Vladimir Blagojevic <dovlex@gmail.com>
2022-09-23 19:32:59 +0200

    Add BERT test, parametrize unit test

--- test/nodes/test_onnx.py
+++ test/nodes/test_onnx.py
@@ -1,11 +1,14 @@
 import os
 from pathlib import Path
 
+import pytest
+
 from haystack.nodes import FARMReader
 
 
-def test_farm_reader_onnx_conversion_and_inference(tmpdir, docs):
-    FARMReader.convert_to_onnx(model_name="deepset/roberta-base-squad2", output_path=Path(tmpdir, "onnx"))
+@pytest.mark.parametrize("model_name", ["deepset/roberta-base-squad2", "deepset/bert-base-uncased-squad2"])
+def test_farm_reader_onnx_conversion_and_inference(model_name, tmpdir, docs):
+    FARMReader.convert_to_onnx(model_name=model_name, output_path=Path(tmpdir, "onnx"))
     assert os.path.exists(Path(tmpdir, "onnx", "model.onnx"))
     assert os.path.exists(Path(tmpdir, "onnx", "processor_config.json"))
     assert os.path.exists(Path(tmpdir, "onnx", "onnx_model_config.json"))
