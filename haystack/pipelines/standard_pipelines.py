e6767fccefdc6c36cd57613453ca98488c49392b
nickchomey <88559987+nickchomey@users.noreply.github.com>
2022-10-04 01:44:48 -0600

    bugfix for TranslationWrapperPipeline (#3290)
    * bugfix for TranslationWrapperPipeline
    
    * Update standard_pipelines.py
    
    * Update haystack/pipelines/standard_pipelines.py
    
    Co-authored-by: Sara Zan <sarazanzo94@gmail.com>

--- haystack/pipelines/standard_pipelines.py
+++ haystack/pipelines/standard_pipelines.py
@@ -596,7 +596,10 @@ class TranslationWrapperPipeline(BaseStandardPipeline):
         if isinstance(pipeline, QuestionAnswerGenerationPipeline):
             setattr(output_translator, "run", output_translator.run_batch)
 
-        graph = pipeline.pipeline.graph
+        if hasattr(pipeline, "pipeline"):
+            graph = pipeline.pipeline.graph
+        else:
+            graph = pipeline.graph  # type: ignore
         previous_node_name = ["InputTranslator"]
         # Traverse in BFS
         for node in graph.nodes:
