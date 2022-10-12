94d8748794484eaa6832a4d9c9128eb658da4ac3
Sebastian Lee <sebastian.lee@deepset.ai>
2022-10-12 10:34:37 +0200

    Added checks for DataParallel and WrappedDataParallel

--- haystack/modeling/training/base.py
+++ haystack/modeling/training/base.py
@@ -18,7 +18,7 @@ from haystack.modeling.data_handler.data_silo import DataSilo, DistillationDataS
 from haystack.modeling.evaluation.eval import Evaluator
 from haystack.modeling.model.adaptive_model import AdaptiveModel
 from haystack.modeling.model.biadaptive_model import BiAdaptiveModel
-from haystack.modeling.model.optimization import get_scheduler
+from haystack.modeling.model.optimization import get_scheduler, WrappedDataParallel
 from haystack.modeling.utils import GracefulKiller
 from haystack.utils.experiment_tracking import Tracker as tracker
 from haystack.utils.early_stopping import EarlyStopping
@@ -292,12 +292,16 @@ class Trainer:
 
     def compute_loss(self, batch: dict, step: int) -> torch.Tensor:
         # Forward & backward pass through model
-        if isinstance(self.model, AdaptiveModel):
+        model_type = type(self.model)
+        if isinstance(self.model, DataParallel) or isinstance(self.model, WrappedDataParallel):
+            model_type = type(self.model.module)
+
+        if model_type is AdaptiveModel:
             logits = self.model.forward(
                 input_ids=batch["input_ids"], segment_ids=None, padding_mask=batch["padding_mask"]
             )
 
-        elif isinstance(self.model, BiAdaptiveModel):
+        elif model_type is BiAdaptiveModel:
             logits = self.model.forward(
                 query_input_ids=batch["query_input_ids"],
                 query_segment_ids=batch["query_segment_ids"],
