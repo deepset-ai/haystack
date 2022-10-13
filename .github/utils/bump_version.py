c4993eb8e25bdb6eece339406006e0d9e91d8455
brandenchan <brandenchan@icloud.com>
2022-10-13 10:57:28 +0200

    Start implementing version bump

---
+++ .github/utils/bump_version.py
@@ -0,0 +1,10 @@
+# Increment the minor version of Haystack
+# e.g. v1.10.0rc0 --> v1.11.0rc0
+
+with open("VERSION.txt") as f:
+    version = f.read().strip()
+version_split = version.split(".")
+version_split[1] = str(int(version_split[1]) + 1)
+new_version = ".".join(version_split)
+with open("VERSION.txt", "w") as f:
+    f.write(new_version)
