38aed87c14eeb68c0d96d641eb2e7c0b4ac820f3
anakin87 <44616784+anakin87@users.noreply.github.com>
2022-10-15 13:27:41 +0200

    fixes

--- ui/ui/__about__.py
+++ ui/ui/__about__.py
@@ -3,13 +3,8 @@ import logging
 from pathlib import Path
 
 
-VERSION = "0.0.0"
+__version__ = "0.0.0"
 try:
-    # After git clone, VERSION.txt is in the root folder
-    VERSION = open(Path(__file__).parent.parent / "VERSION.txt", "r").read()
-except Exception:
-    try:
-        # In Docker, VERSION.txt is in the same folder
-        VERSION = open(Path(__file__).parent / "VERSION.txt", "r").read()
-    except Exception as e:
-        logging.exception("No VERSION.txt found!")
+    __version__ = open(Path(__file__).parent.parent / "VERSION.txt", "r").read()
+except Exception as e:
+    logging.exception("No VERSION.txt found!")
