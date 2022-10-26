d0691a4bd5112a88b76b66a3249bfc20770e2110
Julian Risch <julian.risch@deepset.ai>
2022-10-26 12:09:04 +0200

    bug: replace decorator with counter attribute for pipeline event (#3462)

--- haystack/utils/reflection.py
+++ haystack/utils/reflection.py
@@ -1,5 +1,4 @@
 import inspect
-import functools
 import logging
 import time
 from random import random
@@ -18,28 +17,6 @@ def args_to_kwargs(args: Tuple, func: Callable) -> Dict[str, Any]:
         arg_names = arg_names[1 : 1 + len(args)]
     args_as_kwargs = {arg_name: arg for arg, arg_name in zip(args, arg_names)}
     return args_as_kwargs
-
-
-def pipeline_invocation_counter(func):
-    @functools.wraps(func)
-    def wrapper_invocation_counter(*args, **kwargs):
-        # single query
-        this_invocation_count = 1
-        # were named arguments used?
-        if "queries" in kwargs:
-            this_invocation_count = len(kwargs["queries"]) if kwargs["queries"] else 1
-        elif "documents" in kwargs:
-            this_invocation_count = len(kwargs["documents"]) if kwargs["documents"] else 1
-        else:
-            # positional arguments used? try to infer count from the first parameter in args
-            if args[0] and isinstance(args[0], list):
-                this_invocation_count = len(args[0])
-
-        wrapper_invocation_counter.counter += this_invocation_count
-        return func(*args, **kwargs)
-
-    wrapper_invocation_counter.counter = 0
-    return wrapper_invocation_counter
 
 
 def retry_with_exponential_backoff(
