5a6a537700cd5eff108bca2f75d61925fc830c95
anakin87 <44616784+anakin87@users.noreply.github.com>
2022-10-30 12:58:06 +0100

    progress in document merger

--- haystack/nodes/other/document_merger.py
+++ haystack/nodes/other/document_merger.py
@@ -1,8 +1,9 @@
-from collections import defaultdict
+from collections import MutableMapping
 import logging
 from math import inf
+from copy import deepcopy
 
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from haystack.schema import Document
 from haystack.nodes import BaseNode
@@ -40,7 +41,7 @@ class DocumentMerger(BaseNode):
 
         if not all([doc.content_type == "text" for doc in documents]):
             raise AttributeError(
-                "Not all the provided documents are textual. Document Merger only works on textual documents."
+                "Some of the documents provided are non-textual. Document Merger only works on textual documents."
             )
 
         if separator is None:
@@ -48,16 +49,17 @@ class DocumentMerger(BaseNode):
 
         merged_content = separator.join([doc.content for doc in documents])
 
-        # better with other solutions?
-        # how to approach nested?
-        common_meta_dict = deepcopy(documents[0].meta)
-        for doc in documents[1:]:
-            if len(common_meta_dict) == 0:
+        flattened_meta = [self._flatten_dict(d["meta"]) for d in documents]
+
+        common_meta_flat_dict = deepcopy(flattened_meta[0])
+        for doc in flattened_meta[1:]:
+            if len(common_meta_flat_dict) == 0:
                 break
-            for k, v in doc.meta.items():
-                if k in common_meta_dict:
-                    if common_meta_dict[k] != v:
-                        del common_meta_dict[k]
+            for k, v in doc.items():
+                if k in common_meta_flat_dict:
+                    if common_meta_flat_dict[k] != v:
+                        del common_meta_flat_dict[k]
+        common_meta_nest_dict = self._nest_dict(common_meta_flat_dict)
 
         # common_meta_keys = set(documents[0].meta.keys())
         # for doc in documents[1:]:
@@ -81,3 +83,27 @@ class DocumentMerger(BaseNode):
     ):
 
         return None, "output_1"
+
+    def _flatten_dict(self, d: dict, parent_key="") -> dict:
+        items: List = []
+        for k, v in d.items():
+            new_key = (parent_key, k) if parent_key else k
+            if isinstance(v, MutableMapping):
+                items.extend(self._flatten_dict(v, new_key).items())
+            else:
+                items.append((new_key, v))
+        return dict(items)
+
+    def _nest_dict(self, d: dict) -> dict:
+        nested_dict = {}
+        for key, value in d.items():
+            target = nested_dict
+            if isinstance(key, tuple):
+                for k in key[:-1]:  # traverse all keys but the last
+                    target = target.setdefault(k, {})
+                target[key[-1]] = value
+            else:
+                target[key] = value
+        while any(isinstance(k, tuple) for k in nested_dict.keys()):
+            nested_dict = nest(nested_dict)
+        return common_meta_nested_dict
