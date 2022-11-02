edac1ae2ec50868527566e58dbe92171f64cc890
espoir murhabazi <espoir.mur@gmail.com>
2022-11-02 12:55:47 +0000

    move all the test related to squad data to one file

--- test/others/test_utils.py
+++ test/others/test_utils.py
@@ -53,54 +53,6 @@ def test_tika_convert_files_to_docs():
     assert documents and len(documents) > 0
 
 
-def test_squad_augmentation():
-    input_ = SAMPLES_PATH / "squad" / "tiny.json"
-    output = SAMPLES_PATH / "squad" / "tiny_augmented.json"
-    glove_path = SAMPLES_PATH / "glove" / "tiny.txt"  # dummy glove file, will not even be use when augmenting tiny.json
-    multiplication_factor = 5
-    augment_squad(
-        model="distilbert-base-uncased",
-        tokenizer="distilbert-base-uncased",
-        squad_path=input_,
-        output_path=output,
-        glove_path=glove_path,
-        multiplication_factor=multiplication_factor,
-    )
-    original_squad = SquadData.from_file(input_)
-    augmented_squad = SquadData.from_file(output)
-    assert original_squad.count(unit="paragraph") == augmented_squad.count(unit="paragraph") * multiplication_factor
-
-
-def test_squad_to_df():
-    df = pd.DataFrame(
-        [["title", "context", "question", "id", "answer", 1, False]],
-        columns=["title", "context", "question", "id", "answer_text", "answer_start", "is_impossible"],
-    )
-
-    expected_result = [
-        {
-            "title": "title",
-            "paragraphs": [
-                {
-                    "context": "context",
-                    "qas": [
-                        {
-                            "question": "question",
-                            "id": "id",
-                            "answers": [{"text": "answer", "answer_start": 1}],
-                            "is_impossible": False,
-                        }
-                    ],
-                }
-            ],
-        }
-    ]
-
-    result = SquadData.df_to_data(df)
-
-    assert result == expected_result
-
-
 def test_calculate_context_similarity_on_parts_of_whole_document():
     whole_document = TEST_CONTEXT
     min_length = 100
