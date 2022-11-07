64c56b043dd56f89a8ca9596a44fbc405ab390a6
Massimiliano Pippi <mpippi@gmail.com>
2022-11-07 10:03:54 +0100

    add entrypoint

--- haystack/json-schemas/generate_schema.py
+++ haystack/json-schemas/generate_schema.py
@@ -9,24 +9,29 @@ from requests.exceptions import HTTPError
 logger = logging.getLogger("hatch_autorun")
 
 
-# Prevent 3rd party libraries from calling external services while
-# we generate the schema
 class GuardedSocket(socket.socket):
+    """
+    A socket that raises upon creation to prevent
+    network access.
+    """
+
     def __new__(cls, family=-1, type=-1, proto=-1, fileno=None):
         raise IOError()
 
 
-socket.socket = GuardedSocket  # type: ignore
+def gen_schema(guard_socket, main_only):
+    if guard_socket:
+        # Prevent 3rd party libraries from calling external services while
+        # we generate the schema
+        socket.socket = GuardedSocket
 
+    # import Haystack after setting up socket
+    from haystack.nodes._json_schema import update_json_schema
 
-# finally import Haystack
-from haystack.nodes._json_schema import update_json_schema
-
-try:
     logger.warning(
         "Haystack is generating the YAML schema for Pipelines validation. This only happens once, after installing the package."
     )
-    update_json_schema(main_only=True)
+    update_json_schema(main_only=main_only)
 
     # Destroy the hatch-autorun hook if it exists (needs to run just once after installation)
     try:
@@ -34,5 +39,9 @@ try:
     except FileNotFoundError:
         pass
 
-except Exception as e:
-    logger.exception("Could not generate the Haystack Pipeline schemas.", e)
+
+if __name__ == "__main__":
+    try:
+        gen_schema(guard_socket=True, main_only=True)
+    except Exception as e:
+        logger.exception("Could not generate the Haystack Pipeline schemas.", e)
