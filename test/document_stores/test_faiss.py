749f11d983259f6a6e4429045a078a3a9bd9d60a
Massimiliano Pippi <mpippi@gmail.com>
2022-11-07 14:54:07 +0100

    try

--- test/document_stores/test_faiss.py
+++ test/document_stores/test_faiss.py
@@ -17,40 +17,6 @@ from haystack.nodes.retriever.dense import EmbeddingRetriever
 from ..conftest import MockDenseRetriever
 
 
-DOCUMENTS = [
-    {
-        "meta": {"name": "name_1", "year": "2020", "month": "01"},
-        "content": "text_1",
-        "embedding": np.random.rand(768).astype(np.float32),
-    },
-    {
-        "meta": {"name": "name_2", "year": "2020", "month": "02"},
-        "content": "text_2",
-        "embedding": np.random.rand(768).astype(np.float32),
-    },
-    {
-        "meta": {"name": "name_3", "year": "2020", "month": "03"},
-        "content": "text_3",
-        "embedding": np.random.rand(768).astype(np.float64),
-    },
-    {
-        "meta": {"name": "name_4", "year": "2021", "month": "01"},
-        "content": "text_4",
-        "embedding": np.random.rand(768).astype(np.float32),
-    },
-    {
-        "meta": {"name": "name_5", "year": "2021", "month": "02"},
-        "content": "text_5",
-        "embedding": np.random.rand(768).astype(np.float32),
-    },
-    {
-        "meta": {"name": "name_6", "year": "2021", "month": "03"},
-        "content": "text_6",
-        "embedding": np.random.rand(768).astype(np.float64),
-    },
-]
-
-
 class TestFAISSDocumentStore(DocumentStoreBaseTestAbstract):
     index_name = __name__
 
@@ -245,7 +211,7 @@ class TestFAISSDocumentStore(DocumentStoreBaseTestAbstract):
     @pytest.mark.parametrize("index_factory", ["Flat", "HNSW", "IVF1,Flat"])
     def test_retrieving(self, documents_embedding_only, index_factory, tmp_path):
         document_store = FAISSDocumentStore(
-            sql_url=f"sqlite:////{tmp_path/'test_faiss_retrieving.db'}",
+            sql_url=f"sqlite:////{tmp_path}/test_faiss_retrieving_{index_factory}.db",
             faiss_index_factory_str=index_factory,
             isolation_level="AUTOCOMMIT",
         )
