01e28e5c8b92c9952e3689b292ddc02158cd4802
Massimiliano Pippi <mpippi@gmail.com>
2022-11-14 15:25:52 +0100

    revert deprecation process

--- haystack/document_stores/milvus.py
+++ haystack/document_stores/milvus.py
@@ -251,7 +251,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :return:
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         index = index or self.index
         index_param = index_param or self.index_param
@@ -412,7 +412,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :return:
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         index = index or self.index
         has_collection = utility.has_collection(collection_name=index)
@@ -468,7 +468,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :return: None
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         if ids:
             self._delete_vector_ids_from_milvus(ids=ids, index=index)
@@ -529,7 +529,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :param batch_size: When working with large number of documents, batching can help reduce memory footprint.
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         index = index or self.index
         documents = super().get_all_documents_generator(index=index, filters=filters, batch_size=batch_size)
@@ -560,7 +560,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :param batch_size: When working with large number of documents, batching can help reduce memory footprint.
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         index = index or self.index
         result = self.get_all_documents_generator(
@@ -580,7 +580,7 @@ class MilvusDocumentStore(SQLDocumentStore):
                       DocumentStore's default index (self.index) will be used.
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         documents = self.get_documents_by_id([id], index)
         document = documents[0] if documents else None
@@ -602,7 +602,7 @@ class MilvusDocumentStore(SQLDocumentStore):
         :param batch_size: When working with large number of documents, batching can help reduce memory footprint.
         """
         if headers:
-            raise NotImplementedError("Milvus2DocumentStore does not support headers.")
+            raise NotImplementedError("MilvusDocumentStore does not support headers.")
 
         index = index or self.index
         documents = super().get_documents_by_id(ids=ids, index=index, batch_size=batch_size)
@@ -664,58 +664,3 @@ class MilvusDocumentStore(SQLDocumentStore):
         if filters:
             raise Exception("filters are not supported for get_embedding_count in MilvusDocumentStore.")
         return len(self.get_all_documents(index=index))
-
-
-class Milvus2DocumentStore(MilvusDocumentStore):
-    def __init__(
-        self,
-        sql_url: str = "sqlite:///",
-        host: str = "localhost",
-        port: str = "19530",
-        connection_pool: str = "SingletonThread",
-        index: str = "document",
-        vector_dim: Optional[int] = None,
-        embedding_dim: int = 768,
-        index_file_size: int = 1024,
-        similarity: str = "dot_product",
-        index_type: str = "IVF_FLAT",
-        index_param: Optional[Dict[str, Any]] = None,
-        search_param: Optional[Dict[str, Any]] = None,
-        return_embedding: bool = False,
-        embedding_field: str = "embedding",
-        id_field: str = "id",
-        custom_fields: Optional[List[Any]] = None,
-        progress_bar: bool = True,
-        duplicate_documents: str = "overwrite",
-        isolation_level: Optional[str] = None,
-        consistency_level: int = 0,
-        recreate_index: bool = False,
-    ):
-        warnings.warn(
-            "Milvus2DocumentStore is deprecated and will be removed in Haystack 1.13, please use MilvusDocumentStore instead",
-            category=FutureWarning,
-            stacklevel=2,
-        )
-        super().__init__(
-            sql_url=sql_url,
-            host=host,
-            port=port,
-            connection_pool=connection_pool,
-            index=index,
-            vector_dim=vector_dim,
-            embedding_dim=embedding_dim,
-            index_file_size=index_file_size,
-            similarity=similarity,
-            index_type=index_type,
-            index_param=index_param,
-            search_param=search_param,
-            return_embedding=return_embedding,
-            embedding_field=embedding_field,
-            id_field=id_field,
-            custom_fields=custom_fields,
-            progress_bar=progress_bar,
-            duplicate_documents=duplicate_documents,
-            isolation_level=isolation_level,
-            consistency_level=consistency_level,
-            recreate_index=recreate_index,
-        )
