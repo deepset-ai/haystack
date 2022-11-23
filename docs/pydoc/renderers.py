613871b1e2272af8a3119959393e7e191b0fb374
Massimiliano Pippi <mpippi@gmail.com>
2022-11-23 16:56:27 +0100

    add a POC to render the shortest import path

--- docs/pydoc/renderers.py
+++ docs/pydoc/renderers.py
@@ -3,8 +3,8 @@ import io
 import dataclasses
 import docspec
 import typing as t
-from pathlib import Path
-from pydoc_markdown.interfaces import Context, Renderer
+
+from pydoc_markdown.interfaces import Context, Renderer, Resolver
 from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
 
 
@@ -20,11 +20,26 @@ hidden: false
 """
 
 
+class HaystackMarkdownRenderer(MarkdownRenderer):
+    """
+    Custom Markdown renderer heavily based on the `MarkdownRenderer`,
+    it shows the import path for classes when present.
+
+    This only works in cooperation with a Processor that stores a
+    field called `import_path` into the objects
+    """
+
+    def _render_header(self, fp, level, obj):
+        super()._render_header(fp, level, obj)
+        if hasattr(obj, "import_path"):
+            fp.write(obj.import_path)
+            fp.write("\n\n")
+
+
 @dataclasses.dataclass
 class ReadmeRenderer(Renderer):
     """
-    This custom Renderer is heavily based on the `MarkdownRenderer`,
-    it just prepends a front matter so that the output can be published
+    This custom Renderer prepends a front matter so that the output can be published
     directly to readme.io.
     """
 
@@ -36,7 +51,7 @@ class ReadmeRenderer(Renderer):
     order: int
     # This exposes a special `markdown` settings value that can be used to pass
     # parameters to the underlying `MarkdownRenderer`
-    markdown: MarkdownRenderer = dataclasses.field(default_factory=MarkdownRenderer)
+    markdown: HaystackMarkdownRenderer = dataclasses.field(default_factory=HaystackMarkdownRenderer)
 
     def init(self, context: Context) -> None:
         self.markdown.init(context)
@@ -49,6 +64,14 @@ class ReadmeRenderer(Renderer):
             with io.open(self.markdown.filename, "w", encoding=self.markdown.encoding) as fp:
                 fp.write(self._frontmatter())
                 self.markdown.render_to_stream(modules, t.cast(t.TextIO, fp))
+
+    def process(self, modules: t.List[docspec.Module], resolver: t.Optional[Resolver]) -> None:
+        for mod in modules:
+            for m in mod.members:
+                if type(m) == docspec.Class:
+                    # FIXME: we should compute the real import path here
+                    m.import_path = "foo.bar.baz"
+        self.markdown.process(modules, resolver)
 
     def _frontmatter(self) -> str:
         return README_FRONTMATTER.format(
