eb90341870ff27d1a01c02d84b9079bd35299745
Tuana Celik <tuana.celik@deepset.ai>
2022-11-24 17:49:43 +0000

    updating PR based on comments

--- examples/basic_faq_pipeline.py
+++ examples/basic_faq_pipeline.py
@@ -1,13 +1,15 @@
 import logging
+
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
 logging.getLogger("haystack").setLevel(logging.INFO)
 
 from haystack.document_stores import ElasticsearchDocumentStore
 
 from haystack.nodes import EmbeddingRetriever
+from haystack.nodes.other.docs2answers import Docs2Answers
 from haystack.utils import launch_es, print_answers, fetch_archive_from_http
 import pandas as pd
-from haystack.pipelines import FAQPipeline
+from haystack.pipelines import Pipeline
 
 
 def basic_faq_pipeline():
@@ -24,7 +26,6 @@ def basic_faq_pipeline():
         similarity="cosine",
     )
 
-
     retriever = EmbeddingRetriever(
         document_store=document_store,
         embedding_model="sentence-transformers/all-MiniLM-L6-v2",
@@ -32,12 +33,14 @@ def basic_faq_pipeline():
         scale_score=False,
     )
 
+    doc_to_answers = Docs2Answers()
+
     doc_dir = "data/basic_faq_pipeline"
     s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/small_faq_covid.csv.zip"
     fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
     df = pd.read_csv(f"{doc_dir}/small_faq_covid.csv")
-    
+
     # Minimal cleaning
     df.fillna(value="", inplace=True)
     df["question"] = df["question"].apply(lambda x: x.strip())
@@ -53,9 +56,13 @@ def basic_faq_pipeline():
     document_store.write_documents(docs_to_index)
 
     # Initialize a Pipeline (this time without a reader) and ask questions
-    pipe = FAQPipeline(retriever=retriever)
+    pipeline = Pipeline()
+    pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
+    pipeline.add_node(component=doc_to_answers, name="Docs2Answers", inputs=["Retriever"])
 
-    prediction = pipe.run(query="How is the virus spreading?", params={"Retriever": {"top_k": 10}})
+    # Ask a question
+    prediction = pipeline.run(query="How is the virus spreading?", params={"Retriever": {"top_k": 10}})
+
     print_answers(prediction, details="medium")
 
 
--- examples/basic_qa_pipeline.py
+++ examples/basic_qa_pipeline.py
@@ -1,17 +1,24 @@
 import logging
+from pathlib import Path
+
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
 logging.getLogger("haystack").setLevel(logging.INFO)
 
 from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.utils import clean_wiki_text, convert_files_to_docs, fetch_archive_from_http, print_answers, launch_es
 from haystack.nodes import FARMReader, BM25Retriever
-from haystack.pipelines import ExtractiveQAPipeline
+from haystack.nodes.file_classifier import FileTypeClassifier
+from haystack.nodes.preprocessor import PreProcessor
+from haystack.nodes.file_converter import TextConverter
+from haystack.pipelines import Pipeline 
 from pprint import pprint
 
 
 def basic_qa_pipeline():
     # launch and create DocumentStore
     launch_es()
+
+    # Initialize a DocumentStore
     document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
 
     # fetch, pre-process and write documents
@@ -19,30 +26,49 @@ def basic_qa_pipeline():
     s3_url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip"
     fetch_archive_from_http(url=s3_url, output_dir=doc_dir)
 
-    docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=True)
-  
-    document_store.write_documents(docs)
+    file_paths = [p for p in Path(doc_dir).glob("**/*")]
+    files_metadata = [{"name": path.name} for path in file_paths]
+
+    # Indexing Pipeline
+    indexing_pipeline = Pipeline()
+
+    # Makes sure the file is a TXT file (FileTypeClassifier node)
+    classifier = FileTypeClassifier()
+    indexing_pipeline.add_node(classifier, name="Classifier", inputs=["File"])
+
+    # Converts a file into text and performs basic cleaning (TextConverter node)
+    text_converter = TextConverter(remove_numeric_tables=True)
+    indexing_pipeline.add_node(text_converter, name="Text_converter", inputs=["Classifier.output_1"])
+
+    # - Pre-processes the text by performing splits and adding metadata to the text (Preprocessor node)
+    preprocessor = PreProcessor(
+        clean_whitespace=True,
+        clean_empty_lines=True,
+        split_length=100,
+        split_overlap=50,
+        split_respect_sentence_boundary=True,
+    )
+    indexing_pipeline.add_node(preprocessor, name="Preprocessor", inputs=["Text_converter"])
+
+    # - Writes the resulting documents into the document store
+    indexing_pipeline.add_node(document_store, name="Document_Store", inputs=["Preprocessor"])
+
+    # Then we run it with the documents and their metadata as input
+    indexing_pipeline.run(file_paths=file_paths, meta=files_metadata)
 
     # Initialize Retriever & Reader
     retriever = BM25Retriever(document_store=document_store)
     reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
 
-    # Initialize ExtractiveQAPipeline
-    pipe = ExtractiveQAPipeline(reader, retriever)
+    # Query Pipeline
+    pipeline = Pipeline()
+    pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
+    pipeline.add_node(component=reader, name="Reader", inputs=["Retriever"])
 
-    # Query the pipleine
-    prediction = pipe.run(
+    prediction = pipeline.run(
         query="Who is the father of Arya Stark?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
     )
 
-    # prediction = pipe.run(query="Who created the Dothraki vocabulary?", params={"Reader": {"top_k": 5}})
-    # prediction = pipe.run(query="Who is the sister of Sansa?", params={"Reader": {"top_k": 5}})
-
-    # Now you can either print the object directly
-    print("\n\nRaw object:\n")
-    pprint(prediction)
-
-    print("\n\nSimplified output:\n")
     print_answers(prediction, details="minimum")
 
 
