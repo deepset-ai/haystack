6ca88bfd23b2114fa2748843e6d4e27e7d8e4db0
tstadel <60758086+tstadel@users.noreply.github.com>
2023-01-09 11:58:23 +0100

    fix: Despite return_embedding=False SearchEngineDocumentStore.query retrieves embedding_field (#3662)
    * fix: Despite return_embedding=False SearchEngineDocumentStore.query retrieves embedding_field
    
    * fix pylint
    
    * add tests
    
    * fix mypy
    
    * fix merge
    
    * format
    
    * fix pylint
    
    * move tests to SearchEngineDocumentStoreTestAbstract
    
    * move missed constants
    
    * add mocked_document_store fixture to TestElasticsearchDocumentStore
    
    * fix mocked_document_store
    
    * fix get_all_documents tests for elasticsearch>=7.16
    
    * fix tests
    
    * fix tests try 2

--- test/document_stores/test_opensearch.py
+++ test/document_stores/test_opensearch.py
@@ -26,7 +26,6 @@ from .test_search_engine import SearchEngineDocumentStoreTestAbstract
 class TestOpenSearchDocumentStore(DocumentStoreBaseTestAbstract, SearchEngineDocumentStoreTestAbstract):
 
     # Constants
-
     query_emb = np.random.random_sample(size=(2, 2))
     index_name = __name__
 
