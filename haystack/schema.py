429e393ca3e4e44a63ec3ad4b0dc6e21ad708c7c
ZanSara <sarazanzo94@gmail.com>
2023-01-17 14:04:20 +0100

    review feedback

--- haystack/schema.py
+++ haystack/schema.py
@@ -101,7 +101,9 @@ class Document:
 
         if id_hash_keys is not None:
             if not set(id_hash_keys) <= set(allowed_hash_key_attributes):
-                raise ValueError(
+
+                logging.error(
+                    # raise ValueError(
                     f"You passed custom strings {id_hash_keys} to id_hash_keys which is deprecated. Supply instead a "
                     f"list of Document's attribute names (like {', '.join(allowed_hash_key_attributes)}). "
                     "See https://github.com/deepset-ai/haystack/pull/1910 for details)"
@@ -152,11 +154,11 @@ class Document:
         they are serialized / stored in other places (e.g. elasticsearch)
         Example:
 
-        ```
+        ```python
             doc = Document(content="some text", content_type="text")
             doc.to_dict(field_map={"custom_content_field": "content"})
 
-            >>> {"custom_content_field": "some text", content_type": "text"}
+            # Returns {"custom_content_field": "some text", content_type": "text"}
         ```
 
         :param field_map: Dict with keys being the custom target keys and values being the standard Document attributes
