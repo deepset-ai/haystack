ce1aa55578d59313a5df5985f560cb701bdc4ecc
Mayank Jobanputra <mayankjobanputra@gmail.com>
2023-01-19 14:06:00 +0530

    fix: Allowing InMemStore and FAISSDocStore for indexing using single worker (#3868)
    * Allowing InMemStore and FAISSDocStore for indexing using single worker YAML config
    
    * unified pipeline & doc store loading
    
    * fix pylint warning
    
    * separated tests
    
    * removed unnecessay caplog

--- rest_api/rest_api/pipeline/__init__.py
+++ rest_api/rest_api/pipeline/__init__.py
@@ -13,9 +13,32 @@ from rest_api.controller.utils import RequestLimiter
 
 logger = logging.getLogger(__name__)
 
-# Since each instance of FAISSDocumentStore creates an in-memory FAISS index, the Indexing & Query Pipelines would
-# end up with different indices. The same applies for InMemoryDocumentStore.
-UNSUPPORTED_DOC_STORES = (FAISSDocumentStore, InMemoryDocumentStore)
+# Each instance of FAISSDocumentStore creates an in-memory FAISS index,
+# the Indexing & Query Pipelines will end up with different indices for each worker.
+# The same applies for InMemoryDocumentStore.
+SINGLE_PROCESS_DOC_STORES = (FAISSDocumentStore, InMemoryDocumentStore)
+
+
+def _load_pipeline(pipeline_yaml_path, indexing_pipeline_name):
+    # Load pipeline (if available)
+    try:
+        pipeline = Pipeline.load_from_yaml(Path(pipeline_yaml_path), pipeline_name=indexing_pipeline_name)
+        logging.info("Loaded pipeline nodes: %s", pipeline.graph.nodes.keys())
+        document_store = _get_pipeline_doc_store(pipeline)
+    except PipelineConfigError as e:
+        pipeline, document_store = None, None
+        logger.error(
+            "Error loading %s pipeline from %s. \n %s\n", indexing_pipeline_name, pipeline_yaml_path, e.message
+        )
+    return pipeline, document_store
+
+
+def _get_pipeline_doc_store(pipeline):
+    document_store = pipeline.get_document_store()
+    logging.info("Loaded docstore: %s", document_store)
+    if isinstance(document_store, SINGLE_PROCESS_DOC_STORES):
+        logger.warning("FAISSDocumentStore or InMemoryDocumentStore should only be used with 1 worker.")
+    return document_store
 
 
 def setup_pipelines() -> Dict[str, Any]:
@@ -24,14 +47,9 @@ def setup_pipelines() -> Dict[str, Any]:
 
     pipelines = {}
 
-    # Load query pipeline
-    query_pipeline = Pipeline.load_from_yaml(Path(config.PIPELINE_YAML_PATH), pipeline_name=config.QUERY_PIPELINE_NAME)
-    logging.info("Loaded pipeline nodes: %s", query_pipeline.graph.nodes.keys())
+    # Load query pipeline & document store
+    query_pipeline, document_store = _load_pipeline(config.PIPELINE_YAML_PATH, config.QUERY_PIPELINE_NAME)
     pipelines["query_pipeline"] = query_pipeline
-
-    # Find document store
-    document_store = query_pipeline.get_document_store()
-    logging.info("Loaded docstore: %s", document_store)
     pipelines["document_store"] = document_store
 
     # Setup concurrency limiter
@@ -39,24 +57,11 @@ def setup_pipelines() -> Dict[str, Any]:
     logging.info("Concurrent requests per worker: %s", config.CONCURRENT_REQUEST_PER_WORKER)
     pipelines["concurrency_limiter"] = concurrency_limiter
 
-    # Load indexing pipeline (if available)
-    try:
-        indexing_pipeline = Pipeline.load_from_yaml(
-            Path(config.PIPELINE_YAML_PATH), pipeline_name=config.INDEXING_PIPELINE_NAME
-        )
-        docstore = indexing_pipeline.get_document_store()
-        if isinstance(docstore, UNSUPPORTED_DOC_STORES):
-            indexing_pipeline = None
-            raise PipelineConfigError(
-                "Indexing pipelines with FAISSDocumentStore or InMemoryDocumentStore are not supported by the REST APIs."
-            )
-
-    except PipelineConfigError as e:
-        indexing_pipeline = None
-        logger.error("%s\nFile Upload API will not be available.", e.message)
-
-    finally:
-        pipelines["indexing_pipeline"] = indexing_pipeline
+    # Load indexing pipeline
+    index_pipeline, _ = _load_pipeline(config.PIPELINE_YAML_PATH, config.INDEXING_PIPELINE_NAME)
+    if not index_pipeline:
+        logger.warning("Indexing Pipeline is not setup. File Upload API will not be available.")
+    pipelines["indexing_pipeline"] = index_pipeline
 
     # Create directory for uploaded files
     os.makedirs(config.FILE_UPLOAD_PATH, exist_ok=True)
