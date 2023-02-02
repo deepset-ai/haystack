cbfe42f343058a6010b9d3292b451e133c04f763
jack.butler <jack.butler@faculty.ai>
2023-02-02 15:06:28 +0000

    refactor: simplify model reloading via classmethod

--- haystack/modeling/training/base.py
+++ haystack/modeling/training/base.py
@@ -281,8 +281,7 @@ class Trainer:
         # With early stopping we want to restore the best model
         if self.early_stopping and self.early_stopping.save_dir:
             logger.info("Restoring best model so far from {}".format(self.early_stopping.save_dir))
-            model_class = type(self.model)
-            self.model = model_class.load(self.early_stopping.save_dir, self.device)
+            self.model = self.model.load(self.early_stopping.save_dir, self.device)
             self.model.connect_heads_with_processor(self.data_silo.processor.tasks, require_labels=True)
 
         # Eval on test set
