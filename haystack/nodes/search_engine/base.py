f6afab7df2494654286a3ec60de7514d2e117bc5
Vladimir Blagojevic <dovlex@gmail.com>
2023-02-19 11:49:25 +0100

    Initial commit, add search_engine

---
+++ haystack/nodes/search_engine/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from haystack import Document
+
+
+class SearchEngine(ABC):
+    """
+    Abstract base class for search engines providers.
+    """
+
+    @abstractmethod
+    def search(self, query, **kwargs) -> List[Document]:
+        """
+        Search the search engine for the given query and return the results.
+        :param query: The query to search for.
+        :param kwargs: Additional parameters to pass to the search engine.
+        :return: List of search results as documents.
+        """
