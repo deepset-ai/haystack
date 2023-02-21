2ae77718a103ac8cfa8360f2b1d6f05627dc9ad5
Kshitijpawar <kshitijvijay271199@gmail.com>
2023-02-21 18:17:22 +0530

    formatted code using black

--- haystack/nodes/answer_generator/transformers.py
+++ haystack/nodes/answer_generator/transformers.py
@@ -418,10 +418,8 @@ class Seq2SeqGenerator(BaseGenerator):
             return cls._model_input_converters.get("vblagoje/bart_lfqa")
         elif model_name_or_path.endswith("bart_eli5"):
             return cls._model_input_converters.get("yjernite/bart_eli5")
-        
         # added original return statement for linting
         return cls._model_input_converters.get(model_name_or_path)
-
 
     def predict(self, query: str, documents: List[Document], top_k: Optional[int] = None) -> Dict:
         """
