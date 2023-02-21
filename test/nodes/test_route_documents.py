b9e8a1d51b84ca5b46907ed9a9dea16abfa02abc
Massimiliano Pippi <mpippi@gmail.com>
2023-02-21 10:18:27 +0100

    leftover

--- test/nodes/test_route_documents.py
+++ test/nodes/test_route_documents.py
@@ -22,6 +22,7 @@ def test_routedocuments_by_content_type():
     assert result["output_2"][0].content_type == "table"
 
 
+@pytest.mark.unit
 def test_routedocuments_by_metafield(docs):
     route_documents = RouteDocuments(split_by="meta_field", metadata_values=["test1", "test3", "test5"])
     assert route_documents.outgoing_edges == 3
