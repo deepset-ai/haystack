e1f0ca0abe30788ab85b6491561354f029c3fe8d
ZanSara <sarazanzo94@gmail.com>
2023-02-28 13:44:46 +0100

    mock all translator tests and move one to e2e

--- test/nodes/test_translator.py
+++ test/nodes/test_translator.py
@@ -1,58 +1,102 @@
-from haystack.schema import Document
-
 import pytest
 
-EXPECTED_OUTPUT = "Ich lebe in Berlin"
-INPUT = "I live in Berlin"
+import haystack
+from haystack.schema import Document
+from haystack.nodes import TransformersTranslator
 
-DOCUMENT_INPUT = Document(content=INPUT)
+
+ORIGINAL = "TEST QUERY"
+TRANSLATION = "MOCK TRANSLATION"
+
+
+class MockTokenizer:
+    @classmethod
+    def from_pretrained(cls, *a, **k):
+        return cls()
+
+    def __call__(self, *a, **k):
+        return self
+
+    def to(self, *a, **k):
+        return {}
+
+    def batch_decode(self, *a, **k):
+        return [TRANSLATION]
+
+
+class MockModel:
+    @classmethod
+    def from_pretrained(cls, *a, **k):
+        return cls()
+
+    def generate(self, *a, **k):
+        return None
+
+    def to(self, *a, **k):
+        return None
+
+
+@pytest.fixture
+def mock_models(monkeypatch):
+    monkeypatch.setattr(haystack.nodes.translator.transformers, "AutoModelForSeq2SeqLM", MockModel)
+    monkeypatch.setattr(haystack.nodes.translator.transformers, "AutoTokenizer", MockTokenizer)
+
+
+@pytest.fixture
+def en_to_de_translator(mock_models) -> TransformersTranslator:
+    return TransformersTranslator(model_name_or_path="Helsinki-NLP/opus-mt-en-de")
+
+
+@pytest.fixture
+def de_to_en_translator(mock_models) -> TransformersTranslator:
+    return TransformersTranslator(model_name_or_path="Helsinki-NLP/opus-mt-de-en")
 
 
 def test_translator_with_query(en_to_de_translator):
-    assert en_to_de_translator.translate(query=INPUT) == EXPECTED_OUTPUT
+    assert en_to_de_translator.translate(query=ORIGINAL) == TRANSLATION
 
 
 def test_translator_with_list(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[INPUT])[0] == EXPECTED_OUTPUT
+    assert en_to_de_translator.translate(documents=[ORIGINAL])[0] == TRANSLATION
 
 
 def test_translator_with_document(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[Document(content=INPUT)])[0].content == EXPECTED_OUTPUT
+    assert en_to_de_translator.translate(documents=[Document(content=ORIGINAL)])[0].content == TRANSLATION
 
 
-def test_translator_with_document_preserves_input(en_to_de_translator):
-    original_document = Document(content=INPUT)
-    en_to_de_translator.translate(documents=[original_document])[0]  # pylint: disable=expression-not-assigned
-    assert original_document.content == INPUT
+def test_translator_with_document_preserves_ORIGINAL(en_to_de_translator):
+    original_document = Document(content=ORIGINAL)
+    en_to_de_translator.translate(documents=[original_document])
+    assert original_document.content == ORIGINAL
 
 
 def test_translator_with_dictionary(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[{"content": INPUT}])[0]["content"] == EXPECTED_OUTPUT
+    assert en_to_de_translator.translate(documents=[{"content": ORIGINAL}])[0]["content"] == TRANSLATION
 
 
-def test_translator_with_dictionary_preserves_input(en_to_de_translator):
-    original_document = {"content": INPUT}
-    en_to_de_translator.translate(documents=[original_document])[0]  # pylint: disable=expression-not-assigned
-    assert original_document["content"] == INPUT
+def test_translator_with_dictionary_preserves_ORIGINAL(en_to_de_translator):
+    original_document = {"content": ORIGINAL}
+    en_to_de_translator.translate(documents=[original_document])
+    assert original_document["content"] == ORIGINAL
 
 
 def test_translator_with_dictionary_with_dict_key(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[{"key": INPUT}], dict_key="key")[0]["key"] == EXPECTED_OUTPUT
+    assert en_to_de_translator.translate(documents=[{"key": ORIGINAL}], dict_key="key")[0]["key"] == TRANSLATION
 
 
-def test_translator_with_empty_input(en_to_de_translator):
+def test_translator_with_empty_ORIGINAL(en_to_de_translator):
     with pytest.raises(AttributeError):
         en_to_de_translator.translate()
 
 
 def test_translator_with_query_and_documents(en_to_de_translator):
     with pytest.raises(AttributeError):
-        en_to_de_translator.translate(query=INPUT, documents=[INPUT])
+        en_to_de_translator.translate(query=ORIGINAL, documents=[ORIGINAL])
 
 
 def test_translator_with_dict_without_text_key(en_to_de_translator):
     with pytest.raises(AttributeError):
-        en_to_de_translator.translate(documents=[{"text1": INPUT}])
+        en_to_de_translator.translate(documents=[{"text1": ORIGINAL}])
 
 
 def test_translator_with_dict_with_non_string_value(en_to_de_translator):
