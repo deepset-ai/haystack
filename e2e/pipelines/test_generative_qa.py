6a22da125109bb3e23690e86fbcad5b3cccf597b
ZanSara <sarazanzo94@gmail.com>
2023-03-01 13:01:47 +0100

    rename original and improve pipeline tests

--- e2e/pipelines/test_generative_qa.py
+++ e2e/pipelines/test_generative_qa.py
@@ -1,12 +1,30 @@
+import numpy as np
+import pytest
+
+from haystack import Document
 from haystack.pipelines import TranslationWrapperPipeline, GenerativeQAPipeline
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.nodes import BM25Retriever, RAGenerator, TransformersTranslator
+from haystack.nodes import DensePassageRetriever, RAGenerator, TransformersTranslator
 
 
 def test_generative_pipeline_with_translator():
+    docs = [
+        Document(content="The capital of Germany is the city state of Berlin."),
+        Document(content="Berlin is the capital and largest city of Germany by both area and population."),
+    ]
     ds = InMemoryDocumentStore(use_bm25=True)
-    retriever = BM25Retriever(document_store=ds)
-    rag_generator = RAGenerator(model_name_or_path="facebook/rag-token-nq", generator_type="token", max_length=20)
+    ds.write_documents(docs)
+    retriever = DensePassageRetriever(  # Needs DPR or RAGenerator will thrown an exception...
+        document_store=ds,
+        query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+        passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+        use_gpu=False,
+        embed_title=True,
+    )
+    ds.update_embeddings(retriever=retriever)
+    rag_generator = RAGenerator(
+        model_name_or_path="facebook/rag-token-nq", generator_type="token", max_length=20, retriever=retriever
+    )
     en_to_de_translator = TransformersTranslator(model_name_or_path="Helsinki-NLP/opus-mt-en-de")
     de_to_en_translator = TransformersTranslator(model_name_or_path="Helsinki-NLP/opus-mt-de-en")
 
@@ -18,4 +36,4 @@ def test_generative_pipeline_with_translator():
     output = pipeline.run(query=query, params={"Generator": {"top_k": 2}, "Retriever": {"top_k": 1}})
     answers = output["answers"]
     assert len(answers) == 2
-    assert "berlin" in answers[0].answer
+    assert "berlin" in answers[0].answer.lower()
