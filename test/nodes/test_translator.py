4be603f507c43eb88c5ba0a0a36835f6a6cfd7bc
ZanSara <sarazanzo94@gmail.com>
2023-03-01 13:29:24 +0100

    fixes

--- test/nodes/test_translator.py
+++ test/nodes/test_translator.py
@@ -5,7 +5,7 @@ from haystack.schema import Document
 from haystack.nodes import TransformersTranslator
 
 
-ORIGINAL = "TEST QUERY"
+ORIGINAL_TEXT = "TEST QUERY"
 TRANSLATION = "MOCK TRANSLATION"
 
 
@@ -54,41 +54,41 @@ def de_to_en_translator(mock_models) -> TransformersTranslator:
 
 @pytest.mark.unit
 def test_translator_with_query(en_to_de_translator):
-    assert en_to_de_translator.translate(query=ORIGINAL) == TRANSLATION
+    assert en_to_de_translator.translate(query=ORIGINAL_TEXT) == TRANSLATION
 
 
 @pytest.mark.unit
 def test_translator_with_list(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[ORIGINAL])[0] == TRANSLATION
+    assert en_to_de_translator.translate(documents=[ORIGINAL_TEXT])[0] == TRANSLATION
 
 
 @pytest.mark.unit
 def test_translator_with_document(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[Document(content=ORIGINAL)])[0].content == TRANSLATION
+    assert en_to_de_translator.translate(documents=[Document(content=ORIGINAL_TEXT)])[0].content == TRANSLATION
 
 
 @pytest.mark.unit
 def test_translator_with_document_preserves_original(en_to_de_translator):
-    original_document = Document(content=ORIGINAL)
+    original_document = Document(content=ORIGINAL_TEXT)
     en_to_de_translator.translate(documents=[original_document])
-    assert original_document.content == ORIGINAL
+    assert original_document.content == ORIGINAL_TEXT
 
 
 @pytest.mark.unit
 def test_translator_with_dictionary(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[{"content": ORIGINAL}])[0]["content"] == TRANSLATION
+    assert en_to_de_translator.translate(documents=[{"content": ORIGINAL_TEXT}])[0]["content"] == TRANSLATION
 
 
 @pytest.mark.unit
 def test_translator_with_dictionary_preserves_original(en_to_de_translator):
-    original_document = {"content": ORIGINAL}
+    original_document = {"content": ORIGINAL_TEXT}
     en_to_de_translator.translate(documents=[original_document])
-    assert original_document["content"] == ORIGINAL
+    assert original_document["content"] == ORIGINAL_TEXT
 
 
 @pytest.mark.unit
 def test_translator_with_dictionary_with_dict_key(en_to_de_translator):
-    assert en_to_de_translator.translate(documents=[{"key": ORIGINAL}], dict_key="key")[0]["key"] == TRANSLATION
+    assert en_to_de_translator.translate(documents=[{"key": ORIGINAL_TEXT}], dict_key="key")[0]["key"] == TRANSLATION
 
 
 @pytest.mark.unit
@@ -100,13 +100,13 @@ def test_translator_with_empty_original(en_to_de_translator):
 @pytest.mark.unit
 def test_translator_with_query_and_documents(en_to_de_translator):
     with pytest.raises(AttributeError):
-        en_to_de_translator.translate(query=ORIGINAL, documents=[ORIGINAL])
+        en_to_de_translator.translate(query=ORIGINAL_TEXT, documents=[ORIGINAL_TEXT])
 
 
 @pytest.mark.unit
 def test_translator_with_dict_without_text_key(en_to_de_translator):
     with pytest.raises(AttributeError):
-        en_to_de_translator.translate(documents=[{"text1": ORIGINAL}])
+        en_to_de_translator.translate(documents=[{"text1": ORIGINAL_TEXT}])
 
 
 @pytest.mark.unit
