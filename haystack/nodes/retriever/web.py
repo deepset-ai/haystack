0d474fb6e80dc70716590bbafbbf82286d1fd9f7
Daniel Bichuetti <daniel.bichuetti@gmail.com>
2023-03-10 03:41:49 +0000

    refactor: remove doc ids rebuild + antecipate cache

--- haystack/nodes/retriever/web.py
+++ haystack/nodes/retriever/web.py
@@ -29,13 +29,10 @@ class WebRetriever(BaseRetriever):
         top_p: Optional[float] = 0.95,
         mode: Literal["snippet", "document"] = "document",
         preprocessor: Optional[PreProcessor] = None,
-        document_store: Optional[BaseDocumentStore] = None,
-        document_index: Optional[str] = None,
-        document_headers: Optional[Dict[str, str]] = None,
         cache_document_store: Optional[BaseDocumentStore] = None,
         cache_index: Optional[str] = None,
         cache_headers: Optional[Dict[str, str]] = None,
-        cache_time: int = 24 * 60 * 60,
+        cache_time: int = 2 * 24 * 60 * 60,
     ):
         """
         Collect complete documents from the web using the links provided by a WebSearch node
@@ -44,9 +41,6 @@ class WebRetriever(BaseRetriever):
         self.web_search = web_search
         self.mode = mode
         self.preprocessor = preprocessor
-        self.document_store = document_store
-        self.document_index = document_index
-        self.document_headers = document_headers
         self.cache_document_store = cache_document_store
         self.cache_index = cache_index
         self.cache_headers = cache_headers
@@ -94,8 +88,6 @@ class WebRetriever(BaseRetriever):
         cache_document_store = self.cache_document_store
 
         if cache_document_store is not None:
-            documents = [self._rebuild_cache_document(document) for document in documents]
-
             cache_document_store.write_documents(
                 documents=documents, index=cache_index, headers=cache_headers, duplicate_documents="overwrite"
             )
@@ -117,59 +109,11 @@ class WebRetriever(BaseRetriever):
 
         return False
 
-    def _save_documents(
-        self, documents: List[Document], index: Optional[str] = None, headers: Optional[Dict[str, str]] = None
-    ) -> bool:
-        logger.debug("Saving %d documents", len(documents))
-
-        if self.document_store is not None:
-            documents = [self._rebuild_document(document) for document in documents]
-
-            self.document_store.write_documents(
-                documents=documents, index=index, headers=headers, duplicate_documents="overwrite"
-            )
-
-            return True
-
-        return False
-
-    def _rebuild_document(self, document: Document) -> Document:
-        final_hash_key = document.content
-
-        if document.meta:
-            if "url" in document.meta:
-                final_hash_key += ":" + document.meta["url"]
-            if "split_id" in document.meta:
-                final_hash_key += ":" + document.meta["split_id"]
-
-        document.id = "{:02x}".format(mmh3.hash128(final_hash_key, signed=False))
-
-        return document
-
-    def _rebuild_cache_document(self, document: Document) -> Document:
-        final_hash_key = "cache:"
-        if document.meta:
-            if "query" in document.meta:
-                final_hash_key += document.meta["query"]
-            if "url" in document.meta:
-                final_hash_key += ":" + document.meta["url"]
-            if "split_id" in document.meta:
-                final_hash_key += ":" + document.meta["split_id"]
-        else:
-            final_hash_key = document.content
-
-        document.id = "{:02x}".format(mmh3.hash128(final_hash_key, signed=False))
-
-        return document
-
     def retrieve(
         self,
         query: str,
         top_p: Optional[int] = None,
         preprocessor: Optional[PreProcessor] = None,
-        document_store: Optional[BaseDocumentStore] = None,
-        document_index: Optional[str] = None,
-        document_headers: Optional[Dict[str, str]] = None,
         cache_document_store: Optional[BaseDocumentStore] = None,
         cache_index: Optional[str] = None,
         cache_headers: Optional[Dict[str, str]] = None,
@@ -189,12 +133,6 @@ class WebRetriever(BaseRetriever):
         """
         if preprocessor is None:
             preprocessor = self.preprocessor
-        if document_store is None:
-            document_store = self.document_store
-        if document_index is None:
-            document_index = self.document_index
-        if document_headers is None:
-            document_headers = self.document_headers
         if cache_document_store is None:
             cache_document_store = self.cache_document_store
         if cache_index is None:
@@ -205,116 +143,136 @@ class WebRetriever(BaseRetriever):
             cache_time = self.cache_time
 
         query_norm = self._normalize_query(query)
+
+        extracted_docs = []
         if cache_document_store:
             documents = self._check_cache(
                 query_norm, cache_index=cache_index, cache_headers=cache_headers, cache_time=cache_time
             )
             if documents and len(documents) > 0:
-                return documents
+                extracted_docs = documents
 
-        search_results, _ = self.web_search.run(query=query)
-        if self.sampler and search_results["documents"]:
-            search_results, _ = self.sampler.run(query, search_results["documents"], top_p=top_p)
-        search_results = search_results["documents"]
-        if self.mode == "snippet":
-            return search_results
+        if len(extracted_docs) == 0:
+            search_results, _ = self.web_search.run(query=query)
 
-        links: List[Tuple[str, Union[str, None], Union[str, None]]] = [
-            (
-                r.meta["link"],
-                r.meta["score"] if r.meta.get("score") else None,
-                r.meta["position"] if r.meta.get("position") else None,
-            )
-            for r in search_results
-            if r.meta.get("link")
-        ]
-        extracted_docs = []
+            if self.sampler and search_results["documents"]:
+                search_results, _ = self.sampler.run(query, search_results["documents"], top_p=top_p)
+            search_results = search_results["documents"]
+            if self.mode == "snippet":
+                return search_results
 
-        def scrape_direct(link) -> Dict[str, Any]:
-            try:
-                response = fetch_url(link[0])
-
-                if response is None:
-                    logger.debug("No response from URL %s, trying Google Cache", link[0])
-                    response = fetch_url(f"https://webcache.googleusercontent.com/search?q=cache:{link[0]}")
-
-                if response is not None:
-                    extracted = bare_extraction(
-                        response,
-                        include_comments=False,
-                        include_tables=False,
-                        include_links=False,
-                        deduplicate=True,
-                        date_extraction_params={"extensive_search": True, "outputformat": "%Y-%m-%d"},
-                    )
-
-                    if extracted is not None and isinstance(extracted, dict):
-                        extracted["url"] = clean_url(link[0])
-                        extracted["search.score"] = link[1]
-                        extracted["search.position"] = link[2]
-
-                        return {
-                            k: v
-                            for k, v in extracted.items()
-                            if k not in ["fingerprint", "license", "body", "comments", "raw_text", "commentsbody"]
-                        }
-
-                return {}
-
-            except Exception as e:
-                logger.error("Error retrieving URL %s: %s", link[0], e)
-                return {}
-
-        thread_count = cpu_count() if len(links) > cpu_count() else len(links)
-        with ThreadPoolExecutor(max_workers=thread_count) as executor:
-            results: Iterator[Dict[str, Any]] = executor.map(scrape_direct, links)
-
-            failed = 0
+            links: List[Tuple[str, Union[str, None], Union[str, None]]] = [
+                (
+                    r.meta["link"],
+                    r.meta["score"] if r.meta.get("score") else None,
+                    r.meta["position"] if r.meta.get("position") else None,
+                )
+                for r in search_results
+                if r.meta.get("link")
+            ]
             extracted_docs = []
-            for r, s in zip(results, search_results):
-                if r is not None and "text" in r:
-                    r["id_hash_keys"] = ["content", "meta"]
 
-                    document = Document.from_dict(r, field_map={"text": "content"})
+            def scrape_direct(link) -> Dict[str, Any]:
+                try:
+                    response = fetch_url(link[0])
 
-                    document.meta["timestamp"] = int(datetime.utcnow().timestamp())
-                    document.meta["search.position"] = s.meta.get("position")
-                    document.meta["search.score"] = s.meta.get("score")
-                    document.meta["search.query"] = query_norm
+                    if response is None:
+                        logger.debug("No response from URL %s, trying Google Cache", link[0])
+                        response = fetch_url(f"https://webcache.googleusercontent.com/search?q=cache:{link[0]}")
 
-                    extracted_docs.append(document)
-                else:
-                    logger.warning("Could not extract text from URL %s. Using search snippet.", s.meta["link"])
+                    if response is not None:
+                        extracted = bare_extraction(
+                            response,
+                            include_comments=False,
+                            include_tables=False,
+                            include_links=False,
+                            deduplicate=True,
+                            date_extraction_params={
+                                "extensive_search": True,
+                                "outputformat": "%Y-%m-%d",
+                                "original_date": True,
+                            },
+                        )
 
-                    if "date" in s.meta:
-                        s.meta["date"] = find_date(s.meta["date"], extensive_search=True, outputformat="%Y-%m-%d")
+                        if extracted is not None and isinstance(extracted, dict):
+                            extracted["url"] = clean_url(link[0])
+                            extracted["search.score"] = link[1]
+                            extracted["search.position"] = link[2]
 
-                    if "link" in s.meta:
-                        s.meta["url"] = clean_url(s.meta["link"])
-                        del s.meta["link"]
+                            return {
+                                k: v
+                                for k, v in extracted.items()
+                                if k not in ["fingerprint", "license", "body", "comments", "raw_text", "commentsbody"]
+                            }
 
-                    score = s.meta.get("score")
-                    if score is not None:
-                        del s.meta["score"]
+                    return {}
 
-                    position = s.meta.get("position")
-                    if position is not None:
-                        del s.meta["position"]
+                except Exception as e:
+                    logger.error("Error retrieving URL %s: %s", link[0], e)
+                    return {}
 
-                    s.meta["id_hash_keys"] = ["content", "meta"]
-                    s.meta["search.query"] = query_norm
+            thread_count = cpu_count() if len(links) > cpu_count() else len(links)
+            with ThreadPoolExecutor(max_workers=thread_count) as executor:
+                results: Iterator[Dict[str, Any]] = executor.map(scrape_direct, links)
 
-                    s.meta["search.position"] = position
-                    s.meta["search.score"] = score
-                    s.meta["search.snippet"] = 1
-                    s.meta["timestamp"] = int(datetime.utcnow().timestamp())
+                failed = 0
+                extracted_docs = []
+                for r, s in zip(results, search_results):
+                    if r is not None and "text" in r:
+                        r["id_hash_keys"] = ["meta.url", "meta.search.query"]
+                        r["search.query"] = query_norm
 
-                    extracted_docs.append(s)
-                    failed += 1
+                        document = Document.from_dict(r, field_map={"text": "content"})
 
-            logger.debug(
-                "Extracted %d documents / %s snippet from %s URLs.", len(extracted_docs) - failed, failed, len(links)
-            )
+                        document.meta["timestamp"] = int(datetime.utcnow().timestamp())
+                        document.meta["search.position"] = s.meta.get("position")
+                        document.meta["search.score"] = s.meta.get("score")
+
+                        extracted_docs.append(document)
+                    else:
+                        logger.warning("Could not extract text from URL %s. Using search snippet.", s.meta["link"])
+
+                        if "date" in s.meta:
+                            s.meta["date"] = find_date(s.meta["date"], extensive_search=True, outputformat="%Y-%m-%d")
+
+                        if "link" in s.meta:
+                            s.meta["url"] = clean_url(s.meta["link"])
+                            del s.meta["link"]
+
+                        score = s.meta.get("score")
+                        if score is not None:
+                            del s.meta["score"]
+
+                        position = s.meta.get("position")
+                        if position is not None:
+                            del s.meta["position"]
+
+                        s.meta["id_hash_keys"] = ["meta.url", "meta.search.query"]
+                        s.meta["search.query"] = query_norm
+
+                        s._get_id(id_hash_keys=s.meta["id_hash_keys"])
+
+                        s.meta["search.position"] = position
+                        s.meta["search.score"] = score
+                        s.meta["search.snippet"] = 1
+                        s.meta["timestamp"] = int(datetime.utcnow().timestamp())
+
+                        extracted_docs.append(s)
+                        failed += 1
+
+                logger.debug(
+                    "Extracted %d documents / %s snippet from %s URLs.",
+                    len(extracted_docs) - failed,
+                    failed,
+                    len(links),
+                )
+
+        if cache_document_store:
+            cached = self._save_cache(query_norm, extracted_docs, cache_index=cache_index, cache_headers=cache_headers)
+            if not cached:
+                logger.warning(
+                    "Could not save documents to cache document store. Please check your document store configuration."
+                )
 
         processed_docs = []
 
@@ -345,30 +303,13 @@ class WebRetriever(BaseRetriever):
             processed_docs, _ = self.sampler.run(query, processed_docs)
             processed_docs = processed_docs["documents"]
 
-        if cache_document_store is not None:
-            cached = self._save_cache(query_norm, processed_docs, cache_index=cache_index, cache_headers=cache_headers)
-            if not cached:
-                logger.warning(
-                    "Could not save documents to cache document store. Please check your document store configuration."
-                )
-
-        if document_store is not None:
-            saved = self._save_documents(documents=processed_docs, index=document_index, headers=document_headers)
-            if not saved:
-                logger.warning(
-                    "Could not save documents to document store. Please check your document store configuration."
-                )
-
-        return processed_docs
+        return processed_docs if processed_docs else []
 
     def retrieve_batch(
         self,
         queries: List[str],
         top_p: Optional[int] = None,
         preprocessor: Optional[PreProcessor] = None,
-        document_store: Optional[BaseDocumentStore] = None,
-        document_index: Optional[str] = None,
-        document_headers: Optional[Dict[str, str]] = None,
         cache_document_store: Optional[BaseDocumentStore] = None,
         cache_index: Optional[str] = None,
         cache_headers: Optional[Dict[str, str]] = None,
@@ -383,9 +324,6 @@ class WebRetriever(BaseRetriever):
                     q,
                     top_p=top_p,
                     preprocessor=preprocessor,
-                    document_store=document_store,
-                    document_index=document_index,
-                    document_headers=document_headers,
                     cache_document_store=cache_document_store,
                     cache_index=cache_index,
                     cache_headers=cache_headers,
