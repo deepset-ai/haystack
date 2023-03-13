7ad7f2ddcf98909284e56c7c82dca1c9f15c196a
ZanSara <sara.zanzottera@deepset.ai>
2023-03-13 10:10:02 +0100

    improve the root imports

--- canals/__init__.py
+++ canals/__init__.py
@@ -1,6 +1,15 @@
 # SPDX-FileCopyrightText: 2023-present U.N. Owen <void@some.where>
 #
 # SPDX-License-Identifier: MIT
-from canals.pipeline import Pipeline, save_pipelines, load_pipelines, marshal_pipelines, unmarshal_pipelines
+from canals.pipeline import (
+    Pipeline,
+    PipelineError,
+    PipelineRuntimeError,
+    PipelineValidationError,
+    save_pipelines,
+    load_pipelines,
+    marshal_pipelines,
+    unmarshal_pipelines,
+)
 from canals.node import node
 from canals.__about__ import __version__
