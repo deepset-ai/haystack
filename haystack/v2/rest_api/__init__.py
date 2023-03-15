aa650ca5ffbf332a9ebf1491cefb091a21cfaa90
ZanSara <sara.zanzottera@deepset.ai>
2023-03-15 14:43:06 +0100

    imports

--- haystack/v2/rest_api/__init__.py
+++ haystack/v2/rest_api/__init__.py
@@ -1,2 +1,2 @@
-from haystack.v2.rest_api.test.nodes.test_add_value import AddValue
-from haystack.v2.rest_api.test.nodes.test_double import Double
+from haystack.v2.rest_api.test.nodes.add_value import AddValue
+from haystack.v2.rest_api.test.nodes.double import Double
