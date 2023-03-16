fa138116e82447dc4b6a8e3461a60f28357bc43c
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2023-03-16 14:42:36 +0100

    try to fix black

--- haystack/utils/openai_utils.py
+++ haystack/utils/openai_utils.py
@@ -91,10 +91,10 @@ def _openai_text_completion_tokenization_details(model_name: str):
             tokenizer_name = model_tokenizer
         # Ref: https://platform.openai.com/docs/models/gpt-4
         elif model_name.startswith("gpt-4-32k"):
-            max_tokens_limit = 32768 # tokens
+            max_tokens_limit = 32768  # tokens
             tokenizer_name = model_tokenizer
         elif model_name.startswith("gpt-4"):
-            max_tokens_limit = 8192 # tokens
+            max_tokens_limit = 8192  # tokens
             tokenizer_name = model_tokenizer
         else:
             tokenizer_name = model_tokenizer
