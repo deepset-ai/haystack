371d87a3a10cb3007e86b4a8d12baeb3d487733d
Silvano Cerza <silvanocerza@gmail.com>
2023-03-27 11:22:20 +0200

    Deprecate GraphDBKnowledgeGraph

--- test/document_stores/test_graphdb.py
+++ test/document_stores/test_graphdb.py
@@ -11,8 +11,12 @@ def test_graphdb_knowledge_graph_deprecation_warning():
     with pytest.warns(DeprecationWarning) as w:
         GraphDBKnowledgeGraph()
 
-        assert len(w) == 1
+        assert len(w) == 2
         assert (
             w[0].message.args[0]
+            == "The GraphDBKnowledgeGraph component is deprecated and will be removed in future versions."
+        )
+        assert (
+            w[1].message.args[0]
             == "The BaseKnowledgeGraph component is deprecated and will be removed in future versions."
         )
