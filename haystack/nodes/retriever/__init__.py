95ff954d2d1c5e0e5266776e15dbaa625dead9c8
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-03-27 13:47:47 +0200

    refactor: Remove ElasticsearchRetriever and ElasticsearchFilterOnlyRetriever (#4499)

--- haystack/nodes/retriever/__init__.py
+++ haystack/nodes/retriever/__init__.py
@@ -7,11 +7,5 @@ from haystack.nodes.retriever.dense import (
     TableTextRetriever,
 )
 from haystack.nodes.retriever.multimodal import MultiModalRetriever
-from haystack.nodes.retriever.sparse import (
-    BM25Retriever,
-    ElasticsearchFilterOnlyRetriever,
-    ElasticsearchRetriever,
-    FilterRetriever,
-    TfidfRetriever,
-)
+from haystack.nodes.retriever.sparse import BM25Retriever, FilterRetriever, TfidfRetriever
 from haystack.nodes.retriever.text2sparql import Text2SparqlRetriever
