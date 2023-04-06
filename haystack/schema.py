5e8a519e4bf52292ecb53478192e43ad382a2334
Sebastian Lee <sebastian.lee@deepset.ai>
2023-04-06 15:26:50 +0100

    Undo change to MultiLabel

--- haystack/schema.py
+++ haystack/schema.py
@@ -678,7 +678,7 @@ class MultiLabel:
         # If we do not exclude them from document_ids this would be problematic for retriever evaluation as they do not contain the answer.
         # Hence, we exclude them here as well.
         self._document_ids = [l.document.id for l in self._labels if not l.no_answer]
-        self._contexts = [l.document.to_dict()["content"] for l in self._labels if not l.no_answer]
+        self._contexts = [str(l.document.content) for l in self._labels if not l.no_answer]
 
     @property
     def labels(self):
