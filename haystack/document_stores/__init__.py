63e1a6d210d262ae00e70844c587d5a6159f450f
ZanSara <sara.zanzottera@deepset.ai>
2023-04-14 11:48:45 +0200

    update pyproject.toml

--- haystack/document_stores/__init__.py
+++ haystack/document_stores/__init__.py
@@ -7,12 +7,14 @@ from haystack.document_stores.memory import InMemoryDocumentStore
 from haystack.document_stores.deepsetcloud import DeepsetCloudDocumentStore
 from haystack.document_stores.utils import eval_data_from_json, eval_data_from_jsonl, squad_json_to_jsonl
 
-from haystack.document_stores.elasticsearch import ElasticsearchDocumentStore
 from haystack.document_stores.es_converter import (
     elasticsearch_index_to_document_store,
     open_search_index_to_document_store,
 )
 
+ElasticsearchDocumentStore = safe_import(
+    "haystack.document_stores.elasticsearch", "ElasticsearchDocumentStore", "elasticsearch"
+)
 OpenSearchDocumentStore = safe_import("haystack.document_stores.opensearch", "OpenSearchDocumentStore", "opensearch")
 SQLDocumentStore = safe_import("haystack.document_stores.sql", "SQLDocumentStore", "sql")
 FAISSDocumentStore = safe_import("haystack.document_stores.faiss", "FAISSDocumentStore", "faiss")
