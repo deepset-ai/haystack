ed9c0fce3562edebcaaa5ef75ef9bf2d99ac6a1e
recrudesce <recrudesce@gmail.com>
2023-04-15 12:11:25 +0100

    feat: Add AzureChatGPT Capability using new InvocationLayer style

--- haystack/nodes/prompt/invocation_layer/__init__.py
+++ haystack/nodes/prompt/invocation_layer/__init__.py
@@ -1,6 +1,7 @@
 from haystack.nodes.prompt.invocation_layer.azure_open_ai import AzureOpenAIInvocationLayer
 from haystack.nodes.prompt.invocation_layer.base import PromptModelInvocationLayer
 from haystack.nodes.prompt.invocation_layer.chatgpt import ChatGPTInvocationLayer
+from haystack.nodes.prompt.invocation_layer.azure_chatgpt import AzureChatGPTInvocationLayer
 from haystack.nodes.prompt.invocation_layer.handlers import TokenStreamingHandler, DefaultTokenStreamingHandler
 from haystack.nodes.prompt.invocation_layer.hugging_face import HFLocalInvocationLayer
 from haystack.nodes.prompt.invocation_layer.hugging_face_inference import HFInferenceEndpointInvocationLayer
