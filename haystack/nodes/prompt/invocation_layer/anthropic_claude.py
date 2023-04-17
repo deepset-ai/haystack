9504b304f424b658843f4bf81989ef59dc06c330
Silvano Cerza <silvanocerza@gmail.com>
2023-04-17 15:18:46 +0200

    Use correct tokenizer and fix tests

--- haystack/nodes/prompt/invocation_layer/anthropic_claude.py
+++ haystack/nodes/prompt/invocation_layer/anthropic_claude.py
@@ -4,8 +4,9 @@ import json
 import logging
 
 import requests
+import requests_cache
 import sseclient
-from transformers import GPT2TokenizerFast
+from tokenizers import Tokenizer, Encoding
 
 from haystack.errors import AnthropicError, AnthropicRateLimitError, AnthropicUnauthorizedError
 from haystack.nodes.prompt.invocation_layer.base import PromptModelInvocationLayer
@@ -17,6 +18,11 @@ ANTHROPIC_TIMEOUT = float(os.environ.get(HAYSTACK_REMOTE_API_TIMEOUT_SEC, 30))
 ANTHROPIC_MAX_RETRIES = int(os.environ.get(HAYSTACK_REMOTE_API_MAX_RETRIES, 5))
 
 logger = logging.getLogger(__name__)
+
+# Taken from:
+# https://github.com/anthropics/anthropic-sdk-python/blob/main/anthropic/tokenizer.py#L7
+# This is a JSON config to load the tokenizer used for Anthropic Claude.
+CLAUDE_TOKENIZER_REMOTE_FILE = "https://public-json-tokenization-0d8763e8-0d7e-441b-a1e2-1c73b8e79dc3.storage.googleapis.com/claude-v1-tokenization.json"
 
 
 class AnthropicClaudeInvocationLayer(PromptModelInvocationLayer):
@@ -52,7 +58,16 @@ class AnthropicClaudeInvocationLayer(PromptModelInvocationLayer):
         # Number of max tokens is based on the official Anthropic model pricing from:
         # https://cdn2.assets-servd.host/anthropic-website/production/images/FINAL-PRICING.pdf
         self.max_tokens_limit = 9000
-        self.tokenizer: GPT2TokenizerFast = GPT2TokenizerFast.from_pretrained("gpt2")
+        self.tokenizer: Tokenizer = self._init_tokenizer()
+
+    def _init_tokenizer(self) -> Tokenizer:
+        # Expire cache after a day
+        expire_after = 60 * 60 * 60 * 24
+        # Cache the JSON config to avoid downloading it each time as it's a big file
+        with requests_cache.enabled(expire_after=expire_after):
+            res = request_with_retry(method="GET", url=CLAUDE_TOKENIZER_REMOTE_FILE)
+            res.raise_for_status()
+        return Tokenizer.from_str(res.text)
 
     def invoke(self, *args, **kwargs):
         """
@@ -127,25 +142,28 @@ class AnthropicClaudeInvocationLayer(PromptModelInvocationLayer):
         If needed, truncate the prompt text so that it fits within the limit.
         :param prompt: Prompt text to be sent to the generative model.
         """
-        n_prompt_tokens = len(self.tokenizer.tokenize(prompt))
-        n_answer_tokens = self.max_length
-        if (n_prompt_tokens + n_answer_tokens) <= self.max_tokens_limit:
-            return prompt
+        # Tokenizer can handle truncation by itself
+        token_limit = self.max_tokens_limit - self.max_length
+        self.tokenizer.enable_truncation(token_limit)
 
-        logger.warning(
-            "The prompt has been truncated from %s tokens to %s tokens such that the prompt length and "
-            "answer length (%s tokens) fits within the max token limit (%s tokens). "
-            "Reduce the length of the prompt to prevent it from being cut off.",
-            n_prompt_tokens,
-            self.max_tokens_limit - n_answer_tokens,
-            n_answer_tokens,
-            self.max_tokens_limit,
-        )
+        # Note: This will fail if the prompt is a List of Dicts.
+        # The tokenizer we're using accepts either str or List[str],
+        # if a List[str] is used we must also set is_pretokenized to True.
+        encoded_prompt: Encoding = self.tokenizer.encode(prompt.split(" "), is_pretokenized=True)
 
-        tokenized_payload = self.tokenizer.tokenize(prompt)
-        token_limit = self.max_tokens_limit - n_answer_tokens
-        decoded_string = self.tokenizer.convert_tokens_to_string(tokenized_payload[:token_limit])
-        return decoded_string
+        # overflowing is the list of tokens that have been truncated
+        if encoded_prompt.overflowing:
+            logger.warning(
+                "The prompt has been truncated from %s tokens to %s tokens such that the prompt length and "
+                "answer length (%s tokens) fits within the max token limit (%s tokens). "
+                "Reduce the length of the prompt to prevent it from being cut off.",
+                len(encoded_prompt.ids) + len(encoded_prompt.overflowing),
+                self.max_tokens_limit - self.max_length,
+                self.max_length,
+                self.max_tokens_limit,
+            )
+
+        return " ".join(encoded_prompt.tokens)
 
     def _post(
         self,
