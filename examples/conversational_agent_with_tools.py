3a33b229d8567e60c53e8e076e598bcb7a610dd9
Vladimir Blagojevic <dovlex@gmail.com>
2023-04-21 19:43:19 +0200

    Make ConversationalAgentWithTools use conversational history tool

--- examples/conversational_agent_with_tools.py
+++ examples/conversational_agent_with_tools.py
@@ -6,47 +6,53 @@ from haystack.nodes import PromptNode, WebRetriever, PromptTemplate
 from haystack.pipelines import WebQAPipeline
 
 few_shot_prompt = """
-Answer questions as accurately as possible. If you're unsure or if the information might be outdated or inaccurate,
-use the available tools when necessary to find the most up-to-date and accurate information. The available tools are:
-
+In the conversation below, a Human interacts with an AI Agent. The Human asks questions, and the AI goes through several steps to answer each question. If the AI Agent is unsure about the answer or if the information might be outdated or inaccurate, it uses the available tools when necessary to find the most up-to-date and accurate information. The AI has access to these tools:
 {tool_names_with_descriptions}
+To answer a question, the AI Agent might go through several steps. Each step starts with a Thought, Tool, Tool Input, Observation, or Final Answer. An Observation marks the beginning of the tool result, and the AI trusts these results.
 
-Always follow the steps below to answer a question. Each step starts with either a Thought, Tool, Tool Input,
-Observation, or Final Answer. Trust the results of the tools, don't try to second-guess them.
-
-Example 1:
+Examples:
+##
 Question: What is the capital of France?
-Thought: This is a common knowledge question, so I'll answer it directly.
+Thought: This is a common-knowledge question. Therefore, I'll provide the final answer directly without consulting any tools.
 Final Answer: The capital of France is Paris.
-
-Example 2: Who was the first president of the United States?
-Thought: This is a common knowledge question, so I'll answer it directly.
+##
+Question: Who was the first president of the United States?
+Thought: This is a common knowledge question. I'll provide the final answer directly without using any tools.
 Final Answer: The first president of the United States was George Washington.
-
-Example 3:
+##
 Question: What is the latest version of Python programming language?
-Thought: As of my knowledge cutoff date in September 2021, I'll use the search plugin to help me answer the question.
+Thought: As of my knowledge cutoff date in September 2021, I'll use the search tool to help me answer the question.
 Tool: Search
 Tool Input: What is the latest version of Python programming language as of today?
 Observation: 3.11.2
-Thought: According to Search Tool, the latest Python version is 3.11.2, now I can answer the question.
+Thought: We've learned that the latest Python version is 3.11.2! Now I can provide the final answer.
 Final Answer: The latest Python version is 3.11.2.
-
-Example 4:
-Question: How did life evolve from nonliving matter?
-Thought: This is a complex question, so I'll use the search plugin to help me answer the question.
+##
+Question: What happened with the latest SpaceX Starship launch?
+Thought: I can't answer most recent events due to my knowledge cutoff date; I'll use the search tool to help me answer the question.
 Tool: Search
-Tool Input: How did life evolve from nonliving matter?
-Observation: MIT physicist Dr. Jeremy England recently put forward a theory that attempts to explain the origin of life in terms of fundamental principles of physics. In this view, life is the inevitable result of rising entropy.
-Thought: Search Tool has provided me with additional information to what I already know, therefore I'll now use to answer the question.
-Final Answer: If life is the inevitable result of rising entropy then the arrival of life should not be a surprise. The idea is highly speculative. Recent computer simulations, however, may be lending support to it. The simulations show that ordinary chemical reactions (of the sort that would have been common on the newly formed Earth) can lead to the creation of highly structured compounds — seemingly a crucial stepping-stone on the path to living organisms.
+Tool Input: What happened with the latest SpaceX Starship launch?
+Observation: SpaceX's Starship rocket launched for the first time on Thursday but exploded in mid-flight, falling short of reaching space. No crew were on board. Before the mid-flight failure, the Super Heavy booster successfully separated from the rocket, flipped and began its return to Earth.
+Thought: We've learned what happened with the latest SpaceX Starship launch. I can now provide the final answer.
+Final Answer: The latest Space X Starship launch was an experimental launch that did not reach space and ended in mid-flight failure.
 
-Users sometimes ask questions that are unclear or lack context. In such cases consults with the following
-conversational history to better understand the question context:
-{history}
+Question: What happened after that?
+Thought: I'm not sure what you're referring to. I'll use conversation history to help me understand the context.
+Tool: conversation_history
+Tool Input: What happened after that?
+Observation: Human asked AI about the indictment of the former president Trump and AI responded the details of the indictment.
+Thought: Based on the conversation history, it seems likely that the Human is asking what happened to former President after the indictment. I'll use the search tool to help me answer the question.
+Tool: Search
+Tool Input: What happened after the indictment of the former president Trump?
+Observation: The former president Trump went to trial and the trial is still ongoing.
+Final Answer: The former president Trump went to trial and the trial is still ongoing.
 
-Now, please answer the following question:
+
+The AI Agent should use the conversation history tool to infer context when needed, rather than asking for more clarification.
+The AI Agent should never respond that a question is too vague. If it is vague, the Agent should consult conversation history tool to understand the context.
+
 Question: {query}
+Thought:
 {transcript}
 """
 search_key = os.environ.get("SERPERDEV_API_KEY")
@@ -60,7 +66,7 @@ if not search_key:
 prompt_text = """
 Synthesize a comprehensive answer from the following most relevant paragraphs and the given question.
 Provide a clear and concise response that summarizes the key points and information presented in the paragraphs.
-Your answer should be in your own words and be no longer than 50 words.
+Your answer should be in your own words and be no longer than 20 words.
 \n\n Paragraphs: {documents} \n\n Question: {query} \n\n Answer:
 """
 
@@ -75,26 +81,37 @@ web_retriever = WebRetriever(api_key=search_key, top_search_results=2, mode="pre
 pipeline = WebQAPipeline(retriever=web_retriever, prompt_node=prompt_node)
 
 few_shot_agent = PromptTemplate("conversational-agent-with-tools", prompt_text=few_shot_prompt)
+conversation_history = Tool(
+    name="conversation_history",
+    pipeline_or_node=lambda tool_input, **kwargs: agent.memory.load(),
+    description="useful for when you need to remember what you've already discussed.",
+)
 web_qa_tool = Tool(
     name="Search",
     pipeline_or_node=pipeline,
     description="useful for when you need to Google questions.",
     output_variable="results",
 )
-pn = PromptNode("gpt-3.5-turbo", api_key=os.environ.get("OPENAI_API_KEY"), max_length=256, stop_words=["Observation:"])
+pn = PromptNode(
+    "gpt-3.5-turbo",
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    max_length=256,
+    stop_words=["Observation:"],
+    model_kwargs={"temperature": 0.7},
+)
 agent = ConversationalAgentWithTools(
     pn,
+    max_steps=10,
     prompt_template=few_shot_agent,
-    tools_manager=ToolsManager(tools=[web_qa_tool]),
+    tools_manager=ToolsManager(tools=[web_qa_tool, conversation_history]),
     memory=ConversationSummaryMemory(pn),
 )
 
 while True:
-    user_input = input("Human (type 'exit' or 'quit' to quit): ")
+    user_input = input("\nHuman (type 'exit' or 'quit' to quit): ")
     if user_input.lower() == "exit" or user_input.lower() == "quit":
         break
-    elif user_input.lower() == "memory":
+    if user_input.lower() == "memory":
         print("\nMemory:\n", agent.memory.load())
     else:
         assistant_response = agent.run(user_input)
-        print("\nAssistant:", assistant_response)
