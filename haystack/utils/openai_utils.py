85f8e59cb53d75200e0f6be77368a4853a88fffc
recrudesce <recrudesce@gmail.com>
2023-04-24 12:31:02 +0100

    force support for gpt-35-turbo
    Cos Tiktoken doesn't support it yet - see https://github.com/openai/tiktoken/pull/72

--- haystack/utils/openai_utils.py
+++ haystack/utils/openai_utils.py
@@ -110,6 +110,9 @@ def _openai_text_completion_tokenization_details(model_name: str):
         elif model_name.startswith("gpt-3"):
             max_tokens_limit = 4096
             tokenizer_name = model_tokenizer
+        elif model_name.startswith("gpt-35"): # covering the lack of support in Tiktoken. https://github.com/openai/tiktoken/pull/72
+            max_tokens_limit = 4096
+            tokenizer_name = "cl100k_base"
         # Ref: https://platform.openai.com/docs/models/gpt-4
         elif model_name.startswith("gpt-4-32k"):
             max_tokens_limit = 32768  # tokens
