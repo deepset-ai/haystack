a3c2d27416d203c0944dddeb8b0868561a673221
Marc Khoury <khoury.marc@gmail.com>
2023-05-02 12:33:31 -0400

    Added typings

--- haystack/agents/tool_results.py
+++ haystack/agents/tool_results.py
@@ -1,23 +1,34 @@
 from haystack.agents.agent_step import AgentStep
+from events import Events
+from typing import Any, Optional
+
+from haystack.agents.types import Color
 
 class ToolResultsGatherer:
     results = []
 
-    def __init__(self, agent_callback_manager, tool_manager_callback_manager):
+    def __init__(self, agent_callback_manager: Events, tool_manager_callback_manager: Events):
         agent_callback_manager.on_agent_start += self.on_agent_start
         agent_callback_manager.on_agent_finish += self.on_agent_finish
 
         tool_manager_callback_manager.on_tool_finish += self.on_tool_finish
 
-    def on_agent_start(self, name, query, params):
+    def on_agent_start(self, **kwargs: Any) -> None:
         self.results = []
 
-    def on_tool_finish(self, tool_result: str, observation_prefix, llm_prefix, color, tool_name: str, unprocessed_tool_result):
+    def on_tool_finish(self,
+            tool_result: str,
+            color: Optional[Color] = None,
+            observation_prefix: Optional[str] = None,
+            llm_prefix: Optional[str] = None,
+            tool_name: str = None,
+            unprocessed_tool_result: Any = None,
+            **kwargs: Any) -> None:
         self.results.append({
             "name": tool_name,
             "result": tool_result,
             "unprocessed_result": unprocessed_tool_result
         })
 
-    def on_agent_finish(self, agent_step: AgentStep):
+    def on_agent_finish(self, agent_step: AgentStep) -> None:
         agent_step.tool_results = self.results
