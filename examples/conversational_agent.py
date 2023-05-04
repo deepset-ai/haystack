a085060e44f2c5286ddf15c2d8138a11db9139dc
Vladimir Blagojevic <dovlex@gmail.com>
2023-05-04 12:39:24 +0200

    Fix pylint error

--- examples/conversational_agent.py
+++ examples/conversational_agent.py
@@ -10,7 +10,7 @@ while True:
     user_input = input("Human (type 'exit' or 'quit' to quit): ")
     if user_input.lower() == "exit" or user_input.lower() == "quit":
         break
-    elif user_input.lower() == "memory":
+    if user_input.lower() == "memory":
         print("\nMemory:\n", agent.memory.load())
     else:
         assistant_response = agent.run(user_input)
