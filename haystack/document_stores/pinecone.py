54539d564e3d20e98c97dc1eed77803908c343a8
bogdankostic <bogdankostic@web.de>
2023-05-05 11:20:21 +0200

    fix: Add support for `_split_overlap` meta to Pinecone and `dict` metadata in general to Weaviate (#4805)
    * Add support for dicts to Weaviate
    
    * Add support for _split_overlap to Pinecone
    
    * Add tests
    
    * Fix Pylint
    
    * Fix Pylint
    
    * Fix test
    
    * Implement PR feedback

--- haystack/document_stores/pinecone.py
+++ haystack/document_stores/pinecone.py
@@ -1,3 +1,4 @@
+import json
 from typing import Set, Union, List, Optional, Dict, Generator, Any
 
 import logging
@@ -1217,6 +1218,8 @@ class PineconeDocumentStore(BaseDocumentStore):
         for _id, meta in zip(ids, metadata):
             content = meta.pop("content")
             content_type = meta.pop("content_type")
+            if "_split_overlap" in meta:
+                meta["_split_overlap"] = json.loads(meta["_split_overlap"])
             doc = Document(id=_id, content=content, content_type=content_type, meta=meta)
             documents.append(doc)
         if return_embedding:
@@ -1366,6 +1369,8 @@ class PineconeDocumentStore(BaseDocumentStore):
                 # Replace any None values with empty strings
                 if value is None:
                     value = ""
+                if key == "_split_overlap":
+                    value = json.dumps(value)
                 # format key
                 new_key = f"{parent_key}.{key}" if parent_key else key
                 # if value is dict, expand
