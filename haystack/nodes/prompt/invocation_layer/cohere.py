8465eeb3293e4de89d0974fad347531c39b29674
Vladimir Blagojevic <dovlex@gmail.com>
2023-05-07 11:34:05 +0200

    Add CohereInvocationLayer

---
+++ haystack/nodes/prompt/invocation_layer/cohere.py
@@ -0,0 +1,193 @@
+import json
+import os
+from typing import Optional, Dict, Union, List, Any
+import logging
+
+import requests
+
+from haystack.environment import HAYSTACK_REMOTE_API_TIMEOUT_SEC, HAYSTACK_REMOTE_API_MAX_RETRIES
+from haystack.errors import CohereInferenceLimitError, CohereInferenceUnauthorizedError, CohereInferenceError
+from haystack.nodes.prompt.invocation_layer import (
+    PromptModelInvocationLayer,
+    TokenStreamingHandler,
+    DefaultTokenStreamingHandler,
+)
+from haystack.utils.requests import request_with_retry
+
+logger = logging.getLogger(__name__)
+TIMEOUT = float(os.environ.get(HAYSTACK_REMOTE_API_TIMEOUT_SEC, 30))
+RETRIES = int(os.environ.get(HAYSTACK_REMOTE_API_MAX_RETRIES, 5))
+
+
+class CohereInvocationLayer(PromptModelInvocationLayer):
+    """
+    PromptModelInvocationLayer implementation for Cohere's command models. Invocations are made using REST API.
+    """
+
+    def __init__(self, api_key: str, model_name_or_path: str, max_length: Optional[int] = 100, **kwargs):
+        """
+         Creates an instance of CohereInvocationLayer
+        :param model_name_or_path: can be either:
+        :param max_length: The maximum length of the output text.
+        :param api_key: Cohere API key
+        """
+        super().__init__(model_name_or_path)
+        valid_api_key = isinstance(api_key, str) and api_key
+        if not valid_api_key:
+            raise ValueError(
+                f"api_key {api_key} must be a valid Cohere token. "
+                f"Your token is available in your Cohere settings page."
+            )
+        valid_model_name_or_path = isinstance(model_name_or_path, str) and model_name_or_path
+        if not valid_model_name_or_path:
+            raise ValueError(f"model_name_or_path {model_name_or_path} must be a valid Cohere model name")
+        self.api_key = api_key
+        self.max_length = max_length
+
+        # See https://docs.cohere.com/reference/generate
+        # for a list of supported parameters
+        self.model_input_kwargs = {
+            key: kwargs[key]
+            for key in [
+                "model",
+                "p",
+                "k",
+                "truncate",
+                "max_tokens",
+                "num_generations",
+                "temperature",
+                "end_sequences",
+                "presence_penalty",
+                "logit_bias",
+                "frequency_penalty",
+                "return_likelihoods",
+                "stream",
+                "stream_handler",
+            ]
+            if key in kwargs
+        }
+
+    @property
+    def url(self) -> str:
+        return "https://api.cohere.ai/v1/generate"
+
+    @property
+    def headers(self) -> Dict[str, str]:
+        return {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json",
+            "Request-Source": "python-sdk",
+        }
+
+    def invoke(self, *args, **kwargs):
+        """
+        Invokes a prompt on the model. It takes in a prompt and returns a list of responses using a REST invocation.
+        :return: The responses are being returned.
+        """
+        prompt = kwargs.get("prompt")
+        if not prompt:
+            raise ValueError(
+                f"No prompt provided. Model {self.model_name_or_path} requires prompt."
+                f"Make sure to provide prompt in kwargs."
+            )
+        stop_words = kwargs.pop("stop_words", None)
+        kwargs_with_defaults = self.model_input_kwargs
+        kwargs_with_defaults.update(kwargs)
+
+        # either stream is True (will use default handler) or stream_handler is provided
+        stream = (
+            kwargs_with_defaults.get("stream", False) or kwargs_with_defaults.get("stream_handler", None) is not None
+        )
+
+        kwargs_with_defaults.update(kwargs)
+        # see https://docs.cohere.com/reference/generate
+        params = {
+            "model": kwargs_with_defaults.get("model", self.model_name_or_path),
+            "prompt": prompt,
+            "p": kwargs_with_defaults.get("p", None),
+            "k": kwargs_with_defaults.get("k", None),
+            "truncate": kwargs_with_defaults.get("truncate", None),
+            "max_tokens": kwargs_with_defaults.get("max_tokens", self.max_length),
+            "num_generations": kwargs_with_defaults.get("num_generations", None),
+            "end_sequences": kwargs_with_defaults.get("end_sequences", stop_words),
+            "temperature": kwargs_with_defaults.get("temperature", None),
+            "frequency_penalty": kwargs_with_defaults.get("frequency_penalty", None),
+            "presence_penalty": kwargs_with_defaults.get("presence_penalty", None),
+            "return_likelihoods": kwargs_with_defaults.get("return_likelihoods", None),
+            "stream": stream,
+        }
+        response = self._post(params, stream=stream)
+        if not stream:
+            output = json.loads(response.text)
+            generated_texts = [o["text"] for o in output["generations"] if "text" in o]
+        else:
+            handler: TokenStreamingHandler = kwargs_with_defaults.pop("stream_handler", DefaultTokenStreamingHandler())
+            generated_texts = self._process_streaming_response(response=response, stream_handler=handler)
+        return generated_texts
+
+    def _process_streaming_response(self, response, stream_handler: TokenStreamingHandler):
+        tokens = []
+        for line in response.iter_lines():
+            if line:
+                streaming_item = json.loads(line)
+                text = streaming_item.get("text")
+                if text:
+                    tokens.append(stream_handler(text))
+        return ["".join(tokens)]  # return a list of strings just like non-streaming
+
+    def _post(
+        self,
+        data: Dict[str, Any],
+        stream: bool = False,
+        attempts: int = RETRIES,
+        status_codes: Optional[List[int]] = None,
+        timeout: float = TIMEOUT,
+        **kwargs,
+    ) -> requests.Response:
+        """
+        Post data to the HF inference model. It takes in a prompt and returns a list of responses using a REST
+        invocation.
+        :param data: The data to be sent to the model.
+        :param attempts: The number of attempts to make.
+        :param status_codes: The status codes to retry on.
+        :param timeout: The timeout for the request.
+        :return: The responses are being returned.
+        """
+        response: requests.Response
+        if status_codes is None:
+            status_codes = [429]
+        try:
+            response = request_with_retry(
+                method="POST",
+                status_codes=status_codes,
+                attempts=attempts,
+                url=self.url,
+                headers=self.headers,
+                json=data,
+                timeout=timeout,
+                stream=stream,
+            )
+        except requests.HTTPError as err:
+            res = err.response
+            if res.status_code == 429:
+                raise CohereInferenceLimitError(f"API rate limit exceeded: {res.text}")
+            if res.status_code == 401:
+                raise CohereInferenceUnauthorizedError(f"API key is invalid: {res.text}")
+
+            raise CohereInferenceError(
+                f"Cohere model returned an error.\nStatus code: {res.status_code}\nResponse body: {res.text}",
+                status_code=res.status_code,
+            )
+        return response
+
+    def _ensure_token_limit(self, prompt: Union[str, List[Dict[str, str]]]) -> Union[str, List[Dict[str, str]]]:
+        # TODO: new implementation incoming for all layers, let's omit this for now
+        return prompt
+
+    @classmethod
+    def supports(cls, model_name_or_path: str, **kwargs) -> bool:
+        """
+        Ensures CohereInvocationLayer is selected only when Cohere models are specified in
+        the model name.
+        """
+        return "command" in model_name_or_path
