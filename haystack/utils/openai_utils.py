aa8ed75aa6db1cdbbea6814856f21a05d492e4d1
Silvano Cerza <silvanocerza@gmail.com>
2023-05-08 16:36:04 +0200

    Fix missing error in openai_request retry strategy

--- haystack/utils/openai_utils.py
+++ haystack/utils/openai_utils.py
@@ -128,7 +128,8 @@ def _openai_text_completion_tokenization_details(model_name: str):
 
 
 @tenacity.retry(
-    retry=tenacity.retry_if_exception_type(OpenAIRateLimitError),
+    reraise=True,
+    retry=tenacity.retry_if_exception_type((OpenAIRateLimitError, OpenAIError)),
     wait=tenacity.wait_exponential(multiplier=OPENAI_BACKOFF),
     stop=tenacity.stop_after_attempt(OPENAI_MAX_RETRIES),
 )
