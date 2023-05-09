709111579132de17dd189a9d28eb65e42fa2dfc8
ZanSara <sara.zanzottera@deepset.ai>
2023-05-09 15:13:07 +0200

    add markers

--- test/preview/components/test_transcriber_whisper.py
+++ test/preview/components/test_transcriber_whisper.py
@@ -39,18 +39,22 @@ class TestTranscriber(BaseTestComponent):
         monkeypatch.setattr(WhisperTranscriber, "_transcribe_locally", mock_transcribe)
         monkeypatch.setattr(WhisperTranscriber, "warm_up", lambda self: None)
 
+    @pytest.mark.unit
     def test_init_remote_unknown_model(self):
         with pytest.raises(ValueError, match="not recognized"):
             WhisperTranscriber(model_name_or_path="anything")
 
+    @pytest.mark.unit
     def test_init_default_remote_missing_key(self):
         with pytest.raises(ValueError, match="API key"):
             WhisperTranscriber()
 
+    @pytest.mark.unit
     def test_init_explicit_remote_missing_key(self):
         with pytest.raises(ValueError, match="API key"):
             WhisperTranscriber(model_name_or_path="whisper-1")
 
+    @pytest.mark.unit
     def test_init_remote(self):
         transcriber = WhisperTranscriber(api_key="just a test")
         assert transcriber.model_name == "whisper-1"
@@ -58,6 +62,7 @@ class TestTranscriber(BaseTestComponent):
         assert not hasattr(transcriber, "device")
         assert hasattr(transcriber, "_model") and transcriber._model is None
 
+    @pytest.mark.unit
     def test_init_local(self):
         transcriber = WhisperTranscriber(model_name_or_path="large-v2")
         assert transcriber.model_name == "large-v2"  # Doesn't matter if it's huge, the model is not loaded in init.
@@ -65,6 +70,7 @@ class TestTranscriber(BaseTestComponent):
         assert hasattr(transcriber, "device") and transcriber.device == torch.device("cpu")
         assert hasattr(transcriber, "_model") and transcriber._model is None
 
+    @pytest.mark.unit
     def test_init_local_with_api_key(self):
         transcriber = WhisperTranscriber(model_name_or_path="large-v2")
         assert transcriber.model_name == "large-v2"  # Doesn't matter if it's huge, the model is not loaded in init.
@@ -72,16 +78,19 @@ class TestTranscriber(BaseTestComponent):
         assert hasattr(transcriber, "device") and transcriber.device == torch.device("cpu")
         assert hasattr(transcriber, "_model") and transcriber._model is None
 
+    @pytest.mark.unit
     def test_init_missing_whisper_lib(self, monkeypatch):
         monkeypatch.setitem(sys.modules, "whisper", FakeModule(spec=MagicMock(), message="test message"))
         with pytest.raises(ValueError, match="audio extra"):
             WhisperTranscriber(model_name_or_path="large-v2")
 
+    @pytest.mark.unit
     def test_init_missing_whisper_lib(self, monkeypatch):
         monkeypatch.setitem(sys.modules, "whisper", FakeModule(spec=MagicMock(), message="test message"))
         with pytest.raises(ValueError, match="audio extra"):
             WhisperTranscriber(model_name_or_path="large-v2")
 
+    @pytest.mark.unit
     def test_warmup_remote_model(self, monkeypatch):
         load_model = MagicMock()
         monkeypatch.setattr(whisper, "load_model", load_model)
@@ -89,6 +98,7 @@ class TestTranscriber(BaseTestComponent):
         component.warm_up()
         assert not load_model.called
 
+    @pytest.mark.unit
     def test_warmup_local_model(self, monkeypatch):
         load_model = MagicMock()
         load_model.side_effect = ["FAKE MODEL"]
@@ -101,6 +111,7 @@ class TestTranscriber(BaseTestComponent):
         assert component._model == "FAKE MODEL"
         load_model.assert_called_with("large-v2", device=torch.device(type="cpu"))
 
+    @pytest.mark.unit
     def test_warmup_local_model_doesnt_reload(self, monkeypatch):
         load_model = MagicMock()
         monkeypatch.setattr(whisper, "load_model", load_model)
@@ -109,6 +120,7 @@ class TestTranscriber(BaseTestComponent):
         component.warm_up()
         load_model.assert_called_once()
 
+    @pytest.mark.unit
     def test_transcribe_to_documents(self, mock_models):
         comp = WhisperTranscriber(model_name_or_path="large-v2")
         output = comp.transcribe_to_documents(
