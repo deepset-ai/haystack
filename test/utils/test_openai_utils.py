1e5d44e842bf6347e948e855efd65fdb4dbce63f
Farzad E <372719+dnetguru@users.noreply.github.com>
2023-05-11 19:56:46 +0000

    Broken down tests

--- test/utils/test_openai_utils.py
+++ test/utils/test_openai_utils.py
@@ -2,25 +2,32 @@ import pytest
 
 from haystack.utils.openai_utils import _openai_text_completion_tokenization_details
 
-
 @pytest.mark.unit
-def test_openai_text_completion_tokenization_details():
-    tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-35-turbo")
-    assert tokenizer_name == "cl100k_base"
-    assert max_tokens_limit == 4096
-
-    tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-3.5-turbo")
-    assert tokenizer_name == "cl100k_base"
-    assert max_tokens_limit == 4096
-
+def test_openai_text_completion_tokenization_details_gpt_davinci():
     tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="text-davinci-003")
     assert tokenizer_name == "p50k_base"
     assert max_tokens_limit == 4097
 
+@pytest.mark.unit
+def test_openai_text_completion_tokenization_details_gpt3_5_azure():
+    tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-35-turbo")
+    assert tokenizer_name == "cl100k_base"
+    assert max_tokens_limit == 4096
+
+@pytest.mark.unit
+def test_openai_text_completion_tokenization_details_gpt3_5():
+    tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-3.5-turbo")
+    assert tokenizer_name == "cl100k_base"
+    assert max_tokens_limit == 4096
+
+@pytest.mark.unit
+def test_openai_text_completion_tokenization_details_gpt_4():
     tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-4")
     assert tokenizer_name == "cl100k_base"
     assert max_tokens_limit == 8192
 
+@pytest.mark.unit
+def test_openai_text_completion_tokenization_details_gpt_4_32k():
     tokenizer_name, max_tokens_limit = _openai_text_completion_tokenization_details(model_name="gpt-4-32k")
     assert tokenizer_name == "cl100k_base"
     assert max_tokens_limit == 32768
