45b502ab2348cf13bc975fd2ce1c81a99cd8f3d0
bogdankostic <bogdankostic@web.de>
2023-05-12 15:11:45 +0200

    Adapt TableTextRetriever

--- haystack/nodes/retriever/dense.py
+++ haystack/nodes/retriever/dense.py
@@ -867,12 +867,8 @@ class TableTextRetriever(DenseRetriever):
         self.embed_meta_fields = embed_meta_fields
         self.scale_score = scale_score
 
-        query_tokenizer_class = DPRQuestionEncoderTokenizerFast if use_fast else DPRQuestionEncoderTokenizer
-        passage_tokenizer_class = DPRContextEncoderTokenizerFast if use_fast else DPRContextEncoderTokenizer
-        table_tokenizer_class = DPRContextEncoderTokenizerFast if use_fast else DPRContextEncoderTokenizer
-
         # Init & Load Encoders
-        self.query_tokenizer = query_tokenizer_class.from_pretrained(
+        self.query_tokenizer = AutoTokenizer.from_pretrained(
             query_embedding_model,
             revision=model_version,
             do_lower_case=True,
@@ -882,7 +878,7 @@ class TableTextRetriever(DenseRetriever):
         self.query_encoder = get_language_model(
             pretrained_model_name_or_path=query_embedding_model, revision=model_version, use_auth_token=use_auth_token
         )
-        self.passage_tokenizer = passage_tokenizer_class.from_pretrained(
+        self.passage_tokenizer = AutoTokenizer.from_pretrained(
             passage_embedding_model,
             revision=model_version,
             do_lower_case=True,
@@ -892,7 +888,7 @@ class TableTextRetriever(DenseRetriever):
         self.passage_encoder = get_language_model(
             pretrained_model_name_or_path=passage_embedding_model, revision=model_version, use_auth_token=use_auth_token
         )
-        self.table_tokenizer = table_tokenizer_class.from_pretrained(
+        self.table_tokenizer = AutoTokenizer.from_pretrained(
             table_embedding_model,
             revision=model_version,
             do_lower_case=True,
