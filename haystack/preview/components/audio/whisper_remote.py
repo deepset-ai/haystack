5c2683d666e48ae6048ccef9ee67cf756b70d695
ZanSara <sara.zanzottera@deepset.ai>
2023-05-17 11:57:07 +0200

    fix implementation

--- haystack/preview/components/audio/whisper_remote.py
+++ haystack/preview/components/audio/whisper_remote.py
@@ -26,8 +26,8 @@ WhisperRemoteModel = Literal["whisper-1"]
 class RemoteWhisperTranscriber:
     """
     Transcribes audio files using OpenAI's Whisper using OpenAI API. Requires an API key. See the
-      [OpenAI blog post](https://beta.openai.com/docs/api-reference/whisper for more details.
-      You can get one by signing up for an [OpenAI account](https://beta.openai.com/).
+    [OpenAI blog post](https://beta.openai.com/docs/api-reference/whisper for more details.
+    You can get one by signing up for an [OpenAI account](https://beta.openai.com/).
 
     For the supported audio formats, languages, and other parameters, see the
     [Whisper API documentation](https://platform.openai.com/docs/guides/speech-to-text)
@@ -37,7 +37,7 @@ class RemoteWhisperTranscriber:
     class Output:
         documents: List[Document]
 
-    def __init__(self, model_name_or_path: WhisperRemoteModel = "whisper-1", api_key: Optional[str] = None):
+    def __init__(self, api_key: str, model_name_or_path: WhisperRemoteModel = "whisper-1"):
         """
         Transcribes a list of audio files into a list of Documents.
 
@@ -48,19 +48,30 @@ class RemoteWhisperTranscriber:
             raise ValueError(
                 f"Model name not recognized. Choose one among: " f"{', '.join(get_args(WhisperRemoteModel))}."
             )
-        if not api_key:
-            raise ValueError("Provide a valid API key for OpenAI API.")
 
         self.api_key = api_key
         self.model_name = model_name_or_path
 
-    def run(self, audios: List[Path], whisper_params: Dict[str, Any]) -> Output:
-        documents = self.transcribe_to_documents(audios, **whisper_params)
+    def run(self, audio_files: List[Path], whisper_params: Dict[str, Any]) -> Output:
+        """
+        Transcribe the audio files into a list of Documents, one for each input file.
+
+        For the supported audio formats, languages, and other parameters, see the
+        [Whisper API documentation](https://platform.openai.com/docs/guides/speech-to-text) and the official Whisper
+        [github repo](https://github.com/openai/whisper).
+
+        :param audio_files: a list of paths or binary streams to transcribe
+        :returns: a list of Documents, one for each file. The content of the document is the transcription text,
+            while the document's metadata contains all the other values returned by the Whisper model, such as the
+            alignment data. Another key called `audio_file` contains the path to the audio file used for the
+            transcription.
+        """
+        documents = self.transcribe(audio_files, **whisper_params)
         return RemoteWhisperTranscriber.Output(documents)
 
-    def transcribe_to_documents(self, audio_files: Sequence[Union[str, Path, BinaryIO]], **kwargs) -> List[Document]:
+    def transcribe(self, audio_files: Sequence[Union[str, Path, BinaryIO]], **kwargs) -> List[Document]:
         """
-        Transcribe the given audio files. Returns a list of Documents.
+        Transcribe the audio files into a list of Documents, one for each input file.
 
         For the supported audio formats, languages, and other parameters, see the
         [Whisper API documentation](https://platform.openai.com/docs/guides/speech-to-text) and the official Whisper
@@ -69,13 +80,15 @@ class RemoteWhisperTranscriber:
         :param audio_files: a list of paths or binary streams to transcribe
         :returns: a list of transcriptions.
         """
-        transcriptions = self.transcribe(audio_files=audio_files, **kwargs)
-        return [
-            Document(content=transcript.pop("text"), metadata={"audio_file": audio, **transcript})
-            for audio, transcript in zip(audio_files, transcriptions)
-        ]
+        transcriptions = self._raw_transcribe(audio_files=audio_files, **kwargs)
+        documents = []
+        for audio, transcript in zip(audio_files, transcriptions):
+            content = transcript.pop("text")
+            doc = Document(content=content, metadata={"audio_file": audio, **transcript})
+            documents.append(doc)
+        return documents
 
-    def transcribe(self, audio_files: Sequence[Union[str, Path, BinaryIO]], **kwargs) -> List[Dict[str, Any]]:
+    def _raw_transcribe(self, audio_files: Sequence[Union[str, Path, BinaryIO]], **kwargs) -> List[Dict[str, Any]]:
         """
         Transcribe the given audio files. Returns a list of strings.
 
@@ -83,32 +96,36 @@ class RemoteWhisperTranscriber:
         [Whisper API documentation](https://platform.openai.com/docs/guides/speech-to-text) and the official Whisper
         [github repo](https://github.com/openai/whisper).
 
-        :param audio_files: a list of paths or binary streams to transcribe
-        :returns: a list of transcriptions.
+        :param audio_files: a list of paths or binary streams to transcribe.
+        :param kwargs: any other parameters that Whisper API can understand.
+        :returns: a list of transcriptions as they are produced by the Whisper API (JSON).
         """
+        translate = kwargs.pop("translate", False)
+        url = f"https://api.openai.com/v1/audio/{'translations' if translate else 'transcriptions'}"
+        data = {"model": "whisper-1", **kwargs}
+        headers = {"Authorization": f"Bearer {self.api_key}"}
+
         transcriptions = []
         for audio_file in audio_files:
             if isinstance(audio_file, (str, Path)):
-                audio_file = open(audio_file, "rb")
-                transcription = self._transcribe(audio_file, **kwargs)
-
+                with open(audio_file, "rb") as audio_file:
+                    transcription = self._invoke_api(audio_file, url, data, headers)
             transcriptions.append(transcription)
         return transcriptions
 
     @retry(retry=retry_if_not_result(bool), wait=wait_exponential(min=1, max=10))
-    def _transcribe(self, audio_file: BinaryIO, **kwargs) -> Dict[str, Any]:
+    def _invoke_api(self, audio: BinaryIO, url: str, data: Dict[str, Any], headers: Dict[str, Any]) -> Dict[str, Any]:
         """
-        Calls a remote Whisper model through OpenAI Whisper API.
-        """
-        translate = kwargs.pop("translate", False)
+        Calls a remote Whisper model through OpenAI Whisper API. This function auto-retries at most ten times.
 
-        response = requests.post(
-            url=f"https://api.openai.com/v1/audio/{'translations' if translate else 'transcriptions'}",
-            data={"model": "whisper-1", **kwargs},
-            headers={"Authorization": f"Bearer {self.api_key}"},
-            files=[("file", (audio_file.name, audio_file, "application/octet-stream"))],
-            timeout=OPENAI_TIMEOUT,
-        )
+        :param audio: the binary stream of audio to upload (a handle to the audio file, for example)
+        :param url: the constructed API URL
+        :param data: the data parameters to send to Whisper
+        :param headers: the request headers with the API key
+        :returns: the JSON response of the API.
+        """
+        request_files = ("file", (audio.name, audio, "application/octet-stream"))
+        response = requests.post(url=url, data=data, headers=headers, files=[request_files], timeout=OPENAI_TIMEOUT)
 
         if response.status_code != 200:
             if response.status_code == 429:
