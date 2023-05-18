07e0876d542ee641349f364c80a4c5f97e8c18ff
Vladimir Blagojevic <dovlex@gmail.com>
2023-05-18 13:53:14 +0200

    More unit tests

--- test/agents/test_tools_manager.py
+++ test/agents/test_tools_manager.py
@@ -103,7 +103,6 @@ def test_tool_invocation():
 @pytest.mark.unit
 def test_extract_tool_name_and_tool_multi_line_input(tools_manager):
     # new pattern being supported but with backward compatibility for the old
-
     text = (
         "We need to find out the following information:\n"
         "1. What city was Jeremy McKinnon born in?\n"
@@ -112,17 +111,10 @@ def test_extract_tool_name_and_tool_multi_line_input(tools_manager):
         "Tool Input: Where was Jeremy\n McKinnon born\n and where did he grow up?"
     )
 
-    text2 = (
-        "We need to find out the following information:\n"
-        "1. What city was Jeremy McKinnon born in?\n"
-        "2. What's the capital of Germany?\n"
-        "Tool: Search\n"
-        "Tool Input:"
-    )
-
     tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text)
     assert tool_name == "Search" and tool_input == "Where was Jeremy\n McKinnon born\n and where did he grow up?"
 
+    # tool input is empty
     text2 = (
         "We need to find out the following information:\n"
         "1. What city was Jeremy McKinnon born in?\n"
@@ -132,6 +124,31 @@ def test_extract_tool_name_and_tool_multi_line_input(tools_manager):
     )
     tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text2)
     assert tool_name == "Search" and tool_input == ""
+
+    # Case where the tool name and tool input are provided with extra whitespaces
+    text3 = "   Tool:   Search   \n" "   Tool Input:   What is the tallest building in the world?   "
+    tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text3)
+    assert tool_name.strip() == "Search" and tool_input.strip() == "What is the tallest building in the world?"
+
+    # Case where the tool name is provided but the tool input line is not provided at all
+    # Tool input is not optional, so this should return None for both tool name and tool input
+    text4 = (
+        "We need to find out the following information:\n"
+        "1. Who is the current president of the United States?\n"
+        "Tool: Search\n"
+    )
+    tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text4)
+    assert tool_name is None and tool_input is None
+
+    # Case where neither the tool name nor the tool input is provided
+    text5 = "We need to find out the following information:\n" "1. What is the population of India?"
+    tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text5)
+    assert tool_name is None and tool_input is None
+
+    # Case where the tool name and tool input are provided with extra whitespaces and new lines
+    text6 = "   Tool:   Search   \n" "   Tool Input:   \nWhat is the tallest \nbuilding in the world?   "
+    tool_name, tool_input = tools_manager.extract_tool_name_and_tool_input(text6)
+    assert tool_name.strip() == "Search" and tool_input.strip() == "What is the tallest \nbuilding in the world?"
 
 
 @pytest.mark.unit
