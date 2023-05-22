48e773ef3de1392891515e3d9059f62dc912cf52
Massimiliano Pippi <mpippi@gmail.com>
2023-05-22 10:03:51 +0200

    chore: leftovers from removing knowledge graph support (#4974)
    * leftovers from removing knowledge graph support
    
    * more leftovers

--- test/conftest.py
+++ test/conftest.py
@@ -139,8 +139,6 @@ def pytest_collection_modifyitems(config, items):
         "faiss": [pytest.mark.faiss],
         "weaviate": [pytest.mark.weaviate],
         "pinecone": [pytest.mark.pinecone],
-        # FIXME GraphDB can't be treated as a regular docstore, it fails most of their tests
-        "graphdb": [pytest.mark.integration],
     }
     for item in items:
         for name, markers in name_to_markers.items():
