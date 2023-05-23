f5798561e0321dd4164833da67f48f0299ea0a18
ZanSara <sara.zanzottera@deepset.ai>
2023-05-23 13:42:12 +0200

    simple drawing tests

--- test/conftest.py
+++ test/conftest.py
@@ -5,17 +5,19 @@ import pytest
 from unittest.mock import patch, MagicMock
 
 
-TEST_FILES = Path(__file__).parent / "test_files"
+@pytest.fixture
+def test_files():
+    return Path(__file__).parent / "test_files"
 
 
 @pytest.fixture(autouse=True)
-def mock_mermaid_request():
+def mock_mermaid_request(test_files):
     """
     Prevents real requests to https://mermaid.ink/
     """
     with patch("canals.draw.mermaid.requests.get") as mock_get:
         mock_response = MagicMock()
         mock_response.status_code = 200
-        mock_response.content = open(TEST_FILES / "mermaid_mock" / "test_response.png", "rb").read()
+        mock_response.content = open(test_files / "mermaid_mock" / "test_response.png", "rb").read()
         mock_get.return_value = mock_response
         yield
---
+++ test/pipelines/unit/test_draw.py
@@ -0,0 +1,49 @@
+import os
+import filecmp
+from hashlib import md5
+
+import pytest
+
+from canals.pipeline import Pipeline
+
+from test.sample_components import Double
+
+
+def test_draw_pygraphviz(tmp_path, test_files):
+    pipe = Pipeline()
+    pipe.add_component("comp1", Double())
+    pipe.add_component("comp2", Double())
+    pipe.connect("comp1", "comp2")
+
+    pipe.draw(tmp_path / "test_pipe.jpg", engine="graphviz")
+    assert os.path.exists(tmp_path / "test_pipe.jpg")
+    assert filecmp.cmp(tmp_path / "test_pipe.jpg", test_files / "pipeline_draw" / "pygraphviz.jpg")
+
+
+def test_draw_mermaid_img(tmp_path, test_files):
+    pipe = Pipeline()
+    pipe.add_component("comp1", Double())
+    pipe.add_component("comp2", Double())
+    pipe.connect("comp1", "comp2")
+    pipe.connect("comp2", "comp1")
+
+    pipe.draw(tmp_path / "test_pipe.jpg", engine="mermaid-img")
+    assert os.path.exists(tmp_path / "test_pipe.jpg")
+    assert filecmp.cmp(tmp_path / "test_pipe.jpg", test_files / "mermaid_mock" / "test_response.png")
+
+
+def test_draw_mermaid_txt(tmp_path):
+    pipe = Pipeline()
+    pipe.add_component("comp1", Double())
+    pipe.add_component("comp2", Double())
+    pipe.connect("comp1", "comp2")
+    pipe.connect("comp2", "comp1")
+
+    pipe.draw(tmp_path / "test_pipe.md", engine="mermaid-text")
+    assert os.path.exists(tmp_path / "test_pipe.md")
+    assert (
+        open(tmp_path / "test_pipe.md", "r").read()
+        == """graph TD;
+comp1 -- value -> value --> comp2
+comp2 -- value -> value --> comp1"""
+    )
