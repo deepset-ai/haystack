c5f0f820cf9843c07fd006a38056c880bd1f06aa
bogdankostic <bogdankostic@web.de>
2023-05-25 11:19:46 +0200

    refactor: Adapt benchmarking utils (#5003)
    * Adapt benchmarking utils
    
    * Adapt error message
    
    * Adapt doc store launcher registry
    
    * Revert "Adapt doc store launcher registry"
    
    This reverts commit e034936363dde760d393fe00cac998a54a0f5152.

--- test/benchmarks/utils.py
+++ test/benchmarks/utils.py
@@ -1,168 +1,117 @@
 import os
-from haystack.document_stores import SQLDocumentStore
-from haystack.document_stores import InMemoryDocumentStore
-from haystack.document_stores import ElasticsearchDocumentStore, OpenSearchDocumentStore
-from haystack.document_stores.elasticsearch import Elasticsearch
-from haystack.document_stores import FAISSDocumentStore
-from haystack.nodes import BM25Retriever, TfidfRetriever
-from haystack.nodes import DensePassageRetriever, EmbeddingRetriever
-from haystack.nodes import FARMReader
-from haystack.nodes import TransformersReader
-from haystack.utils import launch_es, launch_opensearch
+import tarfile
+import tempfile
+
+import pandas as pd
+
+from haystack import Label, Document, Answer
+from haystack.document_stores import eval_data_from_json
+from haystack.utils import launch_es, launch_opensearch, launch_weaviate
 from haystack.modeling.data_handler.processor import http_get
 
 import logging
-import subprocess
-import time
-import json
-from typing import Union
+from typing import Dict, Union
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 
-reader_models = [
-    "deepset/roberta-base-squad2",
-    "deepset/minilm-uncased-squad2",
-    "deepset/bert-base-cased-squad2",
-    "deepset/bert-large-uncased-whole-word-masking-squad2",
-    "deepset/xlm-roberta-large-squad2",
-]
-reader_types = ["farm"]
+def prepare_environment(pipeline_config: Dict, benchmark_config: Dict):
+    """
+    Prepare the environment for running a benchmark.
+    """
+    # Download data if specified in benchmark config
+    if "data_url" in benchmark_config:
+        download_from_url(url=benchmark_config["data_url"], target_dir="data/")
 
-doc_index = "eval_document"
-label_index = "label"
-
-
-def get_document_store(document_store_type, similarity="dot_product", index="document"):
-    """TODO This method is taken from test/conftest.py but maybe should be within Haystack.
-    Perhaps a class method of DocStore that just takes string for type of DocStore"""
-    if document_store_type == "sql":
-        if os.path.exists("haystack_test.db"):
-            os.remove("haystack_test.db")
-        document_store = SQLDocumentStore(url="sqlite:///haystack_test.db")
-        assert document_store.get_document_count() == 0
-    elif document_store_type == "memory":
-        document_store = InMemoryDocumentStore()
-    elif document_store_type == "elasticsearch":
-        launch_es()
-        time.sleep(5)
-        # make sure we start from a fresh index
-        client = Elasticsearch()
-        client.indices.delete(index="haystack_test*", ignore=[404])
-        document_store = ElasticsearchDocumentStore(index="eval_document", similarity=similarity, timeout=3000)
-    elif document_store_type in ("faiss_flat", "faiss_hnsw"):
-        if document_store_type == "faiss_flat":
-            index_type = "Flat"
-        elif document_store_type == "faiss_hnsw":
-            index_type = "HNSW"
-        status = subprocess.run(["docker rm -f haystack-postgres"], shell=True)
-        time.sleep(1)
-        status = subprocess.run(
-            ["docker run --name haystack-postgres -p 5432:5432 -e POSTGRES_PASSWORD=password -d postgres"], shell=True
-        )
-        time.sleep(6)
-        status = subprocess.run(
-            ['docker exec haystack-postgres psql -U postgres -c "CREATE DATABASE haystack;"'], shell=True
-        )
-        time.sleep(1)
-        document_store = FAISSDocumentStore(
-            sql_url="postgresql://postgres:password@localhost:5432/haystack",
-            faiss_index_factory_str=index_type,
-            similarity=similarity,
-            index=index,
-        )
-        assert document_store.get_document_count() == 0
-    elif document_store_type in ("opensearch_flat", "opensearch_hnsw"):
-        launch_opensearch(local_port=9201)
-        if document_store_type == "opensearch_flat":
-            index_type = "flat"
-        elif document_store_type == "opensearch_hnsw":
-            index_type = "hnsw"
-        document_store = OpenSearchDocumentStore(index_type=index_type, port=9201, timeout=3000)
-    else:
-        raise Exception(f"No document store fixture for '{document_store_type}'")
-    return document_store
-
-
-def get_retriever(retriever_name, doc_store, devices):
-    if retriever_name == "elastic":
-        return BM25Retriever(doc_store)
-    if retriever_name == "tfidf":
-        return TfidfRetriever(doc_store)
-    if retriever_name == "dpr":
-        return DensePassageRetriever(
-            document_store=doc_store,
-            query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-            passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-            use_gpu=True,
-            use_fast_tokenizers=False,
-            devices=devices,
-        )
-    if retriever_name == "sentence_transformers":
-        return EmbeddingRetriever(
-            document_store=doc_store,
-            embedding_model="nq-distilbert-base-v1",
-            use_gpu=True,
-            model_format="sentence_transformers",
+    n_docs = 0
+    if "documents_directory" in benchmark_config:
+        documents_dir = Path(benchmark_config["documents_directory"])
+        n_docs = len(
+            [
+                file_path
+                for file_path in documents_dir.iterdir()
+                if file_path.is_file() and not file_path.name.startswith(".")
+            ]
         )
 
-
-def get_reader(reader_name, reader_type, max_seq_len=384):
-    reader_class = None
-    if reader_type == "farm":
-        reader_class = FARMReader
-    elif reader_type == "transformers":
-        reader_class = TransformersReader
-    return reader_class(reader_name, top_k_per_candidate=4, max_seq_len=max_seq_len)
-
-
-def index_to_doc_store(doc_store, docs, retriever, labels=None):
-    doc_store.write_documents(docs, doc_index)
-    if labels:
-        doc_store.write_labels(labels, index=label_index)
-    # these lines are not run if the docs.embedding field is already populated with precomputed embeddings
-    # See the prepare_data() fn in the retriever benchmark script
-    if callable(getattr(retriever, "embed_documents", None)) and docs[0].embedding is None:
-        doc_store.update_embeddings(retriever, index=doc_index, batch_size=200)
-
-
-def load_config(config_filename, ci):
-    conf = json.load(open(config_filename))
-    if ci:
-        params = conf["params"]["ci"]
-    else:
-        params = conf["params"]["full"]
-    filenames = conf["filenames"]
-    max_docs = max(params["n_docs_options"])
-    n_docs_keys = sorted([int(x) for x in list(filenames["embeddings_filenames"])])
-    for k in n_docs_keys:
-        if max_docs <= k:
-            filenames["embeddings_filenames"] = [filenames["embeddings_filenames"][str(k)]]
-            filenames["filename_negative"] = filenames["filenames_negative"][str(k)]
+    # Launch DocumentStore Docker container if needed
+    for comp in pipeline_config["components"]:
+        if comp["type"].endswith("DocumentStore"):
+            launch_document_store(comp["type"], n_docs=n_docs)
             break
-    return params, filenames
 
 
-def download_from_url(url: str, filepath: Union[str, Path]):
+def launch_document_store(document_store: str, n_docs: int = 0):
     """
-    Download from a url to a local file. Skip already existing files.
+    Launch a DocumentStore Docker container.
+    """
+    java_opts = None if n_docs < 500000 else "-Xms4096m -Xmx4096m"
+    if document_store == "ElasticsearchDocumentStore":
+        launch_es(sleep=30, delete_existing=True, java_opts=java_opts)
+    elif document_store == "OpenSearchDocumentStore":
+        launch_opensearch(sleep=30, delete_existing=True, java_opts=java_opts)
+    elif document_store == "WeaviateDocumentStore":
+        launch_weaviate(sleep=30, delete_existing=True)
 
-    :param url: Url
-    :param filepath: local path where the url content shall be stored
-    :return: local path of the downloaded file
+
+def download_from_url(url: str, target_dir: Union[str, Path]):
+    """
+    Download from a URL to a local file.
+
+    :param url: URL
+    :param target_dir: Local directory where the URL content will be saved.
     """
 
-    logger.info("Downloading %s", url)
-    # Create local folder
-    folder, filename = os.path.split(filepath)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    # Download file if not present locally
-    if os.path.exists(filepath):
-        logger.info("Skipping %s (exists locally)", url)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    url_path = Path(url)
+    logger.info("Downloading %s to %s", url_path.name, target_dir)
+    with tempfile.NamedTemporaryFile() as temp_file:
+        http_get(url=url, temp_file=temp_file)
+        temp_file.flush()
+        temp_file.seek(0)
+        if tarfile.is_tarfile(temp_file.name):
+            with tarfile.open(temp_file.name) as tar:
+                tar.extractall(target_dir)
+        else:
+            with open(Path(target_dir) / url_path.name, "wb") as file:
+                file.write(temp_file.read())
+
+
+def load_eval_data(eval_set_file: Path):
+    """
+    Load evaluation data from a file.
+    :param eval_set_file: Path to the evaluation data file.
+    """
+    if eval_set_file.suffix == ".json":
+        _, labels = eval_data_from_json(str(eval_set_file))
+        queries = [label.query for label in labels]
+    elif eval_set_file.suffix == ".csv":
+        eval_data = pd.read_csv(eval_set_file)
+
+        labels = []
+        queries = []
+        for idx, row in eval_data.iterrows():
+            query = row["question"]
+            context = row["context"]
+            answer = Answer(answer=row["text"]) if "text" in row else None
+            label = Label(
+                query=query,
+                document=Document(context),
+                answer=answer,
+                is_correct_answer=True,
+                is_correct_document=True,
+                origin="gold-label",
+            )
+            labels.append(label)
+            queries.append(query)
     else:
-        logger.info("Downloading %s to %s", filepath)
-        with open(filepath, "wb") as file:
-            http_get(url=url, temp_file=file)
-    return filepath
+        raise ValueError(
+            f"Unsupported file format: {eval_set_file.suffix}. Provide a SQuAD-style .json or a .csv file containing "
+            f"the columns 'question' and 'context' for Retriever evaluations and additionally 'text' (containing the "
+            f"answer string) for Reader evaluations."
+        )
+
+    return labels, queries
