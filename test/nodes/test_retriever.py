a0cdab3b376ac2f68bdfddabfc29d6ed84ce8b8d
Michael Feil <michael.feil@michaelfeil.eu>
2023-05-30 07:39:35 +0200

    format retriever

--- test/nodes/test_retriever.py
+++ test/nodes/test_retriever.py
@@ -321,7 +321,9 @@ def test_retribert_embedding(document_store, retriever, docs_with_ids):
 def test_openai_embedding_retriever_selection():
     # OpenAI released (Dec 2022) a unifying embedding model called text-embedding-ada-002
     # make sure that we can use it with the retriever selection
-    er = EmbeddingRetriever(embedding_model="text-embedding-ada-002", document_store=None, api_base="https://api.openai.com/v1")
+    er = EmbeddingRetriever(
+        embedding_model="text-embedding-ada-002", document_store=None, api_base="https://api.openai.com/v1"
+    )
     assert er.model_format == "openai"
     assert er.embedding_encoder.query_encoder_model == "text-embedding-ada-002"
     assert er.embedding_encoder.doc_encoder_model == "text-embedding-ada-002"
