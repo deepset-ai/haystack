523f8cc683ad2c585f22071ea7f67fc56e1888ec
bogdankostic <bogdankostic@web.de>
2023-06-01 18:47:24 +0200

    feat: Add batching for querying in `ElasticsearchDocumentStore` and `OpenSearchDocumentStore` (#5063)
    * Include benchmark config in output
    
    * Use queries from aggregated labels
    
    * Introduce batching for querying in ElasticsearchDocStore and OpenSearchDocStore
    
    * Fix mypy
    
    * Use self.batch_size in write_documents
    
    * Use 10_000 as default batch size
    
    * Add unit tests for write documents

--- haystack/document_stores/elasticsearch.py
+++ haystack/document_stores/elasticsearch.py
@@ -56,6 +56,7 @@ class ElasticsearchDocumentStore(SearchEngineDocumentStore):
         synonyms: Optional[List] = None,
         synonym_type: str = "synonym",
         use_system_proxy: bool = False,
+        batch_size: int = 10_000,
     ):
         """
         A DocumentStore using Elasticsearch to store and query the documents for our search.
@@ -127,6 +128,8 @@ class ElasticsearchDocumentStore(SearchEngineDocumentStore):
                              Synonym or Synonym_graph to handle synonyms, including multi-word synonyms correctly during the analysis process.
                              More info at https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-synonym-graph-tokenfilter.html
         :param use_system_proxy: Whether to use system proxy.
+        :param batch_size: Number of Documents to index at once / Number of queries to execute at once. If you face
+                           memory issues, decrease the batch_size.
 
         """
         # Base constructor might need the client to be ready, create it first
@@ -167,6 +170,7 @@ class ElasticsearchDocumentStore(SearchEngineDocumentStore):
             skip_missing_embeddings=skip_missing_embeddings,
             synonyms=synonyms,
             synonym_type=synonym_type,
+            batch_size=batch_size,
         )
 
         # Let the base class trap the right exception from the elasticpy client
