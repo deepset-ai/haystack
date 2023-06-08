5f36daccd466b243f9b424f858cf5aaace03016b
ZanSara <sara.zanzottera@deepset.ai>
2023-06-08 12:11:38 +0200

    feat: introduce `lazy_import` (#5084)
    * generalimport -> lazy-imports
    
    * remove generalimport
    
    * fix pdftotextconverter import check
    
    * customize error messages
    
    * pylint
    
    * fix sql.py
    
    * pylint
    
    * Update haystack/document_stores/sql.py
    
    Co-authored-by: Massimiliano Pippi <mpippi@gmail.com>
    
    * make contextmanager less verbose
    
    * do not catch syntax errors
    
    * review feedback
    
    * Update haystack/nodes/file_converter/pdf.py
    
    ---------
    
    Co-authored-by: Massimiliano Pippi <mpippi@gmail.com>

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -1,92 +1,9 @@
 # pylint: disable=wrong-import-position
 # Logging is not configured here on purpose, see https://github.com/deepset-ai/haystack/issues/2485
 
-import sys
 from importlib import metadata
 
 __version__: str = str(metadata.version("farm-haystack"))
-
-from generalimport import generalimport, MissingOptionalDependency, FakeModule
-
-generalimport(
-    # "pydantic", # Required for all dataclasses
-    # "tenacity",  # Probably needed because it's a decorator, to be evaluated
-    # "pandas",
-    "aiorwlock",
-    "azure",
-    "beautifulsoup4",
-    "beir",
-    "boilerpy3",
-    "canals",
-    "dill",
-    "docx",
-    "elasticsearch",
-    "events",
-    "faiss",
-    "fitz",
-    "frontmatter",
-    "huggingface_hub",
-    "jsonschema",
-    "langdetect",
-    "magic",
-    "markdown",
-    "mlflow",
-    "more_itertools",
-    "networkx",
-    "nltk",
-    "numpy",
-    "onnxruntime",
-    "onnxruntime_tools",
-    "opensearchpy",
-    "pdf2image",
-    "PIL",
-    "pinecone",
-    "posthog",
-    "protobuf",
-    "psycopg2",
-    "pytesseract",
-    "quantulum3",
-    "rank_bm25",
-    "rapidfuzz",
-    "ray",
-    "rdflib",
-    "requests",
-    "scipy",
-    "selenium",
-    "sentence_transformers",
-    "seqeval",
-    "sklearn",
-    "SPARQLWrapper",
-    "sqlalchemy",
-    "sseclient",
-    "tenacity",
-    "tika",
-    "tiktoken",
-    "tokenizers",
-    "torch",
-    "tqdm",
-    "transformers",
-    "weaviate",
-    "webdriver_manager",
-    "whisper",
-    "yaml",
-)
-
-
-# TODO: remove this function once this PR is merged and released by generalimport:
-# https://github.com/ManderaGeneral/generalimport/pull/25
-def is_imported(module_name: str) -> bool:
-    """
-    Returns True if the module was actually imported, False, if generalimport mocked it.
-    """
-    module = sys.modules.get(module_name)
-    try:
-        return bool(module) and not isinstance(module, FakeModule)
-    except MissingOptionalDependency:
-        # isinstance() raises MissingOptionalDependency: fake module
-        pass
-    return False
-
 
 from haystack.schema import Document, Answer, Label, MultiLabel, Span, EvaluationResult, TableCell
 from haystack.nodes.base import BaseComponent
