f68406b8f9ec6e9b6c87335c5b874afcf100ca39
rjanjua <rohan.janjua@gmail.com>
2023-06-12 17:17:30 +0100

    refine script

--- test/benchmarks/setup_benchmarks.sh
+++ test/benchmarks/setup_benchmarks.sh
@@ -1,13 +1,9 @@
-rm -rf haystack
-git clone https://github.com/deepset-ai/haystack.git
+git reset --hard
+git clean -f -x
+git checkout main
+git pull
 pip install .
 pip install .[metrics,elasticsearch,weaviate,opensearch]
-
-cd test/benchmarks
-mkdir data
-
-wget -O data/msmarco.100_000.tar.bz2 https://deepset-test-datasets.s3.eu-central-1.amazonaws.com/msmarco.100_000.tar.bz2
-tar -xf data/msmarco.100_000.tar.bz2 -C data
 
 mkdir out
 
