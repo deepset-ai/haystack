aa5fd695dddbe562637d7c33f4f83dd569c27d3e
rjanjua <rohan.janjua@gmail.com>
2023-06-14 12:26:16 +0100

    update run script

--- test/benchmarks/setup_benchmarks.sh
+++ test/benchmarks/setup_benchmarks.sh
@@ -2,8 +2,10 @@ git reset --hard
 git clean -f -x
 git checkout main
 git pull
+cd ../../
 pip install .
 pip install .[metrics,elasticsearch,weaviate,opensearch]
+cd test/benchmarks
 mkdir out
 
 ./run.sh
