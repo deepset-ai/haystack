cdadb17a383dc9f2a0085f9b4002cc543a4072d1
Vladimir Blagojevic <dovlex@gmail.com>
2023-06-15 08:43:20 +0200

    Add AgentToolLogger, unit test, and example usage (#5087)

--- test/agents/test_agent.py
+++ test/agents/test_agent.py
@@ -4,10 +4,9 @@ import re
 from typing import Tuple
 from unittest.mock import Mock, patch
 
-
 from events import Events
 
-from haystack.agents.types import AgentTokenStreamingHandler
+from haystack.agents.types import AgentTokenStreamingHandler, AgentToolLogger
 from test.conftest import MockRetriever, MockPromptNode
 from unittest import mock
 import pytest
@@ -107,6 +106,33 @@ def test_run_tool():
     step = AgentStep(prompt_node_response=pn_response)
     result = agent.tm.run_tool(step.prompt_node_response)
     assert result == "[]"  # empty list of documents
+
+
+@pytest.mark.unit
+def test_agent_tool_logger():
+    agent = Agent(prompt_node=MockPromptNode())
+    atl = AgentToolLogger(agent_events=agent.callback_manager, tool_events=agent.tm.callback_manager)
+
+    retriever = MockRetriever()
+    agent.add_tool(
+        Tool(
+            name="Retriever",
+            pipeline_or_node=retriever,
+            description="useful for when you need to retrieve documents from your index",
+            output_variable="documents",
+        )
+    )
+    pn_response = "need to find out what city he was born.\nTool: Retriever\nTool Input: Where was Jeremy McKinnon born"
+
+    step = AgentStep(prompt_node_response=pn_response)
+    agent.tm.run_tool(step.prompt_node_response)
+
+    # Check that the AgentToolLogger collected the tool's output
+    assert len(atl.logs) == 1
+    tool_logging_event = atl.logs[0]
+    assert tool_logging_event["tool_name"] == "Retriever"
+    assert tool_logging_event["tool_input"] == "Where was Jeremy McKinnon born"
+    assert tool_logging_event["tool_output"] == "[]"
 
 
 @pytest.mark.unit
