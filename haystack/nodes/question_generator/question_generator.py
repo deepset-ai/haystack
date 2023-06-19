c84adaaf2de22385f1448b9003cc5976d17455e9
Julian Risch <julian.risch@deepset.ai>
2023-06-19 19:29:37 +0200

    remove extra dot

--- haystack/nodes/question_generator/question_generator.py
+++ haystack/nodes/question_generator/question_generator.py
@@ -14,7 +14,7 @@ from haystack.lazy_imports import LazyImport
 logger = logging.getLogger(__name__)
 
 
-with LazyImport(message="Run 'pip install farm-haystack[inference]'.") as torch_and_transformers_import:
+with LazyImport(message="Run 'pip install farm-haystack[inference]'") as torch_and_transformers_import:
     import torch
     from transformers import AutoModelForSeq2SeqLM
     from transformers import AutoTokenizer
