6a1b6b1ae38a328fb3b957c7b8b164349d456efd
Bilge Yücel <bilgeyucel96@gmail.com>
2023-06-20 13:09:21 +0300

    feat: Update ConversationalAgent (#5065)
    * feat: Update ConversationalAgent
    
    * Add Tools
    * Add test
    * Change default params
    
    * fix tests
    
    * Fix circular import error
    * Update conversational-agent prompt
    * Add conversational-agent-without-tools to legacy list
    
    * Add warning to add tools to conversational agent
    
    * Add callable tools
    
    * Add example script
    
    * Fix linter errors
    
    * Update ConversationalAgent depending on the existance of tools
    
    * Initialize the base Agent with different arguments when there's tool
    * Inject memory to the prompt in both cases, update prompts accordingly
    
    * Override the add_tools method to prevent adding tools to ConversationalAgent without tools
    
    * Update test
    
    * Fix linter error
    
    * Remove unused import
    
    * Update docstrings and api reference
    
    * Fix imports and doc string code snippet
    
    * docstrings update
    
    * Update conversational.py
    
    * Mock PromptNode
    
    * Prevent circular import error
    
    * Add max_steps to the ConversationalAgent
    
    * Update resolver description
    
    * Add prompt_template as parameter
    
    * Change docstring
    
    ---------
    
    Co-authored-by: Darja Fokina <daria.f93@gmail.com>

--- test/agents/test_tools_manager.py
+++ test/agents/test_tools_manager.py
@@ -18,6 +18,14 @@ def tools_manager():
 
 
 @pytest.mark.unit
+def test_using_callable_as_tool():
+    # test that we can also pass a callable as a tool
+    tool_input = "Haystack"
+    tool = Tool(name="ToolA", pipeline_or_node=lambda x: x + x, description="Tool A Description")
+    assert tool.run(tool_input) == tool_input + tool_input
+
+
+@pytest.mark.unit
 def test_add_tool(tools_manager):
     new_tool = Tool(name="ToolC", pipeline_or_node=mock.Mock(), description="Tool C Description")
     tools_manager.add_tool(new_tool)
