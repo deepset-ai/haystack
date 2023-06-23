e49cfa1446f341d30e462eb26038fd67330b6206
rjanjua <rohan.janjua@gmail.com>
2023-06-23 10:02:46 +0100

    feat: take branch/tag argument for benchmark setup script

--- test/benchmarks/setup_benchmarks.sh
+++ test/benchmarks/setup_benchmarks.sh
@@ -1,6 +1,16 @@
+to_checkout=${1:-"main"}
+
 git reset --hard
 git clean -f -x
-git checkout main
+git checkout $to_checkout
+
+if [ $? -eq 0 ]; then
+    echo "checkout succeeded"
+else
+    echo "checkout failed..do not continue"
+    exit 1
+fi
+
 git pull
 cd ../../
 pip install .
