c5c7711dba58fa3ba630bec01a817641689b0e2b
Vladimir Blagojevic <dovlex@gmail.com>
2023-06-27 17:42:23 +0200

    feat: BM25 retrieval for `MemoryDocumentStore` (#5151)

--- haystack/preview/document_stores/memory/document_store.py
+++ haystack/preview/document_stores/memory/document_store.py
@@ -1,14 +1,26 @@
+import re
 from typing import Literal, Any, Dict, List, Optional, Iterable
 
 import logging
 
+import numpy as np
+import rank_bm25
+from tqdm.auto import tqdm
+
 from haystack.preview.dataclasses import Document
 from haystack.preview.document_stores.memory._filters import match
 from haystack.preview.document_stores.errors import DuplicateDocumentError, MissingDocumentError
-
+from haystack.utils.scipy_utils import expit
 
 logger = logging.getLogger(__name__)
 DuplicatePolicy = Literal["skip", "overwrite", "fail"]
+
+# document scores are essentially unbounded and will be scaled to values between 0 and 1 if scale_score is set to
+# True (default). Scaling uses the expit function (inverse of the logit function) after applying a SCALING_FACTOR. A
+# larger SCALING_FACTOR decreases scaled scores. For example, an input of 10 is scaled to 0.99 with SCALING_FACTOR=2
+# but to 0.78 with SCALING_FACTOR=8 (default). The default was chosen empirically. Increase the default if most
+# unscaled scores are larger than expected (>30) and otherwise would incorrectly all be mapped to scores ~1.
+SCALING_FACTOR = 8
 
 
 class MemoryDocumentStore:
@@ -16,11 +28,22 @@ class MemoryDocumentStore:
     Stores data in-memory. It's ephemeral and cannot be saved to disk.
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        bm25_tokenization_regex: str = r"(?u)\b\w\w+\b",
+        bm25_algorithm: Literal["BM25Okapi", "BM25L", "BM25Plus"] = "BM25Okapi",
+        bm25_parameters: Optional[Dict] = None,
+    ):
         """
         Initializes the store.
         """
-        self.storage = {}
+        self.storage: Dict[str, Document] = {}
+        self.tokenizer = re.compile(bm25_tokenization_regex).findall
+        algorithm_class = getattr(rank_bm25, bm25_algorithm)
+        if algorithm_class is None:
+            raise ValueError(f"BM25 algorithm '{bm25_algorithm}' not found.")
+        self.bm25_algorithm = algorithm_class
+        self.bm25_parameters = bm25_parameters or {}
 
     def count_documents(self) -> int:
         """
@@ -142,3 +165,75 @@ class MemoryDocumentStore:
             if not doc_id in self.storage.keys():
                 raise MissingDocumentError(f"ID '{doc_id}' not found, cannot delete it.")
             del self.storage[doc_id]
+
+    def bm25_retrieval(
+        self, query: str, filters: Optional[Dict[str, Any]] = None, top_k: int = 10, scale_score: bool = True
+    ) -> List[Document]:
+        """
+        Retrieves documents that are most relevant to the query using BM25 algorithm.
+
+        :param query: The query string.
+        :param filters: A dictionary with filters to narrow down the search space.
+        :param top_k: The number of top documents to retrieve. Default is 10.
+        :param scale_score: Whether to scale the scores of the retrieved documents. Default is True.
+        :return: A list of the top 'k' documents most relevant to the query.
+        """
+        if not query:
+            raise ValueError("Query should be a non-empty string")
+
+        # Get all documents that match the user's filters AND are either 'table' or 'text'.
+        # Raises an exception if the user was trying to include other content types.
+        if filters and "content_type" in filters:
+            content_types = filters["content_type"]
+            if isinstance(content_types, str):
+                content_types = [content_types]
+            if any(type_ not in ["text", "table"] for type_ in content_types):
+                raise ValueError(
+                    "MemoryDocumentStore can do BM25 retrieval on no other document type than text or table."
+                )
+        else:
+            filters = filters or {}
+            filters = {**filters, "content_type": ["text", "table"]}
+        all_documents = self.filter_documents(filters=filters)
+
+        # FIXME: remove this guard after resolving https://github.com/deepset-ai/canals/issues/33
+        top_k = top_k if top_k is not None else 10
+
+        # Lowercase all documents
+        lower_case_documents = []
+        for doc in all_documents:
+            if doc.content_type == "text":
+                lower_case_documents.append(doc.content.lower())
+            elif doc.content_type == "table":
+                str_content = doc.content.astype(str)
+                csv_content = str_content.to_csv(index=False)
+                lower_case_documents.append(csv_content.lower())
+
+        # Tokenize the entire content of the document store
+        tokenized_corpus = [
+            self.tokenizer(doc) for doc in tqdm(lower_case_documents, unit=" docs", desc="Ranking by BM25...")
+        ]
+        if len(tokenized_corpus) == 0:
+            logger.info("No documents found for BM25 retrieval. Returning empty list.")
+            return []
+
+        # initialize BM25
+        bm25_scorer = self.bm25_algorithm(tokenized_corpus, **self.bm25_parameters)
+        # tokenize query
+        tokenized_query = self.tokenizer(query.lower())
+        # get scores for the query against the corpus
+        docs_scores = bm25_scorer.get_scores(tokenized_query)
+        if scale_score:
+            docs_scores = [float(expit(np.asarray(score / SCALING_FACTOR))) for score in docs_scores]
+        # get the last top_k indexes and reverse them
+        top_docs_positions = np.argsort(docs_scores)[-top_k:][::-1]
+
+        # Create documents with the BM25 score to return them
+        return_documents = []
+        for i in top_docs_positions:
+            doc = all_documents[i]
+            doc_fields = doc.to_dict()
+            doc_fields["score"] = docs_scores[i]
+            return_document = Document(**doc_fields)
+            return_documents.append(return_document)
+        return return_documents
