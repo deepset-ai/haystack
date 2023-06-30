ec43167a89e8adf7970ae5f9c93178af39d5d412
Silvano Cerza <silvanocerza@gmail.com>
2023-06-30 15:39:19 +0200

    Simplify functions to find I/O sockets

--- canals/pipeline/sockets.py
+++ canals/pipeline/sockets.py
@@ -1,10 +1,10 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-from typing import Union, Optional, Dict, Set, get_origin, get_args
+from typing import Union, Optional, Dict, Set, Any, get_origin, get_args
 
 import logging
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, fields, Field
 
 
 logger = logging.getLogger(__name__)
@@ -27,30 +27,17 @@ def find_input_sockets(component) -> Dict[str, InputSocket]:
     """
     Find a component's input sockets.
     """
-
-    input_sockets = {}
-    for field in fields(component.__canals_input__):
-        if get_origin(field.type) is Union:
-            types = {t for t in get_args(field.type) if t is not type(None)}
-        else:
-            types = {field.type}
-
-        input_sockets[field.name] = InputSocket(name=field.name, types=types)
-
-    return input_sockets
+    return {f.name: InputSocket(name=f.name, types=_get_types(f)) for f in fields(component.__canals_input__)}
 
 
 def find_output_sockets(component) -> Dict[str, OutputSocket]:
     """
     Find a component's output sockets.
     """
-    output_sockets = {}
-    for field in fields(component.__canals_output__):
-        if get_origin(field.type) is Union:
-            types = {t for t in get_args(field.type) if t is not type(None)}
-        else:
-            types = {field.type}
+    return {f.name: OutputSocket(name=f.name, types=_get_types(f)) for f in fields(component.__canals_output__)}
 
-        output_sockets[field.name] = OutputSocket(name=field.name, types=types)
 
-    return output_sockets
+def _get_types(field: Field) -> Set[Any]:
+    if get_origin(field.type) is Union:
+        return {t for t in get_args(field.type) if t is not type(None)}
+    return {field.type}
