2059b7136f463b0b3baea86d2dfb2697f5f2e1fd
Vladimir Blagojevic <dovlex@gmail.com>
2023-07-12 10:43:21 +0200

    Remove import guard as it is not needed

--- haystack/nodes/prompt/invocation_layer/utils.py
+++ haystack/nodes/prompt/invocation_layer/utils.py
@@ -1,6 +1,6 @@
 from typing import Optional, Union
 
-from haystack.lazy_imports import LazyImport
+from huggingface_hub import model_info
 
 
 def has_azure_parameters(**kwargs) -> bool:
@@ -8,14 +8,13 @@ def has_azure_parameters(**kwargs) -> bool:
     return any(kwargs.get(param) for param in azure_params)
 
 
-with LazyImport(message="Run 'pip install farm-haystack[inference]'") as huggingface_import:
-    from huggingface_hub import model_info
+def get_task(model: str, use_auth_token: Optional[Union[str, bool]] = None, timeout: float = 3.0) -> Optional[str]:
+    """
+    Retrieve the task (pipeline tag) associated with a given model.
 
-    def get_task(model: str, use_auth_token: Optional[Union[str, bool]] = None, timeout: float = 3.0) -> Optional[str]:
-        """
-        Simplified version of transformers.pipelines.get_task with support for timeouts
-        """
-        try:
-            return model_info(model, token=use_auth_token, timeout=timeout).pipeline_tag
-        except Exception as e:
-            raise RuntimeError(f"The task of {model} could not be checked because of the following error: {e}") from e
+    Simplified version of transformers.pipelines.get_task with support for timeouts
+    """
+    try:
+        return model_info(model, token=use_auth_token, timeout=timeout).pipeline_tag
+    except Exception as e:
+        raise RuntimeError(f"The task for {model} couldn't be found!") from e
