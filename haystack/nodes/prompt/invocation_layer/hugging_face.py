8234e9afc190e078be26ef24cbd94c6f03d7cd02
Lin, Fanli <fanli.lin@intel.com>
2023-07-12 23:51:32 -0700

    add doc

--- haystack/nodes/prompt/invocation_layer/hugging_face.py
+++ haystack/nodes/prompt/invocation_layer/hugging_face.py
@@ -28,6 +28,11 @@ with LazyImport(message="Run 'pip install farm-haystack[inference]'") as torch_a
     class StopWordsCriteria(StoppingCriteria):
         """
         Stops text generation if any one of the stop words is generated.
+
+        Note: this class only examines the newly generated text. For LLMs, which is designed
+        for dialogue generation and outputs the newly generated text together with the prompt
+        like `mosaicml/mpt-7b-chat`, this class will stop generating new tokens after the first token.
+        In this case, you need to make sure that your prompt template doesn't contain any stop word.
         """
 
         def __init__(
@@ -40,8 +45,8 @@ with LazyImport(message="Run 'pip install farm-haystack[inference]'") as torch_a
             self.stop_words = tokenizer(stop_words, add_special_tokens=False, return_tensors="pt").to(device)
 
         def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-            stop_result = torch.isin(self.stop_words["input_ids"], input_ids[0][-1])
-            return any(stop_word for stop_word in stop_result)
+            stop_result = torch.isin(self.stop_words["input_ids"], input_ids[-1])
+            return any(all(stop_word) for stop_word in stop_result)
 
     def get_task(model: str, use_auth_token: Optional[Union[str, bool]] = None, timeout: float = 3.0) -> Optional[str]:
         """
