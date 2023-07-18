21a6cc7039cdd6fee5dc9959dda752bfb3476099
Vladimir Blagojevic <dovlex@gmail.com>
2023-07-18 11:17:14 +0200

    Add 'Lost in the Middle' layout to DocumentMerger

--- haystack/nodes/other/document_merger.py
+++ haystack/nodes/other/document_merger.py
@@ -8,6 +8,89 @@ from haystack.nodes.base import BaseComponent
 logger = logging.getLogger(__name__)
 
 
+def truncate_document(
+    document: str, separator: Optional[str] = None, word_count_threshold: Optional[int] = None
+) -> str:
+    """
+    Shortens a document by cutting off the content after a specified number of words.
+
+    :param document: String representing the content of the document.
+    :param separator: Optional string representing the separator between words in the document.
+    :param word_count_threshold: Optional integer representing the maximum number of words
+                                 allowed in the truncated document. If None or less than 1,
+                                 no truncation will occur.
+
+    :return: A string containing the truncated document. If the word count threshold is not
+             specified or is less than 1, the original document is returned.
+    """
+    separator = separator or " "
+    return (
+        separator.join(document.split()[:word_count_threshold])
+        if word_count_threshold and word_count_threshold > 0
+        else document
+    )
+
+
+def default_order(
+    documents: List[Document], separator: Optional[str] = None, word_count_threshold: Optional[int] = None
+) -> str:
+    """
+    Merges documents in the order they are provided.
+
+    :param documents: List of Documents to merge.
+    :param separator: The separator that appears between subsequent merged documents.
+    :param word_count_threshold: The maximum number of words in the merged document.
+
+    :return: The merged document as a string.
+    """
+    separator = separator or " "
+    merged_doc_content = separator.join([doc.content for doc in documents])
+    return truncate_document(merged_doc_content, separator, word_count_threshold)
+
+
+def lost_in_the_middle_order(
+    documents: List[Document], separator: Optional[str] = None, word_count_threshold: Optional[int] = None
+) -> str:
+    """
+    Merges documents as suggested by "Lost in the Middle: How Language Models Use Long Contexts" by Liu et al.
+    Liu et al. suggest to layout paragraphs into LLM context so that relevant paragraphs at the beginning
+    or end of the input context, while the least relevant information should be in the middle of a context.
+
+    See https://arxiv.org/abs/2307.03172 for more details.
+
+    :param documents: List of Documents to merge.
+    :param separator: The separator that appears between subsequent merged documents.
+    :param word_count_threshold: The maximum number of words in the merged document.
+
+    :return: The merged document as a string.
+    """
+    if not documents:
+        return ""
+    if len(documents) == 1:
+        return truncate_document(documents[0].content, separator, word_count_threshold)
+
+    document_index = [i for i in range(len(documents))]
+
+    merged_doc_content = ""
+    separator = separator or " "
+    lost_in_the_middle_indices = [0]
+    for doc in document_index[1:]:
+        insertion_index = len(lost_in_the_middle_indices) // 2 + len(lost_in_the_middle_indices) % 2
+        lost_in_the_middle_indices.insert(insertion_index, doc)
+
+        merged_doc_content = separator.join([documents[index].content for index in lost_in_the_middle_indices])
+
+        # While it might seem intuitive to first order the documents and subsequently truncate the combined
+        # document, this approach would yield an inaccurate order. Instead, we need to truncate the documents
+        # concurrently with their arrangement in the "lost in the middle" order, as this dynamic layout process
+        # determines the correct sequencing of the documents while respecting the word count threshold.
+        if word_count_threshold and len(merged_doc_content.split()) >= word_count_threshold:
+            merged_doc_content = truncate_document(merged_doc_content, separator, word_count_threshold)
+            break
+
+    return merged_doc_content.strip()
+
+
 class DocumentMerger(BaseComponent):
     """
     A node to merge the texts of the documents.
@@ -15,17 +98,27 @@ class DocumentMerger(BaseComponent):
 
     outgoing_edges = 1
 
-    def __init__(self, separator: str = " "):
+    def __init__(self, separator: str = " ", order: str = "default", word_count_threshold: Optional[int] = None):
         """
         :param separator: The separator that appears between subsequent merged documents.
+        :param order: The order in which the documents are merged.
+        :param word_count_threshold: Optional integer representing the maximum number of words in the merged document.
         """
         super().__init__()
         self.separator = separator
+        self.word_count_threshold = word_count_threshold
+        self.layout_algorithms = {"default": default_order, "lost_in_the_middle": lost_in_the_middle_order}
+        if order not in self.layout_algorithms:
+            raise ValueError(
+                f"Unknown DocumentMerger order {order}. Possible values are {list(self.layout_algorithms.keys())}."
+            )
+        self.order = order
 
     def merge(self, documents: List[Document], separator: Optional[str] = None) -> List[Document]:
         """
         Produce a list made up of a single document, which contains all the texts of the documents provided.
 
+        :param documents: List of Documents to merge.
         :param separator: The separator that appears between subsequent merged documents.
         :return: List of Documents
         """
@@ -38,7 +131,9 @@ class DocumentMerger(BaseComponent):
 
         separator = separator if separator is not None else self.separator
 
-        merged_content = separator.join([doc.content for doc in documents])
+        merged_content = self.layout_algorithms[self.order](
+            documents=documents, separator=separator, word_count_threshold=self.word_count_threshold
+        )
         common_meta = self._keep_common_keys([doc.meta for doc in documents])
 
         merged_document = Document(content=merged_content, meta=common_meta)
--- test/nodes/test_document_merger.py
+++ test/nodes/test_document_merger.py
@@ -106,3 +106,111 @@ def test_document_merger_run_batch(documents, doc_dicts):
     assert len(batch_result[0]["documents"]) == 2
     assert batch_result[0]["documents"][0][0].content == separator.join([doc["content"] for doc in doc_dicts])
     assert batch_result[0]["documents"][0][0].meta == {"flat_field": 1, "nested_field": {1: 2, "c": {"3": 3}}}
+
+
+@pytest.mark.unit
+def test_lost_in_the_middle_order_odd():
+    docs = [
+        Document("1"),
+        Document("2"),
+        Document("3"),
+        Document("4"),
+        Document("5"),
+        Document("6"),
+        Document("7"),
+        Document("8"),
+        Document("9"),
+    ]
+    dm = DocumentMerger(order="lost_in_the_middle")
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "1 3 5 7 9 8 6 4 2"
+
+
+@pytest.mark.unit
+def test_batch_lost_in_the_middle_order_():
+    docs = [
+        [Document("1"), Document("2"), Document("3"), Document("4")],
+        [Document("5"), Document("6")],
+        [Document("7"), Document("8"), Document("9")],
+    ]
+    dm = DocumentMerger(order="lost_in_the_middle")
+    result, _ = dm.run_batch(docs)
+
+    assert result["documents"][0][0].content == "1 3 4 2"
+    assert result["documents"][1][0].content == "5 6"
+    assert result["documents"][2][0].content == "7 9 8"
+
+
+@pytest.mark.unit
+def test_lost_in_the_middle_order_even():
+    docs = [
+        Document("1"),
+        Document("2"),
+        Document("3"),
+        Document("4"),
+        Document("5"),
+        Document("6"),
+        Document("7"),
+        Document("8"),
+        Document("9"),
+        Document("10"),
+    ]
+    dm = DocumentMerger(order="lost_in_the_middle")
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "1 3 5 7 9 10 8 6 4 2"
+
+
+@pytest.mark.unit
+def test_lost_in_the_middle_order_corner():
+    dm = DocumentMerger(order="lost_in_the_middle")
+
+    # empty doc list
+    docs = []
+    result, _ = dm.run(docs)
+    assert len(result["documents"]) == 0
+
+    # single doc
+    docs = [Document("1")]
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "1"
+
+    # two docs
+    docs = [Document("1"), Document("2")]
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "1 2"
+
+
+@pytest.mark.unit
+def test_document_merger_init():
+    dm = DocumentMerger()
+    assert dm.order == "default"
+    assert dm.separator == " "
+    assert not dm.word_count_threshold
+
+
+@pytest.mark.unit
+def test_document_merger_init_with_some_unknown_order():
+    with pytest.raises(ValueError, match="Unknown DocumentMerger order"):
+        DocumentMerger(order="some_unknown_order")
+
+
+@pytest.mark.unit
+def test_document_merger_with_word_count_threshold():
+    dm = DocumentMerger(order="lost_in_the_middle", word_count_threshold=6)
+    docs = [
+        Document("word1"),
+        Document("word2"),
+        Document("word3"),
+        Document("word4"),
+        Document("word5"),
+        Document("word6"),
+        Document("word7"),
+        Document("word8"),
+        Document("word9"),
+    ]
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "word1 word3 word5 word6 word4 word2"
+
+    dm = DocumentMerger(order="lost_in_the_middle", word_count_threshold=9)
+    result, _ = dm.run(docs)
+    assert result["documents"][0].content == "word1 word3 word5 word7 word9 word8 word6 word4 word2"
