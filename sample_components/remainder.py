29590a2bb2cff51215d3d74467f6dfc0b122967f
ZanSara <sara.zanzottera@deepset.ai>
2023-07-20 10:54:58 +0200

    dynamic output examples

--- sample_components/remainder.py
+++ sample_components/remainder.py
@@ -1,43 +1,40 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-from dataclasses import make_dataclass
 
 from canals import component
 
 
 class Remainder:
-    ...
+    @staticmethod
+    def create(divisor: int = 3):
+        """
+        Redirects the value, unchanged, along the connection corresponding to the remainder
+        of a division. For example, if `divisor=3`, the value `5` would be sent along
+        the second output connection.
+        """
 
+        @component
+        class RemainderImpl:
+            """
+            Implementation of Reminder()
+            """
 
-# @component
-# class Remainder:
-#     """
-#     Redirects the value, unchanged, along the connection corresponding to the remainder
-#     of a division. For example, if `divisor=3`, the value `5` would be sent along
-#     the second output connection.
-#     """
+            def __init__(self, divisor):
+                if divisor == 0:
+                    raise ValueError("Can't divide by zero")
+                self.divisor = divisor
 
-#     def __new__(cls, **kwargs):
-#         """
-#         Factory method for Remainder
-#         """
-#         obj = super().__new__(cls)
+            @component.return_types(**{f"remainder_is_{val}": int for val in range(divisor)})
+            def run(self, value: int):
+                """
+                :param value: the value to check the remainder of.
+                """
+                remainder = value % self.divisor
+                output = {f"remainder_is_{val}": None if val != remainder else value for val in range(self.divisor)}
+                return output
 
-#         @component.return_types(**{f"remainder_is_{val}": int for val in range(obj.divisor)})
-#         def run(self, value: int):
-#             """
-#             :param value: the value to check the remainder of.
-#             """
-#             remainder = value % obj.divisor
-#             output = {f"remainder_is_{val}": int for val in range(obj.divisor)}
-#             output[f"remainder_is_{remainder}"] = value
-#             return output
+        return RemainderImpl(divisor=divisor)
 
-#         obj.run = run
-#         return obj
-
-#     def __init__(self, divisor: int = 2):
-#         if divisor == 0:
-#             raise ValueError("Can't divide by zero")
-#         self.divisor = divisor
+    def __init__(self):
+        raise NotImplementedError("use Remainder.create()")
--- sample_components/repeat.py
+++ sample_components/repeat.py
@@ -3,38 +3,33 @@
 # SPDX-License-Identifier: Apache-2.0
 from typing import List
 
-from dataclasses import make_dataclass
-
 from canals import component
 
 
 class Repeat:
-    ...
+    @staticmethod
+    def create(outputs: List[str]):
+        """
+        Repeats the input value on all outputs.
+        """
 
+        @component
+        class RepeatImpl:
+            """
+            Implementation of Repeat()
+            """
 
-# @component
-# class Repeat:
-#     """
-#     Repeats the input value on all outputs.
-#     """
+            def __init__(self, outputs: List[str]):
+                self.outputs = outputs
 
-#     @component.input
-#     def input(self):
-#         class Input:
-#             value: int
+            @component.return_types(**{val: int for val in outputs})
+            def run(self, value: int):
+                """
+                :param value: the value to repeat.
+                """
+                return {val: value for val in self.outputs}
 
-#         return Input
+        return RepeatImpl(outputs=outputs)
 
-#     def __init__(self, outputs: List[str] = ["output_1", "output_2", "output_3"]):
-#         self.outputs = outputs
-#         self._output_type = make_dataclass("Output", fields=[(val, int, None) for val in outputs])
-
-#     @component.output
-#     def output(self):
-#         return self._output_type
-
-#     def run(self, data):
-#         output_dataclass = self.output()
-#         for output in self.outputs:
-#             setattr(output_dataclass, output, data.value)
-#         return output_dataclass
+    def __init__(self):
+        raise NotImplementedError("use Repeat.create()")
