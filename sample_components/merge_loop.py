5a2d655bea7dfa84a56e2e37aa52ef374188d321
ZanSara <sara.zanzottera@deepset.ai>
2023-07-20 12:22:20 +0200

    add components fixed

--- sample_components/merge_loop.py
+++ sample_components/merge_loop.py
@@ -1,70 +1,36 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-import builtins
-from typing import List, Union, Optional
-from dataclasses import make_dataclass, is_dataclass, asdict
+from typing import List, Any
 
 from canals import component
 
 
 class MergeLoop:
-    ...
+    @staticmethod
+    def create(expected_type: type, inputs: List[str]):
+        """
+        Takes multiple inputs and returns the first one that is not None.
+        """
 
+        @component
+        class MergeLoopImpl:
+            """
+            Implementation of MergeLoop()
+            """
 
-# @component
-# class MergeLoop:
+            @component.return_types(value=expected_type)
+            @component.run_method_types(**{input_name: expected_type for input_name in inputs})
+            def run(self, **kwargs: Any):
+                """
+                :param kwargs: find the first non-None value and return it.
+                """
+                for v in kwargs.values():
+                    if v is not None:
+                        return {"value": v}
+                return {"value": None}
 
-#     """
-#     Takes multiple inputs and returns the first one that is not None.
-#     """
+        return MergeLoopImpl()
 
-#     def __new__(cls, *args, **kwargs):
-#         """
-#         Factory method for MergeLoop
-#         """
-
-#         @component.return_types(value=str)
-#         def run(self, values: List[Optional[str]]):
-#             """
-#             Takes some inputs and returns the first one that is not None.
-#             """
-#             for v in values:
-#                 if v is not None:
-#                     return {"value": v}
-#             return {"value": None}
-
-#         cls.run = run
-#         return super().__new__(cls)
-
-#     def __init__(self, expected_type: Union[type, str], inputs: List[str] = ["value_1", "value_2"]):
-#         self.expected_type = expected_type
-
-
-# def merge_loop_component(expected_type: Union[type, str], inputs: List[str] = ["value_1", "value_2"]):
-#     """
-#     Factory method for MergeLoop
-#     """
-
-#     @component
-#     class MergeLoop:
-
-#         """
-#         Takes multiple inputs and returns the first one that is not None.
-#         """
-
-#         def __init__(self):
-#             self.expected_type = expected_type
-
-#         @component.run_types(**{input: expected_type for input in inputs})
-#         @component.return_types(value=expected_type)
-#         def run(self, values: List[Optional[str]]):
-#             """
-#             Takes some inputs and returns the first one that is not None.
-#             """
-#             for v in values:
-#                 if v is not None:
-#                     return {"value": v}
-#             return {"value": None}
-
-#     return MergeLoop
+    def __init__(self):
+        raise NotImplementedError("use MergeLoop.create()")
--- sample_components/sum.py
+++ sample_components/sum.py
@@ -32,32 +32,3 @@ class Sum:
 
     def __init__(self):
         raise NotImplementedError("use Sum.create()")
-
-
-# @component
-# class Sum:
-# """
-# Sums the values of all the input connections together.
-# """
-
-#     def __init__(self, inputs=["value_1"]) -> None:
-#         # mypy complains that we can't Optional is not a type, so we ignore the error
-#         # cause we consider this to be correct
-#         self._input = make_dataclass("Input", fields=[(f, Optional[int]) for f in inputs])  # type: ignore
-
-#     @component.input
-#     def input(self):
-#         return self._input
-
-#     @component.output
-#     def output(self):
-#         class Output:
-#             total: int
-
-#         return Output
-
-#     def run(self, data):
-#         values = []
-#         if is_dataclass(data):
-#             values = [n for n in asdict(data).values() if n]
-#         return self.output(total=sum(values))
