4aaa91ed0e712def7357fc2dc119f9fbcfd1be02
Lin, Fanli <fanli.lin@intel.com>
2023-07-20 22:58:07 -0700

    use tokenizer class

--- haystack/nodes/prompt/invocation_layer/hugging_face.py
+++ haystack/nodes/prompt/invocation_layer/hugging_face.py
@@ -337,7 +337,9 @@ class HFLocalInvocationLayer(PromptModelInvocationLayer):
                 raise ValueError(f"Invalid torch_dtype value {torch_dtype}")
         return torch_dtype_resolved
 
-    def _prepare_tokenizer(self, model: str, hub_kwargs: Dict, model_kwargs: Optional[Dict]):
+    def _prepare_tokenizer(
+        self, model: str, hub_kwargs: Dict, model_kwargs: Optional[Dict]
+    ) -> Optional[Union["PreTrainedTokenizer", "PreTrainedTokenizerFast", None]]:
         """
         this method prepares the tokenizer before passing it to transformers' pipeline, so that the instantiated pipeline
         object has a working tokenizer.
