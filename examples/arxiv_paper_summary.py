c262cec64b20cd4d0e88c76f9ff5d44e1ca02d72
Vladimir Blagojevic <dovlex@gmail.com>
2023-07-22 21:16:42 +0200

    Use claude-instant-1 for arxiv_paper_summary.py

--- examples/arxiv_paper_summary.py
+++ examples/arxiv_paper_summary.py
@@ -2,9 +2,9 @@ import os
 from haystack.nodes import PromptNode, LinkContentFetcher, PromptTemplate
 from haystack import Pipeline
 
-openai_key = os.environ.get("OPENAI_API_KEY")
-if not openai_key:
-    raise ValueError("Please set the OPENAI_API_KEY environment variable")
+anthropic_key = os.environ.get("ANTHROPIC_API_KEY")
+if not anthropic_key:
+    raise ValueError("Please set the ANTHROPIC_API_KEY environment variable")
 
 alt_user_agents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15"
@@ -19,11 +19,7 @@ pt = PromptTemplate(
 )
 
 prompt_node = PromptNode(
-    "gpt-3.5-turbo-16k-0613",
-    api_key=openai_key,
-    max_length=512,
-    default_prompt_template=pt,
-    model_kwargs={"stream": True},
+    "claude-instant-1", api_key=anthropic_key, max_length=512, default_prompt_template=pt, model_kwargs={"stream": True}
 )
 
 pipeline = Pipeline()
