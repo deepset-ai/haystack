7940ec048277354e3dadf07584b7a145fc904961
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-07-26 09:32:23 +0200

    Add @store decorator (#5438)

--- haystack/preview/__init__.py
+++ haystack/preview/__init__.py
@@ -1,3 +1,4 @@
 from canals.component import component
+from haystack.preview.document_stores.decorator import store
 from haystack.preview.dataclasses import Document
 from haystack.preview.pipeline import Pipeline, PipelineError, NoSuchStoreError, load_pipelines, save_pipelines
