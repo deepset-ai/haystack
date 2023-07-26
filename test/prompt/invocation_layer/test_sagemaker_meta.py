1600e51f4f652cd06f5e0f9aeda039f05a1bc121
Vladimir Blagojevic <dovlex@gmail.com>
2023-07-26 09:10:07 +0200

    Improve unit tests

--- test/prompt/invocation_layer/test_sagemaker_meta.py
+++ test/prompt/invocation_layer/test_sagemaker_meta.py
@@ -44,6 +44,9 @@ def test_default_constructor(mock_auto_tokenizer, mock_boto3_session):
     assert layer.max_length == 99
     assert layer.model_name_or_path == "some_fake_model"
 
+    assert layer.prompt_handler is not None
+    assert layer.prompt_handler.model_max_length == 4096
+
     # assert mocked boto3 client called exactly once
     mock_boto3_session.assert_called_once()
 
@@ -58,10 +61,19 @@ def test_default_constructor(mock_auto_tokenizer, mock_boto3_session):
 
 
 @pytest.mark.unit
+def test_constructor_prompt_handler_initialized(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the constructor sets the prompt_handler correctly, with the correct model_max_length for llama-2
+    """
+    layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model", prompt_handler=mock_prompt_handler)
+    assert layer.prompt_handler is not None
+    assert layer.prompt_handler.model_max_length == 4096
+
+
+@pytest.mark.unit
 def test_constructor_with_model_kwargs(mock_auto_tokenizer, mock_boto3_session):
     """
     Test that model_kwargs are correctly set in the constructor
-    and that model_kwargs_rejected are correctly filtered out
     """
     model_kwargs = {"temperature": 0.7}
 
@@ -70,9 +82,18 @@ def test_constructor_with_model_kwargs(mock_auto_tokenizer, mock_boto3_session):
 
 
 @pytest.mark.unit
+def test_constructor_with_empty_model_name():
+    """
+    Test that the constructor raises an error when the model_name_or_path is empty
+    """
+    with pytest.raises(ValueError, match="cannot be None or empty string"):
+        SageMakerMetaInvocationLayer(model_name_or_path="")
+
+
+@pytest.mark.unit
 def test_invoke_with_no_kwargs(mock_auto_tokenizer, mock_boto3_session):
     """
-    Test that invoke raises an error if no prompt is provided
+    Test invoke raises an error if no prompt is provided
     """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     with pytest.raises(ValueError) as e:
@@ -83,7 +104,7 @@ def test_invoke_with_no_kwargs(mock_auto_tokenizer, mock_boto3_session):
 @pytest.mark.unit
 def test_invoke_with_stop_words(mock_auto_tokenizer, mock_boto3_session):
     """
-    SageMakerMetaInvocationLayer does not support stop words, they'll be ignored
+    Tests that SageMakerMetaInvocationLayer does not support stop words, they'll be ignored
     """
     stop_words = ["but", "not", "bye"]
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_model")
@@ -100,6 +121,9 @@ def test_invoke_with_stop_words(mock_auto_tokenizer, mock_boto3_session):
 
 @pytest.mark.unit
 def test_short_prompt_is_not_truncated(mock_boto3_session):
+    """
+    Test that a short prompt is not truncated
+    """
     # Define a short mock prompt and its tokenized version
     mock_prompt_text = "I am a tokenized prompt"
     mock_prompt_tokens = mock_prompt_text.split()
@@ -126,6 +150,9 @@ def test_short_prompt_is_not_truncated(mock_boto3_session):
 
 @pytest.mark.unit
 def test_long_prompt_is_truncated(mock_boto3_session):
+    """
+    Test that a long prompt is truncated
+    """
     # Define a long mock prompt and its tokenized version
     long_prompt_text = "I am a tokenized prompt of length eight"
     long_prompt_tokens = long_prompt_text.split()
@@ -155,15 +182,9 @@ def test_long_prompt_is_truncated(mock_boto3_session):
 
 
 @pytest.mark.unit
-def test_empty_model_name():
-    with pytest.raises(ValueError, match="cannot be None or empty string"):
-        SageMakerMetaInvocationLayer(model_name_or_path="")
-
-
-@pytest.mark.unit
 def test_streaming_init_kwarg(mock_auto_tokenizer, mock_boto3_session):
     """
-    Test stream parameter passed as init kwarg is correctly logged as not supported
+    Test stream parameter passed as init kwarg raises an error on layer invocation
     """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="irrelevant", stream=True)
 
@@ -174,7 +195,7 @@ def test_streaming_init_kwarg(mock_auto_tokenizer, mock_boto3_session):
 @pytest.mark.unit
 def test_streaming_invoke_kwarg(mock_auto_tokenizer, mock_boto3_session):
     """
-    Test stream parameter passed as invoke kwarg is correctly logged as not supported
+    Test stream parameter passed as invoke kwarg raises an error on layer invocation
     """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="irrelevant")
 
@@ -185,7 +206,7 @@ def test_streaming_invoke_kwarg(mock_auto_tokenizer, mock_boto3_session):
 @pytest.mark.unit
 def test_streaming_handler_init_kwarg(mock_auto_tokenizer, mock_boto3_session):
     """
-    Test stream_handler parameter passed as init kwarg is correctly logged as not supported
+    Test stream_handler parameter passed as init kwarg raises an error on layer invocation
     """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="irrelevant", stream_handler=Mock())
 
@@ -196,7 +217,7 @@ def test_streaming_handler_init_kwarg(mock_auto_tokenizer, mock_boto3_session):
 @pytest.mark.unit
 def test_streaming_handler_invoke_kwarg(mock_auto_tokenizer, mock_boto3_session):
     """
-    Test stream_handler parameter passed as invoke kwarg is correctly logged as not supported
+    Test stream_handler parameter passed as invoke kwarg raises an error on layer invocation
     """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="irrelevant")
 
@@ -302,6 +323,9 @@ def test_supports_not_on_eula_not_accepted():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_non_empty_dict():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes specified
+    """
     attributes = {"key1": "value1", "key2": "value2"}
     expected_output = "key1=value1;key2=value2"
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -309,6 +333,9 @@ def test_format_custom_attributes_with_non_empty_dict():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_empty_dict():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes not specified
+    """
     attributes = {}
     expected_output = ""
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -316,6 +343,9 @@ def test_format_custom_attributes_with_empty_dict():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_none():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes are None
+    """
     attributes = None
     expected_output = ""
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -323,6 +353,9 @@ def test_format_custom_attributes_with_none():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_bool_value():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes are bool
+    """
     attributes = {"key1": True, "key2": False}
     expected_output = "key1=true;key2=false"
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -330,6 +363,9 @@ def test_format_custom_attributes_with_bool_value():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_single_bool_value():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes are single bool
+    """
     attributes = {"key1": True}
     expected_output = "key1=true"
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -337,6 +373,9 @@ def test_format_custom_attributes_with_single_bool_value():
 
 @pytest.mark.unit
 def test_format_custom_attributes_with_int_value():
+    """
+    Test that the SageMakerMetaInvocationLayer correctly formats the custom attributes, attributes are ints
+    """
     attributes = {"key1": 1, "key2": 2}
     expected_output = "key1=1;key2=2"
     assert SageMakerMetaInvocationLayer.format_custom_attributes(attributes) == expected_output
@@ -344,6 +383,9 @@ def test_format_custom_attributes_with_int_value():
 
 @pytest.mark.unit
 def test_invoke_chat_format(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the SageMakerMetaInvocationLayer accepts a chat in the correct format
+    """
     # test the format of the chat, no exception should be raised
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     prompt = [[{"role": "user", "content": "Hello"}]]
@@ -356,6 +398,9 @@ def test_invoke_chat_format(mock_auto_tokenizer, mock_boto3_session):
 
 @pytest.mark.unit
 def test_invoke_invalid_chat_format(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the SageMakerMetaInvocationLayer raises an exception when the chat is in the wrong format
+    """
     # test the invalid format of the chat, should raise an exception
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     prompt = [{"roe": "user", "cotent": "Hello"}]
@@ -368,7 +413,10 @@ def test_invoke_invalid_chat_format(mock_auto_tokenizer, mock_boto3_session):
 
 
 @pytest.mark.unit
-def test_invoke_instruction_format(mock_auto_tokenizer, mock_boto3_session):
+def test_invoke_prompt_string(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the SageMakerMetaInvocationLayer accepts a prompt in the correct string format
+    """
     # test the format of the prompt instruction, no exception should be raised
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     with patch("haystack.nodes.prompt.invocation_layer.sagemaker_meta.SageMakerMetaInvocationLayer._post") as mock_post:
@@ -378,21 +426,19 @@ def test_invoke_instruction_format(mock_auto_tokenizer, mock_boto3_session):
 
 @pytest.mark.unit
 def test_invoke_empty_prompt(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the SageMakerMetaInvocationLayer raises an exception when the prompt is empty string
+    """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     with pytest.raises(ValueError):
         layer.invoke(prompt="")
 
 
 @pytest.mark.unit
-def test_invoke_improper_chat_format(mock_auto_tokenizer, mock_boto3_session):
-    layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
-    prompt = [[{"role": "user", "conten": "Hello"}]]
-    with pytest.raises(ValueError, match="The prompt format is different than what the model expects"):
-        layer.invoke(prompt=prompt)
-
-
-@pytest.mark.unit
-def test_invoke_improper_instruction_format(mock_auto_tokenizer, mock_boto3_session):
+def test_invoke_improper_prompt_type(mock_auto_tokenizer, mock_boto3_session):
+    """
+    Test that the SageMakerMetaInvocationLayer raises an exception when the prompt is int instead of str
+    """
     layer = SageMakerMetaInvocationLayer(model_name_or_path="some_fake_model")
     prompt = 123
     with pytest.raises(ValueError):
