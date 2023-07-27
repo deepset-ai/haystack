5f12f189324a2b591a089592cf77a654bfa4353f
bogdankostic <bogdankostic@web.de>
2023-07-27 22:37:14 +0200

    use services

--- test/benchmarks/startup.sh
+++
@@ -1,3 +0,0 @@
-docker run --name elasticsearch -d -p 9201:9200 -e "discovery.type=single-node" elasticsearch:7.17.6
-docker run --name opensearch -d -p 9200:9200 -e "discovery.type=single-node" opensearchproject/opensearch:1.3.5
-docker run --name weaviate -d -p 8080:8080 -e "AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true" -e "PERSISTENCE_DATA_PATH=/var/lib/weaviate" semitechnologies/weaviate:1.20.3
