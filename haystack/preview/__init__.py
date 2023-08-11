f47175f34d2c5eac0567feaff18dcb7c7bdcfd68
Michel Bartels <login@michelbartels.com>
2023-08-11 19:14:26 +0200

    initial ExtractiveReader implementation

--- haystack/preview/__init__.py
+++ haystack/preview/__init__.py
@@ -1,4 +1,4 @@
 from canals.component import component
 from haystack.preview.document_stores.decorator import store
-from haystack.preview.dataclasses import Document
+from haystack.preview.dataclasses import Document, Answer
 from haystack.preview.pipeline import Pipeline, PipelineError, NoSuchStoreError, load_pipelines, save_pipelines
--- haystack/preview/dataclasses/__init__.py
+++ haystack/preview/dataclasses/__init__.py
@@ -1,3 +1,4 @@
 from haystack.preview.dataclasses.document import Document, ContentType
+from haystack.preview.dataclasses.answer import Answer
 
-__all__ = ["Document", "ContentType"]
+__all__ = ["Document", "ContentType", "Answer"]
