d61a22ff4f797597d2060859ec8b4ecb47933158
Vladimir Blagojevic <dovlex@gmail.com>
2023-08-15 15:58:55 +0200

    feat: Add FileExtensionClassifier to previews (#5514)
    * Add FileExtensionClassifier preview component
    
    * Add release note
    
    * PR feedback

--- haystack/preview/components/__init__.py
+++ haystack/preview/components/__init__.py
@@ -1,3 +1,4 @@
 from haystack.preview.components.audio.whisper_local import LocalWhisperTranscriber
 from haystack.preview.components.audio.whisper_remote import RemoteWhisperTranscriber
 from haystack.preview.components.file_converters import TextFileToDocument
+from haystack.preview.components.classifiers import FileExtensionClassifier
