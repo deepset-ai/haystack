306e9625b6b307def15494a56145d3ff2ea01579
Michel Bartels <login@michelbartels.com>
2023-08-16 15:04:43 +0200

    use new answer class

--- haystack/preview/components/reader.py
+++ haystack/preview/components/reader.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
-from haystack.preview import component, Document, Answer
+from haystack.preview import component, Document, ExtractedAnswer
 from haystack.lazy_imports import LazyImport
 
 with LazyImport(message="Run 'pip install farm-haystack[inference]'") as torch_and_transformers_import:
@@ -36,18 +36,20 @@ class ExtractiveReader:
             self.model_ = AutoModelForQuestionAnswering.from_pretrained(self.model).to(self.device)
             self.tokenizer = AutoTokenizer.from_pretrained(self.model)
 
-    def _flatten(self, queries: List[str], documents: List[List[Document]]) -> Tuple[List[str], List[str], List[int]]:
+    def _flatten(
+        self, queries: List[str], documents: List[List[Document]]
+    ) -> Tuple[List[str], List[Document], List[int]]:
         flattened_queries = [query for documents_, query in zip(documents, queries) for _ in documents_]
-        flattened_documents = [document.content for documents_ in documents for document in documents_]
+        flattened_documents = [document for documents_ in documents for document in documents_]
         query_ids = [i for i, documents_ in enumerate(documents) for _ in documents_]
         return flattened_queries, flattened_documents, query_ids
 
     def _preprocess(
-        self, queries: List[str], documents: List[str], max_seq_length: int, query_ids: List[int], stride: int
+        self, queries: List[str], documents: List[Document], max_seq_length: int, query_ids: List[int], stride: int
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, List[Encoding], List[int], List[int]]:
         encodings = self.tokenizer(
             queries,
-            documents,
+            [document.content for document in documents],
             padding=True,
             truncation=True,
             max_length=max_seq_length,
@@ -129,19 +131,19 @@ class ExtractiveReader:
         top_k: int,
         encodings: List[Encoding],
         query_ids: List[int],
-    ) -> Tuple[List[List[int]], List[List[int]], List[List[float]]]:
+    ) -> Tuple[List[List[Optional[int]]], List[List[Optional[int]]], List[List[float]]]:
         start = output.start_logits
         end = output.end_logits
         mask = sequence_ids == 1
-        mask[..., 0] = True  # type: ignore # mypy doesn't understand broadcasting
-        mask = torch.logical_and(mask, attention_mask == 1)  # type: ignore # see above
-        start = torch.where(mask, start, -torch.inf)  # type: ignore # see above
-        end = torch.where(mask, end, -torch.inf)  # type: ignore # see above
+        mask[..., 0] = True  # mypy doesn't understand broadcasting
+        mask = torch.logical_and(mask, attention_mask == 1)
+        start = torch.where(mask, start, -torch.inf)
+        end = torch.where(mask, end, -torch.inf)
         start = start.unsqueeze(-1)
         end = end.unsqueeze(-2)
 
         logits = start + end  # shape: (batch_size, seq_length (start), seq_length (end))
-        mask = torch.ones(logits.shape[-2:], dtype=bool)
+        mask = torch.ones(logits.shape[-2:], dtype=torch.bool)
         mask = torch.triu(mask)  # End shouldn't be before start
         mask[0, :1] = False
         masked_logits = torch.where(mask, logits, -torch.inf)
@@ -156,11 +158,11 @@ class ExtractiveReader:
         end_candidates = end_candidates.cpu()
 
         start_candidates = [
-            [encoding.token_to_chars(start)[0] if start != 0 else 0 for start in candidates]
+            [encoding.token_to_chars(start)[0] if start != 0 else None for start in candidates]
             for candidates, encoding in zip(start_candidates, encodings)
         ]
         end_candidates = [
-            [encoding.token_to_chars(end)[1] if end != 0 else 0 for end in candidates]
+            [encoding.token_to_chars(end)[1] if end != 0 else None for end in candidates]
             for candidates, encoding in zip(end_candidates, encodings)
         ]
         probabilities = candidates.values.cpu().tolist()
@@ -169,17 +171,20 @@ class ExtractiveReader:
 
     def _unflatten(
         self,
-        start: List[List[int]],
-        end: List[List[int]],
+        start: List[List[Optional[int]]],
+        end: List[List[Optional[int]]],
         probabilities: List[List[float]],
-        flattened_documents: List[str],
-        documents: List[List[Document]],
+        flattened_documents: List[Document],
+        queries: List[str],
         top_k: int,
         query_ids: List[int],
         document_ids: List[int],
-    ) -> List[List[Answer]]:
+    ) -> List[List[ExtractedAnswer]]:
+        print((len(start), len(document_ids)))
         answers = [
-            (flattened_documents[document_id][start:end], probability)
+            (doc := flattened_documents[document_id], doc.content[start:end], probability)
+            if start is not None and end is not None
+            else (flattened_documents[document_id], None, probability)
             for document_id, start_candidates_, end_candidates_, probabilities_ in zip(
                 document_ids, start, end, probabilities
             )
@@ -191,14 +196,18 @@ class ExtractiveReader:
         for query_id in range(query_ids[-1] + 1):
             current_answers = []
             while i < len(answers) and query_ids[i // top_k] == query_id:
-                current_answers.append(answers[i])
+                doc, data, probability = answers[i]
+                answer = ExtractedAnswer(
+                    data=data, question=queries[query_id], metadata={}, document=doc, probability=probability
+                )
+                current_answers.append(answer)
                 i += 1
-            current_answers = sorted(current_answers, key=lambda answer: answer[1], reverse=True)[:top_k]
+            current_answers = sorted(current_answers, key=lambda answer: answer.probability, reverse=True)[:top_k]
             nested_answers.append(current_answers)
 
         return nested_answers
 
-    @component.output_types(answers=List[List[Answer]])
+    @component.output_types(answers=List[List[ExtractedAnswer]])
     def run(
         self,
         queries: List[str],
@@ -216,12 +225,12 @@ class ExtractiveReader:
             flattened_queries, flattened_documents, max_seq_length, query_ids, stride
         )
 
-        output = self.model_(input_ids=input_ids, attention_mask=attention_mask)
+        output = self.model_(input_ids=input_ids, attention_mask=attention_mask)  # type: ignore # we know that self._model can't be None
 
         start, end, probabilities = self._postprocess(output, sequence_ids, attention_mask, top_k, encodings, query_ids)
 
         answers = self._unflatten(
-            start, end, probabilities, flattened_documents, documents, top_k, query_ids, document_ids
+            start, end, probabilities, flattened_documents, queries, top_k, query_ids, document_ids
         )
 
         return {"answers": answers}
