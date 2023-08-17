0ba2f52a2d12728a0911ee8888bd4d2ebf0a2864
anakin87 <stefanofiorucci@gmail.com>
2023-08-17 18:13:05 +0200

    incorporate feedback

--- haystack/preview/embedding_backends/sentence_transformers_backend.py
+++ haystack/preview/embedding_backends/sentence_transformers_backend.py
@@ -2,23 +2,24 @@ from typing import List, Optional, Union, Dict
 import hashlib
 import numpy as np
 
-from haystack.lazy_imports import LazyImport
+from haystack.preview.lazy_imports import LazyImport
 
 with LazyImport(message="Run 'pip install farm-haystack[inference]'") as sentence_transformers_import:
     from sentence_transformers import SentenceTransformer
 
 
-class SentenceTransformersEmbeddingBackend:
+class _SentenceTransformersEmbeddingBackend:
     """
     Singleton class to manage SentenceTransformers embeddings.
     """
 
-    _instances: Dict[str, "SentenceTransformersEmbeddingBackend"] = {}
+    _instances: Dict[str, "_SentenceTransformersEmbeddingBackend"] = {}
 
-    def __new__(cls, *args, **kwargs):
-        args_kwargs_str = str(args) + str(kwargs)
-        instance_id = hashlib.md5(args_kwargs_str.encode())
-
+    def __new__(
+        cls, model_name_or_path: str, device: Optional[str] = None, use_auth_token: Union[bool, str, None] = None
+    ):
+        args_str = f"{model_name_or_path}{device}{use_auth_token}"
+        instance_id = hashlib.md5(args_str.encode()).hexdigest()
         if instance_id in cls._instances:
             return cls._instances[instance_id]
 
@@ -35,6 +36,6 @@ class SentenceTransformersEmbeddingBackend:
                 model_name_or_path=model_name_or_path, device=device, use_auth_token=use_auth_token
             )
 
-    def embed(self, data: List[str], *inference_params) -> np.ndarray:
-        embedding = self.model.encode(data, *inference_params)
+    def embed(self, data: List[str], **kwargs) -> np.ndarray:
+        embedding = self.model.encode(data, **kwargs)
         return embedding
