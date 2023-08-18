2b396c82f6bba33e1dd30f4fb39a3fd013b3f804
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-08-18 17:03:17 +0200

    Rework `DocumentWriter` (#5583)
    * Remove DocumentStoreAwareMixin from DocumentWriter
    
    * Add release notes

--- test/preview/components/writers/document_writer.py
+++ test/preview/components/writers/document_writer.py
@@ -11,26 +11,12 @@ from test.preview.components.base import BaseTestComponent
 class TestDocumentWriter(BaseTestComponent):
     @pytest.mark.unit
     def test_run(self):
-        writer = DocumentWriter()
+        mocked_document_store = MagicMock()
+        writer = DocumentWriter(mocked_document_store)
         documents = [
             Document(content="This is the text of a document."),
             Document(content="This is the text of another document."),
         ]
 
-        mocked_document_store = MagicMock()
-        mocked_document_store.__haystack_document_store__ = True
-        writer.document_store = mocked_document_store
         writer.run(documents=documents)
-
         mocked_document_store.write_documents.assert_called_once_with(documents=documents, policy=DuplicatePolicy.FAIL)
-
-    @pytest.mark.unit
-    def test_run_without_store(self):
-        writer = DocumentWriter()
-        documents = [Document(content="test")]
-        with pytest.raises(
-            ValueError,
-            match="DocumentWriter needs a DocumentStore to run: set the DocumentStore instance to the "
-            "self.document_store attribute",
-        ):
-            writer.run(documents=documents)
