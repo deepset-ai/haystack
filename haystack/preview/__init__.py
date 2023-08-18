7e633c6b0cf60c3a06094a7a0a31697eeee5950c
Massimiliano Pippi <mpippi@gmail.com>
2023-08-18 12:53:25 +0200

    chore: change import paths under `preview` (#5592)
    * fix import paths
    
    * add release notes

--- haystack/preview/__init__.py
+++ haystack/preview/__init__.py
@@ -1,4 +1,3 @@
 from canals.component import component
-from haystack.preview.document_stores.decorator import document_store
-from haystack.preview.dataclasses import Document, ContentType, ExtractedAnswer, GeneratedAnswer, Answer
+from haystack.preview.dataclasses import *
 from haystack.preview.pipeline import Pipeline, PipelineError, NoSuchDocumentStoreError, load_pipelines, save_pipelines
--- haystack/preview/components/__init__.py
+++
@@ -1,5 +0,0 @@
-from haystack.preview.components.audio.whisper_local import LocalWhisperTranscriber
-from haystack.preview.components.audio.whisper_remote import RemoteWhisperTranscriber
-from haystack.preview.components.file_converters import TextFileToDocument
-from haystack.preview.components.classifiers import FileExtensionClassifier
-from haystack.preview.components.writers.document_writer import DocumentWriter
