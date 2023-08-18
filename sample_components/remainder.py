4320f31ca9cc9983ba90fd7433931ddb6dce6ce0
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-08-18 15:39:41 +0200

    Add Remainder to_dict and from_dict (#91)

--- sample_components/remainder.py
+++ sample_components/remainder.py
@@ -1,18 +1,26 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+from typing import Dict, Any
 
 from canals import component
+from canals.serialization import default_to_dict, default_from_dict
 
 
 @component
-class Remainder:  # pylint: disable=too-few-public-methods
+class Remainder:
     def __init__(self, divisor=3):
         if divisor == 0:
             raise ValueError("Can't divide by zero")
         self.divisor = divisor
-        self.init_parameters = {"divisor": divisor}
         component.set_output_types(self, **{f"remainder_is_{val}": int for val in range(divisor)})
+
+    def to_dict(self) -> Dict[str, Any]:  # pylint: disable=missing-function-docstring
+        return default_to_dict(self, divisor=self.divisor)
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "Remainder":  # pylint: disable=missing-function-docstring
+        return default_from_dict(cls, data)
 
     def run(self, value: int):
         """
--- test/sample_components/test_remainder.py
+++ test/sample_components/test_remainder.py
@@ -8,23 +8,35 @@ from sample_components import Remainder
 
 
 class TestRemainder(BaseTestComponent):
-    def test_saveload_default(self, tmp_path):
-        self.assert_can_be_saved_and_loaded_in_pipeline(Remainder(), tmp_path)
+    def test_to_dict(self):
+        component = Remainder()
+        res = component.to_dict()
+        assert res == {"hash": id(component), "type": "Remainder", "init_parameters": {"divisor": 3}}
 
-    def test_saveload_divisor(self, tmp_path):
-        self.assert_can_be_saved_and_loaded_in_pipeline(Remainder(divisor=10), tmp_path)
+    def test_to_dict_with_custom_divisor_value(self):
+        component = Remainder(divisor=100)
+        res = component.to_dict()
+        assert res == {"hash": id(component), "type": "Remainder", "init_parameters": {"divisor": 100}}
+
+    def test_from_dict(self):
+        data = {"hash": 1234, "type": "Remainder"}
+        component = Remainder.from_dict(data)
+        assert component.divisor == 3
+
+    def test_from_dict_with_custom_divisor_value(self):
+        data = {"hash": 1234, "type": "Remainder", "init_parameters": {"divisor": 100}}
+        component = Remainder.from_dict(data)
+        assert component.divisor == 100
 
     def test_remainder_default(self):
         component = Remainder()
         results = component.run(value=4)
         assert results == {"remainder_is_0": None, "remainder_is_1": 4, "remainder_is_2": None}
-        assert component.init_parameters == {"divisor": 3}
 
     def test_remainder_with_divisor(self):
         component = Remainder(divisor=4)
         results = component.run(value=4)
         assert results == {"remainder_is_0": 4, "remainder_is_1": None, "remainder_is_2": None, "remainder_is_3": None}
-        assert component.init_parameters == {"divisor": 4}
 
     def test_remainder_zero(self):
         with pytest.raises(ValueError):
