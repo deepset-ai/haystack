d638b56f03470467dea33f086395245310c4ce98
ZanSara <sara.zanzottera@deepset.ai>
2023-08-29 14:48:22 +0200

    remove patches

--- test/preview/components/audio/test_whisper_remote.py
+++ test/preview/components/audio/test_whisper_remote.py
@@ -40,14 +40,10 @@ class TestRemoteWhisperTranscriber:
         }
 
     @pytest.mark.unit
-    def test_to_dict_with_custom_init_parameters(self, monkeypatch):
-        monkeypatch.setattr(
-            "haystack.preview.components.audio.whisper_remote.WhisperRemoteModel", Literal["whisper-1", "whisper-2"]
-        )
-
+    def test_to_dict_with_custom_init_parameters(self):
         transcriber = RemoteWhisperTranscriber(
             api_key="test",
-            model_name="whisper-2",
+            model_name="whisper-1",
             api_base="https://my.api.base/something_else/v3",
             whisper_params={"return_segments": True, "temperature": [0.1, 0.6, 0.8]},
         )
@@ -55,7 +51,7 @@ class TestRemoteWhisperTranscriber:
         assert data == {
             "type": "RemoteWhisperTranscriber",
             "init_parameters": {
-                "model_name": "whisper-2",
+                "model_name": "whisper-1",
                 "api_key": "test",
                 "api_base": "https://my.api.base/something_else/v3",
                 "whisper_params": {"return_segments": True, "temperature": [0.1, 0.6, 0.8]},
@@ -63,21 +59,18 @@ class TestRemoteWhisperTranscriber:
         }
 
     @pytest.mark.unit
-    def test_from_dict(self, monkeypatch):
-        monkeypatch.setattr(
-            "haystack.preview.components.audio.whisper_remote.WhisperRemoteModel", Literal["whisper-1", "whisper-2"]
-        )
+    def test_from_dict(self):
         data = {
             "type": "RemoteWhisperTranscriber",
             "init_parameters": {
-                "model_name": "whisper-2",
+                "model_name": "whisper-1",
                 "api_key": "test",
                 "api_base": "https://my.api.base/something_else/v3",
                 "whisper_params": {"return_segments": True, "temperature": [0.1, 0.6, 0.8]},
             },
         }
         transcriber = RemoteWhisperTranscriber.from_dict(data)
-        assert transcriber.model_name == "whisper-2"
+        assert transcriber.model_name == "whisper-1"
         assert transcriber.api_key == "test"
         assert transcriber.api_base == "https://my.api.base/something_else/v3"
         assert transcriber.whisper_params == {"return_segments": True, "temperature": [0.1, 0.6, 0.8]}
