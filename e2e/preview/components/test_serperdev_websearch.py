f817133a6318c0e194d3aa7757c9e7fe4fd9fb29
ZanSara <sara.zanzottera@deepset.ai>
2023-09-06 11:41:19 +0200

    fix e2e

--- e2e/preview/components/test_serperdev_websearch.py
+++ e2e/preview/components/test_serperdev_websearch.py
@@ -10,6 +10,6 @@ from haystack.preview.components.websearch.serper_dev import SerperDevWebSearch
 )
 def test_web_search_top_k():
     ws = SerperDevWebSearch(api_key=os.environ.get("SERPERDEV_API_KEY", None), top_k=10)
-    results = ws.run(query="Who is the boyfriend of Olivia Wilde?")
+    results = ws.run(query="Who is the boyfriend of Olivia Wilde?")["documents"]
     assert len(results) == 10
     assert all(isinstance(doc, Document) for doc in results)
