42ce6228b99492fa0db0801b9f13e228a6148c92
anakin87 <stefanofiorucci@gmail.com>
2023-09-08 12:49:55 +0200

    e2e tests for embedders

---
+++ e2e/preview/components/test_sentence_transformers_document_embedder.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from haystack.preview.dataclasses import Document
+from haystack.preview.components.embedders.sentence_transformers_document_embedder import (
+    SentenceTransformersDocumentEmbedder,
+)
+
+
+def cosine_sim(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+def test_sentence_transformers_document_embedder():
+    docs = [
+        Document(content="Gyraffe is a long-necked animal", metadata={"topic": "animals"}),
+        Document(content="I love animals", metadata={"topic": "animals"}),
+        Document(content="Haystack is a great framework", metadata={"topic": "LLMs"}),
+    ]
+
+    # Use a very small model for testing
+    doc_embedder = SentenceTransformersDocumentEmbedder(
+        model_name_or_path="paraphrase-albert-small-v2", metadata_fields_to_embed=["topic"]
+    )
+    doc_embedder.warm_up()
+
+    output = doc_embedder.run(documents=docs)
+    out_docs = output["documents"]
+
+    assert len(out_docs) == 3
+    assert all(isinstance(doc.embedding, list) for doc in out_docs)
+    assert all(len(doc.embedding) == 768 for doc in out_docs)
+    assert all(isinstance(element, float) for doc in out_docs for element in doc.embedding)
+
+    assert cosine_sim(out_docs[0].embedding, out_docs[1].embedding) > cosine_sim(
+        out_docs[0].embedding, out_docs[2].embedding
+    )
---
+++ e2e/preview/components/test_sentence_transformers_text_embedder.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from haystack.preview.components.embedders.sentence_transformers_text_embedder import SentenceTransformersTextEmbedder
+
+
+def cosine_sim(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+def test_sentence_transformers_text_embedder():
+    texts = ["Gyraffe is a long-necked animal", "I love animals", "Haystack is a great framework"]
+
+    # Use a very small model for testing
+    txt_embedder = SentenceTransformersTextEmbedder(model_name_or_path="paraphrase-albert-small-v2")
+    txt_embedder.warm_up()
+
+    output = txt_embedder.run(texts=texts)
+    embeddings = output["embeddings"]
+
+    assert len(embeddings) == 3
+    assert all(isinstance(embedding, list) for embedding in embeddings)
+    assert all(len(embedding) == 768 for embedding in embeddings)
+    assert all(isinstance(element, float) for embedding in embeddings for element in embedding)
+
+    assert cosine_sim(embeddings[0], embeddings[1]) > cosine_sim(embeddings[0], embeddings[2])
