d6e1c1b8065a319d9455e55e7f9c79a522884278
anakin87 <stefanofiorucci@gmail.com>
2023-09-13 11:53:34 +0200

    fix test

--- test/preview/components/embedders/test_sentence_transformers_document_embedder.py
+++ test/preview/components/embedders/test_sentence_transformers_document_embedder.py
@@ -223,7 +223,7 @@ class TestSentenceTransformersDocumentEmbedder:
         embedder.embedding_backend = MagicMock()
 
         documents = [
-            Document(content=f"document number {i}", metadata={"meta_field": f"meta_value {i}"}) for i in range(5)
+            Document(text=f"document number {i}", metadata={"meta_field": f"meta_value {i}"}) for i in range(5)
         ]
 
         embedder.run(documents=documents)
