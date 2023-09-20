7b1767e447d07029c061434acb71885775265639
ZanSara <sara.zanzottera@deepset.ai>
2023-09-20 17:10:21 +0200

    add tests

--- haystack/preview/components/joiners/join.py
+++ haystack/preview/components/joiners/join.py
@@ -14,24 +14,30 @@ class Join:
         :param inputs_count: the number of inputs to expect.
         :param inputs_type: the type of the inputs. Every type that supports the + operator works.
         """
+        if inputs_count < 1:
+            raise ValueError("inputs_count must be at least 1")
         self.inputs_count = inputs_count
         self.inputs_type = inputs_type
-        component.set_input_types({f"input_{i}": inputs_type for i in range(inputs_count)})
-        component.set_output_types(output=inputs_type)
+        component.set_input_types(self, **{f"input_{i}": inputs_type for i in range(inputs_count)})
+        component.set_output_types(self, output=inputs_type)
 
     def to_dict(self):
         return default_to_dict(self, inputs_count=self.inputs_count, inputs_type=self.inputs_type)
 
     @classmethod
-    def from_dict(self, data):
-        return default_from_dict(self, data)
+    def from_dict(cls, data):
+        return default_from_dict(cls, data)
 
     def run(self, **kwargs):
         """
         Joins together a group of inputs of the same type. Works with every type that supports the + operator,
         like lists, strings, etc.
         """
-        output = []
-        for values in kwargs.values():
+        if len(kwargs) != self.inputs_count:
+            raise ValueError(f"Join expected {self.inputs_count} inputs, but got {len(kwargs)}")
+
+        values = list(kwargs.values())
+        output = values.pop()
+        for values in values:
             output += values
         return {"output": output}
--- test/preview/components/joiner/test_join.py
+++ test/preview/components/joiner/test_join.py
@@ -7,7 +7,29 @@ from haystack.preview.components.joiners.join import Join
 
 class TestJoin:
     @pytest.mark.unit
-    def test_join(self):
+    def test_join_to_dict(self):
+        comp = Join(inputs_count=2, inputs_type=str)
+        assert comp.to_dict() == {"type": "Join", "init_parameters": {"inputs_count": 2, "inputs_type": str}}
+
+    @pytest.mark.unit
+    def test_join_list(self):
         comp = Join(inputs_count=2, inputs_type=List[int])
-        output = comp.run(input_1=[1, 2], input_2=[3, 4])
+        output = comp.run(input_0=[1, 2], input_1=[3, 4])
         assert output == {"output": [1, 2, 3, 4]}
+
+    @pytest.mark.unit
+    def test_join_str(self):
+        comp = Join(inputs_count=2, inputs_type=str)
+        output = comp.run(input_0="hello", input_1="test")
+        assert output == {"output": "hellotest"}
+
+    @pytest.mark.unit
+    def test_join_one_input(self):
+        comp = Join(inputs_count=1, inputs_type=str)
+        output = comp.run(input_0="hello")
+        assert output == {"output": "hello"}
+
+    @pytest.mark.unit
+    def test_join_zero_input(self):
+        with pytest.raises(ValueError):
+            Join(inputs_count=0, inputs_type=str)
