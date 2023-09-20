6400319745b0e51937ea102da616eae71f3ceb27
bogdankostic <bogdankostic@web.de>
2023-09-20 17:06:19 +0200

    Add AzureOCRDocumentConverter

--- haystack/preview/components/file_converters/__init__.py
+++ haystack/preview/components/file_converters/__init__.py
@@ -1,3 +1,4 @@
 from haystack.preview.components.file_converters.txt import TextFileToDocument
+from haystack.preview.components.file_converters.azure import AzureOCRDocumentConverter
 
-__all__ = ["TextFileToDocument"]
+__all__ = ["TextFileToDocument", "AzureOCRDocumentConverter"]
