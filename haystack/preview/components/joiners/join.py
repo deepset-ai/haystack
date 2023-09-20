1a25d2f98898e630125dd9e45fbf98b6a645b19d
ZanSara <sara.zanzottera@deepset.ai>
2023-09-20 16:48:15 +0200

    typo

--- haystack/preview/components/joiners/join.py
+++ haystack/preview/components/joiners/join.py
@@ -9,7 +9,7 @@ class Join:
     the + operator for joining, like lists, strings, etc.
     """
 
-    def __init__(inputs_count: int, inputs_type: Type):
+    def __init__(self, inputs_count: int, inputs_type: Type):
         """
         :param inputs_count: the number of inputs to expect.
         :param inputs_type: the type of the inputs. Every type that supports the + operator works.
