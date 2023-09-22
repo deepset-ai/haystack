a5a0dc9f878ee9d5cbc4e2317fdfa8449f31e7bd
Massimiliano Pippi <mpippi@gmail.com>
2023-09-22 11:09:59 +0200

    feat: optionally pass an id to the Document constructor (#5862)
    * revert #5826
    
    * do not use Optional

--- test/preview/testing/test_factory.py
+++ test/preview/testing/test_factory.py
@@ -32,7 +32,7 @@ def test_document_store_class_is_registered():
 
 @pytest.mark.unit
 def test_document_store_class_with_documents():
-    doc = Document(text="This is a document")
+    doc = Document(id="fake_id", text="This is a document")
     MyStore = document_store_class("MyStore", documents=[doc])
     store = MyStore()
     assert store.count_documents() == 1
@@ -49,7 +49,7 @@ def test_document_store_class_with_documents_count():
 
 @pytest.mark.unit
 def test_document_store_class_with_documents_and_documents_count():
-    doc = Document(text="This is a document")
+    doc = Document(id="fake_id", text="This is a document")
     MyStore = document_store_class("MyStore", documents=[doc], documents_count=100)
     store = MyStore()
     assert store.count_documents() == 100
