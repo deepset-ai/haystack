28246c53a28f7449def977d33941f1ac5f802bdc
Daria Fokina <daria.fokina@deepset.ai>
2023-09-25 12:19:00 +0200

    docstrings update

--- haystack/preview/components/joiners/join.py
+++ haystack/preview/components/joiners/join.py
@@ -6,14 +6,14 @@ from haystack.preview.utils import marshal_type, unmarshal_type
 @component
 class Join:
     """
-    Simple component that joins together a group of inputs of the same type. Works with every type that supports
-    the + operator for joining, like lists, strings, etc.
+    A simple component that joins together a group of inputs of the same type. Works with every type that supports
+    the + operator for joining, such as lists, strings, etc.
     """
 
     def __init__(self, inputs_count: int, inputs_type: Type):
         """
-        :param inputs_count: the number of inputs to expect.
-        :param inputs_type: the type of the inputs. Every type that supports the + operator works.
+        :param inputs_count: The number of inputs to expect.
+        :param inputs_type: The type of the inputs. Every type that supports the + operator works.
         """
         if inputs_count < 1:
             raise ValueError("inputs_count must be at least 1")
@@ -35,7 +35,7 @@ class Join:
     def run(self, **kwargs):
         """
         Joins together a group of inputs of the same type. Works with every type that supports the + operator,
-        like lists, strings, etc.
+        such as lists, strings, etc.
         """
         if len(kwargs) != self.inputs_count:
             raise ValueError(f"Join expected {self.inputs_count} inputs, but got {len(kwargs)}")
