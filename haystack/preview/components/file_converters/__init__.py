8bd18a4a99d57f98fec33143411b9b0b792dac37
bogdankostic <bogdankostic@web.de>
2023-09-26 15:57:55 +0200

    feat: Add `AzureOCRDocumentConverter` (2.0) (#5855)
    * Add AzureOCRDocumentConverter
    
    * Add tests
    
    * Add release note
    
    * Formatting
    
    * update docstrings
    
    * Apply suggestions from code review
    
    Co-authored-by: ZanSara <sara.zanzottera@deepset.ai>
    
    * PR feedback
    
    * PR feedback
    
    * PR feedback
    
    * Add secrets as environment variables
    
    * Adapt test
    
    * Add azure dependency to CI
    
    * Add azure dependency to CI
    
    ---------
    
    Co-authored-by: ZanSara <sara.zanzottera@deepset.ai>
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>

--- haystack/preview/components/file_converters/__init__.py
+++ haystack/preview/components/file_converters/__init__.py
@@ -1,4 +1,5 @@
 from haystack.preview.components.file_converters.txt import TextFileToDocument
 from haystack.preview.components.file_converters.tika import TikaDocumentConverter
+from haystack.preview.components.file_converters.azure import AzureOCRDocumentConverter
 
-__all__ = ["TextFileToDocument", "TikaDocumentConverter"]
+__all__ = ["TextFileToDocument", "TikaDocumentConverter", "AzureOCRDocumentConverter"]
