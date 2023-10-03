e48b15a9da7802fb500ba076a97cea5968d8c544
ZanSara <sara.zanzottera@deepset.ai>
2023-10-03 12:29:09 +0200

    black

--- haystack/preview/telemetry/__init__.py
+++ haystack/preview/telemetry/__init__.py
@@ -1,5 +1,1 @@
-from haystack.preview.telemetry.telemetry import (
-    send_pipeline_run_event,
-    tutorial_running,
-    HAYSTACK_TELEMETRY_ENABLED,
-)
+from haystack.preview.telemetry.telemetry import send_pipeline_run_event, tutorial_running, HAYSTACK_TELEMETRY_ENABLED
