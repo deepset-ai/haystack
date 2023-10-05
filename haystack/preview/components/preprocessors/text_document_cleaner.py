813ee6b68ba27e22a8a707745ec375696b0465a1
Julian Risch <julian.risch@deepset.ai>
2023-10-05 17:19:07 +0200

    remove repeated substrings

--- haystack/preview/components/preprocessors/text_document_cleaner.py
+++ haystack/preview/components/preprocessors/text_document_cleaner.py
@@ -1,6 +1,8 @@
 import re
 from copy import deepcopy
-from typing import Any, Dict, List, Optional
+from functools import partial, reduce
+from itertools import chain
+from typing import Any, Dict, List, Optional, Generator, Set
 
 from haystack.preview import Document, component, default_from_dict, default_to_dict
 
@@ -115,4 +117,89 @@ class TextDocumentCleaner:
         return text
 
     def _remove_repeated_substrings(self, text: str) -> str:
+        return self._find_and_remove_header_footer(
+            text, n_chars=300, n_first_pages_to_ignore=1, n_last_pages_to_ignore=1
+        )
+
+    def _find_and_remove_header_footer(
+        self, text: str, n_chars: int, n_first_pages_to_ignore: int, n_last_pages_to_ignore: int
+    ) -> str:
+        """
+        Heuristic to find footers and headers across different pages by searching for the longest common string.
+        For headers, we only search in the first n_chars characters (for footer: last n_chars).
+        Note: This heuristic uses exact matches and therefore works well for footers like "Copyright 2019 by XXX",
+         but won't detect "Page 3 of 4" or similar.
+
+        :param n_chars: number of first/last characters where the header/footer shall be searched in
+        :param n_first_pages_to_ignore: number of first pages to ignore (e.g. TOCs often don't contain footer/header)
+        :param n_last_pages_to_ignore: number of last pages to ignore
+        :return: (cleaned pages, found_header_str, found_footer_str)
+        """
+
+        pages = text.split("\f")
+
+        # header
+        start_of_pages = [p[:n_chars] for p in pages[n_first_pages_to_ignore:-n_last_pages_to_ignore]]
+        found_header = self._find_longest_common_ngram(start_of_pages)
+        if found_header:
+            pages = [page.replace(found_header, "") for page in pages]
+
+        # footer
+        end_of_pages = [p[-n_chars:] for p in pages[n_first_pages_to_ignore:-n_last_pages_to_ignore]]
+        found_footer = self._find_longest_common_ngram(end_of_pages)
+        if found_footer:
+            pages = [page.replace(found_footer, "") for page in pages]
+        # logger.debug("Removed header '%s' and footer '%s' in document", found_header, found_footer)
+        text = "\f".join(pages)
         return text
+
+    def _ngram(self, seq: str, n: int) -> Generator[str, None, None]:
+        """
+        Return ngram (of tokens - currently split by whitespace)
+        :param seq: str, string from which the ngram shall be created
+        :param n: int, n of ngram
+        :return: str, ngram as string
+        """
+
+        # In order to maintain the original whitespace, but still consider \n and \t for n-gram tokenization,
+        # we add a space here and remove it after creation of the ngrams again (see below)
+        seq = seq.replace("\n", " \n")
+        seq = seq.replace("\t", " \t")
+
+        words = seq.split(" ")
+        ngrams = (
+            " ".join(words[i : i + n]).replace(" \n", "\n").replace(" \t", "\t") for i in range(0, len(words) - n + 1)
+        )
+
+        return ngrams
+
+    def _allngram(self, seq: str, min_ngram: int, max_ngram: int) -> Set[str]:
+        lengths = range(min_ngram, max_ngram) if max_ngram else range(min_ngram, len(seq))
+        ngrams = map(partial(self._ngram, seq), lengths)
+        res = set(chain.from_iterable(ngrams))
+        return res
+
+    def _find_longest_common_ngram(
+        self, sequences: List[str], max_ngram: int = 30, min_ngram: int = 3
+    ) -> Optional[str]:
+        """
+        Find the longest common ngram across different text sequences (e.g. start of pages).
+        Considering all ngrams between the specified range. Helpful for finding footers, headers etc.
+
+        :param sequences: list[str], list of strings that shall be searched for common n_grams
+        :param max_ngram: int, maximum length of ngram to consider
+        :param min_ngram: minimum length of ngram to consider
+        :return: str, common string of all sections
+        """
+        sequences = [s for s in sequences if s]  # filter empty sequences
+        if not sequences:
+            return None
+        seqs_ngrams = map(partial(self._allngram, min_ngram=min_ngram, max_ngram=max_ngram), sequences)
+        intersection = reduce(set.intersection, seqs_ngrams)
+
+        try:
+            longest = max(intersection, key=len)
+        except ValueError:
+            # no common sequence found
+            longest = ""
+        return longest if longest.strip() else None
--- test/preview/components/preprocessors/test_text_document_cleaner.py
+++ test/preview/components/preprocessors/test_text_document_cleaner.py
@@ -14,7 +14,7 @@ class TestTextDocumentCleaner:
             cleaner.run(documents=[Document()])
 
     @pytest.mark.unit
-    def test_single_doc(self):
+    def test_single_document(self):
         with pytest.raises(TypeError, match="TextDocumentCleaner expects a List of Documents as input."):
             cleaner = TextDocumentCleaner()
             cleaner.run(documents=Document())
@@ -26,7 +26,7 @@ class TestTextDocumentCleaner:
         assert result == {"documents": []}
 
     @pytest.mark.unit
-    def test_clean_empty_lines(self):
+    def test_remove_empty_lines(self):
         cleaner = TextDocumentCleaner(remove_extra_whitespaces=False)
         result = cleaner.run(
             documents=[
@@ -46,8 +46,8 @@ class TestTextDocumentCleaner:
         )
 
     @pytest.mark.unit
-    def test_clean_whitespaces(self):
-        cleaner = TextDocumentCleaner(clean_empty_lines=False)
+    def test_remove_whitespaces(self):
+        cleaner = TextDocumentCleaner(remove_empty_lines=False)
         result = cleaner.run(
             documents=[
                 Document(
@@ -69,11 +69,47 @@ class TestTextDocumentCleaner:
         cleaner = TextDocumentCleaner(remove_substrings=["This", "A", "words"])
         result = cleaner.run(documents=[Document(text="This is a text with some words.")])
         assert len(result["documents"]) == 1
-        assert result["documents"][0].text == (" is a text with some .")
+        assert result["documents"][0].text == " is a text with some ."
 
     @pytest.mark.unit
     def test_remove_regex(self):
         cleaner = TextDocumentCleaner(remove_regex=r"\s\s+")
         result = cleaner.run(documents=[Document(text="This is a  text with   some words.")])
         assert len(result["documents"]) == 1
-        assert result["documents"][0].text == ("This is a text with some words.")
+        assert result["documents"][0].text == "This is a text with some words."
+
+    @pytest.mark.unit
+    def test_remove_repeated_substrings(self):
+        cleaner = TextDocumentCleaner(
+            remove_empty_lines=False, remove_extra_whitespaces=False, remove_repeated_substrings=True
+        )
+
+        text = """First PageThis is a header.
+        Page  of
+        2
+        4
+        Lorem ipsum dolor sit amet
+        This is a footer number 1
+        This is footer number 2This is a header.
+        Page  of
+        3
+        4
+        Sid ut perspiciatis unde
+        This is a footer number 1
+        This is footer number 2This is a header.
+        Page  of
+        4
+        4
+        Sed do eiusmod tempor.
+        This is a footer number 1
+        This is footer number 2"""
+
+        expected_text = """First Page 2
+        4
+        Lorem ipsum dolor sit amet 3
+        4
+        Sid ut perspiciatis unde 4
+        4
+        Sed do eiusmod tempor."""
+        result = cleaner.run(documents=[Document(text=text)])
+        assert result["documents"][0].text == expected_text
