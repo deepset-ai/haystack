651cae341220cdd54e206425a12d5c5b8e77ec84
anakin87 <stefanofiorucci@gmail.com>
2023-10-06 17:22:30 +0200

    still a raw draft

--- haystack/preview/components/generators/hugging_face/hugging_face_local.py
+++ haystack/preview/components/generators/hugging_face/hugging_face_local.py
@@ -22,18 +22,17 @@ with LazyImport(message="Run 'pip install torch transformers'") as torch_and_tra
         AutoTokenizer,
         AutoConfig,
         TOKENIZER_MAPPING,
-    from huggingface_hub import model_info
     )
+    from huggingface_hub import model_info
 
 
 logger = logging.getLogger(__name__)
 
+
 @component
 class HuggingFaceLocalGenerator:
-
     def __init__(
         self,
-        api_key: str,
         model_name_or_path: str = "google/flan-t5-base",
         use_auth_token: Optional[Union[str, bool]] = None,
         device: str = "cpu",
@@ -56,24 +55,27 @@ class HuggingFaceLocalGenerator:
         torch_and_transformers_import.check()
 
         self.model_name_or_path = model_name_or_path
+        self.pipeline_kwargs = pipeline_kwargs or {}
+        self.pipeline_kwargs["model"] = model_name_or_path
+
         self.use_auth_token = use_auth_token
         self.device = device
         self.model_kwargs = model_kwargs or {}
-        self.pipeline_kwargs = pipeline_kwargs or {}
+
         self.generation_kwargs = generation_kwargs or {}
 
         # task identification and validation
-        if task is None and "task" in self.pipeline_kwargs:
-            if 'task' in self.pipeline_kwargs:
-                task = self.pipeline_kwargs['task']
+        if task is None:
+            if "task" in self.pipeline_kwargs:
+                task = self.pipeline_kwargs["task"]
             else:
                 task = model_info(model_name_or_path, token=use_auth_token).pipeline_tag
-            
+
         if task not in ["text-generation", "text2text-generation"]:
-                raise ValueError(
-                    f"Task name {task} is not supported. "
-                    f"We only support text2text-generation and text-generation tasks."
-                )
+            raise ValueError(
+                f"Task name {task} is not supported. "
+                f"We only support text2text-generation and text-generation tasks."
+            )
         self.task = task
         self.pipeline_kwargs["task"] = task
 
@@ -82,6 +84,18 @@ class HuggingFaceLocalGenerator:
         self.pipeline = None
 
     def warm_up(self):
-        self.pipeline = pipeline(**self.pipeline_kwargs, model_kwargs=self.model_kwargs)
+        # TODO: how to merge them?
+        print(f"{self.pipeline_kwargs=}")
+        print(f"{self.model_kwargs=}")
 
-    
+        if self.pipeline is None:
+            self.pipeline = pipeline(**self.pipeline_kwargs, model_kwargs=self.model_kwargs)
+
+    @component.output_types(replies=List[str], metadata=List[Dict[str, Any]])
+    def run(self, prompt: str):
+        if self.pipeline is None:
+            raise RuntimeError("The generation model has not been loaded. Please call warm_up() before running.")
+
+        output = self.pipeline(prompt, **self.generation_kwargs)
+        generated_texts = [o["generated_text"] for o in output if "generated_text" in o]
+        return {"replies": generated_texts}
