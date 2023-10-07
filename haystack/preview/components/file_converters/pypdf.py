e3be1c95ad8520aa01aad9500ea3dac33370305a
Vladimir Blagojevic <dovlex@gmail.com>
2023-10-07 12:22:27 +0200

    PyPDFToDocument handles ByteStream input, update pydocs

--- haystack/preview/components/file_converters/pypdf.py
+++ haystack/preview/components/file_converters/pypdf.py
@@ -1,7 +1,9 @@
+import io
 import logging
 from typing import List, Optional, Dict, Any, Union
 from pathlib import Path
 
+from haystack.preview.dataclasses import ByteStream
 from haystack.preview.lazy_imports import LazyImport
 from haystack.preview import Document, component, default_to_dict, default_from_dict
 
@@ -15,12 +17,12 @@ logger = logging.getLogger(__name__)
 @component
 class PyPDFToDocument:
     """
-    A component for converting a PDF file to a Document.
+    Converts a PDF file to a Document.
     """
 
     def __init__(self, id_hash_keys: Optional[List[str]] = None):
         """
-        Create a PyPDFToDocument component.
+        Initializes the PyPDFToDocument component.
 
         :param id_hash_keys: Generate the Document ID from a custom list of strings that refer to the Document's
             attributes. Default: `None`
@@ -31,6 +33,7 @@ class PyPDFToDocument:
     def to_dict(self) -> Dict[str, Any]:
         """
         Serialize this component to a dictionary.
+        :return: The dictionary containing the component's data.
         """
         return default_to_dict(self, id_hash_keys=self.id_hash_keys)
 
@@ -38,13 +41,15 @@ class PyPDFToDocument:
     def from_dict(cls, data: Dict[str, Any]) -> "PyPDFToDocument":
         """
         Deserialize this component from a dictionary.
+        :param data: The dictionary containing the component's data.
+        :return: The component instance.
         """
         return default_from_dict(cls, data)
 
     @component.output_types(documents=List[Document])
-    def run(self, paths: List[Union[str, Path]], id_hash_keys: Optional[List[str]] = None):
+    def run(self, paths: List[Union[str, Path, ByteStream]], id_hash_keys: Optional[List[str]] = None):
         """
-        Convert PDF files to Documents.
+        Converts PDF files to Documents.
 
         :param paths: A list of paths to PDF files.
         :param id_hash_keys: Generate the Document ID from a custom list of strings that refer to the Document's
@@ -56,7 +61,7 @@ class PyPDFToDocument:
             try:
                 text = self._read_pdf_file(path)
             except Exception as e:
-                logger.warning("Could not read file %s. Skipping it. Error message: %s", path, e)
+                logger.warning("Could not read %s. Skipping it. Error message: %s", path, e)
                 continue
 
             document = Document(text=text, id_hash_keys=id_hash_keys)
@@ -64,14 +69,19 @@ class PyPDFToDocument:
 
         return {"documents": documents}
 
-    def _read_pdf_file(self, path: Union[str, Path]) -> str:
+    def _read_pdf_file(self, path: Union[str, Path, ByteStream]) -> str:
         """
-        Read a PDF file and return its text content.
+        Extracts content from the given PDF path or ByteStream.
+        :param path: Path to a PDF file or a ByteStream containing a PDF file.
+        :return: The extracted text.
         """
-        pdf_reader = PdfReader(str(path))
-        text = ""
-        for page in pdf_reader.pages:
-            extracted_text = page.extract_text()
-            if extracted_text:
-                text += extracted_text
+        if isinstance(path, (str, Path)):
+            pdf_reader = PdfReader(str(path))
+        elif isinstance(path, ByteStream):
+            pdf_reader = PdfReader(io.BytesIO(path.data))
+        else:
+            raise ValueError(f"Unsupported path type: {type(path)}")
+
+        text = "".join(extracted_text for page in pdf_reader.pages if (extracted_text := page.extract_text()))
+
         return text
