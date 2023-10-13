a43c87594254793184ff8ba9787208c8176902d5
Daria Fokina <daria.fokina@deepset.ai>
2023-10-13 10:48:29 +0200

    wrong article

--- haystack/nodes/retriever/base.py
+++ haystack/nodes/retriever/base.py
@@ -253,7 +253,7 @@ class BaseRetriever(BaseComponent):
         :param query: Query string.
         :param filters: A dictionary where the keys specify a metadata field and the value is a list of accepted values for that field.
         :param top_k: How many documents to return per query.
-        :param documents: List of Documents of Retrieve.
+        :param documents: List of Documents to Retrieve.
         :param index: The name of the index in the DocumentStore from which to retrieve documents.
         :param headers: Custom HTTP headers to pass to document store client if supported (e.g. {'Authorization': 'Basic YWRtaW46cm9vdA=='} for basic authentication).
         :param scale_score: Whether to scale the similarity score to the unit interval (range of [0,1]).
