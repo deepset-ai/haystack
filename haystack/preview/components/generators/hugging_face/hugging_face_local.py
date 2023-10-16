a31a6f01c1eaa0b6cfb288e42a5fff06c6bfbbe3
ZanSara <sara.zanzottera@deepset.ai>
2023-10-16 11:33:43 +0200

    add telemetry details to HuggingFaceLocalGenerator

--- haystack/preview/components/generators/hugging_face/hugging_face_local.py
+++ haystack/preview/components/generators/hugging_face/hugging_face_local.py
@@ -110,6 +110,12 @@ class HuggingFaceLocalGenerator:
         self.generation_kwargs = generation_kwargs
         self.pipeline = None
 
+    def _get_telemetry_data(self) -> Dict[str, Any]:
+        """
+        Data that is sent to Posthog for usage analytics.
+        """
+        return {"model": self.pipeline_kwargs["model"]}
+
     def warm_up(self):
         if self.pipeline is None:
             self.pipeline = pipeline(**self.pipeline_kwargs)
