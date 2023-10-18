2d2da20179a13f324e67d3c3ab37877ddb3e7667
Julian Risch <julian.risch@deepset.ai>
2023-10-18 12:59:44 +0200

    implement merge and rrf

--- haystack/preview/components/routers/document_joiner.py
+++ haystack/preview/components/routers/document_joiner.py
@@ -1,8 +1,13 @@
 import itertools
+import logging
 from collections import defaultdict
-from typing import Any, Dict, List, Optional
+from math import inf
+from typing import List, Optional
 
-from haystack.preview import component, default_from_dict, default_to_dict, Document
+from haystack.preview import component, Document
+
+
+logger = logging.getLogger(__name__)
 
 
 @component
@@ -16,6 +21,17 @@ class DocumentJoiner:
     * merge: merge scores of documents from multiple nodes. Optionally, each input score can be given a different
              `weight` & a `top_k` limit can be set. This mode can also be used for "reranking" retrieved documents.
     * reciprocal_rank_fusion: combines the documents based on their rank in multiple nodes.
+
+    Example usage in a hybrid retrieval pipeline:
+    ```python
+    document_store = InMemoryDocumentStore()
+    p = Pipeline()
+    p.add_component(instance=InMemoryBM25Retriever(document_store=document_store), name="bm25_retriever")
+    p.add_component(instance=InMemoryEmbeddingRetriever(document_store=document_store), name="embedding_retriever")
+    p.add_component(instance=DocumentJoiner(), name="joiner")
+    p.connect("bm25_retriever", "joiner")
+    p.connect("embedding_retriever", "joiner")
+    ```
     """
 
     def __init__(
@@ -28,10 +44,13 @@ class DocumentJoiner:
         """
         Initialize the DocumentJoiner.
 
-        :param join_mode:
-        :param weights:
+        :param join_mode: `concatenate` to combine documents from multiple retrievers, `merge` to aggregate scores of
+                          individual documents, `reciprocal_rank_fusion` to apply rank based scoring.
+        :param weights: A node-wise list(length of list must be equal to the number of input nodes) of weights for
+                        adjusting document scores when using the `merge` join_mode. By default, equal weight is given
+                        to each retriever score. This param is not compatible with the `concatenate` join_mode.
         :param top_k: How many documents should be returned in the output at maximum. By default, all are returned.
-        :param sort_by_score:
+        :param sort_by_score: Whether the output list of documents should be sorted by document scores in descending order. By default the output is sorted. Documents without score are handled as if their score was -infinity.
         """
         if join_mode not in ["concatenate", "merge", "reciprocal_rank_fusion"]:
             raise ValueError(f"DocumentJoiner component does not support '{join_mode}' join_mode.")
@@ -43,9 +62,9 @@ class DocumentJoiner:
     @component.output_types(documents=List[Document])
     def run(self, *documents: List[Document]):
         """
-        Run the DocumentJoiner. This method joins the input lists of documents to into one output list based on the join_mode specified during initialization.
+        Run the DocumentJoiner. This method joins the input lists of documents into one output list based on the join_mode specified during initialization.
 
-        :param documents: A list of documents to route to different edges.
+        :param documents: An arbitrary number of lists of documents to join.
         """
         output_documents = []
         if self.join_mode == "concatenate":
@@ -57,49 +76,66 @@ class DocumentJoiner:
 
         if self.sort_by_score:
             output_documents = sorted(
-                output_documents, key=lambda doc: doc.score if doc.score is not None else -1, reverse=True
+                output_documents, key=lambda doc: doc.score if doc.score is not None else -inf, reverse=True
             )
+            if any(doc.score is None for doc in output_documents):
+                logger.info(
+                    "The `DocumentJoiner` component has received some documents with `score=None` - and was requested "
+                    "to sort the documents by score, so the `score=None` documents were sorted as if their "
+                    "score was `-infinity`."
+                )
+
         if self.top_k:
             output_documents = output_documents[: self.top_k]
         return {"documents": output_documents}
 
-    def _concatenate(self, *documents):
+    def _concatenate(self, document_lists):
         """
-        Concatenates multiple lists of documents into one list of documents.
+        Concatenate multiple lists of documents and return only the document with the highest score for duplicate documents.
         """
-        return list(itertools.chain.from_iterable(*documents))
+        output = []
+        docs_per_id = defaultdict(list)
+        for doc in itertools.chain.from_iterable(document_lists):
+            docs_per_id[doc.id].append(doc)
+        for docs in docs_per_id.values():
+            doc_with_best_score = max(docs, key=lambda doc: doc.score if doc.score else -inf)
+            output.append(doc_with_best_score)
+        return output
 
-    def _merge(self, *documents):
+    def _merge(self, document_lists):
         """
-        Concatenates multiple lists of documents into one list of documents.
+        Merge multiple lists of documents and calculate a weighted sum of the scores of duplicate documents.
         """
-        return list(itertools.chain.from_iterable(*documents))
+        scores_map = defaultdict(int)
+        documents_map = {}
+        weights = self.weights if self.weights else [1 / len(document_lists)] * len(document_lists)
 
-    def _calculate_rrf(self, results):
+        for documents, weight in zip(document_lists, weights):
+            for doc in documents:
+                scores_map[doc.id] += (doc.score if doc.score else 0) * weight
+                documents_map[doc.id] = doc
+
+        for doc in documents_map.values():
+            doc.score = scores_map[doc.id]
+
+        return documents_map.values()
+
+    def _reciprocal_rank_fusion(self, document_lists):
         """
-        Calculates the reciprocal rank fusion. The constant K is set to 61 (60 was suggested by the original paper,
+        Merge multiple lists of documents and assign scores based on reciprocal rank fusion.
+        The constant k is set to 61 (60 was suggested by the original paper,
         plus 1 as python lists are 0-based and the paper used 1-based ranking).
         """
-        K = 61
+        k = 61
 
         scores_map = defaultdict(int)
-        for result in results:
-            for rank, doc in enumerate(result):
-                scores_map[doc.id] += 1 / (K + rank)
+        documents_map = {}
+        for documents in document_lists:
+            for rank, doc in enumerate(documents):
+                scores_map[doc.id] += 1 / (k + rank)
+                documents_map[doc.id] = doc
 
-        return scores_map
+        for doc in documents_map.values():
+            doc.score = scores_map[doc.id]
 
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Serialize this component to a dictionary.
-        """
-        return default_to_dict(
-            self, join_mode=self.join_mode, weights=self.weights, top_k=self.top_k, sort_by_score=self.sort_by_score
-        )
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "DocumentJoiner":
-        """
-        Deserialize this component from a dictionary.
-        """
-        return default_from_dict(cls, data)
+        return documents_map.values()
--- test/preview/components/routers/test_document_joiner.py
+++ test/preview/components/routers/test_document_joiner.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from haystack.preview import Document
@@ -14,30 +16,8 @@ class TestDocumentJoiner:
         assert joiner.sort_by_score
 
     @pytest.mark.unit
-    def test_to_dict(self):
-        joiner = DocumentJoiner()
-        data = joiner.to_dict()
-        assert data == {
-            "type": "DocumentJoiner",
-            "init_parameters": {"join_mode": "concatenate", "weights": None, "top_k": None, "sort_by_score": True},
-        }
-
-    @pytest.mark.unit
-    def test_to_dict_with_custom_init_parameters(self):
+    def test_init_with_custom_parameters(self):
         joiner = DocumentJoiner(join_mode="merge", weights=[0.4, 0.6], top_k=5, sort_by_score=False)
-        data = joiner.to_dict()
-        assert data == {
-            "type": "DocumentJoiner",
-            "init_parameters": {"join_mode": "merge", "weights": [0.4, 0.6], "top_k": 5, "sort_by_score": False},
-        }
-
-    @pytest.mark.unit
-    def test_from_dict(self):
-        data = {
-            "type": "DocumentJoiner",
-            "init_parameters": {"join_mode": "merge", "weights": [0.4, 0.6], "top_k": 5, "sort_by_score": False},
-        }
-        joiner = DocumentJoiner.from_dict(data)
         assert joiner.join_mode == "merge"
         assert joiner.weights == [0.4, 0.6]
         assert joiner.top_k == 5
@@ -58,13 +38,9 @@ class TestDocumentJoiner:
     @pytest.mark.unit
     def test_list_with_one_empty_list(self):
         joiner = DocumentJoiner()
-        documents_1 = [
-            Document(metadata={"created_at": "2023-02-01"}),
-            Document(metadata={"created_at": "2023-05-01"}),
-            Document(metadata={"created_at": "2023-08-01"}),
-        ]
-        result = joiner.run([], documents_1)
-        assert result == {"documents": documents_1}
+        documents = [Document(text="a"), Document(text="b"), Document(text="c")]
+        result = joiner.run([], documents)
+        assert result == {"documents": documents}
 
     @pytest.mark.unit
     def test_unsupported_join_mode(self):
@@ -72,18 +48,87 @@ class TestDocumentJoiner:
             DocumentJoiner(join_mode="unsupported_mode")
 
     @pytest.mark.unit
-    def test_run(self):
-        joiner = DocumentJoiner()
-        documents_1 = [
-            Document(metadata={"created_at": "2023-02-01"}),
-            Document(metadata={"created_at": "2023-05-01"}),
-            Document(metadata={"created_at": "2023-08-01"}),
-        ]
+    def test_run_with_concatenate_join_mode_and_top_k(self):
+        joiner = DocumentJoiner(top_k=6)
+        documents_1 = [Document(text="a"), Document(text="b"), Document(text="c")]
         documents_2 = [
-            Document(metadata={"created_at": "2023-02-01"}),
-            Document(metadata={"created_at": "2023-05-01"}),
-            Document(metadata={"created_at": "2023-08-01"}),
+            Document(text="d"),
+            Document(text="e"),
+            Document(text="f", metadata={"key": "value"}),
+            Document(text="g"),
         ]
         output = joiner.run(documents_1, documents_2)
         assert len(output["documents"]) == 6
-        assert sorted(documents_1 + documents_2, key=lambda d: d.id) == sorted(output["documents"], key=lambda d: d.id)
+        assert sorted(documents_1 + documents_2[:-1], key=lambda d: d.id) == sorted(
+            output["documents"], key=lambda d: d.id
+        )
+
+    @pytest.mark.unit
+    def test_run_with_concatenate_join_mode_and_duplicate_documents(self):
+        joiner = DocumentJoiner()
+        documents_1 = [Document(text="a", score=0.3, metadata={"key": "1"}), Document(text="b"), Document(text="c")]
+        documents_2 = [
+            Document(text="a", score=0.2, metadata={"key": "2"}),
+            Document(text="a"),
+            Document(text="f", metadata={"key": "value"}),
+        ]
+        output = joiner.run(documents_1, documents_2)
+        assert len(output["documents"]) == 4
+        assert sorted(documents_1 + [documents_2[-1]], key=lambda d: d.id) == sorted(
+            output["documents"], key=lambda d: d.id
+        )
+
+    @pytest.mark.unit
+    def test_run_with_merge_join_mode(self):
+        joiner = DocumentJoiner(join_mode="merge", weights=[1.5, 0.5])
+        documents_1 = [Document(text="a", score=1.0), Document(text="b", score=2.0)]
+        documents_2 = [
+            Document(text="a", score=0.5),
+            Document(text="b", score=3.0),
+            Document(text="f", score=4.0, metadata={"key": "value"}),
+        ]
+        output = joiner.run(documents_1, documents_2)
+        assert len(output["documents"]) == 3
+        expected_documents = [
+            Document(text="a", score=1.25),
+            Document(text="b", score=2.25),
+            Document(text="f", score=4.0, metadata={"key": "value"}),
+        ]
+        assert sorted(expected_documents, key=lambda d: d.id) == sorted(output["documents"], key=lambda d: d.id)
+
+    @pytest.mark.unit
+    def test_run_with_reciprocal_rank_fusion_join_mode(self):
+        joiner = DocumentJoiner(join_mode="reciprocal_rank_fusion")
+        documents_1 = [Document(text="a"), Document(text="b"), Document(text="c")]
+        documents_2 = [
+            Document(text="b", score=1000.0),
+            Document(text="c"),
+            Document(text="a"),
+            Document(text="f", metadata={"key": "value"}),
+        ]
+        output = joiner.run(documents_1, documents_2)
+        assert len(output["documents"]) == 4
+        expected_documents = [
+            Document(text="b"),
+            Document(text="a"),
+            Document(text="c"),
+            Document(text="f", metadata={"key": "value"}),
+        ]
+        assert expected_documents == output["documents"]
+
+    @pytest.mark.unit
+    def test_sort_by_score_without_scores(self, caplog):
+        joiner = DocumentJoiner()
+        with caplog.at_level(logging.INFO):
+            documents = [Document(text="a"), Document(text="b", score=0.5)]
+            output = joiner.run(documents)
+            assert "documents were sorted as if their score was `-infinity`" in caplog.text
+            assert output["documents"] == documents[::-1]
+
+    @pytest.mark.unit
+    def test_output_documents_not_sorted_by_score(self):
+        joiner = DocumentJoiner(sort_by_score=False)
+        documents_1 = [Document(text="a", score=0.1)]
+        documents_2 = [Document(text="d", score=0.2)]
+        output = joiner.run(documents_1, documents_2)
+        assert output["documents"] == documents_1 + documents_2
