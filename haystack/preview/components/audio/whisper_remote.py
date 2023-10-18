cd913e5e744ad1c1cec0ffc3070aa3bdb5fb71f4
Julian Risch <julian.risch@deepset.ai>
2023-10-18 13:36:32 +0200

    whisper transcriber

--- haystack/preview/components/audio/whisper_remote.py
+++ haystack/preview/components/audio/whisper_remote.py
@@ -6,7 +6,7 @@ import logging
 from pathlib import Path
 
 from haystack.preview.utils import request_with_retry
-from haystack.preview import component, Document, default_to_dict, default_from_dict
+from haystack.preview import component, Document
 
 logger = logging.getLogger(__name__)
 
@@ -53,25 +53,6 @@ class RemoteWhisperTranscriber:
         self.api_key = api_key
         self.api_base = api_base
         self.whisper_params = whisper_params or {}
-
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Serialize this component to a dictionary.
-        """
-        return default_to_dict(
-            self,
-            model_name=self.model_name,
-            api_key=self.api_key,
-            api_base=self.api_base,
-            whisper_params=self.whisper_params,
-        )
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "RemoteWhisperTranscriber":
-        """
-        Deserialize this component from a dictionary.
-        """
-        return default_from_dict(cls, data)
 
     @component.output_types(documents=List[Document])
     def run(self, audio_files: List[Path], whisper_params: Optional[Dict[str, Any]] = None):
