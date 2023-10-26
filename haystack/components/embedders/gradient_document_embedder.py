86299e94d013e660987203edb242ce69dc723b03
Mateusz Haligowski <mhaligowski@gmail.com>
2023-10-26 14:05:29 -0700

    Gradient embeddings (#2)

---
+++ haystack/components/embedders/gradient_document_embedder.py
@@ -0,0 +1,96 @@
+import logging
+from typing import List, Optional, Dict, Any
+
+from haystack.preview import component, Document, default_to_dict
+from haystack.preview.lazy_imports import LazyImport
+
+with LazyImport(message="Run 'pip install gradientai'") as gradientai_import:
+    from gradientai import Gradient
+
+logger = logging.getLogger(__name__)
+
+
+@component
+class GradientDocumentEmbedder:
+    """
+    A component for computing Document embeddings using Gradient AI API..
+    The embedding of each Document is stored in the `embedding` field of the Document.
+    """
+
+    def __init__(
+        self,
+        *,
+        model_name: str = "bge-large",
+        access_token: Optional[str] = None,
+        workspace_id: Optional[str] = None,
+        host: Optional[str] = None,
+    ) -> None:
+        """
+        Create a GradientDocumentEmbedder component.
+
+        :param model_name: The name of the model to use.
+        :param access_token: The Gradient access token. If not provided it's read from the environment
+                             variable GRADIENT_ACCESS_TOKEN.
+        :param workspace_id: The Gradient workspace ID. If not provided it's read from the environment
+                             variable GRADIENT_WORKSPACE_ID.
+        :param host: The Gradient host. By default it uses https://api.gradient.ai/.
+        """
+        self._host = host
+        self._model_name = model_name
+
+        self._gradient = Gradient(access_token=access_token, host=host, workspace_id=workspace_id)
+
+    def _get_telemetry_data(self) -> Dict[str, Any]:
+        """
+        Data that is sent to Posthog for usage analytics.
+        """
+        return {"model": self._model_name}
+
+    def to_dict(self) -> dict:
+        """
+        Serialize the component to a Python dictionary.
+        """
+        return default_to_dict(self, workspace_id=self._gradient.workspace_id, model_name=self._model_name)
+
+    def warm_up(self) -> None:
+        """
+        Load the embedding model.
+        """
+        if not hasattr(self, "_embedding_model"):
+            self._embedding_model = self._gradient.get_embeddings_model(slug=self._model_name)
+
+    def _generate_embeddings(self, documents: List[Document], batch_size=100) -> List[List[float]]:
+        """
+        Batches the documents and generates the embeddings.
+        """
+        batches = [documents[i : i + batch_size] for i in range(0, len(documents), batch_size)]
+
+        embeddings = []
+        for batch in batches:
+            response = self._embedding_model.generate_embeddings(inputs=[{"input": doc.text} for doc in batch])
+            embeddings.extend([e.embedding for e in response.embeddings])
+
+        return embeddings
+
+    @component.output_types(documents=List[Document])
+    def run(self, documents: List[Document]):
+        """
+        Embed a list of Documents.
+        The embedding of each Document is stored in the `embedding` field of the Document.
+
+        :param documents: A list of Documents to embed.
+        """
+        if not isinstance(documents, list) or documents and not isinstance(documents[0], Document):
+            raise TypeError(
+                "GradientDocumentEmbedder expects a list of Documents as input."
+                "In case you want to embed a list of strings, please use the GradientTextEmbedder."
+            )
+
+        if not hasattr(self, "_embedding_model"):
+            raise RuntimeError("The embedding model has not been loaded. Please call warm_up() before running.")
+
+        embeddings = self._generate_embeddings(documents=documents)
+        for doc, embedding in zip(documents, embeddings):
+            doc.embedding = embedding
+
+        return {"documents": documents}
---
+++ test/components/embedders/test_gradient_document_embedder.py
@@ -0,0 +1,139 @@
+import pytest
+from haystack.preview.components.embedders.gradient_document_embedder import GradientDocumentEmbedder
+from unittest.mock import MagicMock, NonCallableMagicMock
+import numpy as np
+
+from haystack.preview import Document
+
+
+access_token = "access_token"
+workspace_id = "workspace_id"
+model = "bge-large"
+
+
+def has_gradient():
+    try:
+        import gradientai
+
+        return True
+    except ModuleNotFoundError:
+        return False
+
+
+@pytest.mark.skipif(not has_gradient(), reason="Gradient is not installed")
+class TestGradientDocumentEmbedder:
+    @pytest.mark.unit
+    def test_init_from_env(self, monkeypatch):
+        monkeypatch.setenv("GRADIENT_ACCESS_TOKEN", access_token)
+        monkeypatch.setenv("GRADIENT_WORKSPACE_ID", workspace_id)
+
+        embedder = GradientDocumentEmbedder()
+        assert embedder is not None
+        assert embedder._gradient.workspace_id == workspace_id
+        assert embedder._gradient._api_client.configuration.access_token == access_token
+
+    @pytest.mark.unit
+    def test_init_without_access_token(self, monkeypatch):
+        monkeypatch.delenv("GRADIENT_ACCESS_TOKEN", raising=True)
+
+        with pytest.raises(ValueError):
+            GradientDocumentEmbedder(workspace_id=workspace_id)
+
+    @pytest.mark.unit
+    def test_init_without_workspace(self, monkeypatch):
+        monkeypatch.delenv("GRADIENT_WORKSPACE_ID", raising=True)
+
+        with pytest.raises(ValueError):
+            GradientDocumentEmbedder(access_token=access_token)
+
+    @pytest.mark.unit
+    def test_init_from_params(self):
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        assert embedder is not None
+        assert embedder._gradient.workspace_id == workspace_id
+        assert embedder._gradient._api_client.configuration.access_token == access_token
+
+    @pytest.mark.unit
+    def test_init_from_params_precedence(self, monkeypatch):
+        monkeypatch.setenv("GRADIENT_ACCESS_TOKEN", "env_access_token")
+        monkeypatch.setenv("GRADIENT_WORKSPACE_ID", "env_workspace_id")
+
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        assert embedder is not None
+        assert embedder._gradient.workspace_id == workspace_id
+        assert embedder._gradient._api_client.configuration.access_token == access_token
+
+    @pytest.mark.unit
+    def test_to_dict(self):
+        component = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        data = component.to_dict()
+        assert data == {
+            "type": "GradientDocumentEmbedder",
+            "init_parameters": {"workspace_id": workspace_id, "model_name": "bge-large"},
+        }
+
+    @pytest.mark.unit
+    def test_warmup(self):
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        embedder._gradient.get_embeddings_model = MagicMock()
+        embedder.warm_up()
+        embedder._gradient.get_embeddings_model.assert_called_once_with(slug="bge-large")
+
+    @pytest.mark.unit
+    def test_warmup_doesnt_reload(self):
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        embedder._gradient.get_embeddings_model = MagicMock(default_return_value="fake model")
+        embedder.warm_up()
+        embedder.warm_up()
+        embedder._gradient.get_embeddings_model.assert_called_once_with(slug="bge-large")
+
+    @pytest.mark.unit
+    def test_run_fail_if_not_warmed_up(self):
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+
+        with pytest.raises(RuntimeError, match="warm_up()"):
+            embedder.run(documents=[Document(text=f"document number {i}") for i in range(5)])
+
+    @pytest.mark.unit
+    def test_run(self):
+        from gradientai.openapi.client.models.generate_embedding_success import GenerateEmbeddingSuccess
+
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        embedder._embedding_model = NonCallableMagicMock()
+        embedder._embedding_model.generate_embeddings.return_value = GenerateEmbeddingSuccess(
+            embeddings=[{"embedding": np.random.rand(1024).tolist(), "index": i} for i in range(5)]
+        )
+
+        documents = [Document(text=f"document number {i}") for i in range(5)]
+
+        result = embedder.run(documents=documents)
+
+        assert isinstance(result["documents"], list)
+        assert len(result["documents"]) == len(documents)
+        for doc in result["documents"]:
+            assert isinstance(doc, Document)
+            assert isinstance(doc.embedding, list)
+            assert isinstance(doc.embedding[0], float)
+
+    @pytest.mark.unit
+    def test_run_batch(self):
+        from gradientai.openapi.client.models.generate_embedding_success import GenerateEmbeddingSuccess
+
+        embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
+        embedder._embedding_model = NonCallableMagicMock()
+
+        embedder._embedding_model.generate_embeddings.return_value = GenerateEmbeddingSuccess(
+            embeddings=[{"embedding": np.random.rand(1024).tolist(), "index": i} for i in range(110)]
+        )
+
+        documents = [Document(text=f"document number {i}") for i in range(110)]
+
+        result = embedder.run(documents=documents)
+
+        assert embedder._embedding_model.generate_embeddings.call_count == 2
+        assert isinstance(result["documents"], list)
+        assert len(result["documents"]) == len(documents)
+        for doc in result["documents"]:
+            assert isinstance(doc, Document)
+            assert isinstance(doc.embedding, list)
+            assert isinstance(doc.embedding[0], float)
