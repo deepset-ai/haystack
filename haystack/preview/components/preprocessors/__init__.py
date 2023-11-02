9f23e3504cc9905fe9a53de35c183742380d4836
anakin87 <stefanofiorucci@gmail.com>
2023-11-02 17:01:13 +0100

    fix init

--- haystack/preview/components/preprocessors/__init__.py
+++ haystack/preview/components/preprocessors/__init__.py
@@ -1,5 +1,5 @@
-from haystack.preview.components.preprocessors.text_document_cleaner import DocumentCleaner
-from haystack.preview.components.preprocessors.text_document_splitter import DocumentSplitter
+from haystack.preview.components.preprocessors.document_cleaner import DocumentCleaner
+from haystack.preview.components.preprocessors.document_splitter import DocumentSplitter
 from haystack.preview.components.preprocessors.document_language_classifier import DocumentLanguageClassifier
 from haystack.preview.components.preprocessors.text_language_classifier import TextLanguageClassifier
 
