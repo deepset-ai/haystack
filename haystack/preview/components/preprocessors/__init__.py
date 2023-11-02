2996419d55674ea5d63623bd3002ec72d92546e2
anakin87 <stefanofiorucci@gmail.com>
2023-11-02 16:46:16 +0100

    rename TextDocumentSplitter to DocumentSplitter

--- haystack/preview/components/preprocessors/__init__.py
+++ haystack/preview/components/preprocessors/__init__.py
@@ -1,6 +1,6 @@
 from haystack.preview.components.preprocessors.text_document_cleaner import DocumentCleaner
-from haystack.preview.components.preprocessors.text_document_splitter import TextDocumentSplitter
+from haystack.preview.components.preprocessors.text_document_splitter import DocumentSplitter
 from haystack.preview.components.preprocessors.document_language_classifier import DocumentLanguageClassifier
 from haystack.preview.components.preprocessors.text_language_classifier import TextLanguageClassifier
 
-__all__ = ["TextDocumentSplitter", "DocumentCleaner", "TextLanguageClassifier", "DocumentLanguageClassifier"]
+__all__ = ["DocumentSplitter", "DocumentCleaner", "TextLanguageClassifier", "DocumentLanguageClassifier"]
