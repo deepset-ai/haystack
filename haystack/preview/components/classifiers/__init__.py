e122ab631f9b01476bb26bcc2c2e9855577fdad4
anakin87 <stefanofiorucci@gmail.com>
2023-11-06 10:43:39 +0100

    mv classifiers

---
+++ haystack/preview/components/classifiers/__init__.py
@@ -0,0 +1,4 @@
+from haystack.preview.components.classifiers.document_language_classifier import DocumentLanguageClassifier
+from haystack.preview.components.classifiers.text_language_classifier import TextLanguageClassifier
+
+__all__ = ["DocumentLanguageClassifier", "TextLanguageClassifier"]
