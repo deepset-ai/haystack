7eb09436d465767769900b7a4075aec9e934dfc4
Vladimir Blagojevic <dovlex@gmail.com>
2023-11-21 16:52:48 +0100

    Improve (de)serialization, handle nested generics

--- test/preview/components/routers/test_conditional_router.py
+++ test/preview/components/routers/test_conditional_router.py
@@ -1,6 +1,6 @@
 import copy
 import typing
-from typing import List
+from typing import List, Dict
 from unittest import mock
 
 import pytest
@@ -195,17 +195,36 @@ class TestRouter:
             ConditionalRouter(routes)
 
     def test_output_type_serialization(self):
-        assert serialize_type(str) == "builtins.str"
-        assert serialize_type(List[int]) == "typing.List"
+        assert serialize_type(str) == "str"
+        assert serialize_type(List[int]) == "typing.List[int]"
+        assert serialize_type(List[Dict[str, int]]) == "typing.List[typing.Dict[str, int]]"
         assert serialize_type(ChatMessage) == "haystack.preview.dataclasses.chat_message.ChatMessage"
-        assert serialize_type(int) == "builtins.int"
+        assert serialize_type(typing.List[Dict[str, int]]) == "typing.List[typing.Dict[str, int]]"
+        assert serialize_type(List[ChatMessage]) == "typing.List[haystack.preview.dataclasses.chat_message.ChatMessage]"
+        assert (
+            serialize_type(typing.Dict[int, ChatMessage])
+            == "typing.Dict[int, haystack.preview.dataclasses.chat_message.ChatMessage]"
+        )
+        assert serialize_type(int) == "int"
         assert serialize_type(ChatMessage.from_user("ciao")) == "haystack.preview.dataclasses.chat_message.ChatMessage"
 
     def test_output_type_deserialization(self):
-        assert deserialize_type("builtins.str") == str
-        assert deserialize_type("typing.List") == typing.List
+        assert deserialize_type("str") == str
+        assert deserialize_type("typing.List[int]") == typing.List[int]
+        assert deserialize_type("typing.List[typing.Dict[str, int]]") == typing.List[Dict[str, int]]
+        assert deserialize_type("typing.Dict[str, int]") == Dict[str, int]
+        assert deserialize_type("typing.Dict[str, typing.List[int]]") == Dict[str, List[int]]
+        assert deserialize_type("typing.List[typing.Dict[str, typing.List[int]]]") == List[Dict[str, List[int]]]
+        assert (
+            deserialize_type("typing.List[haystack.preview.dataclasses.chat_message.ChatMessage]")
+            == typing.List[ChatMessage]
+        )
+        assert (
+            deserialize_type("typing.Dict[int, haystack.preview.dataclasses.chat_message.ChatMessage]")
+            == typing.Dict[int, ChatMessage]
+        )
         assert deserialize_type("haystack.preview.dataclasses.chat_message.ChatMessage") == ChatMessage
-        assert deserialize_type("builtins.int") == int
+        assert deserialize_type("int") == int
 
     def test_router_de_serialization(self):
         routes = [
