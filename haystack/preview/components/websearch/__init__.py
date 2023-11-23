4327fd74d38406360be83d836716101156f251f6
Sebastjan Prachovskij <sebastjan.prachovskij@gmail.com>
2023-11-23 15:13:57 +0200

    Add SearchApi integration for websearch

--- haystack/preview/components/websearch/__init__.py
+++ haystack/preview/components/websearch/__init__.py
@@ -1,3 +1,4 @@
 from haystack.preview.components.websearch.serper_dev import SerperDevWebSearch
+from haystack.preview.components.websearch.searchapi import SearchApiWebSearch
 
-__all__ = ["SerperDevWebSearch"]
+__all__ = ["SerperDevWebSearch", "SearchApiWebSearch"]
