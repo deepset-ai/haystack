b850b36a4b39ddf090782d1af8e637127db5f3ea
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2023-11-24 10:58:46 +0100

    fix: Cohere - better handling of `COHERE_API_URL` (#6407)
    * extract API URL from lazy import
    
    * improve solution

--- haystack/preview/components/generators/cohere.py
+++ haystack/preview/components/generators/cohere.py
@@ -33,7 +33,7 @@ class CohereGenerator:
         api_key: Optional[str] = None,
         model_name: str = "command",
         streaming_callback: Optional[Callable] = None,
-        api_base_url: str = COHERE_API_URL,
+        api_base_url: Optional[str] = None,
         **kwargs,
     ):
         """
@@ -61,12 +61,17 @@ class CohereGenerator:
                           The format is {token_id: bias} where bias is a float between -10 and 10.
 
         """
+        cohere_import.check()
+
         if not api_key:
             api_key = os.environ.get("COHERE_API_KEY")
         if not api_key:
             raise ValueError(
                 "CohereGenerator needs an API key to run. Either provide it as init parameter or set the env var COHERE_API_KEY."
             )
+
+        if not api_base_url:
+            api_base_url = COHERE_API_URL
 
         self.api_key = api_key
         self.model_name = model_name
