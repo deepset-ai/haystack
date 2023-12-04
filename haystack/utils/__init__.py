d66d97a2d0a761d140af4a8cfa62f5f63b8cf599
Vladimir Blagojevic <dovlex@gmail.com>
2023-12-04 10:54:01 +0100

    Revert haystack/utils/__init__.py change

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -1,5 +1,3 @@
 from haystack.utils.expit import expit
-from haystack.utils.filters import document_matches_filter
 from haystack.utils.requests_utils import request_with_retry
-
-__all__ = ["expit", "request_with_retry", "document_matches_filter"]
+from haystack.utils.filters import document_matches_filter
