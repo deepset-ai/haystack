ff182dea2127bf3e813870702ed8f81244535755
Vladimir Blagojevic <dovlex@gmail.com>
2023-12-04 11:44:29 +0100

    Small fixes

--- examples/getting_started/rag.py
+++ examples/getting_started/rag.py
@@ -5,8 +5,6 @@ from haystack.pipeline_utils import build_rag_pipeline
 
 # We are model agnostic but for default RAG pipeline we'll use an OpenAI model
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-if not OPENAI_API_KEY:
-    raise ValueError("Please set OPENAI_API_KEY environment variable to use this pipeline.")
 
 # We support many different databases. Here we load a simple and lightweight in-memory document store.
 document_store = InMemoryDocumentStore()
@@ -20,6 +18,6 @@ documents = [
 document_store.write_documents(documents)
 
 # Let's now build a simple RAG pipeline that uses a generative model to answer questions.
-pipe = build_rag_pipeline(document_store=document_store)
-answers = pipe.run(query="Who lives in Rome?")
+rag_pipeline = build_rag_pipeline(document_store=document_store)
+answers = rag_pipeline.run(query="Who lives in Rome?")
 print(answers.data)
