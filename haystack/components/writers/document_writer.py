b6a2bb13e25004e99fa925793551a02b3ff4aa53
Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
2023-12-04 15:17:28 +0100

    refactor!: improve the deserialization logic for components that use a Document Store (#6466)
    * improve deserialization
    
    * rm ds decorator
    
    * improve tests
    
    * fix pylint
    
    * rm decorator from module init
    
    * rm decorator
    
    * rm decorator from factory
    
    * fix tests
    
    * release note
    
    * rm print

--- haystack/components/writers/document_writer.py
+++ haystack/components/writers/document_writer.py
@@ -1,7 +1,12 @@
 from typing import List, Optional, Dict, Any
 
+import importlib
+import logging
+
 from haystack import component, Document, default_from_dict, default_to_dict, DeserializationError
-from haystack.document_stores import DocumentStore, DuplicatePolicy, document_store
+from haystack.document_stores import DocumentStore, DuplicatePolicy
+
+logger = logging.getLogger(__name__)
 
 
 @component
@@ -41,9 +46,17 @@ class DocumentWriter:
             raise DeserializationError("Missing 'document_store' in serialization data")
         if "type" not in init_params["document_store"]:
             raise DeserializationError("Missing 'type' in document store's serialization data")
-        if init_params["document_store"]["type"] not in document_store.registry:
-            raise DeserializationError(f"DocumentStore of type '{init_params['document_store']['type']}' not found.")
-        docstore_class = document_store.registry[init_params["document_store"]["type"]]
+
+        try:
+            module_name, type_ = init_params["document_store"]["type"].rsplit(".", 1)
+            logger.debug("Trying to import %s", module_name)
+            module = importlib.import_module(module_name)
+        except (ImportError, DeserializationError) as e:
+            raise DeserializationError(
+                f"DocumentStore of type '{init_params['document_store']['type']}' not correctly imported"
+            ) from e
+
+        docstore_class = getattr(module, type_)
         docstore = docstore_class.from_dict(init_params["document_store"])
 
         data["init_parameters"]["document_store"] = docstore
