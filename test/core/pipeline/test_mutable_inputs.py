793ba68f8e4b70b74b022de31519c5272e855f2b
Massimiliano Pippi <mpippi@gmail.com>
2023-12-05 15:49:10 +0100

    clean up tests (#6488)

--- test/core/pipeline/test_mutable_inputs.py
+++ test/core/pipeline/test_mutable_inputs.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
 from typing import List
 
 from haystack.core.pipeline import Pipeline
