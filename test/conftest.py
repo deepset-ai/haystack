8a513f3b8c4566cf9e0bf2210b2f5c503e4e3f44
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-12-21 08:51:54 +0100

    test: Add fixture to block requests in tests (#6585)
    * Add fixture to block requests in tests
    
    * Mark tests making requests as integration

--- test/conftest.py
+++ test/conftest.py
@@ -21,3 +21,20 @@ def mock_tokenizer():
 @pytest.fixture()
 def test_files_path():
     return Path(__file__).parent / "test_files"
+
+
+@pytest.fixture(autouse=True)
+def request_blocker(request: pytest.FixtureRequest, monkeypatch):
+    """
+    This fixture is applied automatically to all tests.
+    Those that are not marked as integration will have the requests module
+    monkeypatched to avoid making HTTP requests by mistake.
+    """
+    marker = request.node.get_closest_marker("integration")
+    if marker is not None:
+        return
+
+    def urlopen_mock(self, method, url, *args, **kwargs):
+        raise RuntimeError(f"The test was about to {method} {self.scheme}://{self.host}{url}")
+
+    monkeypatch.setattr("urllib3.connectionpool.HTTPConnectionPool.urlopen", urlopen_mock)
