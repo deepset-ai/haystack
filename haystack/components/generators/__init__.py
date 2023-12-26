94ce4883812f4a8c3135fd474b334d132a0892f4
Vladimir Blagojevic <dovlex@gmail.com>
2023-12-26 16:38:12 +0100

    Add AzureGenerator and AzureChatGenerator

--- haystack/components/generators/__init__.py
+++ haystack/components/generators/__init__.py
@@ -1,5 +1,6 @@
 from haystack.components.generators.hugging_face_local import HuggingFaceLocalGenerator
 from haystack.components.generators.hugging_face_tgi import HuggingFaceTGIGenerator
 from haystack.components.generators.openai import OpenAIGenerator, GPTGenerator
+from haystack.components.generators.azure import AzureGenerator
 
-__all__ = ["HuggingFaceLocalGenerator", "HuggingFaceTGIGenerator", "OpenAIGenerator", "GPTGenerator"]
+__all__ = ["HuggingFaceLocalGenerator", "HuggingFaceTGIGenerator", "OpenAIGenerator", "GPTGenerator", "AzureGenerator"]
