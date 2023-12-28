090d66b5318e53628fcad9d30515deded6bb6fd1
Vladimir Blagojevic <dovlex@gmail.com>
2023-12-28 17:29:47 +0100

    feat: Update OpenAIChatGenerator to handle both tools and functions calling (#6639)
    * Handle tools parameter in OpenAIChatGenerator
    
    * Handle tools/functions parameter in OpenAIChatGenerator streaming mode
    
    * Adjust OpenAPIServiceConnector to handle tools parameter
    
    * We never deal with functions/tools in non-chat generator
    
    * Add release note

--- test/components/connectors/test_openapi_service.py
+++ test/components/connectors/test_openapi_service.py
@@ -3,6 +3,7 @@ import pytest
 from unittest.mock import MagicMock, Mock
 from openapi3 import OpenAPI
 from haystack.components.connectors import OpenAPIServiceConnector
+from haystack.dataclasses import ChatMessage
 
 
 @pytest.fixture
@@ -23,42 +24,42 @@ class TestOpenAPIServiceConnector:
     def test_parse_message_invalid_json(self, connector):
         # Test invalid JSON content
         with pytest.raises(ValueError):
-            connector._parse_message("invalid json")
+            connector._parse_message(ChatMessage.from_assistant("invalid json"))
 
     def test_parse_valid_json_message(self):
         connector = OpenAPIServiceConnector()
 
         # The content format here is OpenAI function calling descriptor
         content = (
-            '{"name": "compare_branches","arguments": "{\\n  \\"parameters\\": {\\n   '
+            '[{"function":{"name": "compare_branches","arguments": "{\\n  \\"parameters\\": {\\n   '
             ' \\"basehead\\": \\"main...openapi_container_v5\\",\\n   '
-            ' \\"owner\\": \\"deepset-ai\\",\\n    \\"repo\\": \\"haystack\\"\\n  }\\n}"}'
+            ' \\"owner\\": \\"deepset-ai\\",\\n    \\"repo\\": \\"haystack\\"\\n  }\\n}"}, "type": "function"}]'
         )
-        descriptor = connector._parse_message(content)
+        descriptors = connector._parse_message(ChatMessage.from_assistant(content))
 
         # Assert that the descriptor contains the expected method name and arguments
-        assert descriptor["name"] == "compare_branches"
-        assert descriptor["arguments"]["parameters"] == {
+        assert descriptors[0]["name"] == "compare_branches"
+        assert descriptors[0]["arguments"]["parameters"] == {
             "basehead": "main...openapi_container_v5",
             "owner": "deepset-ai",
             "repo": "haystack",
         }
         # but not the requestBody
-        assert "requestBody" not in descriptor["arguments"]
+        assert "requestBody" not in descriptors[0]["arguments"]
 
         # The content format here is OpenAI function calling descriptor
-        content = '{"name": "search","arguments": "{\\n  \\"requestBody\\": {\\n    \\"q\\": \\"haystack\\"\\n  }\\n}"}'
-        descriptor = connector._parse_message(content)
-        assert descriptor["name"] == "search"
-        assert descriptor["arguments"]["requestBody"] == {"q": "haystack"}
+        content = '[{"function": {"name": "search","arguments": "{\\n  \\"requestBody\\": {\\n    \\"q\\": \\"haystack\\"\\n  }\\n}"}, "type": "function"}]'
+        descriptors = connector._parse_message(ChatMessage.from_assistant(content))
+        assert descriptors[0]["name"] == "search"
+        assert descriptors[0]["arguments"]["requestBody"] == {"q": "haystack"}
 
         # but not the parameters
-        assert "parameters" not in descriptor["arguments"]
+        assert "parameters" not in descriptors[0]["arguments"]
 
     def test_parse_message_missing_fields(self, connector):
         # Test JSON content with missing fields
         with pytest.raises(ValueError):
-            connector._parse_message(json.dumps({"name": "test_method"}))
+            connector._parse_message(ChatMessage.from_assistant('[{"function": {"name": "test_method"}}]'))
 
     def test_authenticate_service_invalid(self, connector, openapi_service_mock):
         # Test invalid or missing authentication
