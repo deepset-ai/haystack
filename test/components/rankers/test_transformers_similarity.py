a62a16ca9c9d19482b79c0088568f8e003b0cc5e
Sebastian Husch Lee <sjrl423@gmail.com>
2024-01-04 15:19:40 +0100

    Add unit test for score threshold

--- test/components/rankers/test_transformers_similarity.py
+++ test/components/rankers/test_transformers_similarity.py
@@ -94,10 +94,23 @@ class TestSimilarityRanker:
         embedder.tokenizer = MagicMock()
 
         documents = [Document(content="document number 0"), Document(content="document number 1")]
-
         out = embedder.run(query="test", documents=documents)
         assert out["documents"][0].score == pytest.approx(-10.6859, rel=1e-4)
         assert out["documents"][1].score == pytest.approx(-8.9874, rel=1e-4)
+
+    @patch("torch.sort")
+    def test_score_threshold(self, mocked_sort):
+        mocked_sort.return_value = (None, torch.tensor([0, 1]))
+        embedder = TransformersSimilarityRanker(model_name_or_path="model", scale_score=False, score_threshold=0.1)
+        embedder.model = MagicMock()
+        embedder.model.return_value = SequenceClassifierOutput(
+            loss=None, logits=torch.FloatTensor([[0.955], [0.001]]), hidden_states=None, attentions=None
+        )
+        embedder.tokenizer = MagicMock()
+
+        documents = [Document(content="document number 0"), Document(content="document number 1")]
+        out = embedder.run(query="test", documents=documents)
+        assert len(out["documents"]) == 1
 
     @pytest.mark.integration
     @pytest.mark.parametrize(
