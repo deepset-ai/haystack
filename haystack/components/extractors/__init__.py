a48ed8fd6839792b2d07ac0cd41105a4549cf62e
shadeMe <shadeMe@users.noreply.github.com>
2024-01-05 17:16:57 +0100

    feat: Add `NamedEntityExtractor`component
    This component accepts a list of `Document`s which it annotates with named entities. The annotations are stored in the `meta` dictionary of each `Document` under a specific key.
    
    The component currently support two backends for the annotation models: Hugging Face `transformers` and spaCy.

---
+++ haystack/components/extractors/__init__.py
@@ -0,0 +1,3 @@
+from .named_entity_extractor import NamedEntityAnnotation, NamedEntityExtractor, NamedEntityExtractorBackend
+
+__all__ = ["NamedEntityExtractor", "NamedEntityExtractorBackend", "NamedEntityAnnotation"]
