8ad2e40bb310b4a3bade5a8ff0c97d85f3f8ccaf
ZanSara <sara.zanzottera@deepset.ai>
2024-01-11 16:41:48 +0100

    add test for pipeline that needs to wait on an optional input

--- test/core/pipeline/test_default_value.py
+++ test/core/pipeline/test_default_value.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 @component
 class WithDefault:
-    @component.output_types(b=int)
+    @component.output_types(c=int)
     def run(self, a: int, b: int = 2):
         return {"c": a + b}
 
@@ -30,3 +30,17 @@ def test_pipeline(tmp_path):
     # Rely on default value for 'b'
     results = pipeline.run({"with_defaults": {"a": 40}})
     assert results == {"with_defaults": {"c": 42}}
+
+
+def test_pipeline_wait_on_connected_optional_inputs(tmp_path):
+    pipeline = Pipeline()
+    pipeline.add_component("first", WithDefault())
+    pipeline.add_component("second", WithDefault())
+    pipeline.add_component("third", WithDefault())
+    pipeline.connect("first", "second.a")
+    pipeline.connect("second", "third.b")
+
+    pipeline.draw(tmp_path / "default_value.png")
+
+    results = pipeline.run({"first": {"a": 1}, "third": {"a": 2}})
+    assert results == {"third": {"c": 7}}
