f911ab1fc70622024bf8436c4db87f723df61765
Vladimir Blagojevic <dovlex@gmail.com>
2024-01-17 08:56:50 +0100

    Improve unit tests

--- test/components/generators/chat/test_hugging_face_local.py
+++ test/components/generators/chat/test_hugging_face_local.py
@@ -1,8 +1,10 @@
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch, Mock
 
 import pytest
+from transformers import PreTrainedTokenizer
 
 from haystack.components.generators.chat import HuggingFaceLocalChatGenerator
+from haystack.dataclasses import ChatMessage, ChatRole
 
 
 # used to test serialization of streaming_callback
@@ -10,10 +12,30 @@ def streaming_callback_handler(x):
     return x
 
 
+@pytest.fixture
+def model_info_mock():
+    with patch(
+        "haystack.components.generators.chat.hugging_face_local.model_info",
+        new=Mock(return_value=Mock(pipeline_tag="text2text-generation")),
+    ) as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_pipeline_tokenizer():
+    # Mocking the pipeline
+    mock_pipeline = Mock(return_value=[{"generated_text": "Berlin is cool"}])
+
+    # Mocking the tokenizer
+    mock_tokenizer = Mock(spec=PreTrainedTokenizer)
+    mock_tokenizer.encode.return_value = ["Berlin", "is", "cool"]
+    mock_pipeline.tokenizer = mock_tokenizer
+
+    return mock_pipeline
+
+
 class TestHuggingFaceLocalChatGenerator:
-    @patch("haystack.components.generators.chat.hugging_face_local.model_info")
     def test_initialize_with_valid_model_and_generation_parameters(self, model_info_mock):
-        model_info_mock.return_value.pipeline_tag = "text2text-generation"
         model = "HuggingFaceH4/zephyr-7b-alpha"
         generation_kwargs = {"n": 1}
         stop_words = ["stop"]
@@ -70,9 +92,7 @@ class TestHuggingFaceLocalChatGenerator:
             "token": None,
         }
 
-    @patch("haystack.components.generators.chat.hugging_face_local.model_info")
     def test_init_task_inferred_from_model_name(self, model_info_mock):
-        model_info_mock.return_value.pipeline_tag = "text2text-generation"
         generator = HuggingFaceLocalChatGenerator(model="mistralai/Mistral-7B-Instruct-v0.2")
 
         assert generator.huggingface_pipeline_kwargs == {
@@ -85,10 +105,7 @@ class TestHuggingFaceLocalChatGenerator:
         with pytest.raises(ValueError, match="is not supported."):
             HuggingFaceLocalChatGenerator(task="text-classification")
 
-    @patch("haystack.components.generators.chat.hugging_face_local.model_info")
     def test_to_dict(self, model_info_mock):
-        model_info_mock.return_value.pipeline_tag = "text2text-generation"
-        # Initialize the HuggingFaceLocalChatGenerator object with valid parameters
         generator = HuggingFaceLocalChatGenerator(
             model="NousResearch/Llama-2-7b-chat-hf",
             token="token",
@@ -106,9 +123,7 @@ class TestHuggingFaceLocalChatGenerator:
         assert "token" not in init_params["huggingface_pipeline_kwargs"]
         assert init_params["generation_kwargs"] == {"max_new_tokens": 512, "n": 5, "stop_sequences": ["stop", "words"]}
 
-    @patch("haystack.components.generators.chat.hugging_face_local.model_info")
     def test_from_dict(self, model_info_mock):
-        model_info_mock.return_value.pipeline_tag = "text2text-generation"
         generator = HuggingFaceLocalChatGenerator(
             model="NousResearch/Llama-2-7b-chat-hf",
             generation_kwargs={"n": 5},
@@ -136,3 +151,29 @@ class TestHuggingFaceLocalChatGenerator:
         pipeline_mock.assert_called_once_with(
             model="mistralai/Mistral-7B-Instruct-v0.2", task="text2text-generation", token=None
         )
+
+    def test_generate_text_response_with_valid_prompt_and_generation_parameters(
+        self, model_info_mock, mock_pipeline_tokenizer, chat_messages
+    ):
+        model = "meta-llama/Llama-2-13b-chat-hf"
+        generation_kwargs = {"n": 1}
+        stop_words = ["stop"]
+        streaming_callback = None
+
+        generator = HuggingFaceLocalChatGenerator(
+            model=model,
+            generation_kwargs=generation_kwargs,
+            stop_words=stop_words,
+            streaming_callback=streaming_callback,
+        )
+
+        # Use the mocked pipeline from the fixture
+        generator.pipeline = mock_pipeline_tokenizer
+
+        results = generator.run(messages=chat_messages)
+
+        assert "replies" in results
+        assert isinstance(results["replies"][0], ChatMessage)
+        chat_message = results["replies"][0]
+        assert chat_message.is_from(ChatRole.ASSISTANT)
+        assert chat_message.content == "Berlin is cool"
