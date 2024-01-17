bef05685690ca6731c8579749ae7402c55606ee2
Sebastian Husch Lee <sjrl423@gmail.com>
2024-01-17 14:03:20 +0100

    Speedup tests for PyPDFToDocument

--- test/components/converters/test_pypdf_to_document.py
+++ test/components/converters/test_pypdf_to_document.py
@@ -7,35 +7,36 @@ from haystack.components.converters.pypdf import PyPDFToDocument, CONVERTERS_REG
 from haystack.dataclasses import ByteStream
 
 
-@pytest.mark.integration
+@pytest.fixture
+def pypdf_converter():
+    return PyPDFToDocument()
+
+
 class TestPyPDFToDocument:
-    def test_init(self):
-        component = PyPDFToDocument()
-        assert component.converter_name == "default"
-        assert hasattr(component, "_converter")
+    def test_init(self, pypdf_converter):
+        assert pypdf_converter.converter_name == "default"
+        assert hasattr(pypdf_converter, "_converter")
 
     def test_init_fail_nonexisting_converter(self):
         with pytest.raises(ValueError):
             PyPDFToDocument(converter_name="non_existing_converter")
 
-    def test_run(self, test_files_path):
+    def test_run(self, test_files_path, pypdf_converter):
         """
         Test if the component runs correctly.
         """
-        paths = [test_files_path / "pdf" / "react_paper.pdf"]
-        converter = PyPDFToDocument()
-        output = converter.run(sources=paths)
+        paths = [test_files_path / "pdf" / "sample_pdf_1.pdf"]
+        output = pypdf_converter.run(sources=paths)
         docs = output["documents"]
         assert len(docs) == 1
-        assert "ReAct" in docs[0].content
+        assert "History" in docs[0].content
 
-    def test_run_with_meta(self, test_files_path):
+    def test_run_with_meta(self, test_files_path, pypdf_converter):
         bytestream = ByteStream(data=b"test", meta={"author": "test_author", "language": "en"})
 
-        converter = PyPDFToDocument()
         with patch("haystack.components.converters.pypdf.PdfReader"):
-            output = converter.run(
-                sources=[bytestream, test_files_path / "pdf" / "react_paper.pdf"], meta={"language": "it"}
+            output = pypdf_converter.run(
+                sources=[bytestream, test_files_path / "pdf" / "sample_pdf_1.pdf"], meta={"language": "it"}
             )
 
         # check that the metadata from the bytestream is merged with that from the meta parameter
@@ -43,30 +44,28 @@ class TestPyPDFToDocument:
         assert output["documents"][0].meta["language"] == "it"
         assert output["documents"][1].meta["language"] == "it"
 
-    def test_run_error_handling(self, test_files_path, caplog):
+    def test_run_error_handling(self, test_files_path, pypdf_converter, caplog):
         """
         Test if the component correctly handles errors.
         """
         paths = ["non_existing_file.pdf"]
-        converter = PyPDFToDocument()
         with caplog.at_level(logging.WARNING):
-            converter.run(sources=paths)
+            pypdf_converter.run(sources=paths)
             assert "Could not read non_existing_file.pdf" in caplog.text
 
-    def test_mixed_sources_run(self, test_files_path):
+    def test_mixed_sources_run(self, test_files_path, pypdf_converter):
         """
         Test if the component runs correctly when mixed sources are provided.
         """
-        paths = [test_files_path / "pdf" / "react_paper.pdf"]
-        with open(test_files_path / "pdf" / "react_paper.pdf", "rb") as f:
+        paths = [test_files_path / "pdf" / "sample_pdf_1.pdf"]
+        with open(test_files_path / "pdf" / "sample_pdf_1.pdf", "rb") as f:
             paths.append(ByteStream(f.read()))
 
-        converter = PyPDFToDocument()
-        output = converter.run(sources=paths)
+        output = pypdf_converter.run(sources=paths)
         docs = output["documents"]
         assert len(docs) == 2
-        assert "ReAct" in docs[0].content
-        assert "ReAct" in docs[1].content
+        assert "History and standardization" in docs[0].content
+        assert "History and standardization" in docs[1].content
 
     def test_custom_converter(self, test_files_path):
         """
@@ -74,7 +73,7 @@ class TestPyPDFToDocument:
         """
         from pypdf import PdfReader
 
-        paths = [test_files_path / "pdf" / "react_paper.pdf"]
+        paths = [test_files_path / "pdf" / "sample_pdf_1.pdf"]
 
         class MyCustomConverter:
             def convert(self, reader: PdfReader) -> Document:
