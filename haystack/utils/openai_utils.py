a6469e135041f3a48c03884f2f313f62dd1bad06
Sebastian Husch Lee <sjrl@users.noreply.github.com>
2024-01-22 11:08:40 +0100

    feat: Add support for `gpt-3.5-turbo-1106` (#6776)
    * Add max token limit support for gpt-3.5-turbo-1106
    
    * Add unit test
    
    * release notes

--- haystack/utils/openai_utils.py
+++ haystack/utils/openai_utils.py
@@ -82,6 +82,9 @@ def _openai_text_completion_tokenization_details(model_name: str):
         elif model_name.startswith("gpt-3.5-turbo-16k") or model_name.startswith("gpt-35-turbo-16k"):
             max_tokens_limit = 16384
             tokenizer_name = model_tokenizer
+        elif model_name.startswith("gpt-3.5-turbo-1106") or model_name.startswith("gpt-35-turbo-1106"):
+            max_tokens_limit = 16384
+            tokenizer_name = model_tokenizer
         elif model_name.startswith("gpt-3"):
             max_tokens_limit = 4096
             tokenizer_name = model_tokenizer
