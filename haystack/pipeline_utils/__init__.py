c47b82c54f313cf269ab0c6d4989e6857fdb082e
Vladimir Blagojevic <dovlex@gmail.com>
2024-01-23 18:40:43 +0100

    Remove pipeline_utils package and dependent code (#6806)

--- haystack/pipeline_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from haystack.pipeline_utils.rag import build_rag_pipeline
-from haystack.pipeline_utils.indexing import build_indexing_pipeline
-
-__all__ = ["build_rag_pipeline", "build_indexing_pipeline"]
