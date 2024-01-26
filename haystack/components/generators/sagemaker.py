7402130815d6ff7fc7670cc93157b002cdf9742b
ZanSara <sara.zanzottera@deepset.ai>
2024-01-26 13:10:38 +0100

    add check on credentials in init

--- haystack/components/generators/sagemaker.py
+++ haystack/components/generators/sagemaker.py
@@ -25,15 +25,16 @@ class SagemakerGenerator:
     Inference Endpoint. For guidance on how to deploy a model to SageMaker, refer to the
     [SageMaker JumpStart foundation models documentation](https://docs.aws.amazon.com/sagemaker/latest/dg/jumpstart-foundation-models-use.html).
 
-    Example:
+    **Example:**
 
+    First export your AWS credentials as environment variables:
     ```bash
     export AWS_ACCESS_KEY_ID=<your_access_key_id>
     export AWS_SECRET_ACCESS_KEY=<your_secret_access_key>
-    export AWS_SESSION_TOKEN=<your_session_token>  # This is optional
-    export AWS_REGION_NAME=<your_region_name>
     ```
+    (Note: you may also need to set the session token and region name, depending on your AWS configuration)
 
+    Then you can use the generator as follows:
     ```python
     from haystack.components.generators.sagemaker import SagemakerGenerator
     generator = SagemakerGenerator(model="jumpstart-dft-hf-llm-falcon-7b-instruct-bf16")
@@ -42,8 +43,7 @@ class SagemakerGenerator:
     print(response)
     ```
 
-    TODO review reply format
-
+    ```
     >> {'replies': ['Natural Language Processing (NLP) is a branch of artificial intelligence that focuses on
     >> the interaction between computers and human language. It involves enabling computers to understand, interpret,
     >> and respond to natural human language in a way that is both meaningful and useful.'], 'meta': [{}]}
@@ -53,11 +53,11 @@ class SagemakerGenerator:
     def __init__(
         self,
         model: str,
-        aws_access_key_id: Optional[str] = None,
-        aws_secret_access_key: Optional[str] = None,
-        aws_session_token: Optional[str] = None,
-        aws_region_name: Optional[str] = None,
-        aws_profile_name: Optional[str] = None,
+        aws_access_key_id_var: str = "AWS_ACCESS_KEY_ID",
+        aws_secret_access_key_var: str = "AWS_SECRET_ACCESS_KEY",
+        aws_session_token_var: str = "AWS_SESSION_TOKEN",
+        aws_region_name_var: str = "AWS_REGION",
+        aws_profile_name_var: str = "AWS_PROFILE",
         aws_custom_attributes: Optional[Dict[str, Any]] = None,
         generation_kwargs: Optional[Dict[str, Any]] = None,
     ):
@@ -65,11 +65,11 @@ class SagemakerGenerator:
         Instantiates the session with SageMaker.
 
         :param model: The name for SageMaker Model Endpoint.
-        :param aws_access_key_id: AWS access key ID.
-        :param aws_secret_access_key: AWS secret access key.
-        :param aws_session_token: AWS session token.
-        :param aws_region_name: AWS region name.
-        :param aws_profile_name: AWS profile name.
+        :param aws_access_key_id_var: The name of the env var where the AWS access key ID is stored.
+        :param aws_secret_access_key_var: The name of the env var where the AWS secret access key is stored.
+        :param aws_session_token_var: The name of the env var where the AWS session token is stored.
+        :param aws_region_name_var: The name of the env var where the AWS region name is stored.
+        :param aws_profile_name_var: The name of the env var where the AWS profile name is stored.
         :param aws_custom_attributes: Custom attributes to be passed to SageMaker, for example `{"accept_eula": True}`
             in case of Llama-2 models.
         :param generation_kwargs: Additional keyword arguments for text generation. For a list of supported parameters
@@ -89,15 +89,20 @@ class SagemakerGenerator:
                 be boolean. The default value for it is `False`.
         """
         self.model = model
-        self.aws_access_key_id = aws_access_key_id or os.getenv("AWS_ACCESS_KEY_ID", None)
-        self.aws_secret_access_key = aws_secret_access_key or os.getenv("AWS_SECRET_KEY", None)
-        self.aws_session_token = aws_session_token or os.getenv("AWS_SESSION_TOKEN", None)
-        self.aws_region_name = aws_region_name or os.getenv("AWS_REGION_NAME", None)
-        self.aws_profile_name = aws_profile_name or os.getenv("AWS_PROFILE_NAME", None)
+        self.aws_access_key_id_var = aws_access_key_id_var
+        self.aws_secret_access_key_var = aws_secret_access_key_var
+        self.aws_session_token_var = aws_session_token_var
+        self.aws_region_name_var = aws_region_name_var
+        self.aws_profile_name_var = aws_profile_name_var
         self.aws_custom_attributes = aws_custom_attributes or {}
         self.generation_kwargs = generation_kwargs or {"max_new_tokens": 1024}
-
         self.client: Optional[BaseClient] = None
+
+        if not os.getenv(self.aws_access_key_id_var) or not os.getenv(self.aws_secret_access_key_var):
+            raise ValueError(
+                f"Please provide AWS credentials via environment variables '{self.aws_access_key_id_var}' and "
+                f"'{self.aws_secret_access_key_var}'."
+            )
 
     def _get_telemetry_data(self) -> Dict[str, Any]:
         """
@@ -105,41 +110,18 @@ class SagemakerGenerator:
         """
         return {"model": self.model}
 
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Serialize this component to a dictionary. We must avoid serializing AWS credentials, so
-        we serialize only region and profile names.
-
-        :return: The serialized component as a dictionary.
-        """
-        return default_to_dict(
-            self,
-            model=self.model,
-            aws_region_name=self.aws_region_name,
-            aws_profile_name=self.aws_profile_name,
-            aws_custom_attributes=self.aws_custom_attributes,
-            generation_kwargs=self.generation_kwargs,
-        )
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "SagemakerGenerator":
-        """
-        Deserialize this component from a dictionary.
-
-        :param data: The dictionary representation of this component.
-        :return: The deserialized component instance.
-        """
-        return default_from_dict(cls, data)
-
     def warm_up(self):
+        """
+        Initializes the SageMaker Inference client.
+        """
         boto3_import.check()
         try:
             session = boto3.Session(
-                aws_access_key_id=self.aws_access_key_id,
-                aws_secret_access_key=self.aws_secret_access_key,
-                aws_session_token=self.aws_session_token,
-                region_name=self.aws_region_name,
-                profile_name=self.aws_profile_name,
+                aws_access_key_id=os.getenv(self.aws_access_key_id_var),
+                aws_secret_access_key=os.getenv(self.aws_secret_access_key_var),
+                aws_session_token=os.getenv(self.aws_session_token_var),
+                region_name=os.getenv(self.aws_region_name_var),
+                profile_name=os.getenv(self.aws_profile_name_var),
             )
             self.client = session.client("sagemaker-runtime")
         except Exception as e:
@@ -155,7 +137,7 @@ class SagemakerGenerator:
 
         :param prompt: The string prompt to use for text generation.
         :param generation_kwargs: Additional keyword arguments for text generation. These parameters will
-        potentially override the parameters passed in the __init__ method.
+        potentially override the parameters passed in the `__init__` method.
 
         :return: A list of strings containing the generated responses and a list of dictionaries containing the metadata
         for each response.
@@ -188,6 +170,7 @@ class SagemakerGenerator:
 
             replies = [o.pop(key, None) for o in output]
             return {"replies": replies, "meta": output * len(replies)}
+
         except requests.HTTPError as err:
             res = err.response
             if res.status_code == 429:
@@ -197,20 +180,3 @@ class SagemakerGenerator:
                 f"SageMaker Inference returned an error. Status code: {res.status_code} Response body: {res.text}",
                 status_code=res.status_code,
             ) from err
-
-
-"""
-
-"""
-
-# import os
-# from haystack.nodes import PromptNode
-
-# # We can also configure Sagemaker via AWS environment variables without AWS profile name
-# pn = PromptNode(model_name_or_path="jumpstart-dft-hf-llm-falcon-7b-instruct-bf16", max_length=256,
-#                 model_kwargs={"aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
-#                             "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-#                             "aws_region_name": "eu-west-1"})
-
-# response = pn("Tell me more about Berlin, be elaborate")
-# print(response)
