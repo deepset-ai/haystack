d858dfc501a437da2b9b3b0991bcd54f3f57755f
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2024-01-29 17:26:11 +0100

    chore: Remove all mentions of Canals (#6844)
    * Remove unnecessary Connection class
    
    * Remove all mentions of canals
    
    * Add release notes

--- test/components/builders/test_dynamic_prompt_builder.py
+++ test/components/builders/test_dynamic_prompt_builder.py
@@ -3,7 +3,7 @@ from typing import List
 import pytest
 from jinja2 import TemplateSyntaxError
 
-from haystack import component, Document, Pipeline
+from haystack import Document, Pipeline, component
 from haystack.components.builders import DynamicPromptBuilder
 
 
@@ -16,16 +16,16 @@ class TestDynamicPromptBuilder:
         # regardless of the chat mode
         # we have inputs that contain: prompt_source, template_variables + runtime_variables
         expected_keys = set(runtime_variables + ["prompt_source", "template_variables"])
-        assert set(builder.__canals_input__.keys()) == expected_keys
+        assert set(builder.__haystack_input__.keys()) == expected_keys
 
         # response is always prompt regardless of chat mode
-        assert set(builder.__canals_output__.keys()) == {"prompt"}
+        assert set(builder.__haystack_output__.keys()) == {"prompt"}
 
         # prompt_source is a list of ChatMessage or a string
-        assert builder.__canals_input__["prompt_source"].type == str
+        assert builder.__haystack_input__["prompt_source"].type == str
 
         # output is always prompt, but the type is different depending on the chat mode
-        assert builder.__canals_output__["prompt"].type == str
+        assert builder.__haystack_output__["prompt"].type == str
 
     def test_processing_a_simple_template_with_provided_variables(self):
         runtime_variables = ["var1", "var2", "var3"]
