d7ad9b9941daa908fe4ca0f331cdb4a81082642b
Bijay Gurung <4636315+bglearning@users.noreply.github.com>
2024-01-30 16:42:26 +0100

    Implement FilterRetriever and add tests

--- haystack/components/retrievers/filter_retriever.py
+++ haystack/components/retrievers/filter_retriever.py
@@ -1,7 +1,13 @@
+import importlib
+import logging
+
 from typing import Dict, List, Any, Optional
 
 from haystack import component, Document, default_to_dict, default_from_dict, DeserializationError
 from haystack.document_stores.types import DocumentStore
+
+
+logger = logging.getLogger(__name__)
 
 
 @component
@@ -10,25 +16,15 @@ class FilterRetriever:
     Retrieves documents that match the provided filters.
     """
 
-    def __init__(
-        self, document_store: DocumentStore, filters: Optional[Dict[str, Any]] = None, top_k: Optional[int] = None
-    ):
+    def __init__(self, document_store: DocumentStore, filters: Optional[Dict[str, Any]] = None):
         """
         Create the FilterRetriever component.
 
         :param document_store: An instance of InMemoryDocumentStore.
         :param filters: A dictionary with filters to narrow down the search space. Defaults to `None`.
-        :param top_k: The maximum number of documents to retrieve. Defaults to `None` in which case all documents are retrieved.
-
-        :raises ValueError: If the `top_k` is specified but is not > 0.
         """
         self.document_store = document_store
-
-        if top_k is not None and top_k <= 0:
-            raise ValueError(f"top_k must be greater than 0. Currently, the top_k is {top_k}")
-
         self.filters = filters
-        self.top_k = top_k
 
     def _get_telemetry_data(self) -> Dict[str, Any]:
         """
@@ -41,7 +37,7 @@ class FilterRetriever:
         Serialize this component to a dictionary.
         """
         docstore = self.document_store.to_dict()
-        return default_to_dict(self, document_store=docstore, filters=self.filters, top_k=self.top_k)
+        return default_to_dict(self, document_store=docstore, filters=self.filters)
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "FilterRetriever":
@@ -53,27 +49,29 @@ class FilterRetriever:
             raise DeserializationError("Missing 'document_store' in serialization data")
         if "type" not in init_params["document_store"]:
             raise DeserializationError("Missing 'type' in document store's serialization data")
-        data["init_parameters"]["document_store"] = FilterRetriever.from_dict(data["init_parameters"]["document_store"])
+        try:
+            module_name, type_ = init_params["document_store"]["type"].rsplit(".", 1)
+            logger.debug("Trying to import %s", module_name)
+            module = importlib.import_module(module_name)
+        except (ImportError, DeserializationError) as e:
+            raise DeserializationError(
+                f"DocumentStore of type '{init_params['document_store']['type']}' not correctly imported"
+            ) from e
+
+        docstore_class = getattr(module, type_)
+        data["init_parameters"]["document_store"] = docstore_class.from_dict(data["init_parameters"]["document_store"])
         return default_from_dict(cls, data)
 
     @component.output_types(documents=List[Document])
-    def run(self, query: str, filters: Optional[Dict[str, Any]] = None, top_k: Optional[int] = None):
+    def run(self, filters: Optional[Dict[str, Any]] = None):
         """
         Run the FilterRetriever on the given input data.
 
-        :param query: The query string for the Retriever. It is ignored by the retriever.
         :param filters: A dictionary with filters to narrow down the search space.
-        :param top_k: The maximum number of documents to return.
+            If not specified, the value provided at initialization is used.
         :return: The retrieved documents.
-
-        :raises ValueError: If the specified DocumentStore is not found
         """
         if filters is None:
             filters = self.filters
-        if top_k is None:
-            top_k = self.top_k
 
-        docs = self.document_store.filter_documents(filters=filters)
-        if top_k is not None:
-            docs = docs[:top_k]
-        return {"documents": docs}
+        return {"documents": self.document_store.filter_documents(filters=filters)}
