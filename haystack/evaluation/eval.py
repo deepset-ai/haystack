393a7993c329edd3bbb99ceb8fec3a0bc7cfb370
Ashwin Mathur <97467100+awinml@users.noreply.github.com>
2024-02-02 21:37:52 +0530

    feat: Add Semantic Answer Similarity metric (#6877)
    * Add SAS metric
    
    * Add release notes
    
    * Round similarity scores for precision consistency
    
    * Add tolerance to tests
    
    * Update haystack/evaluation/eval.py
    
    Co-authored-by: Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
    
    * Add types for preprocess_text; Add additional types for f1 and em methods
    
    ---------
    
    Co-authored-by: Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>

--- haystack/evaluation/eval.py
+++ haystack/evaluation/eval.py
@@ -1,5 +1,5 @@
 import collections
-from typing import Any, Callable, Dict, List, Union
+from typing import Any, Callable, Dict, List, Union, Optional
 
 import numpy as np
 
@@ -7,6 +7,13 @@ from haystack import Pipeline
 from haystack.core.component import Component
 from haystack.evaluation.eval_utils import get_answers_from_output, preprocess_text
 from haystack.evaluation.metrics import Metric, MetricsResult
+
+from haystack.lazy_imports import LazyImport
+from haystack.utils import ComponentDevice, expit
+
+with LazyImport(message="Run 'pip install scikit-learn \"sentence-transformers>=2.2.0\"'") as metrics_import:
+    from sentence_transformers import SentenceTransformer, CrossEncoder, util
+    from transformers import AutoConfig
 
 
 class EvaluationResult:
@@ -89,7 +96,12 @@ class EvaluationResult:
         return f1
 
     def _calculate_f1(
-        self, output_key: str, regexes_to_ignore=None, ignore_case=False, ignore_punctuation=False, ignore_numbers=False
+        self,
+        output_key: str,
+        regexes_to_ignore: Optional[List[str]] = None,
+        ignore_case: bool = False,
+        ignore_punctuation: bool = False,
+        ignore_numbers: bool = False,
     ) -> MetricsResult:
         """
         Calculates the F1 score between two lists of predictions and labels.
@@ -103,7 +115,7 @@ class EvaluationResult:
             comparison. Defaults to False.
         :param ignore_numbers (bool, optional): If True, removes numerical digits from both predictions and labels
             before comparison. Defaults to False.
-        :return: A MetricsResult object containing the calculated Exact Match (EM) score.
+        :return: A MetricsResult object containing the calculated F1 score.
         """
 
         predictions = get_answers_from_output(
@@ -136,7 +148,12 @@ class EvaluationResult:
         return MetricsResult({"f1": f1})
 
     def _calculate_em(
-        self, output_key: str, regexes_to_ignore=None, ignore_case=False, ignore_punctuation=False, ignore_numbers=False
+        self,
+        output_key: str,
+        regexes_to_ignore: Optional[List[str]] = None,
+        ignore_case: bool = False,
+        ignore_punctuation: bool = False,
+        ignore_numbers: bool = False,
     ) -> MetricsResult:
         """
         Calculates the Exact Match (EM) score between two lists of predictions and labels.
@@ -175,8 +192,106 @@ class EvaluationResult:
 
         return MetricsResult({"exact_match": exact_match_score})
 
-    def _calculate_sas(self):
-        return MetricsResult({"exact_match": None})
+    def _calculate_sas(
+        self,
+        output_key: str,
+        regexes_to_ignore: Optional[List[str]] = None,
+        ignore_case: bool = False,
+        ignore_punctuation: bool = False,
+        ignore_numbers: bool = False,
+        model: str = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
+        batch_size: int = 32,
+        device: Optional[ComponentDevice] = None,
+        token: Optional[Union[str, bool]] = None,
+    ) -> MetricsResult:
+        """
+        Calculates the Semantic Answer Similarity (SAS) score between two lists of predictions and labels.
+        Semantic Answer Similarity (SAS) score measures the Transformer-based similarity between the predicted text and
+        the corresponding ground truth label.
+
+        :param output_key: The key of the output to use for comparison.
+        :param regexes_to_ignore (list, optional): A list of regular expressions. If provided, it removes substrings
+            matching these regular expressions from both predictions and labels before comparison. Defaults to None.
+        :param ignore_case (bool, optional): If True, performs case-insensitive comparison. Defaults to False.
+        :param ignore_punctuation (bool, optional): If True, removes punctuation from both predictions and labels before
+            comparison. Defaults to False.
+        :param ignore_numbers (bool, optional): If True, removes numerical digits from both predictions and labels
+            before comparison. Defaults to False.
+        :param model: SentenceTransformers semantic textual similarity model, should be path or string pointing to
+            a downloadable model.
+        :param batch_size: Number of prediction-label pairs to encode at once.
+        :param device: The device on which the model is loaded. If `None`, the default device is automatically
+            selected.
+        :param token: The token to use as HTTP bearer authorization for private models from Huggingface.
+            If True, will use the token generated when running huggingface-cli login (stored in ~/.huggingface).
+            Additional information can be found here:
+            https://huggingface.co/transformers/main_classes/model.html#transformers.PreTrainedModel.from_pretrained
+        :return: A MetricsResult object containing the calculated Semantic Answer Similarity (SAS) score and the
+            list of similarity scores obtained for each prediction-label pair.
+        """
+        metrics_import.check()
+
+        predictions = get_answers_from_output(
+            outputs=self.outputs, output_key=output_key, runnable_type=self.runnable_type
+        )
+        labels = get_answers_from_output(
+            outputs=self.expected_outputs, output_key=output_key, runnable_type=self.runnable_type
+        )
+
+        if len(predictions) != len(labels):
+            raise ValueError("The number of predictions and labels must be the same.")
+        if len(predictions) == len(labels) == 0:
+            # Return SAS as 0 for no inputs
+            return MetricsResult({"sas": 0.0, "scores": [0.0]})
+
+        predictions = preprocess_text(predictions, regexes_to_ignore, ignore_case, ignore_punctuation, ignore_numbers)
+        labels = preprocess_text(labels, regexes_to_ignore, ignore_case, ignore_punctuation, ignore_numbers)
+
+        config = AutoConfig.from_pretrained(model, use_auth_token=token)
+        cross_encoder_used = False
+        if config.architectures:
+            cross_encoder_used = any(arch.endswith("ForSequenceClassification") for arch in config.architectures)
+
+        device = ComponentDevice.resolve_device(device)
+
+        # Based on the Model string we can load either Bi-Encoders or Cross Encoders.
+        # Similarity computation changes for both approaches
+
+        if cross_encoder_used:
+            # For Cross Encoders we create a list of pairs of predictions and labels
+            similarity_model = CrossEncoder(
+                model,
+                device=device.to_torch_str(),
+                tokenizer_args={"use_auth_token": token},
+                automodel_args={"use_auth_token": token},
+            )
+            sentence_pairs = [[pred, label] for pred, label in zip(predictions, labels)]
+            similarity_scores = similarity_model.predict(sentence_pairs, batch_size=batch_size, convert_to_numpy=True)
+
+            # All Cross Encoders do not return a set of logits scores that are normalized
+            # We normalize scores if they are larger than 1
+            if (similarity_scores > 1).any():
+                similarity_scores = expit(similarity_scores)
+
+            # Convert scores to list of floats from numpy array
+            similarity_scores = similarity_scores.tolist()
+
+        else:
+            # For Bi-encoders we create embeddings separately for predictions and labels
+            similarity_model = SentenceTransformer(model, device=device.to_torch_str(), use_auth_token=token)
+            pred_embeddings = similarity_model.encode(predictions, batch_size=batch_size, convert_to_tensor=True)
+            label_embeddings = similarity_model.encode(labels, batch_size=batch_size, convert_to_tensor=True)
+
+            # Compute cosine-similarities
+            scores = util.cos_sim(pred_embeddings, label_embeddings)
+
+            # cos_sim computes cosine similarity between all pairs of vectors in pred_embeddings and label_embeddings
+            # It returns a matrix with shape (len(predictions), len(labels))
+            similarity_scores = [scores[i][i].item() for i in range(len(predictions))]
+
+        sas_score = np.mean(similarity_scores)
+
+        return MetricsResult({"sas": sas_score, "scores": similarity_scores})
 
 
 def eval(
