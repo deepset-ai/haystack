37d9de3c4e011fb4aac3644a6b7ca5ee541fe088
Vladimir Blagojevic <dovlex@gmail.com>
2024-02-09 16:03:27 +0100

    feat: Add service_credentials to OpenAPIServiceConnector run (#6962)
    * Add service_credentials to OpenAPIServiceConnector run
    * PR feedback Silvano

--- test/components/connectors/test_openapi_service.py
+++ test/components/connectors/test_openapi_service.py
@@ -1,6 +1,10 @@
 import json
+import os
+
 import pytest
 from unittest.mock import MagicMock, Mock
+
+import requests
 from openapi3 import OpenAPI
 from openapi3.schemas import Model
 from haystack.components.connectors import OpenAPIServiceConnector
@@ -13,14 +17,40 @@ def openapi_service_mock():
 
 
 @pytest.fixture
-def service_auths():
-    return {"TestService": "auth_token"}
+def random_open_pull_request_head_branch() -> str:
+    token = os.getenv("GITHUB_TOKEN")
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"token {token}"}
+    response = requests.get("https://api.github.com/repos/deepset-ai/haystack/pulls?state=open", headers=headers)
+
+    if response.status_code == 200:
+        pull_requests = response.json()
+        for pr in pull_requests:
+            if pr["base"]["ref"] == "main":
+                return pr["head"]["ref"]
+    else:
+        raise Exception(f"Failed to fetch pull requests. Status code: {response.status_code}")
+
+
+@pytest.fixture
+def genuine_fc_message(random_open_pull_request_head_branch):
+    basehead = "main..." + random_open_pull_request_head_branch
+    # arguments, see below, are always passed as a string representation of a JSON object
+    params = '{"parameters": {"basehead": "' + basehead + '", "owner": "deepset-ai", "repo": "haystack"}}'
+    payload_json = [
+        {
+            "id": "call_NJr1NBz2Th7iUWJpRIJZoJIA",
+            "function": {"arguments": params, "name": "compare_branches"},
+            "type": "function",
+        }
+    ]
+
+    return json.dumps(payload_json)
 
 
 class TestOpenAPIServiceConnector:
     @pytest.fixture
-    def connector(self, service_auths):
-        return OpenAPIServiceConnector(service_auths)
+    def connector(self):
+        return OpenAPIServiceConnector()
 
     def test_parse_message_invalid_json(self, connector):
         # Test invalid JSON content
@@ -62,11 +92,32 @@ class TestOpenAPIServiceConnector:
         with pytest.raises(ValueError):
             connector._parse_message(ChatMessage.from_assistant('[{"function": {"name": "test_method"}}]'))
 
-    def test_authenticate_service_invalid(self, connector, openapi_service_mock):
-        # Test invalid or missing authentication
-        openapi_service_mock.components.securitySchemes = {"apiKey": {}}
+    def test_authenticate_service_missing_authentication_token(self, connector, openapi_service_mock):
+        securitySchemes_mock = MagicMock()
+        securitySchemes_mock.raw_element = {"apiKey": {"in": "header", "name": "x-api-key", "type": "apiKey"}}
         with pytest.raises(ValueError):
             connector._authenticate_service(openapi_service_mock)
+
+    def test_authenticate_service_having_authentication_token(self, connector, openapi_service_mock):
+        securitySchemes_mock = MagicMock()
+        securitySchemes_mock.raw_element = {"apiKey": {"in": "header", "name": "x-api-key", "type": "apiKey"}}
+        openapi_service_mock.components.securitySchemes = securitySchemes_mock
+        connector._authenticate_service(openapi_service_mock, "some_fake_token")
+
+    def test_authenticate_service_having_authentication_dict(self, connector, openapi_service_mock):
+        securitySchemes_mock = MagicMock()
+        securitySchemes_mock.raw_element = {"apiKey": {"in": "header", "name": "x-api-key", "type": "apiKey"}}
+        openapi_service_mock.components.securitySchemes = securitySchemes_mock
+        connector._authenticate_service(openapi_service_mock, {"apiKey": "some_fake_token"})
+
+    def test_authenticate_service_having_authentication_dict_but_unsupported_auth(
+        self, connector, openapi_service_mock
+    ):
+        securitySchemes_mock = MagicMock()
+        securitySchemes_mock.raw_element = {"oauth2": {"type": "oauth2"}}
+        openapi_service_mock.components.securitySchemes = securitySchemes_mock
+        with pytest.raises(ValueError):
+            connector._authenticate_service(openapi_service_mock, {"apiKey": "some_fake_token"})
 
     def test_invoke_method_valid(self, connector, openapi_service_mock):
         # Test valid method invocation
@@ -88,3 +139,23 @@ class TestOpenAPIServiceConnector:
             "openapi3 changed. Model should have a _raw_data field, we rely on it in OpenAPIServiceConnector"
             " to get the raw data from the service response"
         )
+
+    @pytest.mark.integration
+    @pytest.mark.skipif(not os.getenv("GITHUB_TOKEN"), reason="GITHUB_TOKEN is not set")
+    def test_run(self, genuine_fc_message, test_files_path):
+        openapi_service = OpenAPIServiceConnector()
+
+        open_api_spec_path = test_files_path / "json" / "github_compare_branch_openapi_spec.json"
+        with open(open_api_spec_path, "r") as file:
+            github_compare_schema = json.load(file)
+        messages = [ChatMessage.from_assistant(genuine_fc_message)]
+
+        # genuine call to the GitHub OpenAPI service
+        result = openapi_service.run(messages, github_compare_schema, os.getenv("GITHUB_TOKEN"))
+        assert result
+
+        # load json from the service response
+        service_payload = json.loads(result["service_response"][0].content)
+
+        # verify that the service response contains the expected fields
+        assert "url" in service_payload and "files" in service_payload
