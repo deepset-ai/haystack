11502a597d02926e5c1f422c64329c0a3383a5dc
Vladimir Blagojevic <dovlex@gmail.com>
2024-02-13 08:57:40 +0100

    Callback (de)serialization

--- haystack/components/generators/utils.py
+++ haystack/components/generators/utils.py
@@ -1,9 +1,7 @@
-import inspect
-import sys
 from typing import Optional, Callable
 
-from haystack import DeserializationError
 from haystack.dataclasses import StreamingChunk
+from haystack.utils.callable_serialization import serialize_callable, deserialize_callable
 
 
 def print_streaming_chunk(chunk: StreamingChunk) -> None:
@@ -20,14 +18,7 @@ def serialize_callback_handler(streaming_callback: Callable[[StreamingChunk], No
     :param streaming_callback: The streaming callback handler function
     :return: The full path of the streaming callback handler function
     """
-    module = inspect.getmodule(streaming_callback)
-
-    # Get the full package path of the function
-    if module is not None:
-        full_path = f"{module.__name__}.{streaming_callback.__name__}"
-    else:
-        full_path = streaming_callback.__name__
-    return full_path
+    return serialize_callable(streaming_callback)
 
 
 def deserialize_callback_handler(callback_name: str) -> Optional[Callable[[StreamingChunk], None]]:
@@ -37,13 +28,4 @@ def deserialize_callback_handler(callback_name: str) -> Optional[Callable[[Strea
     :return: The streaming callback handler function
     :raises DeserializationError: If the streaming callback handler function cannot be found
     """
-    parts = callback_name.split(".")
-    module_name = ".".join(parts[:-1])
-    function_name = parts[-1]
-    module = sys.modules.get(module_name, None)
-    if not module:
-        raise DeserializationError(f"Could not locate the module of the streaming callback: {module_name}")
-    streaming_callback = getattr(module, function_name, None)
-    if not streaming_callback:
-        raise DeserializationError(f"Could not locate the streaming callback: {function_name}")
-    return streaming_callback
+    return deserialize_callable(callback_name)
