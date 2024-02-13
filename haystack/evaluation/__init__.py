50640353b42d8d95d051d0881db9d70e301bb425
Silvano Cerza <silvanocerza@gmail.com>
2024-02-13 13:09:08 +0100

    Delete old eval API

--- haystack/evaluation/__init__.py
+++
@@ -1,4 +0,0 @@
-from haystack.evaluation.eval import EvaluationResult, eval
-from haystack.evaluation.metrics import Metric, MetricsResult
-
-__all__ = ["eval", "EvaluationResult", "Metric", "MetricsResult"]
