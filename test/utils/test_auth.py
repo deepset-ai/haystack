b552b0b37cec210dc763d01b1dcea0e0788e5d3e
Madeesh Kannan <shadeMe@users.noreply.github.com>
2024-02-16 12:46:38 +0100

    fix: Ensure `Secret` types are immutable (#6994)

--- test/utils/test_auth.py
+++ test/utils/test_auth.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from haystack.utils.auth import Secret, EnvVarSecret, SecretType, TokenSecret
+from dataclasses import FrozenInstanceError
 
 
 def test_secret_type():
@@ -15,7 +16,7 @@ def test_secret_type():
 
 def test_token_secret():
     secret = Secret.from_token("test-token")
-    assert secret._type == SecretType.TOKEN
+    assert secret.type == SecretType.TOKEN
     assert isinstance(secret, TokenSecret)
     assert secret._token == "test-token"
     assert secret.resolve_value() == "test-token"
@@ -26,14 +27,19 @@ def test_token_secret():
     with pytest.raises(ValueError, match="cannot be empty"):
         Secret.from_token("")
 
+    with pytest.raises(FrozenInstanceError):
+        secret._token = "abba"
+    with pytest.raises(FrozenInstanceError):
+        secret._type = SecretType.ENV_VAR
+
 
 def test_env_var_secret():
     secret = Secret.from_env_var("TEST_ENV_VAR1")
     os.environ["TEST_ENV_VAR1"] = "test-token"
 
-    assert secret._type == SecretType.ENV_VAR
+    assert secret.type == SecretType.ENV_VAR
     assert isinstance(secret, EnvVarSecret)
-    assert secret._env_vars == ["TEST_ENV_VAR1"]
+    assert secret._env_vars == ("TEST_ENV_VAR1",)
     assert secret._strict is True
     assert secret.resolve_value() == "test-token"
 
@@ -46,7 +52,7 @@ def test_env_var_secret():
     assert secret.resolve_value() == None
 
     secret = Secret.from_env_var(["TEST_ENV_VAR2", "TEST_ENV_VAR1"], strict=True)
-    assert secret._env_vars == ["TEST_ENV_VAR2", "TEST_ENV_VAR1"]
+    assert secret._env_vars == ("TEST_ENV_VAR2", "TEST_ENV_VAR1")
     with pytest.raises(ValueError, match="None of the following .* variables are set"):
         secret.resolve_value()
     os.environ["TEST_ENV_VAR1"] = "test-token-2"
@@ -61,3 +67,10 @@ def test_env_var_secret():
     assert (
         Secret.from_dict({"type": "env_var", "env_vars": ["TEST_ENV_VAR2", "TEST_ENV_VAR1"], "strict": True}) == secret
     )
+
+    with pytest.raises(FrozenInstanceError):
+        secret._env_vars = ("A", "B")
+    with pytest.raises(FrozenInstanceError):
+        secret._strict = False
+    with pytest.raises(FrozenInstanceError):
+        secret._type = SecretType.TOKEN
