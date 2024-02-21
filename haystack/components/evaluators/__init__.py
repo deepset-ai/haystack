ff6e1a7e2fc765ce5d0a32bd1d2f1a5cf6127008
Silvano Cerza <silvanocerza@gmail.com>
2024-02-21 17:33:59 +0100

    Remove all evaluator components

--- haystack/components/evaluators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sas_evaluator import SASEvaluator
-from .statistical_evaluator import StatisticalEvaluator, StatisticalMetric
-
-__all__ = ["SASEvaluator", "StatisticalEvaluator", "StatisticalMetric"]
