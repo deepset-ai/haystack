9362b3aac5efb4f076016491542345a5b7938c0c
David S. Batista <dsbatista@gmail.com>
2024-02-22 17:55:41 +0100

    wip

--- haystack/components/builders/hypothetical_doc_query.py
+++ haystack/components/builders/hypothetical_doc_query.py
@@ -1,19 +1,52 @@
-from haystack import Pipeline, component, Document
+from haystack import Pipeline, component, Document, default_to_dict, default_from_dict
 from haystack.components.embedders.sentence_transformers_document_embedder import SentenceTransformersDocumentEmbedder
 from haystack.components.generators.openai import OpenAIGenerator
 from haystack.components.builders import PromptBuilder
 
-from typing import List, Dict, Any
+# from haystack.utils import default_to_dict, default_from_dict
+
+from typing import Dict, Any
 from numpy import array, mean
+
+from haystack.utils import deserialize_secrets_inplace
 
 
 @component
-class InstructLM:
+class HypotheticalDocumentEmbedder:
     """
-    Subcomponent to generate hypothetical documents using an instruction-following language model (InstructLM).
+    Hypothetical Document Embeddings (HyDE)
+
+    Given a query, HyDE first zero-shot prompts an instruction-following language model to generate a "fake"
+    hypothetical document but that captures relevant textual patterns.
+
+    Then, it encodes the document into an embedding vector, identifying a neighborhood in the corpus
+    embedding space, from which similar real documents are retrieved based on vector similarity.
+
+    see: "Precise Zero-Shot Dense Retrieval without Relevance Labels" (https://aclanthology.org/2023.acl-long.99/)
+
+    Usage example:
+    ```python
+    from haystack.components.embedders import HypotheticalDocumentEmbedder
+    query = "What should I see in the capital of France?"
+    hyde.run(query=query)
+    # [0.08679415807127952, -0.007719121221452951, 0.056458056718111035, -0.005992368655279278, ...]
+    ```
     """
 
-    def __init__(self, instruct_llm, nr_completions):
+    def __init__(self, instruct_llm: str = "gpt-3.5-turbo", nr_completions: int = 5):
+        """
+        Create a HypotheticalDocumentEmbedder component.
+
+        :param instruct_llm: The name of the instruction-following language model to use.
+        :param nr_completions: The number of completions to generate.
+        """
+
+        self.nr_completions = nr_completions
+
+        self.embedder = SentenceTransformersDocumentEmbedder(
+            model="sentence-transformers/all-MiniLM-L6-v2", progress_bar=False
+        )
+        self.embedder.warm_up()
         self.prompt_builder = PromptBuilder(
             template="""
         Given a question, generate a paragraph of text that answers the question.
@@ -31,97 +64,19 @@ class InstructLM:
         self.pipeline.connect("prompt_builder", "generator")
 
     def to_dict(self) -> Dict[str, Any]:
-        # ToDo
+        return default_to_dict(self, instruct_llm=self.generator.to_dict(), nr_completions=self.nr_completions)
+
+    def from_dict(cls, data: Dict[str, Any]) -> "HypotheticalDocumentEmbedder":
+        deserialize_secrets_inplace(data["init_parameters"], keys=["api_key", "azure_ad_token"])
+        return default_from_dict(cls, data)
         pass
 
-    def from_dict(cls, data: Dict[str, Any]) -> "InstructLM":
-        # ToDo
-        pass
-
-    @component.output_types(replies=List[str])
+    @component.output_types(replies=Dict[str, Any])
     def run(self, query: str):
         result = self.pipeline.run(data={"prompt_builder": {"question": query}})
-        return result["generator"]
-
-
-@component
-class HyDEBuilder:
-    """
-    Subcomponent to build the Hypothetical Document Embedding (HyDE) from the generated hypothetical documents.
-    """
-
-    def __init__(self):
-        self.embedder = SentenceTransformersDocumentEmbedder(
-            model="sentence-transformers/all-MiniLM-L6-v2", progress_bar=False
-        )
-        self.embedder.warm_up()
-
-    def to_dict(self) -> Dict[str, Any]:
-        # ToDo
-        pass
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "HyDEBuilder":
-        # ToDo
-        pass
-
-    @component.output_types(embedding=List[float])
-    def run(self, answers: List[str]):
+        answers = result["generator"]["replies"]
         embeddings = self.embedder.run([Document(content=answer) for answer in answers])
         stacked_embeddings = array([doc.embedding for doc in embeddings["documents"]])
         avg_embeddings = mean(stacked_embeddings, axis=0)
         hyde_vector = avg_embeddings.reshape((1, len(avg_embeddings)))
         return {"hypothetical_documents": answers, "hypothetical_embedding": hyde_vector[0].tolist()}
-
-
-@component
-class HypotheticalDocumentEmbedder:
-    """
-    Hypothetical Document Embeddings (HyDE)
-
-    Given a query, HyDE first zero-shot prompts an instruction-following language model to generate a "fake"
-    hypothetical document but that captures relevant textual patterns.
-
-    Then, it encodes the document into an embedding vector, identifying a neighborhood in the corpus
-    embedding space, from which similar real documents are retrieved based on vector similarity.
-
-    see:  "Precise Zero-Shot Dense Retrieval without Relevance Labels" (https://aclanthology.org/2023.acl-long.99/)
-
-    Usage example:
-    ```python
-    from haystack.components.embedders import HypotheticalDocumentEmbedder
-    query = "What should I see in the capital of France?"
-    hyde.run(query=query)
-    # [0.08679415807127952, -0.007719121221452951, 0.056458056718111035, -0.005992368655279278, ...]
-    ```
-    """
-
-    def __init__(self, instruct_llm: str = "gpt-3.5-turbo", nr_completions: int = 5):
-        """
-        Initialize the HyDE component with a language model and the number of completions.
-
-        :param instruct_llm: name of the language model to use for instruction following
-        :param nr_completions: Number of completions to generate
-        """
-        self.instruct_lm = InstructLM(instruct_llm, nr_completions=nr_completions)
-        self.hyde_builder = HyDEBuilder()
-
-        # connecting the components
-        self.pipeline = Pipeline()
-        self.pipeline.add_component(name="instruct_lm", instance=self.instruct_lm)
-        self.pipeline.add_component(name="hyde_builder", instance=self.hyde_builder)
-        self.pipeline.connect("instruct_lm.replies", "hyde_builder.answers")
-
-    def to_dict(self) -> Dict[str, Any]:
-        # ToDo
-        pass
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "HypotheticalDocumentEmbedder":
-        # ToDo
-        pass
-
-    @component.output_types(embedding=List[float])
-    def run(self, query: str):
-        result = self.pipeline.run({"query": query})
-        return result["hyde_builder"]["hypothetical_embedding"]
