616ca67604980708d591fd01bfb8b912c852b1d1
Vladimir Blagojevic <dovlex@gmail.com>
2024-02-23 15:36:24 +0100

    Add OutputAdapter custom filters

--- haystack/components/converters/output_adapter_filters.py
+++ haystack/components/converters/output_adapter_filters.py
@@ -3,6 +3,71 @@ from typing import List, Dict, Any
 
 from haystack.dataclasses import ChatMessage, ChatRole
 
+"""
+## Custom Filters for OutputAdapter
+
+This module defines a collection of custom filters for use with the `OutputAdapter` in processing and transforming
+data within a pipeline. These filters allow for dynamic data manipulation based on the specific needs of the
+pipeline components.
+
+### Defined Filters:
+
+- `change_role`: Adjusts the role of the last message in a list of chat messages.
+- `prepare_fc_params`: Prepares function call parameters for OpenAI functions.
+- `tojson`: Converts a string in JSON format to a Python dictionary.
+
+### Usage:
+
+To utilize these filters, import the `ALL_FILTERS` dictionary and pass it to the `OutputAdapter` as the
+`custom_filters` argument. You can either specify a subset of these filters or use all of them.
+
+### Examples:
+
+#### a) Using a Subset of Custom Filters:
+
+```python
+from haystack.components.converters import OutputAdapter
+from haystack.components.converters.output_adapter_filters import ALL_FILTERS
+
+# Use only the 'prepare_fc_params' filter
+custom_filters_subset = {"prepare_fc_params": ALL_FILTERS["prepare_fc_params"]}
+
+# Example pipeline component setup with one custom filter
+pipe.add_component("a2", OutputAdapter(template="{{fc_kwargs | prepare_fc_params}}",
+                                       output_type=Dict[str, Any],
+                                       custom_filters=custom_filters_subset))
+```
+
+#### b) Using All Filters:
+
+```python
+from haystack.components.converters import OutputAdapter
+from haystack.components.converters.output_adapter_filters import ALL_FILTERS
+
+# Example pipeline setup using all available custom filters
+pipe = Pipeline()
+pipe.add_component("fc_llm", OpenAIChatGenerator(model="gpt-3.5-turbo-0125"))
+pipe.add_component("spec_to_functions", OpenAPIServiceToFunctions())
+pipe.add_component("openapi_container", OpenAPIServiceConnector())
+pipe.add_component("a1", OutputAdapter(template="{{documents[0].content | tojson}}",
+                                       output_type=Dict[str, Any],
+                                       custom_filters=ALL_FILTERS))
+pipe.add_component("a2", OutputAdapter(template="{{fc_kwargs | prepare_fc_params}}",
+                                       output_type=Dict[str, Any],
+                                       custom_filters=ALL_FILTERS))
+# Additional components utilizing different filters from ALL_FILTERS
+```
+
+This approach enables flexible and dynamic data manipulation within the Haystack pipeline, providing the capability
+to adjust data formats and values between different components seamlessly.
+
+### Note:
+
+When use above below listed custom filters or your own developed filters, ensure that the data passed through the
+pipelines, OutputAdapters, and filters is properly sanitized and validated, especially if it originates from untrusted
+sources, to mitigate security risks.
+"""
+
 
 def change_role(messages: List[ChatMessage], role: ChatRole) -> List[ChatMessage]:
     messages[-1].role = role
