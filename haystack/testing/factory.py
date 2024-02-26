9f07db0209c91da2d78d43eccc1bc0f238f58bd5
anakin87 <stefanofiorucci@gmail.com>
2024-02-26 19:23:34 +0100

    replace text with content

--- haystack/testing/factory.py
+++ haystack/testing/factory.py
@@ -35,7 +35,7 @@ def document_store_class(
 
     Create a DocumentStore class that returns a single document:
     ```python
-    doc = Document(id="fake_id", text="Fake content")
+    doc = Document(id="fake_id", content="Fake content")
     MyFakeStore = document_store_class("MyFakeComponent", documents=[doc])
     document_store = MyFakeStore()
     assert document_store.documents_count() == 1
@@ -52,7 +52,7 @@ def document_store_class(
 
     Create a DocumentStore class that returns a document and a custom count:
     ```python
-    doc = Document(id="fake_id", text="Fake content")
+    doc = Document(id="fake_id", content="Fake content")
     MyFakeStore = document_store_class("MyFakeComponent", documents=[doc], documents_count=100)
     document_store = MyFakeStore()
     assert document_store.documents_count() == 100
