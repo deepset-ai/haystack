5c9021141bceb7aec74ee43022b8415ee5360a97
Julian Risch <julian.risch@deepset.ai>
2024-02-29 10:10:46 +0100

    retriever docstrings

--- haystack/components/retrievers/in_memory/embedding_retriever.py
+++ haystack/components/retrievers/in_memory/embedding_retriever.py
@@ -7,9 +7,36 @@ from haystack.document_stores.in_memory import InMemoryDocumentStore
 @component
 class InMemoryEmbeddingRetriever:
     """
-    Uses a vector similarity metric to retrieve documents from the InMemoryDocumentStore.
+    Retrieves documents using vector similarity.
 
-    Needs to be connected to the InMemoryDocumentStore to run.
+    Usage example:
+    ```python
+    from haystack import Document
+    from haystack.components.embedders import SentenceTransformersDocumentEmbedder, SentenceTransformersTextEmbedder
+    from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+    from haystack.document_stores.in_memory import InMemoryDocumentStore
+
+    docs = [
+        Document(content="Python is a popular programming language"),
+        Document(content="python ist eine beliebte Programmiersprache"),
+    ]
+    doc_embedder = SentenceTransformersDocumentEmbedder()
+    doc_embedder.warm_up()
+    docs_with_embeddings = doc_embedder.run(docs)["documents"]
+
+    doc_store = InMemoryDocumentStore()
+    doc_store.write_documents(docs_with_embeddings)
+    retriever = InMemoryEmbeddingRetriever(doc_store)
+
+    query="Programmiersprache"
+    text_embedder = SentenceTransformersTextEmbedder()
+    text_embedder.warm_up()
+    query_embedding = text_embedder.run(query)["embedding"]
+
+    result = retriever.run(query_embedding=query_embedding)
+
+    print(result["documents"])
+    ```
     """
 
     def __init__(
@@ -23,14 +50,19 @@ class InMemoryEmbeddingRetriever:
         """
         Create the InMemoryEmbeddingRetriever component.
 
-        :param document_store: An instance of InMemoryDocumentStore.
-        :param filters: A dictionary with filters to narrow down the search space. Defaults to `None`.
-        :param top_k: The maximum number of documents to retrieve. Defaults to `10`.
-        :param scale_score: Scales the BM25 score to a unit interval in the range of 0 to 1, where 1 means extremely relevant. If set to `False`, uses raw similarity scores.
-        Defaults to `False`.
-        :param return_embedding: Whether to return the embedding of the retrieved Documents. Default is `False`.
+        :param document_store:
+            An instance of InMemoryDocumentStore.
+        :param filters:
+            A dictionary with filters to narrow down the search space.
+        :param top_k:
+            The maximum number of documents to retrieve.
+        :param scale_score:
+            Scales the BM25 score to a unit interval in the range of 0 to 1, where 1 means extremely relevant. If set to `False`, uses raw similarity scores.
+        :param return_embedding:
+            Whether to return the embedding of the retrieved Documents.
 
-        :raises ValueError: If the specified top_k is not > 0.
+        :raises ValueError:
+            If the specified top_k is not > 0.
         """
         if not isinstance(document_store, InMemoryDocumentStore):
             raise ValueError("document_store must be an instance of InMemoryDocumentStore")
@@ -53,7 +85,10 @@ class InMemoryEmbeddingRetriever:
 
     def to_dict(self) -> Dict[str, Any]:
         """
-        Serialize this component to a dictionary.
+        Serializes the component to a dictionary.
+
+        :returns:
+            Dictionary with serialized data.
         """
         docstore = self.document_store.to_dict()
         return default_to_dict(
@@ -68,7 +103,12 @@ class InMemoryEmbeddingRetriever:
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "InMemoryEmbeddingRetriever":
         """
-        Deserialize this component from a dictionary.
+        Deserializes the component from a dictionary.
+
+        :param data:
+            The dictionary to deserialize from.
+        :returns:
+            The deserialized component.
         """
         init_params = data.get("init_parameters", {})
         if "document_store" not in init_params:
@@ -92,15 +132,22 @@ class InMemoryEmbeddingRetriever:
         """
         Run the InMemoryEmbeddingRetriever on the given input data.
 
-        :param query_embedding: Embedding of the query.
-        :param filters: A dictionary with filters to narrow down the search space.
-        :param top_k: The maximum number of documents to return.
-        :param scale_score: Scales the similarity score to a unit interval in the range of 0 to 1, where 1 means extremely relevant. If set to `False`, uses raw similarity scores.
+        :param query_embedding:
+            Embedding of the query.
+        :param filters:
+            A dictionary with filters to narrow down the search space.
+        :param top_k:
+            The maximum number of documents to return.
+        :param scale_score:
+            Scales the similarity score to a unit interval in the range of 0 to 1, where 1 means extremely relevant. If set to `False`, uses raw similarity scores.
             If not specified, the value provided at initialization is used.
-        :param return_embedding: Whether to return the embedding of the retrieved Documents.
-        :return: The retrieved documents.
+        :param return_embedding:
+            Whether to return the embedding of the retrieved Documents.
+        :returns:
+            The retrieved documents.
 
-        :raises ValueError: If the specified DocumentStore is not found or is not an InMemoryDocumentStore instance.
+        :raises ValueError:
+            If the specified DocumentStore is not found or is not an InMemoryDocumentStore instance.
         """
         if filters is None:
             filters = self.filters
