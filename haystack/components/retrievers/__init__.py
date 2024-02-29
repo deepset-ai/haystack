484675365141342ae94cccc9086085373be05200
David S. Batista <dsbatista@gmail.com>
2024-02-29 11:10:02 +0100

    adding missing imports in __init__.py

--- haystack/components/retrievers/__init__.py
+++ haystack/components/retrievers/__init__.py
@@ -1,3 +1,5 @@
 from haystack.components.retrievers.filter_retriever import FilterRetriever
+from haystack.components.retrievers.in_memory.embedding_retriever import InMemoryEmbeddingRetriever
+from haystack.components.retrievers.in_memory.bm25_retriever import InMemoryBM25Retriever
 
-__all__ = ["FilterRetriever"]
+__all__ = ["FilterRetriever", "InMemoryEmbeddingRetriever", "InMemoryBM25Retriever"]
