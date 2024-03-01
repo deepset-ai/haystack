bf8aad066a2d97a19f6e0941476a5b87ea490a83
Vladimir Blagojevic <dovlex@gmail.com>
2024-03-01 12:50:57 +0100

    Update PR - Stefano

--- haystack/components/others/multiplexer.py
+++ haystack/components/others/multiplexer.py
@@ -17,20 +17,18 @@ logger = logging.getLogger(__name__)
 @component(is_greedy=True)
 class Multiplexer:
     """
-    The Multiplexer is a pivotal component in Haystack's framework enabled with the dual capability to receive data from
-    multiple upstream components and to distribute data to numerous downstream components. This multiplexing capability
-    is particularly advantageous in complex data processing pipelines.
+    `Multiplexer` is an important component offering the ability to both receive data connections from multiple other
+    components and to distribute it to various other components thus enhancing the functionality of complex data
+    processing pipelines.
 
-    It enables outputs from components, like those from a Large Language Model (LLM), to be efficiently disseminated
-    across various processing pipeline branches. Additionally, the Multiplexer is particularly useful in error
-    correction loops: it facilitates the redirection of data back into the processing cycle for re-evaluation whenever
-    a discrepancy or error is identified in the processing pipeline.
-
-    In this example below, a pipeline is set up with a `Multiplexer` to generate a JSON object based on a person's data.
-    The `OpenAIChatGenerator` is used as an LLM component to generate the JSON object. The `JsonSchemaValidator`
-    ensures that the generated JSON adheres to the specified schema. If the generated JSON does not conform to the
-    schema, a correction loop is initiated which sends the data back to the LLM via Multiplexer for re-generation until
-    the output validates against the schema.
+    `Multiplexer` is essential for spreading outputs from a single source like a Large Language Model (LLM) across
+    different branches of a pipeline and is especially valuable in error correction loops by rerouting data for
+    reevaluation if errors are detected. For instance, in an example pipeline below `Multiplexer` aids the creation of
+    a schema valid JSON object (given a person's data) in concert with an `OpenAIChatGenerator` and a `JsonSchemaValidator`.
+    In case the generated JSON object fails schema validation, `JsonSchemaValidator` initiates a correction loop, sending
+    the data back through the `Multiplexer` to the `OpenAIChatGenerator` until it passes schema validation. If we didn't
+    have `Multiplexer`, we wouldn't be able to loop back the data to `OpenAIChatGenerator` for re-generation as components
+    offer only one input connection for the declared run method parameters.
 
     Usage example:
 
@@ -50,7 +48,7 @@ class Multiplexer:
         "properties": {
             "first_name": {"type": "string", "pattern": "^[A-Z][a-z]+$"},
             "last_name": {"type": "string", "pattern": "^[A-Z][a-z]+$"},
-            "nationality": {"type": "string", "enum": ["Italian", "Portuguese", "Hungarian"]},
+            "nationality": {"type": "string", "enum": ["Italian", "Portuguese", "American"]},
         },
         "required": ["first_name", "last_name", "nationality"]
     }
@@ -60,44 +58,43 @@ class Multiplexer:
 
     # Add components to the pipeline
     pipe.add_component('mx', Multiplexer(List[ChatMessage]))
-    pipe.add_component('llm', OpenAIChatGenerator(model="gpt-3.5-turbo-0125"))
+    pipe.add_component('fc_llm', OpenAIChatGenerator(model="gpt-3.5-turbo-0125"))
     pipe.add_component('validator', JsonSchemaValidator(json_schema=person_schema))
-    pipe.add_component('a1', OutputAdapter("{{name | create_cm}}",
-                                               List[ChatMessage],
-                                               {"create_cm": lambda s: [ChatMessage.from_user("Create json from " + s)]}))
-
-    pipe.connect("a1", "mx")
-    pipe.connect("mx", "llm")
-    pipe.connect("llm.replies", "validator.messages")
+    pipe.add_component('adapter', OutputAdapter("{{chat_message}}", List[ChatMessage])),
+    # And connect them
+    pipe.connect("adapter", "mx")
+    pipe.connect("mx", "fc_llm")
+    pipe.connect("fc_llm.replies", "validator.messages")
     pipe.connect("validator.validation_error", "mx")
 
-    result = pipe.run(data={"a1": {"name": "Sara Zanzottera"},
-                                "llm":  {"generation_kwargs": {"response_format": { "type": "json_object" }}}})
+    result = pipe.run(data={"fc_llm": {"generation_kwargs": {"response_format": {"type": "json_object"}}},
+                            "adapter": {"chat_message": [ChatMessage.from_user("Create json object from Peter Parker")]}})
 
     print(json.loads(result["validator"]["validated"][0].content))
 
-    >> {'first_name': 'Sara', 'last_name': 'Zanzottera', 'nationality': 'Italian', 'occupation': 'Software Engineer',
-    >> 'skills': ['JavaScript', 'Python', 'Java', 'HTML', 'CSS']}
+
+    >> {'first_name': 'Peter', 'last_name': 'Parker', 'nationality': 'American', 'name': 'Spider-Man', 'occupation':
+    >> 'Superhero', 'age': 23, 'location': 'New York City'}
     ```
 
-    The most important thing to note is that the Multiplexer is created with a single type parameter. The type
-    determines the type of the data that the Multiplexer will receive from the upstream connected components and also
-    the type of the data that the Multiplexer will distribute to the downstream connected components. In the example
-    above, the Multiplexer is created with the type `List[ChatMessage]`. This means that the Multiplexer will receive
-    a list of `ChatMessage` objects from the upstream connected components. And the Multiplexer will then distribute
-    a list of `ChatMessage` objects to the downstream connected components.
+    One of the things to note is that the `Multiplexer` is created with a single type parameter. The type determines the
+    type of the data that the `Multiplexer` will receive from the upstream connected components and, of course, also the
+    type of the data that the `Multiplexer` will distribute to the downstream connected components. In the example
+    above, the `Multiplexer` is created with the type `List[ChatMessage]` - meaning `Multiplexer` will receive
+    a list of `ChatMessage` objects from the upstream connected components and also distribute a list of `ChatMessage`
+    objects to the downstream connected components.
 
-    In the example above the Multiplexer receives a looped back `List[ChatMessage]` from the `JsonSchemaValidator` and
-    sends it back to the `OpenAIChatGenerator` for re-generation. We can have multiple loop back connections in the
+    In the example above the `Multiplexer` receives a looped back `List[ChatMessage]` from the `JsonSchemaValidator` and
+    sends it down to the `OpenAIChatGenerator` for re-generation. We can have multiple loop back connections in the
     pipeline. In this example, the downstream component is only one, the `OpenAIChatGenerator`, but it can have more
     than one downstream component.
     """
 
     def __init__(self, type_: TypeAlias):
         """
-        Create a Multiplexer component.
+        Create a `Multiplexer` component.
 
-        :param type_: The type of data that the Multiplexer will receive from the upstream connected components and
+        :param type_: The type of data that the `Multiplexer` will receive from the upstream connected components and
                         distribute to the downstream connected components.
         """
         self.type_ = type_
@@ -131,7 +128,7 @@ class Multiplexer:
         Multiplexes the input data from the upstream connected components and distributes it to the downstream connected
         components.
 
-        :param kwargs: The input data which has to be a type of declared in the init.
+        :param **kwargs: The input data. Must be of the type declared in `__init__`.
         :return: A dictionary with the following keys:
             - `value`: The input data.
         """
