0c3642fa4f985952e38f25dc494f032b47318f63
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-03-04 11:32:33 +0100

    add new OpenAI model names (#7297)

--- haystack/nodes/retriever/dense.py
+++ haystack/nodes/retriever/dense.py
@@ -1895,9 +1895,14 @@ class EmbeddingRetriever(DenseRetriever):
     @staticmethod
     def _infer_model_format(model_name_or_path: str, use_auth_token: Optional[Union[str, bool]]) -> str:
         # pylint: disable=too-many-return-statements
-        valid_openai_model_name = model_name_or_path in ["ada", "babbage", "davinci", "curie"] or any(
-            m in model_name_or_path for m in ["-ada-", "-babbage-", "-davinci-", "-curie-"]
-        )
+        valid_openai_model_name = model_name_or_path in [
+            "ada",
+            "babbage",
+            "davinci",
+            "curie",
+            "text-embedding-3-small",
+            "text-embedding-3-large",
+        ] or any(m in model_name_or_path for m in ["-ada-", "-babbage-", "-davinci-", "-curie-"])
         if valid_openai_model_name:
             return "openai"
         if model_name_or_path in COHERE_EMBEDDING_MODELS:
