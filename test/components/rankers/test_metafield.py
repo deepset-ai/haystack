38a80b0235bd57ef66a612e8f086a3dba4b24f34
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-03-05 12:13:56 +0100

    fix: `MetaFieldRanker` - use `weight` if passed in the `run` method (#7305)
    * fix:  - use  if passed in the  method
    
    * reno

--- test/components/rankers/test_metafield.py
+++ test/components/rankers/test_metafield.py
@@ -43,6 +43,16 @@ class TestMetaFieldRanker:
         sorted_scores = sorted([doc.meta["rating"] for doc in docs_after], reverse=True)
         assert [doc.meta["rating"] for doc in docs_after] == sorted_scores
 
+    def test_run_with_weight_equal_to_1_passed_in_run_method(self):
+        ranker = MetaFieldRanker(meta_field="rating", weight=0.0)
+        docs_before = [Document(content="abc", meta={"rating": value}) for value in [1.1, 0.5, 2.3]]
+        output = ranker.run(documents=docs_before, weight=1.0)
+        docs_after = output["documents"]
+
+        assert len(docs_after) == 3
+        sorted_scores = sorted([doc.meta["rating"] for doc in docs_after], reverse=True)
+        assert [doc.meta["rating"] for doc in docs_after] == sorted_scores
+
     def test_sort_order_ascending(self):
         ranker = MetaFieldRanker(meta_field="rating", weight=1.0, sort_order="ascending")
         docs_before = [Document(content="abc", meta={"rating": value}) for value in [1.1, 0.5, 2.3]]
