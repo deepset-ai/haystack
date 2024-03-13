cd618ff04368cb0ff7d12410ccc7e92dd2ff2d89
Sebastian Husch Lee <sjrl423@gmail.com>
2024-03-13 12:02:05 +0100

    Adding more tests

--- test/components/routers/test_zero_shot_text_router.py
+++ test/components/routers/test_zero_shot_text_router.py
@@ -1,10 +1,12 @@
-import pytest
 from unittest.mock import patch
+
+import pytest
+
 from haystack.components.routers.zero_shot_text_router import ZeroShotTextRouter
 from haystack.utils import ComponentDevice, Secret
 
 
-class TestFileTypeRouter:
+class TestZeroShotTextRouter:
     def test_to_dict(self):
         router = ZeroShotTextRouter(labels=["query", "passage"])
         router_dict = router.to_dict()
@@ -46,6 +48,12 @@ class TestFileTypeRouter:
             "token": None,
         }
 
+    @patch("haystack.components.routers.zero_shot_text_router.pipeline")
+    def test_warm_up(self, hf_pipeline_mock):
+        router = ZeroShotTextRouter(labels=["query", "passage"])
+        router.warm_up()
+        assert router.pipeline is not None
+
     def test_run_error(self):
         router = ZeroShotTextRouter(labels=["query", "passage"])
         with pytest.raises(RuntimeError):
@@ -58,6 +66,17 @@ class TestFileTypeRouter:
         router.warm_up()
         with pytest.raises(TypeError):
             router.run(text=["wrong_input"])
+
+    @patch("haystack.components.routers.zero_shot_text_router.pipeline")
+    def test_run_unit(self, hf_pipeline_mock):
+        hf_pipeline_mock.return_value = [
+            {"sequence": "What is the color of the sky?", "labels": ["query", "passage"], "scores": [0.9, 0.1]}
+        ]
+        router = ZeroShotTextRouter(labels=["query", "passage"])
+        router.pipeline = hf_pipeline_mock
+        out = router.run("What is the color of the sky?")
+        assert router.pipeline is not None
+        assert out == {"query": "What is the color of the sky?"}
 
     @pytest.mark.integration
     def test_run(self):
