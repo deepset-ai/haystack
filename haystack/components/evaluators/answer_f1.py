eb1a48cfd3c12a67112b20a34fe039443a9ed347
Silvano Cerza <silvanocerza@gmail.com>
2024-03-19 17:12:41 +0100

    Remove to_dict and from_dict and update docstrings

--- haystack/components/evaluators/answer_f1.py
+++ haystack/components/evaluators/answer_f1.py
@@ -1,6 +1,5 @@
-from typing import Any, Dict, List
+from typing import Dict, List
 
-from haystack import default_from_dict, default_to_dict
 from haystack.core.component import component
 
 
@@ -12,14 +11,21 @@ class AnswerF1Evaluator:
     The result is a number from 0.0 to 1.0.
 
     Each question can have multiple ground truth answers and multiple predicted answers.
+
+    Usage example:
+    ```python
+    from haystack.components.evaluators import AnswerF1Evaluator
+
+    evaluator = AnswerF1Evaluator()
+    result = evaluator.run(
+        questions=["What is the capital of Germany?", "What is the capital of France?"],
+        ground_truth_answers=[["Berlin"], ["Paris"]],
+        predicted_answers=[["Berlin"], ["London"]],
+    )
+    print(result["result"])
+    # 0.5
+    ```
     """
-
-    def to_dict(self) -> Dict[str, Any]:
-        return default_to_dict(self)
-
-    @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "AnswerF1Evaluator":
-        return default_from_dict(cls, data)
 
     @component.output_types(result=float)
     def run(
@@ -29,12 +35,16 @@ class AnswerF1Evaluator:
         Run the AnswerF1Evaluator on the given inputs.
         All lists must have the same length.
 
-        :param questions: A list of questions.
-        :param ground_truth_answers: A list of expected answers for each question.
-        :param predicted_answers: A list of predicted answers for each question.
-        :returns: A dictionary with the following outputs:
-                * `result` - A number from 0.0 to 1.0 that represents the average F1 score of the predicted
-                answer matched with the ground truth answers.
+        :param questions:
+            A list of questions.
+        :param ground_truth_answers:
+            A list of expected answers for each question.
+        :param predicted_answers:
+            A list of predicted answers for each question.
+        :returns:
+            A dictionary with the following outputs:
+            - `result`: A number from 0.0 to 1.0 that represents the average F1 score of the predicted
+                        answer matched with the ground truth answers.
         """
         if not len(questions) == len(ground_truth_answers) == len(predicted_answers):
             raise ValueError("The length of questions, ground_truth_answers, and predicted_answers must be the same.")
