4a8ba84ed9fa067c83b6674e24290cb630d8a613
Silvano Cerza <silvanocerza@gmail.com>
2024-03-20 15:21:13 +0100

    Add test with more data

--- test/components/evaluators/test_answer_f1.py
+++ test/components/evaluators/test_answer_f1.py
@@ -36,6 +36,40 @@ def test_run_with_partial_matching():
     assert result == {"scores": [1.0, 0.0], "average": 0.5}
 
 
+def test_run_with_complex_data():
+    evaluator = AnswerF1Evaluator()
+    result = evaluator.run(
+        questions=[
+            "In what country is Normandy located?",
+            "When was the Latin version of the word Norman first recorded?",
+            "What developed in Normandy during the 1100s?",
+            "In what century did important classical music developments occur in Normandy?",
+            "From which countries did the Norse originate?",
+            "What century did the Normans first gain their separate identity?",
+        ],
+        ground_truth_answers=[
+            ["France"],
+            ["9th century", "9th"],
+            ["classical music", "classical"],
+            ["11th century", "the 11th"],
+            ["Denmark", "Iceland", "Norway", "Denmark, Iceland and Norway"],
+            ["10th century", "10th"],
+        ],
+        predicted_answers=[
+            ["France"],
+            ["9th century", "10th century", "9th"],
+            ["classical", "rock music", "dubstep"],
+            ["11th", "the 11th", "11th century"],
+            ["Denmark", "Norway", "Iceland"],
+            ["10th century", "the first half of the 10th century", "10th", "10th"],
+        ],
+    )
+    assert result == {
+        "scores": [1.0, 0.8, 0.4, 0.8, 0.8571428571428571, 0.6666666666666666],
+        "average": 0.753968253968254,
+    }
+
+
 def test_run_with_different_lengths():
     evaluator = AnswerF1Evaluator()
 
