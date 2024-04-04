92d9bb9da08345836c1fc1dd7e35fdae6194b78e
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2024-04-04 17:39:35 +0200

    Export evaluators (#7469)

--- haystack/components/evaluators/__init__.py
+++ haystack/components/evaluators/__init__.py
@@ -1,4 +1,15 @@
 from .answer_exact_match import AnswerExactMatchEvaluator
+from .document_map import DocumentMAPEvaluator
+from .document_mrr import DocumentMRREvaluator
+from .document_recall import DocumentRecallEvaluator
 from .llm_evaluator import LLMEvaluator
+from .sas_evaluator import SASEvaluator
 
-__all__ = ["AnswerExactMatchEvaluator", "LLMEvaluator"]
+__all__ = [
+    "AnswerExactMatchEvaluator",
+    "DocumentMAPEvaluator",
+    "DocumentMRREvaluator",
+    "DocumentRecallEvaluator",
+    "LLMEvaluator",
+    "SASEvaluator",
+]
