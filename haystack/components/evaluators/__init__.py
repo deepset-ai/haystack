b4248491ee354dfb6d21323efb1e11edfdd4a348
Julian Risch <julian.risch@deepset.ai>
2024-04-04 18:21:21 +0200

    sort imports

--- haystack/components/evaluators/__init__.py
+++ haystack/components/evaluators/__init__.py
@@ -1,8 +1,8 @@
 from .answer_exact_match import AnswerExactMatchEvaluator
-from .faithfulness import FaithfulnessEvaluator
 from .document_map import DocumentMAPEvaluator
 from .document_mrr import DocumentMRREvaluator
 from .document_recall import DocumentRecallEvaluator
+from .faithfulness import FaithfulnessEvaluator
 from .llm_evaluator import LLMEvaluator
 from .sas_evaluator import SASEvaluator
 
