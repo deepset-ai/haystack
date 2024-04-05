1d083861ffb1e04ef332a2d17ba9fd1a7d3487fb
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-04-05 18:48:13 +0200

    feat: `HuggingFaceAPIGenerator` (#7464)
    * draft
    
    * docstrings and more tests
    
    * deprecation; reno
    
    * pydoc config
    
    * better error messages
    
    * rm unneeded else
    
    * make params mandatory
    
    * Apply suggestions from code review
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * document enum
    
    * Update haystack/utils/hf.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * fix test
    
    ---------
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>

--- haystack/utils/hf.py
+++ haystack/utils/hf.py
@@ -21,6 +21,33 @@ with LazyImport(message="Run 'pip install \"huggingface_hub>=0.22.0\"'") as hugg
 logger = logging.getLogger(__name__)
 
 
+class HFGenerationAPIType(Enum):
+    """
+    API type to use for Hugging Face API Generators.
+    """
+
+    # HF [Text Generation Inference (TGI)](https://github.com/huggingface/text-generation-inference).
+    TEXT_GENERATION_INFERENCE = "text_generation_inference"
+
+    # HF [Inference Endpoints](https://huggingface.co/inference-endpoints).
+    INFERENCE_ENDPOINTS = "inference_endpoints"
+
+    # HF [Serverless Inference API](https://huggingface.co/inference-api).
+    SERVERLESS_INFERENCE_API = "serverless_inference_api"
+
+    def __str__(self):
+        return self.value
+
+    @staticmethod
+    def from_str(string: str) -> "HFGenerationAPIType":
+        enum_map = {e.value: e for e in HFGenerationAPIType}
+        mode = enum_map.get(string)
+        if mode is None:
+            msg = f"Unknown Hugging Face API type '{string}'. Supported types are: {list(enum_map.keys())}"
+            raise ValueError(msg)
+        return mode
+
+
 class HFModelType(Enum):
     EMBEDDING = 1
     GENERATION = 2
