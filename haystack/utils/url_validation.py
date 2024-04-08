64814583e47b40d2d132015c91a8d70fe70bc55b
David S. Batista <dsbatista@gmail.com>
2024-04-08 10:29:31 +0200

    just some more quick fixes

--- haystack/utils/url_validation.py
+++ haystack/utils/url_validation.py
@@ -2,5 +2,6 @@ from urllib.parse import urlparse
 
 
 def is_valid_http_url(url) -> bool:
+    """Check if a URL is a valid HTTP/HTTPS URL."""
     r = urlparse(url)
     return all([r.scheme in ["http", "https"], r.netloc])
