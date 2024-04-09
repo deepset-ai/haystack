830cd1358bdb145b0ba3d71c44a6d55f67f10233
David S. Batista <dsbatista@gmail.com>
2024-04-09 17:52:14 +0200

    fixing tests

--- haystack/components/evaluators/results_evaluator.py
+++ haystack/components/evaluators/results_evaluator.py
@@ -2,6 +2,7 @@ from statistics import quantiles
 from typing import Any, Dict
 
 from pandas import DataFrame
+from pandas import concat as pd_concat
 
 
 class ResultsEvaluator:
@@ -29,34 +30,54 @@ class ResultsEvaluator:
         }
 
     def individual_detailed_score_report(self) -> DataFrame:
-        """Return a DataFrame with the scores for each metric in the results."""
-        values = [[entry["scores"]] for entry in self.results["metrics"]]
-        columns = [entry["name"] for entry in self.results["metrics"]]
-        values = list(map(list, zip(*values)))  # transpose the values
-        return DataFrame(values, columns=columns)
+        """
+        Creates a DataFrame with the scores for each metric in the results.
+
+        :return: A DataFrame with the scores for each metric.
+        """
+        inputs_columns = list(self.results["inputs"].keys())
+        inputs_values = list(self.results["inputs"].values())
+        inputs_values = list(map(list, zip(*inputs_values)))  # transpose the values
+        df_inputs = DataFrame(inputs_values, columns=inputs_columns)
+
+        scores_columns = [entry["name"] for entry in self.results["metrics"]]
+        scores_values = [entry["scores"] for entry in self.results["metrics"]]
+        scores_values = list(map(list, zip(*scores_values)))  # transpose the values
+        df_scores = DataFrame(scores_values, columns=scores_columns)
+
+        return df_inputs.join(df_scores)
 
     def comparative_detailed_score_report(self, other: "ResultsEvaluator") -> DataFrame:
-        # check if the columns are the same
-        if not self.individual_detailed_score_report().columns == other.individual_detailed_score_report().columns:
-            raise ValueError("The two dataframes do not have the same columns.")
+        """
+        Creates a DataFrame with the scores for each metric in the results of two different pipelines.
 
-        # add the pipeline name to the columns
+        :param other:
+        :type other:
+        :return:
+        :rtype:
+        """
+
         pipe_a_df = self.individual_detailed_score_report()
         pipe_b_df = other.individual_detailed_score_report()
-        ignore_columns = ["question", "context", "answer"]
-        pipe_a_df.columns = [
-            f"{self.pipeline_name}_{column}"
-            for column in self.individual_detailed_score_report().columns
-            if column not in ignore_columns
-        ]
-        pipe_b_df.columns = [
-            f"{other.pipeline_name}_{column}"
-            for column in other.individual_detailed_score_report().columns
-            if column not in ignore_columns
-        ]
 
-        # merge the two dataframes
-        return pipe_a_df.append(pipe_b_df)
+        # check if the columns are the same except for query_id, question, context, and answer
+        ignore = ["query_id", "question", "contexts", "answer"]
+        columns_a = [column for column in pipe_a_df.columns if column not in ignore]
+        columns_b = [column for column in pipe_b_df.columns if column not in ignore]
+        if not columns_a == columns_b:
+            raise ValueError("The two dataframes do not have the same columns.")
+
+        # ToDo: check if they have the same number of rows
+
+        # ToDo: check if query_id, question, context, and answer are the same, or some other way to figure that
+        #  the evaluation comes from the same data
+
+        # add the pipeline name to the columns
+        pipe_b_df.drop(columns=ignore, inplace=True)
+        pipe_b_df.columns = [f"{other.pipeline_name}_{column}" for column in pipe_b_df.columns]
+        pipe_a_df.columns = [f"{self.pipeline_name}_{col}" if col not in ignore else col for col in pipe_a_df.columns]
+
+        return pd_concat([pipe_a_df, pipe_b_df], axis=1)
 
     def find_thresholds(self, metric: str) -> Dict[str, float]:
         """
--- test/components/evaluators/test_results_evaluator.py
+++ test/components/evaluators/test_results_evaluator.py
@@ -108,11 +108,16 @@ def test_comparative_aggregate_score_report():
 def test_individual_detailed_score_report():
     data = {
         "inputs": {
-            "query_id": ["53c3b3e6", "225f87f7"],
-            "question": ["What is the capital of France?", "What is the capital of Spain?"],
-            "contexts": ["wiki_France", "wiki_Spain"],
-            "answer": ["Paris", "Madrid"],
-            "predicted_answer": ["Paris", "Madrid"],
+            "query_id": ["53c3b3e6", "225f87f7", "53c3b3e6", "225f87f7"],
+            "question": [
+                "What is the capital of France?",
+                "What is the capital of Spain?",
+                "What is the capital of Luxembourg?",
+                "What is the capital of Portugal?",
+            ],
+            "contexts": ["wiki_France", "wiki_Spain", "wiki_Luxembourg", "wiki_Portugal"],
+            "answer": ["Paris", "Madrid", "Luxembourg", "Lisbon"],
+            "predicted_answer": ["Paris", "Madrid", "Luxembourg", "Lisbon"],
         },
         "metrics": [
             {"name": "reciprocal_rank", "scores": [0.378064, 0.534964, 0.216058, 0.778642]},
@@ -170,6 +175,9 @@ def test_comparative_detailed_score_report():
             {"name": "reciprocal_rank", "scores": [0.378064, 0.534964, 0.216058, 0.778642]},
             {"name": "single_hit", "scores": [1, 1, 0, 1]},
             {"name": "multi_hit", "scores": [0.706125, 0.454976, 0.445512, 0.250522]},
+            {"name": "context_relevance", "scores": [0.805466, 0.410251, 0.750070, 0.361332]},
+            {"name": "faithfulness", "scores": [0.135581, 0.695974, 0.749861, 0.041999]},
+            {"name": "semantic_answer_similarity", "scores": [0.971241, 0.159320, 0.019722, 1]},
         ],
     }
 
