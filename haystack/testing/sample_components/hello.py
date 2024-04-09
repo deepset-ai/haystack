caa67a5fb82d3e934e6e2849ac8c7cc9e214c227
Massimiliano Pippi <mpippi@gmail.com>
2024-04-09 08:51:22 +0200

    fix docstring for the sample_components package (#7499)

--- haystack/testing/sample_components/hello.py
+++ haystack/testing/sample_components/hello.py
@@ -8,8 +8,5 @@ from haystack.core.component import component
 class Hello:
     @component.output_types(output=str)
     def run(self, word: str):
-        """
-        Takes a string in input and returns "Hello, <string>!"
-        in output.
-        """
+        """Takes a string in input and returns "Hello, <string>!"in output."""
         return {"output": f"Hello, {word}!"}
--- haystack/testing/sample_components/joiner.py
+++ haystack/testing/sample_components/joiner.py
@@ -12,9 +12,9 @@ class StringJoiner:
     @component.output_types(output=str)
     def run(self, input_str: Variadic[str]):
         """
-        Take strings from multiple input nodes and join them
-        into a single one returned in output. Since `input_str`
-        is Variadic, we know we'll receive a List[str].
+        Take strings from multiple input nodes and join them into a single one returned in output.
+
+        Since `input_str` is Variadic, we know we'll receive a List[str].
         """
         return {"output": " ".join(input_str)}
 
@@ -24,9 +24,9 @@ class StringListJoiner:
     @component.output_types(output=str)
     def run(self, inputs: Variadic[List[str]]):
         """
-        Take list of strings from multiple input nodes and join them
-        into a single one returned in output. Since `input_str`
-        is Variadic, we know we'll receive a List[List[str]].
+        Take list of strings from multiple input nodes and join them into a single one returned in output.
+
+        Since `input_str` is Variadic, we know we'll receive a List[List[str]].
         """
         retval: List[str] = []
         for list_of_strings in inputs:
