39be515ba6db3213aaeda9ac7b8b79172ba996ce
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-04-09 17:47:13 +0200

    skip HF integrations tests if running from fork (#7517)

--- test/components/embedders/test_hugging_face_tei_document_embedder.py
+++ test/components/embedders/test_hugging_face_tei_document_embedder.py
@@ -1,3 +1,4 @@
+import os
 from unittest.mock import MagicMock, patch
 
 import numpy as np
@@ -305,6 +306,10 @@ class TestHuggingFaceTEIDocumentEmbedder:
 
     @pytest.mark.flaky(reruns=5, reruns_delay=5)
     @pytest.mark.integration
+    @pytest.mark.skipif(
+        not os.environ.get("HF_API_TOKEN", None),
+        reason="Export an env var called HF_API_TOKEN containing the Hugging Face token to run this test.",
+    )
     def test_run_inference_api_endpoint(self):
         docs = [
             Document(content="I love cheese", meta={"topic": "Cuisine"}),
