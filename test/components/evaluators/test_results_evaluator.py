a42377e39ef71adce0e2ae0ff27bbe9854782317
David S. Batista <dsbatista@gmail.com>
2024-04-10 11:23:53 +0200

    updating tests

--- test/components/evaluators/test_results_evaluator.py
+++ test/components/evaluators/test_results_evaluator.py
@@ -1,5 +1,4 @@
 from pandas import DataFrame
-from pandas.testing import assert_frame_equal
 
 from haystack.components.evaluators.results_evaluator import EvaluationResults
 
@@ -46,21 +45,11 @@ def test_score_report():
     }
 
     evaluator = EvaluationResults(pipeline_name="testing_pipeline_1", results=data)
-    result = evaluator.score_report()
-    expected = DataFrame(
-        {
-            "score": {
-                "reciprocal_rank": 0.476932,
-                "single_hit": 0.75,
-                "multi_hit": 0.46428375,
-                "context_relevance": 0.58177975,
-                "faithfulness": 0.40585375,
-                "semantic_answer_similarity": 0.53757075,
-            }
-        }
+    result = evaluator.score_report().to_json()
+    assert (
+        result
+        == '{"score":{"reciprocal_rank":0.476932,"single_hit":0.75,"multi_hit":0.46428375,"context_relevance":0.58177975,"faithfulness":0.40585375,"semantic_answer_similarity":0.53757075}}'
     )
-
-    assert_frame_equal(result, expected)
 
 
 def test_to_pandas():
@@ -89,44 +78,24 @@ def test_to_pandas():
 
     evaluator = EvaluationResults(pipeline_name="testing_pipeline_1", results=data)
 
-    data_types = {
-        "query_id": str,
-        "question": str,
-        "contexts": str,
-        "answer": str,
-        "predicted_answer": str,
-        "reciprocal_rank": float,
-        "single_hit": int,
-        "multi_hit": float,
-        "context_relevance": float,
-        "faithfulness": float,
-        "semantic_answer_similarity": float,
-    }
-
-    expected = DataFrame(
-        {
-            "query_id": {"0": "53c3b3e6", "1": "225f87f7", "2": "53c3b3e6", "3": "225f87f7"},
-            "question": {
-                "0": "What is the capital of France?",
-                "1": "What is the capital of Spain?",
-                "2": "What is the capital of Luxembourg?",
-                "3": "What is the capital of Portugal?",
-            },
-            "contexts": {"0": "wiki_France", "1": "wiki_Spain", "2": "wiki_Luxembourg", "3": "wiki_Portugal"},
-            "answer": {"0": "Paris", "1": "Madrid", "2": "Luxembourg", "3": "Lisbon"},
-            "predicted_answer": {"0": "Paris", "1": "Madrid", "2": "Luxembourg", "3": "Lisbon"},
-            "reciprocal_rank": {"0": 0.378064, "1": 0.534964, "2": 0.216058, "3": 0.778642},
-            "single_hit": {"0": 1, "1": 1, "2": 0, "3": 1},
-            "multi_hit": {"0": 0.706125, "1": 0.454976, "2": 0.445512, "3": 0.250522},
-            "context_relevance": {"0": 0.805466, "1": 0.410251, "2": 0.75007, "3": 0.361332},
-            "faithfulness": {"0": 0.135581, "1": 0.695974, "2": 0.749861, "3": 0.041999},
-            "semantic_answer_similarity": {"0": 0.971241, "1": 0.15932, "2": 0.019722, "3": 1.0},
+    assert evaluator.to_pandas().to_json() == {
+        "query_id": {"0": "53c3b3e6", "1": "225f87f7", "2": "53c3b3e6", "3": "225f87f7"},
+        "question": {
+            "0": "What is the capital of France?",
+            "1": "What is the capital of Spain?",
+            "2": "What is the capital of Luxembourg?",
+            "3": "What is the capital of Portugal?",
         },
-        dtype=data_types,
-    )
-
-    result = evaluator.to_pandas()
-    assert_frame_equal(result, expected)
+        "contexts": {"0": "wiki_France", "1": "wiki_Spain", "2": "wiki_Luxembourg", "3": "wiki_Portugal"},
+        "answer": {"0": "Paris", "1": "Madrid", "2": "Luxembourg", "3": "Lisbon"},
+        "predicted_answer": {"0": "Paris", "1": "Madrid", "2": "Luxembourg", "3": "Lisbon"},
+        "reciprocal_rank": {"0": 0.378064, "1": 0.534964, "2": 0.216058, "3": 0.778642},
+        "single_hit": {"0": 1, "1": 1, "2": 0, "3": 1},
+        "multi_hit": {"0": 0.706125, "1": 0.454976, "2": 0.445512, "3": 0.250522},
+        "context_relevance": {"0": 0.805466, "1": 0.410251, "2": 0.75007, "3": 0.361332},
+        "faithfulness": {"0": 0.135581, "1": 0.695974, "2": 0.749861, "3": 0.041999},
+        "semantic_answer_similarity": {"0": 0.971241, "1": 0.15932, "2": 0.019722, "3": 1.0},
+    }
 
 
 def test_comparative_detailed_score_report():
@@ -169,30 +138,23 @@ def test_comparative_detailed_score_report():
     evaluator_1 = EvaluationResults(pipeline_name="testing_pipeline_1", results=data_1)
     evaluator_2 = EvaluationResults(pipeline_name="testing_pipeline_2", results=data_2)
     results = evaluator_1.comparative_individual_score_report(evaluator_2)
-
-    print(results.dtypes)
-
-    expected = DataFrame(
-        {
-            "query_id": {"0": "53c3b3e6", "1": "225f87f7"},
-            "question": {"0": "What is the capital of France?", "1": "What is the capital of Spain?"},
-            "contexts": {"0": "wiki_France", "1": "wiki_Spain"},
-            "answer": {"0": "Paris", "1": "Madrid"},
-            "testing_pipeline_1_predicted_answer": {"0": "Paris", "1": "Madrid"},
-            "testing_pipeline_1_reciprocal_rank": {"0": 0.378064, "1": 0.534964},
-            "testing_pipeline_1_single_hit": {"0": 1, "1": 1},
-            "testing_pipeline_1_multi_hit": {"0": 0.706125, "1": 0.454976},
-            "testing_pipeline_1_context_relevance": {"0": 0.805466, "1": 0.410251},
-            "testing_pipeline_1_faithfulness": {"0": 0.135581, "1": 0.695974},
-            "testing_pipeline_1_semantic_answer_similarity": {"0": 0.971241, "1": 0.15932},
-            "testing_pipeline_2_predicted_answer": {"0": "Paris", "1": "Madrid"},
-            "testing_pipeline_2_reciprocal_rank": {"0": 0.378064, "1": 0.534964},
-            "testing_pipeline_2_single_hit": {"0": 1, "1": 1},
-            "testing_pipeline_2_multi_hit": {"0": 0.706125, "1": 0.454976},
-            "testing_pipeline_2_context_relevance": {"0": 0.805466, "1": 0.410251},
-            "testing_pipeline_2_faithfulness": {"0": 0.135581, "1": 0.695974},
-            "testing_pipeline_2_semantic_answer_similarity": {"0": 0.971241, "1": 0.15932},
-        }
-    )
-
-    assert_frame_equal(results, expected)
+    assert results.to_json() == {
+        "query_id": {"0": "53c3b3e6", "1": "225f87f7"},
+        "question": {"0": "What is the capital of France?", "1": "What is the capital of Spain?"},
+        "contexts": {"0": "wiki_France", "1": "wiki_Spain"},
+        "answer": {"0": "Paris", "1": "Madrid"},
+        "testing_pipeline_1_predicted_answer": {"0": "Paris", "1": "Madrid"},
+        "testing_pipeline_1_reciprocal_rank": {"0": 0.378064, "1": 0.534964},
+        "testing_pipeline_1_single_hit": {"0": 1, "1": 1},
+        "testing_pipeline_1_multi_hit": {"0": 0.706125, "1": 0.454976},
+        "testing_pipeline_1_context_relevance": {"0": 0.805466, "1": 0.410251},
+        "testing_pipeline_1_faithfulness": {"0": 0.135581, "1": 0.695974},
+        "testing_pipeline_1_semantic_answer_similarity": {"0": 0.971241, "1": 0.15932},
+        "testing_pipeline_2_predicted_answer": {"0": "Paris", "1": "Madrid"},
+        "testing_pipeline_2_reciprocal_rank": {"0": 0.378064, "1": 0.534964},
+        "testing_pipeline_2_single_hit": {"0": 1, "1": 1},
+        "testing_pipeline_2_multi_hit": {"0": 0.706125, "1": 0.454976},
+        "testing_pipeline_2_context_relevance": {"0": 0.805466, "1": 0.410251},
+        "testing_pipeline_2_faithfulness": {"0": 0.135581, "1": 0.695974},
+        "testing_pipeline_2_semantic_answer_similarity": {"0": 0.971241, "1": 0.15932},
+    }
