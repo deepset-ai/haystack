b858935e3088d71276e3115febb016210f28952b
tstadel <thomas.stadelmann@deepset.ai>
2024-05-07 14:54:12 +0200

    revert test_dynamic_prompt_builder.py

--- test/components/builders/test_dynamic_prompt_builder.py
+++ test/components/builders/test_dynamic_prompt_builder.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 
 import pytest
 from jinja2 import TemplateSyntaxError
@@ -14,37 +14,15 @@ class TestDynamicPromptBuilder:
         assert builder.runtime_variables == runtime_variables
 
         # regardless of the chat mode
-        # we have inputs that contain: template, template_variables + runtime_variables
-        expected_keys = set(runtime_variables + ["template", "template_variables"])
+        # we have inputs that contain: prompt_source, template_variables + runtime_variables
+        expected_keys = set(runtime_variables + ["prompt_source", "template_variables"])
         assert set(builder.__haystack_input__._sockets_dict.keys()) == expected_keys
 
         # response is always prompt regardless of chat mode
         assert set(builder.__haystack_output__._sockets_dict.keys()) == {"prompt"}
 
-        # template is a list of ChatMessage or a string
-        assert builder.__haystack_input__._sockets_dict["template"].type == Optional[str]
-
-        # output is always prompt, but the type is different depending on the chat mode
-        assert builder.__haystack_output__._sockets_dict["prompt"].type == str
-
-    def test_initialization_with_default_template(self):
-        runtime_variables = ["var1", "var2"]
-        template = "Hello, {{ var1 }}, {{ var2 }}!"
-        builder = DynamicPromptBuilder(template=template, runtime_variables=runtime_variables)
-        assert builder.runtime_variables == runtime_variables
-        assert builder.default_template is not None
-        assert builder.required_default_template_variables == {"var1", "var2"}
-
-        # regardless of the chat mode
-        # we have inputs that contain: template, template_variables + runtime_variables
-        expected_keys = set(runtime_variables + ["template", "template_variables"])
-        assert set(builder.__haystack_input__._sockets_dict.keys()) == expected_keys
-
-        # response is always prompt regardless of chat mode
-        assert set(builder.__haystack_output__._sockets_dict.keys()) == {"prompt"}
-
-        # template is a list of ChatMessage or a string
-        assert builder.__haystack_input__._sockets_dict["template"].type == Optional[str]
+        # prompt_source is a list of ChatMessage or a string
+        assert builder.__haystack_input__._sockets_dict["prompt_source"].type == str
 
         # output is always prompt, but the type is different depending on the chat mode
         assert builder.__haystack_output__._sockets_dict["prompt"].type == str
@@ -60,29 +38,6 @@ class TestDynamicPromptBuilder:
 
         assert builder.run(template, template_variables) == expected_result
 
-    def test_processing_a_simple_default_template_with_provided_variables(self):
-        runtime_variables = ["var1", "var2", "var3"]
-        template = "Hello, {{ name }}!"
-
-        builder = DynamicPromptBuilder(runtime_variables, template)
-
-        template_variables = {"name": "John"}
-        expected_result = {"prompt": "Hello, John!"}
-
-        assert builder.run(template_variables=template_variables) == expected_result
-
-    def test_overwriting_default_template(self):
-        runtime_variables = ["var1", "var2", "var3"]
-        default_template = "Hello, {{ name }}!"
-
-        builder = DynamicPromptBuilder(runtime_variables, default_template)
-
-        template = "Hello, {{ var1 }} {{ name }}!"
-        template_variables = {"name": "John"}
-        expected_result = {"prompt": "Hello, Big John!"}
-
-        assert builder.run(template, template_variables, var1="Big") == expected_result
-
     def test_processing_a_simple_template_with_invalid_template(self):
         runtime_variables = ["var1", "var2", "var3"]
         builder = DynamicPromptBuilder(runtime_variables)
@@ -92,26 +47,12 @@ class TestDynamicPromptBuilder:
         with pytest.raises(TemplateSyntaxError):
             builder.run(template, template_variables)
 
-    def test_processing_a_simple_default_template_with_invalid_template(self):
-        runtime_variables = ["var1", "var2", "var3"]
-        template = "Hello, {{ name }!"
-        with pytest.raises(TemplateSyntaxError):
-            DynamicPromptBuilder(runtime_variables, template)
-
     def test_processing_a_simple_template_with_missing_variables(self):
         runtime_variables = ["var1", "var2", "var3"]
         builder = DynamicPromptBuilder(runtime_variables)
 
         with pytest.raises(ValueError):
-            builder.run("Hello, {{ name }}!")
-
-    def test_processing_a_simple_default_template_with_missing_variables(self):
-        runtime_variables = ["var1", "var2", "var3"]
-        template = "Hello, {{ name }}!"
-        builder = DynamicPromptBuilder(runtime_variables, template)
-
-        with pytest.raises(ValueError):
-            builder.run()
+            builder.run("Hello, {{ name }}!", {})
 
     def test_missing_template_variables(self):
         prompt_builder = DynamicPromptBuilder(runtime_variables=["documents"])
@@ -138,8 +79,7 @@ class TestDynamicPromptBuilder:
         prompt_builder._validate_template("Hello, I'm {{ name }}, and I live in {{ city }}.", {"name", "city", "age"})
 
     def test_example_in_pipeline(self):
-        default_template = "Here is the document: {{documents[0].content}} \\n Answer: {{query}}"
-        prompt_builder = DynamicPromptBuilder(runtime_variables=["documents"], template=default_template)
+        prompt_builder = DynamicPromptBuilder(runtime_variables=["documents"])
 
         @component
         class DocumentProducer:
@@ -152,12 +92,12 @@ class TestDynamicPromptBuilder:
         pipe.add_component("prompt_builder", prompt_builder)
         pipe.connect("doc_producer.documents", "prompt_builder.documents")
 
-        template = "Here is the document: {{documents[0].content}} \\n Query: {{query}}"
+        template = "Here is the document: {{documents[0].content}} \\n Answer: {{query}}"
         result = pipe.run(
             data={
                 "doc_producer": {"doc_input": "Hello world, I live in Berlin"},
                 "prompt_builder": {
-                    "template": template,
+                    "prompt_source": template,
                     "template_variables": {"query": "Where does the speaker live?"},
                 },
             }
@@ -165,6 +105,6 @@ class TestDynamicPromptBuilder:
 
         assert result == {
             "prompt_builder": {
-                "prompt": "Here is the document: Hello world, I live in Berlin \\n Query: Where does the speaker live?"
+                "prompt": "Here is the document: Hello world, I live in Berlin \\n Answer: Where does the speaker live?"
             }
         }
