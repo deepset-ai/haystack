d284d85f9324f634c8ef29be639247003b725dff
Vladimir Blagojevic <dovlex@gmail.com>
2024-05-13 19:44:02 +0200

    feat: Set ByteStream's mime_type attribute for web based resources (#7681)

--- haystack/components/routers/file_type_router.py
+++ haystack/components/routers/file_type_router.py
@@ -90,7 +90,7 @@ class FileTypeRouter:
             if isinstance(source, Path):
                 mime_type = self._get_mime_type(source)
             elif isinstance(source, ByteStream):
-                mime_type = source.meta.get("content_type", None)
+                mime_type = source.mime_type
             else:
                 raise ValueError(f"Unsupported data source type: {type(source).__name__}")
 
