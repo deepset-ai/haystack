a271db7a399bd053a12644110b46e036679d61b8
David S. Batista <dsbatista@gmail.com>
2024-05-22 12:24:41 +0200

    fixing docstrings

--- haystack/components/evaluators/llm_evaluator.py
+++ haystack/components/evaluators/llm_evaluator.py
@@ -168,10 +168,10 @@ class LLMEvaluator:
         Run the LLM evaluator.
 
         Running the LLM evaluator is done within a try-except block to catch any exceptions that may
-        occur during the run. If an exception occurs, the method will return a np.nan value for the result.
+        occur during the run. If an exception occurs, the method will return a `np.nan` value for the result.
 
-        Likewise, if the output is not a valid JSON with the expected keys, the method will return a np.nan value
-        for the result.
+        Likewise, if the output is not a valid JSON or does not have the  expected keys, the method will return a
+        `np.nan` value for the result.
 
         :param inputs:
             The input values to evaluate. The keys are the input names and the values are lists of input values.
