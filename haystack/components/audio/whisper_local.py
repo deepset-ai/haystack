513ed0ad15133d0e0e0ebafed6101daf4bfcbdb6
Massimiliano Pippi <mpippi@gmail.com>
2024-05-28 15:17:43 +0200

    revert

--- haystack/components/audio/whisper_local.py
+++ haystack/components/audio/whisper_local.py
@@ -155,12 +155,12 @@ class LocalWhisperTranscriber:
         :returns:
             A dictionary mapping 'file_path' to 'transcription'.
         """
+        if self._model is None:
+            raise RuntimeError("Model is not loaded, please run 'warm_up()' before calling 'run()'")
+
         return_segments = kwargs.pop("return_segments", False)
         transcriptions: Dict[Path, Any] = {}
         for source in sources:
-            if self._model is None:
-                continue
-
             if not isinstance(source, ByteStream):
                 path = Path(source)
                 source = ByteStream.from_file_path(path)
@@ -168,8 +168,8 @@ class LocalWhisperTranscriber:
             else:
                 # If we received a ByteStream instance that doesn't have the "file_path" metadata set,
                 # we dump the bytes into a temporary file.
-                path = source.meta.get("file_path", Path())
-                if path.name:
+                path = source.meta.get("file_path")
+                if path is None:
                     fp = tempfile.NamedTemporaryFile(delete=False)
                     path = Path(fp.name)
                     source.to_file(path)
