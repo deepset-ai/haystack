55a657ba813b3aae1fcd645f7a5a6f327632c890
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-06-04 10:17:23 +0200

    export ChatPromptBuilder and add it to pydoc config (#7796)

--- haystack/components/builders/__init__.py
+++ haystack/components/builders/__init__.py
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from haystack.components.builders.answer_builder import AnswerBuilder
+from haystack.components.builders.chat_prompt_builder import ChatPromptBuilder
 from haystack.components.builders.dynamic_chat_prompt_builder import DynamicChatPromptBuilder
 from haystack.components.builders.dynamic_prompt_builder import DynamicPromptBuilder
 from haystack.components.builders.prompt_builder import PromptBuilder
 
-__all__ = ["AnswerBuilder", "PromptBuilder", "DynamicPromptBuilder", "DynamicChatPromptBuilder"]
+__all__ = ["AnswerBuilder", "PromptBuilder", "DynamicPromptBuilder", "DynamicChatPromptBuilder", "ChatPromptBuilder"]
