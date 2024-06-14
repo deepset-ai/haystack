9e07bf18832c7a162d10b29f00e41d8b5ede02db
Madeesh Kannan <shadeMe@users.noreply.github.com>
2024-06-14 17:52:32 +0200

    feat: Extend YAML serialization to allow Python tuples (#7853)

--- haystack/marshal/yaml.py
+++ haystack/marshal/yaml.py
@@ -7,6 +7,16 @@ from typing import Any, Dict, Union
 import yaml
 
 
+# Custom YAML safe loader that supports loading Python tuples
+class YamlLoader(yaml.SafeLoader):  # pylint: disable=too-many-ancestors
+    def construct_python_tuple(self, node: yaml.SequenceNode):
+        """Construct a Python tuple from the sequence."""
+        return tuple(self.construct_sequence(node))
+
+
+YamlLoader.add_constructor("tag:yaml.org,2002:python/tuple", YamlLoader.construct_python_tuple)
+
+
 class YamlMarshaller:
     def marshal(self, dict_: Dict[str, Any]) -> str:
         """Return a YAML representation of the given dictionary."""
@@ -14,4 +24,4 @@ class YamlMarshaller:
 
     def unmarshal(self, data_: Union[str, bytes, bytearray]) -> Dict[str, Any]:
         """Return a dictionary from the given YAML data."""
-        return yaml.safe_load(data_)
+        return yaml.load(data_, Loader=YamlLoader)
