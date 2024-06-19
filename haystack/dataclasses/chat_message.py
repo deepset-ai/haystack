af5304eaf20f497ed629a7973ac5c8846ab0dca8
Carlos Fernandez <carlos.ferlo@outlook.com>
2024-06-19 22:49:08 +0200

    remove unused import

--- haystack/dataclasses/chat_message.py
+++ haystack/dataclasses/chat_message.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from dataclasses import asdict, dataclass, field, fields
+from dataclasses import asdict, dataclass, field
 from enum import Enum
 from typing import Any, Dict, Optional
 
