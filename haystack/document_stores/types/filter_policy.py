4cffd39a6f83de5d8243d935aa7f3a85d96377d3
Vladimir Blagojevic <dovlex@gmail.com>
2024-06-20 13:44:23 +0200

    feat: Add apply_filter_policy function (#7902)
    * Add apply_filter_policy
    
    * Add release note

--- haystack/document_stores/types/filter_policy.py
+++ haystack/document_stores/types/filter_policy.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from enum import Enum
+from typing import Any, Dict, Optional
 
 
 class FilterPolicy(Enum):
@@ -33,3 +34,28 @@ class FilterPolicy(Enum):
             msg = f"Unknown FilterPolicy type '{filter_policy}'. Supported types are: {list(enum_map.keys())}"
             raise ValueError(msg)
         return policy
+
+
+def apply_filter_policy(
+    filter_policy: FilterPolicy,
+    init_filters: Optional[Dict[str, Any]] = None,
+    runtime_filters: Optional[Dict[str, Any]] = None,
+) -> Optional[Dict[str, Any]]:
+    """
+    Apply the filter policy to the given initial and runtime filters to determine the final set of filters used.
+
+    The function combines or replaces the initial and runtime filters based on the specified filter policy.
+
+    :param filter_policy: The policy to apply when handling the filters. It can be one of the following:
+        - `FilterPolicy.REPLACE`: Runtime filters will replace the initial filters.
+        - `FilterPolicy.MERGE`: Runtime filters will be merged with the initial filters. If there are overlapping keys,
+          values from the runtime filters will overwrite those from the initial filters.
+    :param init_filters: The initial filters set during the initialization of the relevant retriever.
+    :param runtime_filters: The filters provided at runtime, usually during a query operation execution. These filters
+                            can change for each query/retreiver run invocation.
+    :returns: A dictionary containing the resulting filters based on the provided policy.
+    """
+    if filter_policy == FilterPolicy.MERGE and runtime_filters:
+        return {**(init_filters or {}), **runtime_filters}
+    else:
+        return runtime_filters or init_filters
