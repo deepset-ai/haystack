6b65606d616693ea95ac6df0e1e9df877da11905
nickprock <nico.pro412@gmail.com>
2024-07-01 15:52:19 +0200

    another tentative

--- haystack/components/joiners/document_joiner.py
+++ haystack/components/joiners/document_joiner.py
@@ -8,7 +8,7 @@ from math import inf
 from typing import List, Optional
 
 from haystack import Document, component, logging
-from haystack.core.component.types import Iterable, Variadic
+from haystack.core.component.types import Variadic
 
 logger = logging.getLogger(__name__)
 
@@ -116,7 +116,7 @@ class DocumentJoiner:
 
         return {"documents": output_documents}
 
-    def _concatenate(self, document_lists: Iterable[List[Document]]) -> List[Document]:
+    def _concatenate(self, document_lists: Variadic[List[Document]]) -> List[Document]:
         """
         Concatenate multiple lists of Documents and return only the Document with the highest score for duplicates.
         """
@@ -129,7 +129,7 @@ class DocumentJoiner:
             output.append(doc_with_best_score)
         return output
 
-    def _merge(self, document_lists: Iterable[List[Document]]) -> List[Document]:
+    def _merge(self, document_lists: Variadic[List[Document]]) -> List[Document]:
         """
         Merge multiple lists of Documents and calculate a weighted sum of the scores of duplicate Documents.
         """
@@ -147,7 +147,7 @@ class DocumentJoiner:
 
         return documents_map.values()
 
-    def _reciprocal_rank_fusion(self, document_lists: Iterable[List[Document]]) -> List[Document]:
+    def _reciprocal_rank_fusion(self, document_lists: Variadic[List[Document]]) -> List[Document]:
         """
         Merge multiple lists of Documents and assign scores based on reciprocal rank fusion.
 
@@ -176,7 +176,7 @@ class DocumentJoiner:
 
         return documents_map.values()
 
-    def _distribution_based_rank_fusion(self, document_lists: Iterable[List[Document]]) -> List[Document]:
+    def _distribution_based_rank_fusion(self, document_lists: Variadic[List[Document]]) -> List[Document]:
         """
         Merge multiple lists of Documents and assign scores based on Distribution-Based Score Fusion.
 
