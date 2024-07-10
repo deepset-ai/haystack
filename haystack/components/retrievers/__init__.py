a23f8be0b941e71e7f421d0647e843687ff2b919
David S. Batista <dsbatista@gmail.com>
2024-07-10 15:13:46 +0200

    feat: add sentence window retrieval (#7997)
    * initial import
    
    * adding tests
    
    * adding license and release notes
    
    * adding missing release notes
    
    * working with any type of doc store
    
    * nit
    
    * adding get_class_object to serialization package
    
    * nit
    
    * refactoring get_class_object()
    
    * refactoring get_class_object()
    
    * chaning type and var names
    
    * more refactoring
    
    * Update haystack/core/serialization.py
    
    Co-authored-by: Vladimir Blagojevic <dovlex@gmail.com>
    
    * Update haystack/core/serialization.py
    
    Co-authored-by: Vladimir Blagojevic <dovlex@gmail.com>
    
    * Update test/core/test_serialization.py
    
    Co-authored-by: Vladimir Blagojevic <dovlex@gmail.com>
    
    * more refactoring
    
    * more refactoring
    
    * Pydoc syntax
    
    ---------
    
    Co-authored-by: Vladimir Blagojevic <dovlex@gmail.com>

--- haystack/components/retrievers/__init__.py
+++ haystack/components/retrievers/__init__.py
@@ -5,5 +5,6 @@
 from haystack.components.retrievers.filter_retriever import FilterRetriever
 from haystack.components.retrievers.in_memory.bm25_retriever import InMemoryBM25Retriever
 from haystack.components.retrievers.in_memory.embedding_retriever import InMemoryEmbeddingRetriever
+from haystack.components.retrievers.sentence_window_retrieval import SentenceWindowRetrieval
 
-__all__ = ["FilterRetriever", "InMemoryEmbeddingRetriever", "InMemoryBM25Retriever"]
+__all__ = ["FilterRetriever", "InMemoryEmbeddingRetriever", "InMemoryBM25Retriever", "SentenceWindowRetrieval"]
