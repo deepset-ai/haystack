0c9dc008f05c11b3741a44c58c041773ff94bf50
David S. Batista <dsbatista@gmail.com>
2024-07-22 15:13:46 +0200

    fix: improve context relevancy metric (#7964)
    * fixing tests
    
    * fixing tests
    
    * updating tests
    
    * updating tests
    
    * updating docstring
    
    * adding release notes
    
    * making the insufficient information more robust
    
    * updating docstring and release notes
    
    * empty list instead of informative string
    
    * Update haystack/components/evaluators/context_relevance.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * Update haystack/components/evaluators/context_relevance.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * fixing tests
    
    * Update haystack/components/evaluators/context_relevance.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * reverting commit
    
    * reverting again commit
    
    * fixing docstrings
    
    * removing deprecation warning
    
    * removing warning import
    
    ---------
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>

--- test/components/evaluators/test_context_relevance_evaluator.py
+++ test/components/evaluators/test_context_relevance_evaluator.py
@@ -20,24 +20,19 @@ class TestContextRelevanceEvaluator:
         assert component.api == "openai"
         assert component.generator.client.api_key == "test-api-key"
         assert component.instructions == (
-            "Your task is to judge how relevant the provided context is for answering a question. "
-            "First, please extract statements from the provided context. "
-            "Second, calculate a relevance score for each statement in the context. "
-            "The score is 1 if the statement is relevant to answer the question or 0 if it is not relevant. "
-            "Each statement should be scored individually."
+            "Please extract only sentences from the provided context which are absolutely relevant and "
+            "required to answer the following question. If no relevant sentences are found, or if you "
+            "believe the question cannot be answered from the given context, return an empty list, example: []"
         )
         assert component.inputs == [("questions", List[str]), ("contexts", List[List[str]])]
-        assert component.outputs == ["statements", "statement_scores"]
+        assert component.outputs == ["relevant_statements"]
         assert component.examples == [
             {
                 "inputs": {
                     "questions": "What is the capital of Germany?",
-                    "contexts": ["Berlin is the capital of Germany and was founded in 1244."],
+                    "contexts": ["Berlin is the capital of Germany. Berlin and was founded in 1244."],
                 },
-                "outputs": {
-                    "statements": ["Berlin is the capital of Germany.", "Berlin was founded in 1244."],
-                    "statement_scores": [1, 0],
-                },
+                "outputs": {"relevant_statements": ["Berlin is the capital of Germany."]},
             },
             {
                 "inputs": {
@@ -48,19 +43,11 @@ class TestContextRelevanceEvaluator:
                         "Madrid is the capital of Spain.",
                     ],
                 },
-                "outputs": {
-                    "statements": [
-                        "Berlin is the capital of Germany.",
-                        "Berlin was founded in 1244.",
-                        "Europe is a continent with 44 countries.",
-                        "Madrid is the capital of Spain.",
-                    ],
-                    "statement_scores": [0, 0, 0, 0],
-                },
+                "outputs": {"relevant_statements": []},
             },
             {
                 "inputs": {"questions": "What is the capital of Italy?", "contexts": ["Rome is the capital of Italy."]},
-                "outputs": {"statements": ["Rome is the capital of Italy."], "statement_scores": [1]},
+                "outputs": {"relevant_statements": ["Rome is the capital of Italy."]},
             },
         ]
 
@@ -133,9 +120,9 @@ class TestContextRelevanceEvaluator:
 
         def generator_run(self, *args, **kwargs):
             if "Football" in kwargs["prompt"]:
-                return {"replies": ['{"statements": ["a", "b"], "statement_scores": [1, 0]}']}
+                return {"replies": ['{"relevant_statements": ["a", "b"], "score": 1}']}
             else:
-                return {"replies": ['{"statements": ["c", "d"], "statement_scores": [1, 1]}']}
+                return {"replies": ['{"relevant_statements": [], "score": 0}']}
 
         monkeypatch.setattr("haystack.components.generators.openai.OpenAIGenerator.run", generator_run)
 
@@ -148,20 +135,19 @@ class TestContextRelevanceEvaluator:
                 "Messi, drawing a followership of more than 4 billion people."
             ],
             [
-                "Python, created by Guido van Rossum in the late 1980s, is a high-level general-purpose programming "
-                "language. Its design philosophy emphasizes code readability, and its language constructs aim to help "
+                "Python is design philosophy emphasizes code readability, and its language constructs aim to help "
                 "programmers write clear, logical code for both small and large-scale software projects."
             ],
         ]
         results = component.run(questions=questions, contexts=contexts)
+
+        print(results)
+
         assert results == {
-            "individual_scores": [0.5, 1],
-            "results": [
-                {"score": 0.5, "statement_scores": [1, 0], "statements": ["a", "b"]},
-                {"score": 1, "statement_scores": [1, 1], "statements": ["c", "d"]},
-            ],
-            "score": 0.75,
+            "results": [{"score": 1, "relevant_statements": ["a", "b"]}, {"score": 0, "relevant_statements": []}],
+            "score": 0.5,
             "meta": None,
+            "individual_scores": [1, 0],
         }
 
     def test_run_no_statements_extracted(self, monkeypatch):
@@ -170,9 +156,9 @@ class TestContextRelevanceEvaluator:
 
         def generator_run(self, *args, **kwargs):
             if "Football" in kwargs["prompt"]:
-                return {"replies": ['{"statements": ["a", "b"], "statement_scores": [1, 0]}']}
+                return {"replies": ['{"relevant_statements": ["a", "b"], "score": 1}']}
             else:
-                return {"replies": ['{"statements": [], "statement_scores": []}']}
+                return {"replies": ['{"relevant_statements": [], "score": 0}']}
 
         monkeypatch.setattr("haystack.components.generators.openai.OpenAIGenerator.run", generator_run)
 
@@ -188,13 +174,10 @@ class TestContextRelevanceEvaluator:
         ]
         results = component.run(questions=questions, contexts=contexts)
         assert results == {
-            "individual_scores": [0.5, 0],
-            "results": [
-                {"score": 0.5, "statement_scores": [1, 0], "statements": ["a", "b"]},
-                {"score": 0, "statement_scores": [], "statements": []},
-            ],
-            "score": 0.25,
+            "results": [{"score": 1, "relevant_statements": ["a", "b"]}, {"score": 0, "relevant_statements": []}],
+            "score": 0.5,
             "meta": None,
+            "individual_scores": [1, 0],
         }
 
     def test_run_missing_parameters(self, monkeypatch):
@@ -211,7 +194,7 @@ class TestContextRelevanceEvaluator:
             if "Python" in kwargs["prompt"]:
                 raise Exception("OpenAI API request failed.")
             else:
-                return {"replies": ['{"statements": ["c", "d"], "statement_scores": [1, 1]}']}
+                return {"replies": ['{"relevant_statements": ["c", "d"], "score": 1}']}
 
         monkeypatch.setattr("haystack.components.generators.openai.OpenAIGenerator.run", generator_run)
 
@@ -232,14 +215,8 @@ class TestContextRelevanceEvaluator:
         results = component.run(questions=questions, contexts=contexts)
 
         assert math.isnan(results["score"])
-
-        assert results["individual_scores"][0] == 1.0
-        assert math.isnan(results["individual_scores"][1])
-
-        assert results["results"][0] == {"statements": ["c", "d"], "statement_scores": [1, 1], "score": 1.0}
-
-        assert results["results"][1]["statements"] == []
-        assert results["results"][1]["statement_scores"] == []
+        assert results["results"][0] == {"relevant_statements": ["c", "d"], "score": 1}
+        assert results["results"][1]["relevant_statements"] == []
         assert math.isnan(results["results"][1]["score"])
 
     @pytest.mark.skipif(
@@ -254,39 +231,12 @@ class TestContextRelevanceEvaluator:
         evaluator = ContextRelevanceEvaluator()
         result = evaluator.run(questions=questions, contexts=contexts)
 
-        required_fields = {"individual_scores", "results", "score"}
+        required_fields = {"results"}
         assert all(field in result for field in required_fields)
-        nested_required_fields = {"score", "statement_scores", "statements"}
+        nested_required_fields = {"score", "relevant_statements"}
         assert all(field in result["results"][0] for field in nested_required_fields)
 
         assert "meta" in result
         assert "prompt_tokens" in result["meta"][0]["usage"]
         assert "completion_tokens" in result["meta"][0]["usage"]
         assert "total_tokens" in result["meta"][0]["usage"]
-
-    @pytest.mark.skipif(
-        not os.environ.get("OPENAI_API_KEY", None),
-        reason="Export an env var called OPENAI_API_KEY containing the OpenAI API key to run this test.",
-    )
-    @pytest.mark.integration
-    def test_all_statements_are_scored(self):
-        from haystack.components.evaluators import ContextRelevanceEvaluator
-
-        questions = ["Who created the Python language?"]
-        contexts = [
-            [
-                "Python, created by Guido van Rossum in the late 1980s, is a high-level general-purpose programming "
-                "language. Its design philosophy emphasizes code readability, and its language constructs aim to help "
-                "programmers write clear, logical code for both small and large-scale software projects.",
-                "Java is a high-level, class-based, object-oriented programming language which allows you to write once, "
-                "run anywhere, meaning that compiled Java code can run on all platforms that support Java without the "
-                "need for recompilation.",
-                "Scala is a high-level, statically typed programming language.",
-            ]
-        ]
-
-        evaluator = ContextRelevanceEvaluator()
-        result = evaluator.run(questions=questions, contexts=contexts)
-
-        assert len(result["results"][0]["statements"]) == 4
-        assert len(result["results"][0]["statement_scores"]) == 4
