faeeb13863daacb500f33b404a496792ed044b97
David S. Batista <dsbatista@gmail.com>
2024-08-08 14:07:38 +0200

    wip: refactoring and cleaning

--- haystack/components/builders/hierarchical_doc_builder.py
+++ haystack/components/builders/hierarchical_doc_builder.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal
 
 from haystack import Document, component
 from haystack.components.preprocessors import DocumentSplitter
@@ -13,15 +13,30 @@ class HierarchicalDocumentBuilder:
     such that the smaller blocks are children of the parent-larger blocks.
     """
 
-    def __init__(self, block_sizes: List[int]):
+    def __init__(
+        self,
+        block_sizes: List[int],
+        split_overlap: int = 0,
+        split_by: Literal["word", "sentence", "page", "passage"] = "word",
+    ):
+        """
+        Initialize HierarchicalDocumentBuilder.
+
+        :param block_sizes: List of block sizes to split the document into. The blocks are split in descending order.
+        :param split_overlap: The number of overlapping units for each split.
+        :param split_by: The unit for splitting your documents.
+        """
+
         if list(set(block_sizes)) != block_sizes:
             raise ValueError("block_sizes must not contain duplicates")
         self.block_sizes = sorted(set(block_sizes), reverse=True)
+        self.split_overlap = split_overlap
+        self.split_by = split_by
 
     @component.output_types(documents=List[HierarchicalDocument])
     def run(self, documents: List[Document]):
         """
-        Build a hierarchical document structure from a list of documents.
+        Builds a hierarchical document structure for each document in a list of documents.
 
         :param documents: List of Documents to split into hierarchical blocks.
         :return: List of HierarchicalDocument
@@ -31,18 +46,21 @@ class HierarchicalDocumentBuilder:
             hierarchical_docs.extend(self.build_hierarchy_from_doc(doc))
         return hierarchical_docs
 
-    @staticmethod
-    def _split_doc(doc: Document, block_size: int) -> List[HierarchicalDocument]:
-        """
-        Split a document into multiple documents with a fixed block size.
-        """
-        splitter = DocumentSplitter(split_length=block_size, split_overlap=0, split_by="word")
+    def _split_doc(self, doc: Document, block_size: int):
+        splitter = DocumentSplitter(split_length=block_size, split_overlap=self.split_overlap, split_by=self.split_by)
         split_docs = splitter.run([doc])
         return split_docs["documents"]
 
     def build_hierarchy_from_doc(self, document: Document) -> List[HierarchicalDocument]:
         """
         Build a hierarchical tree document structure from a single document.
+
+        Given a document, this function splits the document into hierarchical blocks of different sizes represented
+        as HierarchicalDocument objects
+
+        :param document: Document to split into hierarchical blocks.
+        :return:
+            List of HierarchicalDocument
         """
 
         root = HierarchicalDocument(document)
