9d54cf6f1bc02f2afb415fd44040697cf31db13c
David S. Batista <dsbatista@gmail.com>
2024-08-09 10:37:06 +0200

    adding tests

--- test/dataclasses/test_hierarchical_document.py
+++ test/dataclasses/test_hierarchical_document.py
@@ -8,3 +8,15 @@ def test_init():
     assert doc.children_ids == []
     assert doc.level == 0
     assert doc.block_size == 0
+
+
+def test_repr():
+    doc = HierarchicalDocument(Document(content="test text"))
+    assert "HierarchicalDocument" in doc.__repr__()
+    assert "content: 'test text'" in doc.__repr__()
+    assert "children: []" in doc.__repr__()
+    assert "level: 0" in doc.__repr__()
+    assert "block_size: 0" in doc.__repr__()
+    assert "parent_id: None" in doc.__repr__()
+    doc.content = "a" * 10
+    assert "content: ' " in doc.__repr__()
