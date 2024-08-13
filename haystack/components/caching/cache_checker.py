1b966b3d0b9b8ed9c118d30738878873d9b426a5
anakin87 <stefanofiorucci@gmail.com>
2024-08-13 10:51:30 +0200

    unused import

--- haystack/components/caching/cache_checker.py
+++ haystack/components/caching/cache_checker.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Any, Dict, List, Type, cast
+from typing import Any, Dict, List
 
 from haystack import DeserializationError, Document, component, default_from_dict, default_to_dict, logging
 from haystack.core.serialization import import_class_by_name
