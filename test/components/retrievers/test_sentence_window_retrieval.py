13586a3a4df0852eb85d6d25d6d26903001da11e
David S. Batista <dsbatista@gmail.com>
2024-08-27 15:38:28 +0200

    removing deprecated SentenceWindowRetrieval

--- test/components/retrievers/test_sentence_window_retrieval.py
+++ test/components/retrievers/test_sentence_window_retrieval.py
@@ -1,4 +1,3 @@
-from haystack.components.retrievers.sentence_window_retrieval import SentenceWindowRetrieval
 from haystack.components.retrievers.sentence_window_retriever import SentenceWindowRetriever
 from haystack.document_stores.in_memory import InMemoryDocumentStore
 from unittest.mock import patch
@@ -6,11 +5,11 @@ from unittest.mock import patch
 
 class TestSentenceWindowRetrieval:
     def test_init_default(self):
-        retriever = SentenceWindowRetrieval(InMemoryDocumentStore())
+        retriever = SentenceWindowRetriever(InMemoryDocumentStore())
         assert retriever.window_size == 3
 
     def test_init_calls_parent(self):
         with patch.object(SentenceWindowRetriever, "__init__", return_value=None) as mock_init:
             document_store = InMemoryDocumentStore()
-            retriever = SentenceWindowRetrieval(document_store)
+            retriever = SentenceWindowRetriever(document_store)
             mock_init.assert_called_once_with(document_store)
