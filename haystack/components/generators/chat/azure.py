71fac0f3619d7ead794c331273b457bebc9de023
Ulises M <30765968+lbux@users.noreply.github.com>
2024-09-10 03:29:56 -0700

    feat: Expose default_headers and add kwargs for Azure Client (#8244)
    * default_headers and azure_kwargs added
    
    * update docstrings
    
    * dont forget about chat generator
    
    * Remove azure_kwargs argument
    
    ---------
    
    Co-authored-by: Silvano Cerza <silvanocerza@gmail.com>

--- haystack/components/generators/chat/azure.py
+++ haystack/components/generators/chat/azure.py
@@ -74,6 +74,7 @@ class AzureOpenAIChatGenerator(OpenAIChatGenerator):
         timeout: Optional[float] = None,
         max_retries: Optional[int] = None,
         generation_kwargs: Optional[Dict[str, Any]] = None,
+        default_headers: Optional[Dict[str, str]] = None,
     ):
         """
         Initialize the Azure OpenAI Chat Generator component.
@@ -110,6 +111,7 @@ class AzureOpenAIChatGenerator(OpenAIChatGenerator):
                 Higher values make the model less likely to repeat the token.
             - `logit_bias`: Adds a logit bias to specific tokens. The keys of the dictionary are tokens, and the
                 values are the bias to add to that token.
+        :param default_headers: Default headers to use for the AzureOpenAI client.
         """
         # We intentionally do not call super().__init__ here because we only need to instantiate the client to interact
         # with the API.
@@ -138,6 +140,7 @@ class AzureOpenAIChatGenerator(OpenAIChatGenerator):
         self.model = azure_deployment or "gpt-35-turbo"
         self.timeout = timeout or float(os.environ.get("OPENAI_TIMEOUT", 30.0))
         self.max_retries = max_retries or int(os.environ.get("OPENAI_MAX_RETRIES", 5))
+        self.default_headers = default_headers or {}
 
         self.client = AzureOpenAI(
             api_version=api_version,
@@ -148,6 +151,7 @@ class AzureOpenAIChatGenerator(OpenAIChatGenerator):
             organization=organization,
             timeout=self.timeout,
             max_retries=self.max_retries,
+            default_headers=self.default_headers,
         )
 
     def to_dict(self) -> Dict[str, Any]:
@@ -170,6 +174,7 @@ class AzureOpenAIChatGenerator(OpenAIChatGenerator):
             max_retries=self.max_retries,
             api_key=self.api_key.to_dict() if self.api_key is not None else None,
             azure_ad_token=self.azure_ad_token.to_dict() if self.azure_ad_token is not None else None,
+            default_headers=self.default_headers,
         )
 
     @classmethod
