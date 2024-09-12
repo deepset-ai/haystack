55a2d6050669c19bd5bf24efc9300f0dc393796d
David S. Batista <dsbatista@gmail.com>
2024-09-12 16:34:03 +0200

    Update haystack/components/evaluators/llm_evaluator.py

--- haystack/components/evaluators/llm_evaluator.py
+++ haystack/components/evaluators/llm_evaluator.py
@@ -84,7 +84,7 @@ class LLMEvaluator:
             The API to use for calling an LLM through a Generator.
             Supported APIs: "openai".
         :param api_key:
-            The API key. Optional to support if using a locally hosted model.
+            The API key to be passed to a LLM provider. If using a locally hosted model probably is not needed.
         :param api_params:
             Parameters for an OpenAI API compatible completions call.
 
