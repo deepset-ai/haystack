587301849560e6b221e490df49a0610e802927ff
Sebastian Husch Lee <sjrl423@gmail.com>
2024-09-17 13:47:44 +0200

    Undo changes

--- test/components/joiners/test_answer_joiner.py
+++ test/components/joiners/test_answer_joiner.py
@@ -1,13 +1,17 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
+
 import pytest
 
 from haystack.components.builders import AnswerBuilder
 
 from haystack import Document, Pipeline
 from haystack.dataclasses.answer import ExtractedAnswer, GeneratedAnswer, ExtractedTableAnswer
+from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.components.joiners.answer_joiner import AnswerJoiner, JoinMode
+from haystack.dataclasses import ChatMessage
 
 
 class TestAnswerJoiner:
@@ -103,22 +107,33 @@ class TestAnswerJoiner:
         with pytest.raises(ValueError):
             AnswerJoiner(join_mode=unsupported_mode)
 
+    @pytest.mark.skipif(not os.environ.get("OPENAI_API_KEY", ""), reason="Needs OPENAI_API_KEY to run this test.")
     @pytest.mark.integration
     def test_with_pipeline(self):
         query = "What's Natural Language Processing?"
+        messages = [
+            ChatMessage.from_system("You are a helpful, respectful and honest assistant. Be super concise."),
+            ChatMessage.from_user(query),
+        ]
 
         pipe = Pipeline()
+        pipe.add_component("gpt-4o", OpenAIChatGenerator(model="gpt-4o"))
+        pipe.add_component("llama", OpenAIChatGenerator(model="gpt-3.5-turbo"))
         pipe.add_component("aba", AnswerBuilder())
         pipe.add_component("abb", AnswerBuilder())
         pipe.add_component("joiner", AnswerJoiner())
 
+        pipe.connect("gpt-4o.replies", "aba")
+        pipe.connect("llama.replies", "abb")
         pipe.connect("aba.answers", "joiner")
         pipe.connect("abb.answers", "joiner")
 
         results = pipe.run(
             data={
-                "aba": {"query": query, "replies": ["This is a test answer"]},
-                "abb": {"query": query, "replies": ["This is a second test answer"]},
+                "gpt-4o": {"messages": messages},
+                "llama": {"messages": messages},
+                "aba": {"query": query},
+                "abb": {"query": query},
             }
         )
 
