18249844a296cad1c724cadd4d45a1bd7095a8bd
Amna Mubashar <amnahkhan.ak@gmail.com>
2024-09-26 17:01:35 +0200

    Expose raise_on_invalid_filter_syntax in haystack.utils (#8413)

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -7,7 +7,7 @@ from .callable_serialization import deserialize_callable, serialize_callable
 from .device import ComponentDevice, Device, DeviceMap, DeviceType
 from .docstore_deserialization import deserialize_document_store_in_init_params_inplace
 from .expit import expit
-from .filters import document_matches_filter
+from .filters import document_matches_filter, raise_on_invalid_filter_syntax
 from .jinja2_extensions import Jinja2TimeExtension
 from .jupyter import is_in_jupyter
 from .requests_utils import request_with_retry
@@ -22,6 +22,7 @@ __all__ = [
     "DeviceType",
     "expit",
     "document_matches_filter",
+    "raise_on_invalid_filter_syntax",
     "is_in_jupyter",
     "request_with_retry",
     "serialize_callable",
