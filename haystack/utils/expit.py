7d980611a68c58fb15e542d5eb9ea2a23a7528f9
Ajit <ajit.97singh@gmail.com>
2024-10-03 10:37:33 +0530

    edited expit function to support both float as well as list (this case was giving error CI)

--- haystack/utils/expit.py
+++ haystack/utils/expit.py
@@ -7,4 +7,5 @@ from math import exp
 
 def expit(x: float) -> float:
     """Compute logistic sigmoid function. Maps input values to a range between 0 and 1"""
-    return 1 / (1 + exp(-x))
+    sigmoid = lambda v: 1 / (1 + exp(-v))
+    return list(map(sigmoid, x)) if isinstance(x, list) else sigmoid(x)
