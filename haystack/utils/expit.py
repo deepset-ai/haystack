92dbe67e6983ad9a9ae50dba56bce917be971826
Ajit <ajit.97singh@gmail.com>
2024-10-03 10:44:31 +0530

    revert code , numpy can't be removed here

--- haystack/utils/expit.py
+++ haystack/utils/expit.py
@@ -2,10 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from math import exp
+import numpy as np
 
 
 def expit(x: float) -> float:
     """Compute logistic sigmoid function. Maps input values to a range between 0 and 1"""
-    sigmoid = lambda v: 1 / (1 + exp(-v))
-    return list(map(sigmoid, x)) if isinstance(x, list) else sigmoid(x)
+    return 1 / (1 + np.exp(-x))
