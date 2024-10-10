145c852f2545423ea1f4a81c73e39199fa83722a
anakin87 <stefanofiorucci@gmail.com>
2024-10-10 16:18:56 +0200

    more tests

--- test/tracing/test_logging_tracer.py
+++ test/tracing/test_logging_tracer.py
@@ -1,13 +1,10 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-import functools
-import json
-from typing import List, Dict, Optional
+
+from typing import Optional
 import logging
 
-
-import pytest
 
 from haystack.tracing.logging_tracer import LoggingTracer
 from haystack import component, Pipeline
@@ -21,10 +18,20 @@ class Hello:
         return {"output": f"Hello, {word}!"}
 
 
+@component
+class FailingComponent:
+    @component.output_types(output=str)
+    def run(self, word: Optional[str]):
+        raise Exception("Failing component")
+
+
 class TestLoggingTracer:
     def test_init(self) -> None:
         tracer = LoggingTracer()
-        assert tracer
+        assert tracer.tags_color_strings == {}
+
+        tracer = LoggingTracer(tags_color_strings={"tag_name": "color_string"})
+        assert tracer.tags_color_strings == {"tag_name": "color_string"}
 
     def test_logging_tracer(self, caplog) -> None:
         tracer = LoggingTracer()
@@ -59,6 +66,16 @@ class TestLoggingTracer:
         assert caplog.records[1].tag_name == "key"
         assert caplog.records[1].tag_value == {"a": 1, "b": [2, 3, 4]}
 
+    def test_apply_color_strings(self, caplog) -> None:
+        tracer = LoggingTracer(tags_color_strings={"key": "color_string"})
+
+        caplog.set_level(logging.DEBUG)
+
+        with tracer.trace("test") as span:
+            span.set_tag("key", "value")
+
+        assert "color_string" in caplog.text
+
     def test_logging_pipeline(self, caplog) -> None:
         pipeline = Pipeline()
         pipeline.add_component("hello", Hello())
@@ -82,3 +99,54 @@ class TestLoggingTracer:
         tags_records = [record for record in records if hasattr(record, "tag_name")]
         assert any(record.tag_name == "haystack.component.name" for record in tags_records)
         assert any(record.tag_value == "hello" for record in tags_records)
+
+    def test_logging_pipeline_with_content_tracing(self, caplog) -> None:
+        pipeline = Pipeline()
+        pipeline.add_component("hello", Hello())
+
+        tracing.tracer.is_content_tracing_enabled = True
+        tracing.enable_tracing(LoggingTracer())
+        caplog.set_level(logging.DEBUG)
+
+        pipeline.run(data={"word": "world"})
+        records = caplog.records
+
+        tags_records = [record for record in records if hasattr(record, "tag_name")]
+
+        input_tag_value = [
+            record.tag_value for record in tags_records if record.tag_name == "haystack.component.input"
+        ][0]
+        assert input_tag_value == {"word": "world"}
+
+        output_tag_value = [
+            record.tag_value for record in tags_records if record.tag_name == "haystack.component.output"
+        ][0]
+        assert output_tag_value == {"output": "Hello, world!"}
+
+    def test_logging_pipeline_on_failure(self, caplog) -> None:
+        """
+        Test that the LoggingTracer also logs events when a component fails.
+        """
+        pipeline = Pipeline()
+        pipeline.add_component("failing_component", FailingComponent())
+
+        tracing.enable_tracing(LoggingTracer())
+        caplog.set_level(logging.DEBUG)
+
+        try:
+            pipeline.run(data={"word": "world"})
+        except:
+            pass
+
+        records = caplog.records
+
+        assert any(
+            record.operation_name == "haystack.component.run" for record in records if hasattr(record, "operation_name")
+        )
+        assert any(
+            record.operation_name == "haystack.pipeline.run" for record in records if hasattr(record, "operation_name")
+        )
+
+        tags_records = [record for record in records if hasattr(record, "tag_name")]
+        assert any(record.tag_name == "haystack.component.name" for record in tags_records)
+        assert any(record.tag_value == "failing_component" for record in tags_records)
