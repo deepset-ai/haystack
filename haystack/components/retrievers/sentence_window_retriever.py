638f9f0b09e158a50f6679837bd01c631d32eb3e
David S. Batista <dsbatista@gmail.com>
2024-10-21 12:49:13 +0200

    reverting docstring example

--- haystack/components/retrievers/sentence_window_retriever.py
+++ haystack/components/retrievers/sentence_window_retriever.py
@@ -54,10 +54,10 @@ class SentenceWindowRetriever:
 
     rag = Pipeline()
     rag.add_component("bm25_retriever", InMemoryBM25Retriever(doc_store, top_k=1))
-    rag.add_component("sentence_window_retriever", SentenceWindowRetriever(document_store=doc_store))
+    rag.add_component("sentence_window_retriever", SentenceWindowRetriever(document_store=doc_store, window_size=2))
     rag.connect("bm25_retriever", "sentence_window_retriever")
 
-    rag.run({'bm25_retriever': {"query":"third"}, 'sentence_window_retriever': {'window_size': 2}})
+    rag.run({'bm25_retriever': {"query":"third"}})
 
     >> {'sentence_window_retriever': {'context_windows': ['some words. There is a second sentence.
     >> And there is also a third sentence. It also contains a fourth sentence. And a fifth sentence. And a sixth
