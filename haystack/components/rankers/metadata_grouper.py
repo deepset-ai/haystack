e47b4ede32256cb93455b699818855ac41e3e289
David S. Batista <dsbatista@gmail.com>
2024-11-11 11:44:13 +0100

    handling too-many-arguments

--- haystack/components/rankers/metadata_grouper.py
+++ haystack/components/rankers/metadata_grouper.py
@@ -162,7 +162,7 @@ class MetaDataGrouper:
         return result_docs
 
     @component.output_types(documents=List[Document])
-    def run(self, documents: List[Document]) -> Dict[str, Any]:
+    def run(self, documents: List[Document]) -> Dict[str, Any]:  # disable = too-many-positional-arguments
         """
         Groups the provided list of documents based on the `group_by` parameter and optionally the `subgroup_by`.
 
