65722626c44ce1c59a86ca614153f828b4d3ff39
David S. Batista <dsbatista@gmail.com>
2024-11-11 14:44:22 +0100

    fixing imports

--- haystack/components/rankers/__init__.py
+++ haystack/components/rankers/__init__.py
@@ -4,14 +4,14 @@
 
 from haystack.components.rankers.lost_in_the_middle import LostInTheMiddleRanker
 from haystack.components.rankers.meta_field import MetaFieldRanker
-from haystack.components.rankers.metadata_grouper import MetaFieldSorter
+from haystack.components.rankers.meta_field_grouper_ranker import MetaFieldGroupingRanker
 from haystack.components.rankers.sentence_transformers_diversity import SentenceTransformersDiversityRanker
 from haystack.components.rankers.transformers_similarity import TransformersSimilarityRanker
 
 __all__ = [
     "LostInTheMiddleRanker",
     "MetaFieldRanker",
-    "MetaFieldSorter",
+    "MetaFieldGroupingRanker",
     "SentenceTransformersDiversityRanker",
     "TransformersSimilarityRanker",
 ]
