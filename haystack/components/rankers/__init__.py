e5a80722c22c59eb99416bf0cd712f6de7cd581a
David S. Batista <dsbatista@gmail.com>
2024-11-12 16:01:53 +0100

    feat: adding metadata grouper component (#8512)
    * initial import
    
    * making tests more readable; adding docstring
    
    * adding release notes
    
    * adding LICENSE header
    
    * Update test/components/rankers/test_metadata_grouper.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * refactoring
    
    * fixing docstring
    
    * fixing types
    
    * test docstrings
    
    * renaming test
    
    * handling too-many-arguments
    
    * liting
    
    * Update haystack/components/rankers/metadata_grouper.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * changing name
    
    * Update haystack/components/rankers/metadata_grouper.py
    
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>
    
    * Update haystack/components/rankers/metadata_grouper.py
    
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>
    
    * assiging value inside function for re-use
    
    * improving docstring
    
    * updating name to MetaFieldGroupingRanker
    
    * adding to pydocs
    
    * fixing imports
    
    * adding output docstring
    
    * Update haystack/components/rankers/meta_field_grouper_ranker.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update haystack/components/rankers/__init__.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update releasenotes/notes/add-metadata-grouper-21ec05fd4a307425.yaml
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update test/components/rankers/test_metadata_grouper.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * update docstring tests
    
    * fixing imports
    
    * rename modules for consistency
    
    * fix pydocs
    
    * simplification + more tests
    
    ---------
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>

--- haystack/components/rankers/__init__.py
+++ haystack/components/rankers/__init__.py
@@ -4,12 +4,14 @@
 
 from haystack.components.rankers.lost_in_the_middle import LostInTheMiddleRanker
 from haystack.components.rankers.meta_field import MetaFieldRanker
+from haystack.components.rankers.meta_field_grouping_ranker import MetaFieldGroupingRanker
 from haystack.components.rankers.sentence_transformers_diversity import SentenceTransformersDiversityRanker
 from haystack.components.rankers.transformers_similarity import TransformersSimilarityRanker
 
 __all__ = [
     "LostInTheMiddleRanker",
     "MetaFieldRanker",
+    "MetaFieldGroupingRanker",
     "SentenceTransformersDiversityRanker",
     "TransformersSimilarityRanker",
 ]
