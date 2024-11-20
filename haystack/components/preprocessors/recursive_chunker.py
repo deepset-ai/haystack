41f5f64c58fe54f739f17ed98bc5396c2310e31b
David S. Batista <dsbatista@gmail.com>
2024-11-20 16:56:11 +0100

    initial import

--- haystack/components/preprocessors/recursive_chunker.py
+++ haystack/components/preprocessors/recursive_chunker.py
@@ -1,12 +1,26 @@
+from typing import List
+
 from haystack import component
 
 
 @component
 class RecursiveChunker:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        chunk_size: int,
+        chunk_overlap: int,
+        separators: List[str],
+        keep_separator: bool = True,
+        is_separator_regex: bool = False,
+    ):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.separators = separators
+        self.keep_separator = keep_separator
+        self.is_separator_regex = is_separator_regex
 
-    def _chunk_text(self, text):
+    @staticmethod
+    def _chunk_text(text):
         # some logic to split text into smaller chunks
         return text
 
