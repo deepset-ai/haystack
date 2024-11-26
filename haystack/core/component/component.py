2f1f04e4dbcc63e2e0138d7c7d2794dc085aa6bb
Michele Pangrazzi <xmikex83@gmail.com>
2024-11-26 11:32:15 +0100

    Remove unused import

--- haystack/core/component/component.py
+++ haystack/core/component/component.py
@@ -71,7 +71,6 @@ method decorated with `@component.input`. This dataclass contains:
 
 import inspect
 import sys
-import warnings
 from collections.abc import Callable
 from contextlib import contextmanager
 from contextvars import ContextVar
