4b941b06544651b0cc4ea788ee7b66fb21c0f519
anakin87 <stefanofiorucci@gmail.com>
2024-12-13 08:46:55 +0100

    draft

--- haystack/dataclasses/chat_message.py
+++ haystack/dataclasses/chat_message.py
@@ -5,16 +5,81 @@
 import warnings
 from dataclasses import asdict, dataclass, field
 from enum import Enum
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union, List, Sequence
 
 
 class ChatRole(str, Enum):
-    """Enumeration representing the roles within a chat."""
+    """
+    Enumeration representing the roles within a chat.
+    """
 
-    ASSISTANT = "assistant"
+    #: The user role. A message from the user contains only text.
     USER = "user"
+
+    #: The system role. A message from the system contains only text.
     SYSTEM = "system"
-    FUNCTION = "function"
+
+    #: The assistant role. A message from the assistant can contain text and Tool calls. It can also store metadata.
+    ASSISTANT = "assistant"
+
+    #: The tool role. A message from a tool contains the result of a Tool invocation.
+    TOOL = "tool"
+
+    @staticmethod
+    def from_str(string: str) -> "ChatRole":
+        """
+        Convert a string to a ChatRole enum.
+        """
+        enum_map = {e.value: e for e in ChatRole}
+        role = enum_map.get(string)
+        if role is None:
+            msg = f"Unknown chat role '{string}'. Supported roles are: {list(enum_map.keys())}"
+            raise ValueError(msg)
+        return role
+
+
+@dataclass
+class ToolCall:
+    """
+    Represents a Tool call prepared by the model, usually contained in an assistant message.
+
+    :param id: The ID of the Tool call.
+    :param tool_name: The name of the Tool to call.
+    :param arguments: The arguments to call the Tool with.
+    """
+
+    tool_name: str
+    arguments: Dict[str, Any]
+    id: Optional[str] = None  # noqa: A003
+
+
+@dataclass
+class ToolCallResult:
+    """
+    Represents the result of a Tool invocation.
+
+    :param result: The result of the Tool invocation.
+    :param origin: The Tool call that produced this result.
+    :param error: Whether the Tool invocation resulted in an error.
+    """
+
+    result: str
+    origin: ToolCall
+    error: bool
+
+
+@dataclass
+class TextContent:
+    """
+    The textual content of a chat message.
+
+    :param text: The text content of the message.
+    """
+
+    text: str
+
+
+ChatMessageContentT = Union[TextContent, ToolCall, ToolCallResult]
 
 
 @dataclass
@@ -22,26 +87,88 @@ class ChatMessage:
     """
     Represents a message in a LLM chat conversation.
 
-    :param content: The text content of the message.
-    :param role: The role of the entity sending the message.
-    :param name: The name of the function being called (only applicable for role FUNCTION).
-    :param meta: Additional metadata associated with the message.
+    Use the `from_assistant`, `from_user`, `from_system`, and `from_tool` class methods to create a ChatMessage.
     """
 
-    content: str
-    role: ChatRole
-    name: Optional[str]
-    meta: Dict[str, Any] = field(default_factory=dict, hash=False)
+    _role: ChatRole
+    _content: Sequence[ChatMessageContentT]
+    _meta: Dict[str, Any] = field(default_factory=dict, hash=False)
+
+    def __len__(self):
+        return len(self._content)
+
+    @property
+    def role(self) -> ChatRole:
+        """
+        Returns the role of the entity sending the message.
+        """
+        return self._role
+
+    @property
+    def meta(self) -> Dict[str, Any]:
+        """
+        Returns the metadata associated with the message.
+        """
+        return self._meta
+
+    @property
+    def texts(self) -> List[str]:
+        """
+        Returns the list of all texts contained in the message.
+        """
+        return [content.text for content in self._content if isinstance(content, TextContent)]
 
     @property
     def text(self) -> Optional[str]:
         """
-        Returns the textual content of the message.
+        Returns the first text contained in the message.
         """
-        # Currently, this property mirrors the `content` attribute. This will change in 2.9.0.
-        # The current actual return type is str. We are using Optional[str] to be ready for 2.9.0,
-        # when None will be a valid value for `text`.
-        return object.__getattribute__(self, "content")
+        if texts := self.texts:
+            return texts[0]
+        return None
+
+    @property
+    def tool_calls(self) -> List[ToolCall]:
+        """
+        Returns the list of all Tool calls contained in the message.
+        """
+        return [content for content in self._content if isinstance(content, ToolCall)]
+
+    @property
+    def tool_call(self) -> Optional[ToolCall]:
+        """
+        Returns the first Tool call contained in the message.
+        """
+        if tool_calls := self.tool_calls:
+            return tool_calls[0]
+        return None
+
+    @property
+    def tool_call_results(self) -> List[ToolCallResult]:
+        """
+        Returns the list of all Tool call results contained in the message.
+        """
+        return [content for content in self._content if isinstance(content, ToolCallResult)]
+
+    @property
+    def tool_call_result(self) -> Optional[ToolCallResult]:
+        """
+        Returns the first Tool call result contained in the message.
+        """
+        if tool_call_results := self.tool_call_results:
+            return tool_call_results[0]
+        return None
+
+    def is_from(self, role: Union[ChatRole, str]) -> bool:
+        """
+        Check if the message is from a specific role.
+
+        :param role: The role to check against.
+        :returns: True if the message is from the specified role, False otherwise.
+        """
+        if isinstance(role, str):
+            role = ChatRole.from_str(role)
+        return self._role == role
 
     def __getattribute__(self, name):
         # this method is reimplemented to warn about the deprecation of the `content` attribute
@@ -53,56 +180,69 @@ class ChatMessage:
             warnings.warn(msg, DeprecationWarning)
         return object.__getattribute__(self, name)
 
-    def is_from(self, role: ChatRole) -> bool:
-        """
-        Check if the message is from a specific role.
-
-        :param role: The role to check against.
-        :returns: True if the message is from the specified role, False otherwise.
-        """
-        return self.role == role
-
     @classmethod
-    def from_assistant(cls, content: str, meta: Optional[Dict[str, Any]] = None) -> "ChatMessage":
-        """
-        Create a message from the assistant.
-
-        :param content: The text content of the message.
-        :param meta: Additional metadata associated with the message.
-        :returns: A new ChatMessage instance.
-        """
-        return cls(content, ChatRole.ASSISTANT, None, meta or {})
-
-    @classmethod
-    def from_user(cls, content: str) -> "ChatMessage":
+    def from_user(cls, text: str, meta: Optional[Dict[str, Any]] = None) -> "ChatMessage":
         """
         Create a message from the user.
 
-        :param content: The text content of the message.
+        :param text: The text content of the message.
+        :param meta: Additional metadata associated with the message.
         :returns: A new ChatMessage instance.
         """
-        return cls(content, ChatRole.USER, None)
+        return cls(_role=ChatRole.USER, _content=[TextContent(text=text)], _meta=meta or {})
 
     @classmethod
-    def from_system(cls, content: str) -> "ChatMessage":
+    def from_system(cls, text: str, meta: Optional[Dict[str, Any]] = None) -> "ChatMessage":
         """
         Create a message from the system.
 
-        :param content: The text content of the message.
+        :param text: The text content of the message.
+        :param meta: Additional metadata associated with the message.
         :returns: A new ChatMessage instance.
         """
-        return cls(content, ChatRole.SYSTEM, None)
+        return cls(_role=ChatRole.SYSTEM, _content=[TextContent(text=text)], _meta=meta or {})
 
     @classmethod
-    def from_function(cls, content: str, name: str) -> "ChatMessage":
+    def from_assistant(
+        cls,
+        text: Optional[str] = None,
+        meta: Optional[Dict[str, Any]] = None,
+        tool_calls: Optional[List[ToolCall]] = None,
+    ) -> "ChatMessage":
         """
-        Create a message from a function call.
+        Create a message from the assistant.
 
-        :param content: The text content of the message.
-        :param name: The name of the function being called.
+        :param text: The text content of the message.
+        :param meta: Additional metadata associated with the message.
+        :param tool_calls: The Tool calls to include in the message.
         :returns: A new ChatMessage instance.
         """
-        return cls(content, ChatRole.FUNCTION, name)
+        content: List[ChatMessageContentT] = []
+        if text is not None:
+            content.append(TextContent(text=text))
+        if tool_calls:
+            content.extend(tool_calls)
+
+        return cls(_role=ChatRole.ASSISTANT, _content=content, _meta=meta or {})
+
+    @classmethod
+    def from_tool(
+        cls, tool_result: str, origin: ToolCall, error: bool = False, meta: Optional[Dict[str, Any]] = None
+    ) -> "ChatMessage":
+        """
+        Create a message from a Tool.
+
+        :param tool_result: The result of the Tool invocation.
+        :param origin: The Tool call that produced this result.
+        :param error: Whether the Tool invocation resulted in an error.
+        :param meta: Additional metadata associated with the message.
+        :returns: A new ChatMessage instance.
+        """
+        return cls(
+            _role=ChatRole.TOOL,
+            _content=[ToolCallResult(result=tool_result, origin=origin, error=error)],
+            _meta=meta or {},
+        )
 
     def to_dict(self) -> Dict[str, Any]:
         """
@@ -111,10 +251,23 @@ class ChatMessage:
         :returns:
             Serialized version of the object.
         """
-        data = asdict(self)
-        data["role"] = self.role.value
+        serialized: Dict[str, Any] = {}
+        serialized["_role"] = self._role.value
+        serialized["_meta"] = self._meta
 
-        return data
+        content: List[Dict[str, Any]] = []
+        for part in self._content:
+            if isinstance(part, TextContent):
+                content.append({"text": part.text})
+            elif isinstance(part, ToolCall):
+                content.append({"tool_call": asdict(part)})
+            elif isinstance(part, ToolCallResult):
+                content.append({"tool_call_result": asdict(part)})
+            else:
+                raise TypeError(f"Unsupported type in ChatMessage content: `{type(part).__name__}` for `{part}`.")
+
+        serialized["_content"] = content
+        return serialized
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "ChatMessage":
@@ -126,6 +279,24 @@ class ChatMessage:
         :returns:
             The created object.
         """
-        data["role"] = ChatRole(data["role"])
+        data["_role"] = ChatRole(data["_role"])
+
+        content: List[ChatMessageContentT] = []
+
+        for part in data["_content"]:
+            if "text" in part:
+                content.append(TextContent(text=part["text"]))
+            elif "tool_call" in part:
+                content.append(ToolCall(**part["tool_call"]))
+            elif "tool_call_result" in part:
+                result = part["tool_call_result"]["result"]
+                origin = ToolCall(**part["tool_call_result"]["origin"])
+                error = part["tool_call_result"]["error"]
+                tcr = ToolCallResult(result=result, origin=origin, error=error)
+                content.append(tcr)
+            else:
+                raise ValueError(f"Unsupported content in serialized ChatMessage: `{part}`")
+
+        data["_content"] = content
 
         return cls(**data)
