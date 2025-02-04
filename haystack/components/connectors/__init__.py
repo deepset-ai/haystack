03b453770c1d6e83e1d5486c33f3c222d3a6a54f
Vladimir Blagojevic <dovlex@gmail.com>
2025-02-04 13:51:20 +0100

    Initial OpenAPIConnector

--- haystack/components/connectors/__init__.py
+++ haystack/components/connectors/__init__.py
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from haystack.components.connectors.openapi_service import OpenAPIServiceConnector
+from haystack.components.connectors.openapi import OpenAPIConnector
 
-__all__ = ["OpenAPIServiceConnector"]
+__all__ = ["OpenAPIServiceConnector", "OpenAPIConnector"]
