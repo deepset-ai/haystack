681436b6bba7b0a111a9816e420f5e6330e5d09a
Vladimir Blagojevic <dovlex@gmail.com>
2025-02-04 15:00:43 +0100

    Use haystack logger

--- haystack/components/connectors/openapi.py
+++ haystack/components/connectors/openapi.py
@@ -2,10 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
 from typing import Any, Dict, Optional
 
-from haystack import component, default_from_dict, default_to_dict
+from haystack import component, default_from_dict, default_to_dict, logging
 from haystack.lazy_imports import LazyImport
 from haystack.utils import Secret, deserialize_secrets_inplace
 
