1ae4ca5efc1a366d4d5c465843aad0e66e1a3056
Vladimir Blagojevic <dovlex@gmail.com>
2025-02-04 13:51:20 +0100

    Initial OpenAPIConnector

--- haystack/components/connectors/__init__.py
+++ haystack/components/connectors/__init__.py
@@ -1,3 +1,4 @@
 from haystack.components.connectors.openapi_service import OpenAPIServiceConnector
+from haystack.components.connectors.openapi import OpenAPIConnector
 
-__all__ = ["OpenAPIServiceConnector"]
+__all__ = ["OpenAPIServiceConnector", "OpenAPIConnector"]
