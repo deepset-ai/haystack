57b1cac6b3d1c3fe20d5738ba82ed1197ef7be5d
mathislucka <mathis.lucka@gmail.com>
2025-02-07 16:08:06 +0100

    fix: use asyncio.run

--- test/core/pipeline/test_async_pipeline.py
+++ test/core/pipeline/test_async_pipeline.py
@@ -9,18 +9,13 @@ def test_async_pipeline_reentrance(waiting_component, spying_tracer):
 
     run_data = [{"wait_for": 1}, {"wait_for": 2}]
 
-    async_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(async_loop)
-
     async def run_all():
         # Create concurrent tasks for each pipeline run
         tasks = [pp.run_async(data) for data in run_data]
         await asyncio.gather(*tasks)
 
-    try:
-        async_loop.run_until_complete(run_all())
-        component_spans = [sp for sp in spying_tracer.spans if sp.operation_name == "haystack.component.run_async"]
-        for span in component_spans:
-            assert span.tags["haystack.component.visits"] == 1
-    finally:
-        async_loop.close()
+
+    asyncio.run(run_all())
+    component_spans = [sp for sp in spying_tracer.spans if sp.operation_name == "haystack.component.run_async"]
+    for span in component_spans:
+        assert span.tags["haystack.component.visits"] == 1
