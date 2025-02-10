471de84497853b6cec24a748c581a6a762897b2e
Sebastian Husch Lee <sjrl@users.noreply.github.com>
2025-02-10 18:10:18 +0100

    feat: Add new component CSVDocumentSplitter to recursively split CSV documents (#8815)
    * CSV Document Splitter
    
    * Add license header
    
    * Add newline
    
    * Add to docs
    
    * Add lineterminator
    
    * Updated csv splitter to allow user to specify to split by row, column or both
    
    * Adding more tests
    
    * Column tests
    
    * Some refactoring to remove incorrect dropna call
    
    * Fix
    
    * More complicated test
    
    * Adding more relevant metadata to match whats provided in our other splitters
    
    * value error tests
    
    * Fix mypy
    
    * Docstring updates
    
    * Add skip_blank_lines=False
    
    * Add to dict test
    
    * More from and to dict tests
    
    * Fixes
    
    * Move dict creation outside of for loop

--- haystack/components/preprocessors/__init__.py
+++ haystack/components/preprocessors/__init__.py
@@ -3,9 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .csv_document_cleaner import CSVDocumentCleaner
+from .csv_document_splitter import CSVDocumentSplitter
 from .document_cleaner import DocumentCleaner
 from .document_splitter import DocumentSplitter
 from .recursive_splitter import RecursiveDocumentSplitter
 from .text_cleaner import TextCleaner
 
-__all__ = ["CSVDocumentCleaner", "DocumentCleaner", "DocumentSplitter", "RecursiveDocumentSplitter", "TextCleaner"]
+__all__ = [
+    "CSVDocumentCleaner",
+    "CSVDocumentSplitter",
+    "DocumentCleaner",
+    "DocumentSplitter",
+    "RecursiveDocumentSplitter",
+    "TextCleaner",
+]
