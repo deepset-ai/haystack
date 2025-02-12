88ed301712c38d40fe4ef55c796677daf8a43eec
mathislucka <mathis.lucka@gmail.com>
2025-02-12 12:21:07 +0100

    fix: AsyncPipeline logging  name can't be overwritten (#8845)
    * fix: component_name not name in logging statement
    
    * fix: component_name not name in logging statement
    
    * fix: don't use fstrings in logging
    
    ---------
    
    Co-authored-by: Amna Mubashar <amnahkhan.ak@gmail.com>

--- haystack/core/pipeline/async_pipeline.py
+++ haystack/core/pipeline/async_pipeline.py
@@ -204,7 +204,7 @@ class AsyncPipeline(PipelineBase):
                     parent_span=parent_span,
                 ) as span:
                     span.set_content_tag("haystack.component.input", deepcopy(component_inputs))
-                    logger.info("Running component {name}", name=component_name)
+                    logger.info("Running component {component_name}", component_name=component_name)
 
                     if getattr(instance, "__haystack_supports_async__", False):
                         outputs = await instance.run_async(**component_inputs)  # type: ignore
