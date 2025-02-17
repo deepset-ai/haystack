e9908faabd5170dbc0695e31e87e1393890155ac
Sebastian Husch Lee <sjrl423@gmail.com>
2025-02-17 10:38:28 +0100

    Expand tests

--- test/core/pipeline/test_type_utils.py
+++ test/core/pipeline/test_type_utils.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 from enum import Enum
 from pathlib import Path
-from typing import Any, Dict, List, Literal, Mapping, Optional, Sequence, Set, Tuple, Union
+from typing import Any, Dict, Iterable, List, Literal, Mapping, Optional, Sequence, Set, Tuple, Union
 
 import pytest
 
 from haystack.dataclasses import ByteStream, ChatMessage, Document, GeneratedAnswer
+from haystack.core.component.types import Variadic
 from haystack.core.type_utils import _type_name, _types_are_compatible
 
 
@@ -87,6 +88,7 @@ def test_type_name(type_, repr_):
 primitive_types = [bool, bytes, dict, float, int, list, str, Path]
 class_types = [Class1, Class3]
 container_types = [
+    Iterable[int],
     List[int],
     List[Class1],
     Set[str],
@@ -94,6 +96,7 @@ container_types = [
     Dict[str, Class1],
     Sequence[bool],
     Mapping[str, Dict[str, int]],
+    Variadic[int],
 ]
 nested_container_types = [
     List[Set[Sequence[bool]]],
@@ -109,21 +112,35 @@ extras = [Union[int, str]]
 
 def generate_id(sender, receiver):
     """Helper function to generate readable IDs"""
-    sender_name = str(sender).replace("typing.", "").replace("<class '", "").replace("'>", "")
-    receiver_name = str(receiver).replace("typing.", "").replace("<class '", "").replace("'>", "")
+    sender_name = _type_name(sender)
+    receiver_name = _type_name(receiver)
     return f"{sender_name}-to-{receiver_name}"
 
 
-# Function to generate only valid test cases
-def generate_test_cases():
+def generate_symmetric_test_cases():
     test_cases = []
-
     # Identity: same type should be compatible
-    # Primitives: Optional, Union, Any
     for t in (
         primitive_types + class_types + container_types + literals + haystack_types + nested_container_types + extras
     ):
         test_cases.append(pytest.param(t, t, id=generate_id(t, t)))
+    return test_cases
+
+
+@pytest.mark.parametrize("sender_type, receiver_type", generate_symmetric_test_cases())
+def test_same_types_are_compatible(sender_type, receiver_type):
+    assert _types_are_compatible(sender_type, receiver_type)
+
+
+# TODO Add tests for
+#  - Document subclasses
+def generate_asymmetric_test_cases():
+    test_cases = []
+
+    # Primitives: Optional, Union, Any
+    for t in (
+        primitive_types + class_types + container_types + literals + haystack_types + nested_container_types + extras
+    ):
         test_cases.append(pytest.param(t, Optional[t], id=generate_id(t, Optional[t])))
         test_cases.append(pytest.param(t, Union[t, complex], id=generate_id(t, Union[t, complex])))
         test_cases.append(pytest.param(t, Any, id=generate_id(t, Any)))
@@ -137,19 +154,16 @@ def generate_test_cases():
     # Subclasses:
     # Subclass → Superclass
     test_cases.append(pytest.param(Class3, Class1, id=generate_id(Class3, Class1)))
-
     # Subclass → Union of Superclass and other type
     test_cases.append(pytest.param(Class3, Union[int, Class1], id=generate_id(Class3, Union[int, Class1])))
-
     # List of Class3 → List of Class1
     test_cases.append(pytest.param(List[Class3], List[Class1], id=generate_id(List[Class3], List[Class1])))
-
     # Dict of subclass → Dict of superclass
     test_cases.append(
         pytest.param(Dict[str, Class3], Dict[str, Class1], id=generate_id(Dict[str, Class3], Dict[str, Class1]))
     )
 
-    # Containers: Optional, Any, and Subtype compatibility
+    # Containers: Optional, Union, and Any compatibility
     for container in container_types:
         test_cases.append(pytest.param(container, Optional[container], id=generate_id(container, Optional[container])))
         test_cases.append(
@@ -160,24 +174,19 @@ def generate_test_cases():
     return test_cases
 
 
-@pytest.mark.parametrize("sender_type, receiver_type", generate_test_cases())
-def test_types_are_compatible(sender_type, receiver_type):
+@pytest.mark.parametrize("receiver_type, sender_type", generate_asymmetric_test_cases())
+def test_asymmetric_types_are_compatible(receiver_type, sender_type):
     assert _types_are_compatible(sender_type, receiver_type)
 
 
-def generate_symmetric_test_cases():
-    sym_test_cases = []
-    test_cases = generate_test_cases()
-    for item in test_cases:
-        sender, receiver = item.values
-        sym_test_cases.append(pytest.param(receiver, sender, id=f"sym-{generate_id(receiver, sender)}"))
-    return sym_test_cases
+@pytest.mark.parametrize("receiver_type, sender_type", generate_asymmetric_test_cases())
+def test_asymmetric_types_are_not_compatible_strict(receiver_type, sender_type):
+    assert not _types_are_compatible(receiver_type, sender_type)
 
 
-# TODO Below fails since _types_are_compatible is not symmetric
-# @pytest.mark.parametrize("receiver_type, sender_type", generate_symmetric_test_cases())
-# def test_types_are_symmetric(receiver_type, sender_type):
-#     assert _types_are_compatible(receiver_type, sender_type)
+@pytest.mark.parametrize("receiver_type, sender_type", generate_asymmetric_test_cases())
+def test_asymmetric_types_are_compatible_relaxed(receiver_type, sender_type):
+    assert _types_are_compatible(receiver_type, sender_type)
 
 
 @pytest.mark.parametrize(
@@ -266,16 +275,14 @@ def generate_symmetric_test_cases():
 )
 def test_asymmetric_container_types_are_compatible(sender_type, receiver_type):
     assert _types_are_compatible(sender_type, receiver_type)
+    # TODO Reverse tests and add strict and relaxed tests
 
 
 @pytest.mark.parametrize(
     "sender_type,receiver_type",
     [
         pytest.param(int, bool, id="different-primitives"),
-        pytest.param(Class1, Class2, id="different-classes"),
         pytest.param(Class1, Class3, id="class-to-subclass"),
-        pytest.param(Any, int, id="any-to-primitive"),
-        pytest.param(Any, Class2, id="any-to-class"),
         pytest.param((Optional[str]), str, id="sending-primitive-is-optional"),
         pytest.param((Optional[Class1]), Class1, id="sending-class-is-optional"),
         pytest.param((Optional[List[int]]), (List[int]), id="sending-list-is-optional"),
@@ -283,75 +290,23 @@ def test_asymmetric_container_types_are_compatible(sender_type, receiver_type):
         pytest.param((Union[(int, str, bool)]), (Union[(int, str)]), id="sending-union-is-superset-of-receiver"),
         pytest.param((Union[(int, bool)]), (Union[(int, str)]), id="partially-overlapping-unions-with-primitives"),
         pytest.param((Union[(int, Class1)]), (Union[(int, Class2)]), id="partially-overlapping-unions-with-classes"),
-        pytest.param((List[int]), (List[str]), id="different-lists-of-primitives"),
         pytest.param((List[int]), List, id="list-of-primitive-to-bare-list"),
         pytest.param((List[int]), list, id="list-of-primitive-to-list-object"),
-        pytest.param((List[Class1]), (List[Class2]), id="different-lists-of-classes"),
         pytest.param((List[Class1]), (List[Class3]), id="lists-of-classes-to-subclasses"),
         pytest.param((List[Any]), (List[str]), id="list-of-any-to-list-of-primitives"),
         pytest.param((List[Any]), (List[Class2]), id="list-of-any-to-list-of-classes"),
         pytest.param(
-            (List[Set[Sequence[str]]]), (List[Set[Sequence[bool]]]), id="nested-sequences-of-different-primitives"
-        ),
-        pytest.param(
-            (List[Set[Sequence[str]]]), (Set[List[Sequence[str]]]), id="different-nested-sequences-of-same-primitives"
-        ),
-        pytest.param(
-            (List[Set[Sequence[Class1]]]), (List[Set[Sequence[Class2]]]), id="nested-sequences-of-different-classes"
-        ),
-        pytest.param(
             (List[Set[Sequence[Class1]]]), (List[Set[Sequence[Class3]]]), id="nested-sequences-of-classes-to-subclasses"
-        ),
-        pytest.param(
-            (List[Set[Sequence[Class1]]]), (Set[List[Sequence[Class1]]]), id="different-nested-sequences-of-same-class"
         ),
         pytest.param(
             (List[Set[Sequence[Any]]]),
             (List[Set[Sequence[bool]]]),
             id="nested-list-of-Any-to-nested-list-of-primitives",
         ),
-        pytest.param(
-            (List[Set[Sequence[Any]]]), (List[Set[Sequence[Class2]]]), id="nested-list-of-Any-to-nested-list-of-classes"
-        ),
-        pytest.param((Dict[(str, int)]), (Dict[(int, int)]), id="different-dict-of-primitive-keys"),
-        pytest.param((Dict[(str, int)]), (Dict[(str, bool)]), id="different-dict-of-primitive-values"),
-        pytest.param((Dict[(str, Class1)]), (Dict[(str, Class2)]), id="different-dict-of-class-values"),
         pytest.param((Dict[(str, Class1)]), (Dict[(str, Class3)]), id="different-dict-of-class-to-subclass-values"),
         pytest.param((Dict[(Any, int)]), (Dict[(int, int)]), id="dict-of-Any-keys-to-dict-of-primitives"),
-        pytest.param((Dict[(str, Any)]), (Dict[(int, int)]), id="dict-of-Any-values-to-dict-of-primitives"),
-        pytest.param((Dict[(str, Any)]), (Dict[(int, Class1)]), id="dict-of-Any-values-to-dict-of-classes"),
         pytest.param((Dict[(Any, Any)]), (Dict[(int, int)]), id="dict-of-Any-keys-and-values-to-dict-of-primitives"),
         pytest.param((Dict[(Any, Any)]), (Dict[(int, Class1)]), id="dict-of-Any-keys-and-values-to-dict-of-classes"),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
-            (Mapping[(str, Dict[(str, Dict[(str, int)])])]),
-            id="different-nested-mappings-of-same-primitives",
-        ),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
-            (Dict[(str, Mapping[(str, Dict[(int, int)])])]),
-            id="same-nested-mappings-of-different-primitive-keys",
-        ),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
-            (Dict[(str, Mapping[(int, Dict[(str, int)])])]),
-            id="same-nested-mappings-of-different-higer-level-primitive-keys",
-        ),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
-            (Dict[(str, Mapping[(str, Dict[(str, bool)])])]),
-            id="same-nested-mappings-of-different-primitive-values",
-        ),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, Class1)])])]),
-            (Dict[(str, Mapping[(str, Dict[(str, Class2)])])]),
-            id="same-nested-mappings-of-different-class-values",
-        ),
-        pytest.param(
-            (Dict[(str, Mapping[(str, Dict[(str, Class1)])])]),
-            (Dict[(str, Mapping[(str, Dict[(str, Class2)])])]),
-            id="same-nested-mappings-of-class-to-subclass-values",
-        ),
         pytest.param(
             (Dict[(str, Mapping[(str, Dict[(Any, int)])])]),
             (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
@@ -382,15 +337,79 @@ def test_asymmetric_container_types_are_compatible(sender_type, receiver_type):
             (Dict[(str, Mapping[(str, Dict[(str, Class1)])])]),
             id="nested-mapping-of-Any-keys-and-values-to-nested-mapping-of-classes",
         ),
-        pytest.param((Literal["a", "b", "c"]), (Literal["x", "y"]), id="different-literal-of-same-primitive"),
         pytest.param((Literal["a", "b", "c"]), (Literal["a", "b"]), id="subset-literal"),
-        pytest.param((Literal[Enum1.TEST1]), (Literal[Enum1.TEST2]), id="different-literal-of-same-enum"),
         pytest.param(
             (Tuple[(Optional[Literal["a", "b", "c"]], Union[(Path, Dict[(int, Class1)])])]),
             (Tuple[(Literal["a", "b", "c"], Union[(Path, Dict[(int, Class1)])])]),
             id="deeply-nested-complex-type-is-compatible-but-cannot-be-checked",
         ),
+        pytest.param(
+            (List[Set[Sequence[Any]]]), (List[Set[Sequence[Class2]]]), id="nested-list-of-Any-to-nested-list-of-classes"
+        ),
     ],
 )
-def test_types_are_not_compatible(sender_type, receiver_type):
+def test_types_are_not_compatible_strict(sender_type, receiver_type):
+    assert not _types_are_compatible(sender_type, receiver_type)
+    # TODO Add relaxed version of test
+
+
+@pytest.mark.parametrize(
+    "sender_type,receiver_type",
+    [
+        pytest.param(int, str, id="different-primitives"),
+        pytest.param(Class1, Class2, id="different-classes"),
+        pytest.param((List[int]), (List[str]), id="different-lists-of-primitives"),
+        pytest.param((List[Class1]), (List[Class2]), id="different-lists-of-classes"),
+        pytest.param(
+            (List[Set[Sequence[str]]]), (List[Set[Sequence[bool]]]), id="nested-sequences-of-different-primitives"
+        ),
+        pytest.param(
+            (List[Set[Sequence[str]]]), (Set[List[Sequence[str]]]), id="different-nested-sequences-of-same-primitives"
+        ),
+        pytest.param(
+            (List[Set[Sequence[Class1]]]), (List[Set[Sequence[Class2]]]), id="nested-sequences-of-different-classes"
+        ),
+        pytest.param(
+            (List[Set[Sequence[Class1]]]), (Set[List[Sequence[Class1]]]), id="different-nested-sequences-of-same-class"
+        ),
+        pytest.param((Dict[(str, int)]), (Dict[(int, int)]), id="different-dict-of-primitive-keys"),
+        pytest.param((Dict[(str, int)]), (Dict[(str, float)]), id="different-dict-of-primitive-values"),
+        pytest.param((Dict[(str, Class1)]), (Dict[(str, Class2)]), id="different-dict-of-class-values"),
+        pytest.param((Dict[(str, Any)]), (Dict[(int, int)]), id="dict-of-Any-values-to-dict-of-primitives"),
+        pytest.param((Dict[(str, Any)]), (Dict[(int, Class1)]), id="dict-of-Any-values-to-dict-of-classes"),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
+            (Mapping[(str, Dict[(str, Dict[(str, int)])])]),
+            id="different-nested-mappings-of-same-primitives",
+        ),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
+            (Dict[(str, Mapping[(str, Dict[(int, int)])])]),
+            id="same-nested-mappings-of-different-primitive-keys",
+        ),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
+            (Dict[(str, Mapping[(int, Dict[(str, int)])])]),
+            id="same-nested-mappings-of-different-higher-level-primitive-keys",
+        ),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, int)])])]),
+            (Dict[(str, Mapping[(str, Dict[(str, dict)])])]),
+            id="same-nested-mappings-of-different-primitive-values",
+        ),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, Class1)])])]),
+            (Dict[(str, Mapping[(str, Dict[(str, Class2)])])]),
+            id="same-nested-mappings-of-different-class-values",
+        ),
+        pytest.param(
+            (Dict[(str, Mapping[(str, Dict[(str, Class1)])])]),
+            (Dict[(str, Mapping[(str, Dict[(str, Class2)])])]),
+            id="same-nested-mappings-of-class-to-subclass-values",
+        ),
+        pytest.param((Literal["a", "b", "c"]), (Literal["x", "y"]), id="different-literal-of-same-primitive"),
+        pytest.param((Literal[Enum1.TEST1]), (Literal[Enum1.TEST2]), id="different-literal-of-same-enum"),
+    ],
+)
+def test_types_are_always_not_compatible(sender_type, receiver_type):
     assert not _types_are_compatible(sender_type, receiver_type)
