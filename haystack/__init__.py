75d0a33cc5b274bff1d1f1cf12696cf7dfe9e271
anakin87 <stefanofiorucci@gmail.com>
2025-02-19 18:13:52 +0100

    lot more lazy inits

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -2,14 +2,38 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
+from typing import TYPE_CHECKING
+
+from lazy_imports import LazyImporter
+
 import haystack.logging
 import haystack.tracing
-from haystack.core.component import component
-from haystack.core.errors import ComponentError, DeserializationError
-from haystack.core.pipeline import AsyncPipeline, Pipeline, PredefinedPipeline
-from haystack.core.serialization import default_from_dict, default_to_dict
-from haystack.dataclasses import Answer, Document, ExtractedAnswer, GeneratedAnswer
 from haystack.version import __version__
+
+_import_structure = {
+    "core.component": ["component"],
+    "core.errors": ["ComponentError", "DeserializationError"],
+    "core.pipeline": ["AsyncPipeline", "Pipeline", "PredefinedPipeline"],
+    "core.serialization": ["default_from_dict", "default_to_dict"],
+    "dataclasses": ["Answer", "Document", "ExtractedAnswer", "GeneratedAnswer"],
+}
+
+if TYPE_CHECKING:
+    from .core.component import component
+    from .core.errors import ComponentError, DeserializationError
+    from .core.pipeline import AsyncPipeline, Pipeline, PredefinedPipeline
+    from .core.serialization import default_from_dict, default_to_dict
+    from .dataclasses import Answer, Document, ExtractedAnswer, GeneratedAnswer
+
+else:
+    sys.modules[__name__] = LazyImporter(
+        name=__name__,
+        module_file=__file__,
+        import_structure=_import_structure,
+        # we need to pass the some objects as extra objects since we do not want to import them lazily
+        extra_objects={"__version__": __version__, "logging": haystack.logging, "tracing": haystack.tracing},
+    )
 
 # Initialize the logging configuration
 # This is a no-op unless `structlog` is installed
@@ -17,18 +41,3 @@ haystack.logging.configure_logging()
 
 # Same for tracing (no op if `opentelemetry` or `ddtrace` is not installed)
 haystack.tracing.auto_enable_tracing()
-
-__all__ = [
-    "Answer",
-    "AsyncPipeline",
-    "ComponentError",
-    "DeserializationError",
-    "Document",
-    "ExtractedAnswer",
-    "GeneratedAnswer",
-    "Pipeline",
-    "PredefinedPipeline",
-    "component",
-    "default_from_dict",
-    "default_to_dict",
-]
--- haystack/components/classifiers/__init__.py
+++ haystack/components/classifiers/__init__.py
@@ -2,7 +2,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from haystack.components.classifiers.document_language_classifier import DocumentLanguageClassifier
-from haystack.components.classifiers.zero_shot_document_classifier import TransformersZeroShotDocumentClassifier
+import sys
+from typing import TYPE_CHECKING
 
-__all__ = ["DocumentLanguageClassifier", "TransformersZeroShotDocumentClassifier"]
+from lazy_imports import LazyImporter
+
+_import_structure = {
+    "document_language_classifier": ["DocumentLanguageClassifier"],
+    "zero_shot_document_classifier": ["TransformersZeroShotDocumentClassifier"],
+}
+
+if TYPE_CHECKING:
+    from .document_language_classifier import DocumentLanguageClassifier
+    from .zero_shot_document_classifier import TransformersZeroShotDocumentClassifier
+
+else:
+    sys.modules[__name__] = LazyImporter(name=__name__, module_file=__file__, import_structure=_import_structure)
