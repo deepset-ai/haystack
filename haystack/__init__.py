37348790862864c76df72aac6fdf72545c021f70
anakin87 <stefanofiorucci@gmail.com>
2025-02-20 21:15:16 +0100

    better comments

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -7,8 +7,9 @@ from typing import TYPE_CHECKING
 
 from lazy_imports import LazyImporter
 
-# these imports need to be loaded eagerly
-import haystack.core
+# These imports need to be loaded eagerly:
+# - they configure essential services (logging, tracing)
+# - they define core classes which should be accessible through the haystack namespace
 import haystack.logging
 import haystack.tracing
 from haystack.core.component import component
@@ -32,12 +33,19 @@ else:
         name=__name__,
         module_file=__file__,
         import_structure=_import_structure,
-        # in extra_objects, we pass the objects that we imported eagerly
+        # These modules were imported eagerly above,
+        # but must also be added to extra_objects so LazyImporter exposes them
+        # through the haystack namespace.
         extra_objects={
             "__version__": __version__,
             "logging": haystack.logging,
             "tracing": haystack.tracing,
             "component": component,
+            # haystack.core requires special handling:
+            # - It has an empty __init__.py to avoid circular imports.
+            # - For this reason, it does not play well with LazyImporter.
+            # - We pass it directly in extra_objects to preserve the module reference.
+            # - This preserves the ability to monkey-patch the module in tests.
             "core": haystack.core,
         },
     )
