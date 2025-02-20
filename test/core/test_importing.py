1929657bc7c04eae6d458cb930a2d64ec5378253
David S. Batista <dsbatista@gmail.com>
2025-02-20 18:08:03 +0100

    test: make sure no extra modules are being imported due to `__init__` definitions

---
+++ test/core/test_importing.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+import sys
+from unittest.mock import patch
+
+
+def test_lazy_import():
+    # Save the original state of the module if it exists
+    original_imported = sys.modules.get("haystack.components.generators.chat.azure")
+
+    with patch.dict(sys.modules):
+        # Remove the module from sys.modules if it was already imported
+        if original_imported:
+            del sys.modules["haystack.components.generators.chat.azure"]
+
+    from haystack.components.generators.chat import OpenAIChatGenerator  # Import the intended class
+
+    should_not_be_there = [
+        "haystack.components.generators.chat.azure"
+        # "haystack.components.generators.chat.hugging_face_local",
+        # "haystack.components.generators.chat.hugging_face_api"
+    ]
+
+    should_be_there = ["haystack.components.generators.chat.openai"]
+
+    for module in should_be_there:
+        if module not in sys.modules.keys():
+            print(f"Module {module} not found in sys.modules")
+            break
+
+    for modules in sys.modules.keys():
+        for module in should_not_be_there:
+            if module in modules:
+                print(f"Found {module} in sys.modules")
+                break
+
+    # Restore the module if it was previously imported (preserves test isolation)
+    if original_imported:
+        sys.modules["haystack.components.generators.chat.azure"] = original_imported
