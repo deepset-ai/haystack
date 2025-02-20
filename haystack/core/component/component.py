7ce9a7a2f54a3e3c96dbad3c5c05a6e1be9e0d9e
Sebastian Husch Lee <sjrl423@gmail.com>
2025-02-20 09:50:14 +0100

    Remove mention of Canals

--- haystack/core/component/component.py
+++ haystack/core/component/component.py
@@ -329,9 +329,7 @@ class _Component:
     See module's docstring.
 
     Args:
-        class_: the class that Canals should use as a component.
-        serializable: whether to check, at init time, if the component can be saved with
-        `save_pipelines()`.
+        cls: the class that should be used as a component.
 
     Returns:
         A class that can be recognized as a component.
