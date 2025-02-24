670eac7d8949f79448c22d326ac5b38e19184e97
Sebastian Husch Lee <sjrl423@gmail.com>
2025-02-24 08:55:24 +0100

    Update tests

--- test/components/routers/test_file_router.py
+++ test/components/routers/test_file_router.py
@@ -87,14 +87,16 @@ class TestFileTypeRouter:
             test_files_path / "txt" / "doc_2.txt",
             test_files_path / "audio" / "the context for this answer is here.wav",
             test_files_path / "images" / "apple.jpg",
+            test_files_path / "msg" / "sample.msg",
         ]
 
-        router = FileTypeRouter(mime_types=[r"text/plain", r"audio/x-wav", r"image/jpeg"])
+        router = FileTypeRouter(mime_types=[r"text/plain", r"audio/x-wav", r"image/jpeg", "application/vnd.ms-outlook"])
         output = router.run(sources=file_paths)
         assert output
         assert len(output[r"text/plain"]) == 2
         assert len(output[r"audio/x-wav"]) == 1
         assert len(output[r"image/jpeg"]) == 1
+        assert len(output["application/vnd.ms-outlook"]) == 1
         assert not output.get("unclassified")
 
     def test_run_with_single_meta(self, test_files_path):
@@ -287,15 +289,6 @@ class TestFileTypeRouter:
         router = FileTypeRouter(mime_types=[r"text/plain", r"audio/x-wav", r"image/jpeg"])
         with pytest.raises(ValueError, match="Unsupported data source type:"):
             router.run(sources=[{"unsupported": "type"}])
-
-    def test_msg_mime_type(self, test_files_path):
-        """
-        Test that the component correctly classifies MSG files.
-        """
-        file_paths = [test_files_path / "msg" / "sample.msg"]
-        router = FileTypeRouter(mime_types=["application/vnd.ms-outlook"])
-        output = router.run(sources=file_paths)
-        assert len(output["application/vnd.ms-outlook"]) == 1
 
     def test_invalid_regex_pattern(self):
         """
