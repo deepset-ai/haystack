161e9e209399bd996c11b22607e7742d95720ecb
Julian Risch <julian.risch@deepset.ai>
2025-03-04 12:18:51 +0100

    Update warning message

--- haystack/evaluation/eval_run_result.py
+++ haystack/evaluation/eval_run_result.py
@@ -223,21 +223,21 @@ class EvaluationRunResult:
 
     def score_report(self) -> "DataFrame":
         """Generates a DataFrame report with aggregated scores for each metric."""
-        msg = "The `score_report` method is deprecated and will be changed to `aggregated_report` in Haystack 2.12.0."
+        msg = "The `score_report` method is deprecated and will be removed in Haystack 2.12.0. Use `aggregated_report` instead."
         warn(msg, DeprecationWarning, stacklevel=2)
         return self.aggregated_report(output_format="df")
 
     def to_pandas(self) -> "DataFrame":
         """Generates a DataFrame report with detailed scores for each metric."""
-        msg = "The `to_pandas` method is deprecated and will be changed to `detailed_report` in Haystack 2.12.0."
+        msg = "The `to_pandas` method is deprecated and will be removed in Haystack 2.12.0. Use `detailed_report` instead."
         warn(msg, DeprecationWarning, stacklevel=2)
         return self.detailed_report(output_format="df")
 
     def comparative_individual_scores_report(self, other: "EvaluationRunResult") -> "DataFrame":
         """Generates a DataFrame report with detailed scores for each metric from two evaluation runs for comparison."""
         msg = (
-            "The `comparative_individual_scores_report` method is deprecated and will be changed to "
-            "`comparative_detailed_report` in Haystack 2.12.0."
+            "The `comparative_individual_scores_report` method is deprecated will be removed in Haystack 2.12.0. "
+            "Use `comparative_detailed_report` instead."
         )
         warn(msg, DeprecationWarning, stacklevel=2)
         return self.comparative_detailed_report(other, output_format="df")
