59a88bd984fdf4a89639dcd6de0daff43ffcf847
Michele Pangrazzi <xmikex83@gmail.com>
2025-03-26 16:45:20 +0100

    Update type_utils.py
    Fix license header

--- haystack/core/type_utils.py
+++ haystack/core/type_utils.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+
 import collections.abc
 from typing import Any, TypeVar, Union, get_args, get_origin
 
