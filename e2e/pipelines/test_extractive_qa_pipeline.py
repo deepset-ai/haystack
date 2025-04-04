1cd63e36b03c804fa8318ac952d1f83826c32827
David S. Batista <dsbatista@gmail.com>
2025-04-04 11:16:44 +0200

    removing unused imports (#9172)

--- e2e/pipelines/test_extractive_qa_pipeline.py
+++ e2e/pipelines/test_extractive_qa_pipeline.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import json
-
 from haystack import Document, Pipeline
 from haystack.components.readers import ExtractiveReader
 from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
