bddd43b4c68a4e8fc507177b849718b9fa027ef1
anakin87 <stefanofiorucci@gmail.com>
2025-04-07 12:59:18 +0200

    license header

--- haystack/tools/serde_utils.py
+++ haystack/tools/serde_utils.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import Any, Dict, List, Union
 
 from haystack.core.errors import DeserializationError
