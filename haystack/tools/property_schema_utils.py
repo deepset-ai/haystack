2bd630463c27bb3fd0d1e2bc09f0dd934938f160
Sebastian Husch Lee <sjrl423@gmail.com>
2025-05-05 16:15:26 +0200

    Add license header

--- haystack/tools/property_schema_utils.py
+++ haystack/tools/property_schema_utils.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from dataclasses import fields, is_dataclass
 from typing import Any, Dict, Union, get_args, get_origin
 
