af20ba087f1027bc29cd68401bafe8ba95a71273
David S. Batista <dsbatista@gmail.com>
2025-05-21 18:19:51 +0200

    adding LICENSE to stub files

--- haystack/components/converters/multi_file_converter.pyi
+++ haystack/components/converters/multi_file_converter.pyi
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import Any, Dict, List, Optional
 
 from haystack import super_component
