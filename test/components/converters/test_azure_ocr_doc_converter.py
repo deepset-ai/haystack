449997acdabd032af07db2091d302ea00ab54845
David S. Batista <dsbatista@gmail.com>
2025-05-26 17:54:24 +0200

    removing unused importas and adding/fixing LICENSE header on tests

--- test/components/converters/test_azure_ocr_doc_converter.py
+++ test/components/converters/test_azure_ocr_doc_converter.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # ruff: noqa: E501
+
 import json
 import os
 import os.path
--- test/core/pipeline/test_validation_pipeline_io.py
+++ test/core/pipeline/test_validation_pipeline_io.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+
 from typing import Optional
 
 import pytest
