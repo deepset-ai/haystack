eef403002077a6ecbf38d52633f8eb0afb6ac883
David S. Batista <dsbatista@gmail.com>
2025-05-26 18:12:56 +0200

    fixing LICENSE header in test files

--- test/core/pipeline/test_validation_pipeline_io.py
+++ test/core/pipeline/test_validation_pipeline_io.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
 
--- test/core/super_component/test_super_component.py
+++ test/core/super_component/test_super_component.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present deepset GmbH <info@deepset.ai>
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
 
