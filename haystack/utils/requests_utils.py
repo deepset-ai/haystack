8a436db95b90500198d2b949073330fdecd66acc
Denis Washington <denis@denisw.de>
2025-05-26 08:51:51 +0200

    Remove unused import

--- haystack/utils/requests_utils.py
+++ haystack/utils/requests_utils.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import requests
 from tenacity import after_log, before_log, retry, retry_if_exception_type, stop_after_attempt, wait_exponential
