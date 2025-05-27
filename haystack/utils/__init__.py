3deaa20cb61661700e212bb81b4f255237481f8b
atopx <58352247+atopx@users.noreply.github.com>
2025-05-27 18:44:54 +0800

    feat: Add HuggingFace API (text-embeddings-inference for rerank model) for component.rankers (#9414)
    * feat(component.rankers): Add HuggingFace API (text-embeddings-inference for rerank) ranker component
    
    * update test flow & doc loaders
    
    * Support run_async for HuggingFaceAPIRanker
    
    * Add release note for HuggingFace API support in component.rankers
    
    * Add release note for HuggingFace API support in component.rankers
    
    * Add release note for HuggingFace API support in component.rankers
    
    * Add release note for HuggingFace API support in component.rankers
    
    * fix:
    1. `hugging_face_api.HuggingFaceAPIRanker` rename to `hugging_face_tei.HuggingFaceAPIRanker`
    2. HuggingFaceAPIRanker: use our Secret API for token
    3. add the missing modules for `docs/pydoc/config/rankers_api.yml`
    4. added function `async_request_with_retry` for `haystack/utils/requests_utils.py` and added unittest on `test/utils/test_requests_utils.py`
    4. HuggingFaceAPIRanker: refactor the retry function to support configuration based on attempts and status code.
    5. HuggingFaceAPIRanker: refactor the test into unit tests using mocks
    
    * fix(HuggingFaceTEIRanker): change the token check logic to use the resolve_value method.
    
    * fix(format): run `hatch run format`
    
    * fix:
    - Force keyword-only arguments in __init__ method by adding *,
    - Clarify token docstring that it's not always required
    - Copy documents to avoid modifying original objects
    - Remove test file from slow workflow
    - Add monkeypatch eånvironment variable cleanup in tests
    - Fix missing module in rankers_api.yml and sort modules alphabetically
    - Remove unnecessary test info from release notes
    
    * fix HuggingFaceTEIRanker：
    - "None" of "Optional[Secret]" has no attribute "resolve_value"
    - run/run_async: too many parameters
    
    * fix(HuggingFaceTEIRanker) :Revise the docstring of the HuggingFaceTEIRanker, improve the parameter descriptions, ensure consistency and clarity. Add error handling information to enhance the readability of the API response.
    
    * fix：unit test for HuggingFaceTEIRanker raise message
    
    * fix fmt
    
    * minor refinements
    
    * refine release note
    
    ---------
    
    Co-authored-by: anakin87 <stefanofiorucci@gmail.com>

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -18,7 +18,7 @@ _import_structure = {
     "jinja2_extensions": ["Jinja2TimeExtension"],
     "jupyter": ["is_in_jupyter"],
     "misc": ["expit", "expand_page_range"],
-    "requests_utils": ["request_with_retry"],
+    "requests_utils": ["request_with_retry", "async_request_with_retry"],
     "type_serialization": ["deserialize_type", "serialize_type"],
 }
 
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
     from .jinja2_extensions import Jinja2TimeExtension
     from .jupyter import is_in_jupyter
     from .misc import expand_page_range, expit
-    from .requests_utils import request_with_retry
+    from .requests_utils import async_request_with_retry, request_with_retry
     from .type_serialization import deserialize_type, serialize_type
 else:
     sys.modules[__name__] = LazyImporter(name=__name__, module_file=__file__, import_structure=_import_structure)
