70a78f4b5113ca5482ce07605836345ace961643
Amna Mubashar <amnahkhan.ak@gmail.com>
2025-06-06 11:46:24 +0200

    fix: serialization of nested `ChatMessage` in `GeneratedAnswer`dataclass (#9497)
    * Fix serialization
    
    * small fix
    
    * fix the erros
    
    * Fix tests
    
    * PR comments

--- haystack/dataclasses/answer.py
+++ haystack/dataclasses/answer.py
@@ -6,7 +6,7 @@ from dataclasses import asdict, dataclass, field
 from typing import Any, Dict, List, Optional, Protocol, runtime_checkable
 
 from haystack.core.serialization import default_from_dict, default_to_dict
-from haystack.dataclasses.document import Document
+from haystack.dataclasses import ChatMessage, Document
 
 
 @runtime_checkable
@@ -99,7 +99,16 @@ class GeneratedAnswer:
             Serialized dictionary representation of the object.
         """
         documents = [doc.to_dict(flatten=False) for doc in self.documents]
-        return default_to_dict(self, data=self.data, query=self.query, documents=documents, meta=self.meta)
+
+        # Serialize ChatMessage objects to dicts
+        meta = self.meta
+        all_messages = meta.get("all_messages")
+
+        # all_messages is either a list of ChatMessage objects or a list of strings
+        if all_messages and isinstance(all_messages[0], ChatMessage):
+            meta = {**meta, "all_messages": [msg.to_dict() for msg in all_messages]}
+
+        return default_to_dict(self, data=self.data, query=self.query, documents=documents, meta=meta)
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "GeneratedAnswer":
@@ -113,7 +122,13 @@ class GeneratedAnswer:
             Deserialized object.
         """
         init_params = data.get("init_parameters", {})
+
         if (documents := init_params.get("documents")) is not None:
-            data["init_parameters"]["documents"] = [Document.from_dict(d) for d in documents]
+            init_params["documents"] = [Document.from_dict(d) for d in documents]
+
+        meta = init_params.get("meta", {})
+        if (all_messages := meta.get("all_messages")) is not None and isinstance(all_messages[0], dict):
+            meta["all_messages"] = [ChatMessage.from_dict(m) for m in all_messages]
+        init_params["meta"] = meta
 
         return default_from_dict(cls, data)
