173b84c8666ca438f24aef73be021c1290b7e77d
Sebastian Husch Lee <sjrl423@gmail.com>
2025-06-30 13:12:01 +0200

    Add to_dict and from_dict to ByteStream

--- haystack/dataclasses/document.py
+++ haystack/dataclasses/document.py
@@ -128,7 +128,7 @@ class Document(metaclass=_BackwardCompatible):  # noqa: PLW1641
         """
         data = asdict(self)
         if (blob := data.get("blob")) is not None:
-            data["blob"] = {"data": list(blob["data"]), "mime_type": blob["mime_type"]}
+            data["blob"] = blob.to_dict()
 
         if flatten:
             meta = data.pop("meta")
@@ -144,7 +144,7 @@ class Document(metaclass=_BackwardCompatible):  # noqa: PLW1641
         The `blob` field is converted to its original type.
         """
         if blob := data.get("blob"):
-            data["blob"] = ByteStream(data=bytes(blob["data"]), mime_type=blob["mime_type"])
+            data["blob"] = ByteStream.from_dict(blob)
         if sparse_embedding := data.get("sparse_embedding"):
             data["sparse_embedding"] = SparseEmbedding.from_dict(sparse_embedding)
 
