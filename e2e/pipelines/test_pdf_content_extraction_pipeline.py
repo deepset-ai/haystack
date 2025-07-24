152c6394476ce055d3d83aaf236c4959066fb510
anakin87 <stefanofiorucci@gmail.com>
2025-07-24 11:12:30 +0200

    fix + relnote

--- e2e/pipelines/test_pdf_content_extraction_pipeline.py
+++ e2e/pipelines/test_pdf_content_extraction_pipeline.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from pathlib import Path
-
 from haystack import Pipeline
 from haystack.components.converters.pypdf import PyPDFToDocument
 from haystack.components.joiners import DocumentJoiner
