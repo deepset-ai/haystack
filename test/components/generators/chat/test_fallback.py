ed5d790cd4a89588d8fceb5a960c153132a3dd77
Vladimir Blagojevic <dovlex@gmail.com>
2025-10-09 12:09:00 +0200

    Update licence files

--- test/components/generators/chat/test_fallback.py
+++ test/components/generators/chat/test_fallback.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-present deepset GmbH <info@deepset.ai>
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
 
