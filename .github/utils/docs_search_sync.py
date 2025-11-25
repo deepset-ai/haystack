aa0a7e28665f9b638d2cb8cfba10157025ea6ae6
anakin87 <stefanofiorucci@gmail.com>
2025-11-25 17:53:06 +0100

    refinements

--- .github/utils/docs_search_sync.py
+++ .github/utils/docs_search_sync.py
@@ -1,7 +1,13 @@
+import os
+import sys
 import time
 from pathlib import Path
 
-from deepset_cloud_sdk.workflows.sync_client.files import DeepsetCloudFile
+import requests
+from deepset_cloud_sdk.workflows.sync_client.files import DeepsetCloudFile, WriteMode, list_files, upload_texts
+
+WORKSPACE = os.environ["WORKSPACE"]
+API_KEY = os.environ["API_KEY"]
 
 
 def collect_docs_files(version: int) -> list[DeepsetCloudFile]:
@@ -35,10 +41,55 @@ def collect_docs_files(version: int) -> list[DeepsetCloudFile]:
     return files
 
 
+def delete_files(file_names: list[str]):
+    """
+    Delete files from the deepset workspace.
+    """
+    url = f"https://api.cloud.deepset.ai/api/v1/workspaces/{WORKSPACE}/files"
+    payload = {"names": file_names}
+    headers = {"Accept": "application/json", "Authorization": f"Bearer {API_KEY}"}
+    response = requests.delete(url, json=payload, headers=headers, timeout=300)
+    return response.json()
+
+
 if __name__ == "__main__":
     version = time.time_ns()
+    print(f"Docs version: {version}")
 
+    print("Collecting docs files from build directory")
     dc_files = collect_docs_files(version)
+    print(f"Collected {len(dc_files)} docs files")
 
-    print(dc_files[:10])
-    print(len(dc_files))
+    print("Uploading docs files to deepset")
+    summary = upload_texts(
+        workspace_name=WORKSPACE,
+        files=dc_files,
+        api_key=API_KEY,
+        blocking=True,  # Very important to ensure that DC is up to date when we query for deletion
+        timeout_s=300,
+        show_progress=True,
+        write_mode=WriteMode.OVERWRITE,
+        enable_parallel_processing=True,
+    )
+    print(f"Uploaded docs files to deepset\n{summary}")
+    if summary.failed_upload_count > 0:
+        print("Failed to upload some docs files. Stopping to prevent risky deletion of old files.")
+        sys.exit(1)
+
+    print("Listing old docs files from deepset")
+    old_files_names = []
+    odata_filter = f"version lt '{version}'"
+    old_files_names = [
+        f.name
+        for batch in list_files(workspace_name=WORKSPACE, api_key=API_KEY, odata_filter=odata_filter)
+        for f in batch
+    ]
+
+    print(f"Found {len(old_files_names)} old files to delete")
+    if len(old_files_names) > 20:
+        print("Found >20 old files to delete. Stopping because something could have gone wrong in the upload process.")
+        sys.exit(1)
+
+    print("Deleting old docs files from deepset")
+    result = delete_files(old_files_names)
+    print(f"Deleted old docs files from deepset\n{result}")
