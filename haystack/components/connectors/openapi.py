123962eb19857ad1ecc8828aa67e99e4677ff4f0
anakin87 <stefanofiorucci@gmail.com>
2025-12-12 13:19:13 +0100

    revert

--- haystack/components/connectors/openapi.py
+++ haystack/components/connectors/openapi.py
@@ -23,8 +23,6 @@ class OpenAPIConnector:
     arguments to it from a Haystack pipeline run method or by other components in a pipeline that
     pass input arguments to this component.
 
-    BIG TRIGGER
-
     Example:
     ```python
     from haystack.utils import Secret
