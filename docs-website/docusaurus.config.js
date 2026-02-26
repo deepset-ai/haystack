e2051fcf6bbd44c77738375a529323ffa42eaeda
HaystackBot <73523382+HaystackBot@users.noreply.github.com>
2026-02-26 12:58:19 +0000

    Promote unstable docs for Haystack 2.25

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
               banner: 'unreleased',
             },
           },
-          lastVersion: '2.24',
+          lastVersion: '2.25',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -102,7 +102,7 @@ const config = {
             banner: 'unreleased',
           },
         },
-        lastVersion: '2.24',
+        lastVersion: '2.25',
       },
     ],
     [
