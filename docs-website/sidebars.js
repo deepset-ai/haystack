36cc5e481b0d70e3b53a2480918e4598e088b58d
bogdankostic <bogdankostic@web.de>
2026-03-04 10:22:42 +0100

    docs: add FirecrawlCrawler documentation (#10722)
    * docs: add FirecrawlCrawler documentation
    
    * fix: update FirecrawlCrawler documentation to use 'url' instead of 'sourceURL' in output
    
    * Remove unnecessary parentheses
    
    * Update docs-website/sidebars.js
    
    ---------
    
    Co-authored-by: Julian Risch <julian.risch@deepset.ai>

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -355,6 +355,7 @@ export default {
             id: 'pipeline-components/fetchers'
           },
           items: [
+            'pipeline-components/fetchers/firecrawlcrawler',
             'pipeline-components/fetchers/linkcontentfetcher',
             'pipeline-components/fetchers/external-integrations-fetchers',
           ],
