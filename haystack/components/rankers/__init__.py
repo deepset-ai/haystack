80d7d83e4d06cee623d8256384d530a662e2d88b
it-education-md <128720033+it-education-md@users.noreply.github.com>
2026-03-12 03:59:09 -0700

    feat: add LLMRanker (#10794)
    * feat: add LLMRanker
    
    * chore: address review feedback
    
    * chore: address review feedback

--- haystack/components/rankers/__init__.py
+++ haystack/components/rankers/__init__.py
@@ -9,6 +9,7 @@ from lazy_imports import LazyImporter
 
 _import_structure = {
     "hugging_face_tei": ["HuggingFaceTEIRanker"],
+    "llm_ranker": ["LLMRanker"],
     "lost_in_the_middle": ["LostInTheMiddleRanker"],
     "meta_field": ["MetaFieldRanker"],
     "meta_field_grouping_ranker": ["MetaFieldGroupingRanker"],
@@ -19,6 +20,7 @@ _import_structure = {
 
 if TYPE_CHECKING:
     from .hugging_face_tei import HuggingFaceTEIRanker as HuggingFaceTEIRanker
+    from .llm_ranker import LLMRanker as LLMRanker
     from .lost_in_the_middle import LostInTheMiddleRanker as LostInTheMiddleRanker
     from .meta_field import MetaFieldRanker as MetaFieldRanker
     from .meta_field_grouping_ranker import MetaFieldGroupingRanker as MetaFieldGroupingRanker
