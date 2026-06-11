feb01bd291da10fd221e36d688e5def6209bb43b
David S. Batista <dsbatista@gmail.com>
2026-06-11 14:52:37 +0200

    fixes

--- haystack/components/audio/whisper_local.py
+++ haystack/components/audio/whisper_local.py
@@ -47,7 +47,7 @@ class LocalWhisperTranscriber:
     whisper = LocalWhisperTranscriber(model="small")
     transcription = whisper.run(sources=["test/test_files/audio/answer.wav"])
     print(transcription)
-    print(transcription)
+
     # >> {'documents': [Document(id=dae7051417caaf19304a4ef2ec845e981abe1efd4c8e6ee7ffb25867f165c411,
     # >> content: ' Answer.', meta: {'audio_file': PosixPath('test/test_files/audio/answer.wav'), 'language': 'en'})]}
     ```
