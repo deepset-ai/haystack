c2159efaaa5006a9be3766421778151c3491f12f
David S. Batista <dsbatista@gmail.com>
2026-08-17 11:17:18 +0200

    Apply suggestion from @davidsbatista

--- haystack/components/rankers/meta_field.py
+++ haystack/components/rankers/meta_field.py
@@ -404,7 +404,6 @@ class MetaFieldRanker:
 
         scored_docs = [replace(doc, score=scores_map[doc.id]) for doc in documents]
 
-        # ponytail: fix score=0.0 being treated as falsy and converted to -1
         return sorted(scored_docs, key=lambda doc: doc.score if doc.score is not None else -1, reverse=True)
 
     @staticmethod
