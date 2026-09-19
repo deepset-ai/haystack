5151b53a2bcb7ab385d0823d2a4f989db36f3ae0
Ewuji John <johnseyi51@gmail.com>
2026-09-19 21:34:58 +0100

    fix issue #12817
    Signed-off-by: Ewuji John <johnseyi51@gmail.com>

--- test/components/fetchers/test_link_content_fetcher.py
+++ test/components/fetchers/test_link_content_fetcher.py
@@ -515,20 +515,25 @@ class TestLinkContentFetcherAsync:
         # Patch the AsyncClient class to control the instance created by LinkContentFetcher
         with patch("haystack.components.fetchers.link_content.httpx.AsyncClient") as AsyncClientMock:
             aclient = AsyncClientMock.return_value
-            aclient.headers = {}  # base headers used in the merge
+            aclient.headers = {"X-Client-Default": "client-value"}
 
             mock_response = Mock(status_code=200, text="OK", headers={"Content-Type": "text/plain"})
             aclient.get = AsyncMock(return_value=mock_response)
 
             fetcher = LinkContentFetcher(
                 user_agents=["ua-async-1", "ua-async-2"],
+                client_kwargs={"headers": {"X-Client-Default": "client-value"}},
                 request_headers={"Accept-Language": "de-DE", "X-Async": "true", "User-Agent": "ignored-here-too"},
             )
 
             _ = (await fetcher.run_async(urls=["https://example.com"]))["streams"]
 
+            AsyncClientMock.assert_called_once_with(
+                headers={"X-Client-Default": "client-value"}, timeout=3, follow_redirects=True
+            )
             assert aclient.get.await_count == 1
             sent_headers = aclient.get.call_args.kwargs["headers"]
+            assert sent_headers["X-Client-Default"] == "client-value"
             assert sent_headers["X-Async"] == "true"
             assert sent_headers["Accept-Language"] == "de-DE"
             assert sent_headers["User-Agent"] == "ua-async-1"  # rotating UA wins
