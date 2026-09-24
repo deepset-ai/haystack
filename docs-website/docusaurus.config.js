8a962257504e170f7b25b88ec66c3484ecc0fc81
HaystackBot <73523382+HaystackBot@users.noreply.github.com>
2026-09-24 10:49:22 +0000

    Promote unstable docs for Haystack 3.2

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -86,7 +86,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               banner: 'unreleased',
             },
           },
-          lastVersion: '3.1',
+          lastVersion: '3.2',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -139,7 +139,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             banner: 'unreleased',
           },
         },
-        lastVersion: '3.1',
+        lastVersion: '3.2',
       },
     ],
     [
